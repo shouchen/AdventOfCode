9f8ed23a812175d386db44953854a3a7ce659746
Stephen Houchen <shouchen@outlook.com>
2015-12-10 06:18:52 -0800

    Refactor GetHashText()

--- Day4/MD5/MD5.cpp
+++ Day4/MD5/MD5.cpp
@@ -13,51 +13,38 @@ using namespace std;
 
 std::string GetHashText(const void *data, const size_t data_size)
 {
-    HCRYPTPROV hProv = NULL;
-    if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
-        return "";
-
-    HCRYPTPROV hHash = NULL;
-    if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
-    {
-        CryptReleaseContext(hProv, 0);
-        return "";
-    }
-
-    if (!CryptHashData(hHash, static_cast<const BYTE *>(data), data_size, 0))
-    {
-        CryptDestroyHash(hHash);
-        CryptReleaseContext(hProv, 0);
-        return "";
-    }
-
-    DWORD cbHashSize = 0, dwCount = sizeof(DWORD);
-    if (!CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE *)&cbHashSize, &dwCount, 0))
-    {
-        CryptDestroyHash(hHash);
-        CryptReleaseContext(hProv, 0);
-        return "";
-    }
-
-    std::vector<BYTE> buffer(cbHashSize);
-    if (!CryptGetHashParam(hHash, HP_HASHVAL, reinterpret_cast<BYTE*>(&buffer[0]), &cbHashSize, 0))
-    {
-        CryptDestroyHash(hHash);
-        CryptReleaseContext(hProv, 0);
-        return "";
-    }
-
     std::ostringstream oss;
 
-    for (std::vector<BYTE>::const_iterator iter = buffer.begin(); iter != buffer.end(); ++iter)
+    HCRYPTPROV hProv = NULL;
+    if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
     {
-        oss.fill('0');
-        oss.width(2);
-        oss << std::hex << static_cast<const int>(*iter);
+        HCRYPTPROV hHash = NULL;
+        if (CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+        {
+            if (CryptHashData(hHash, static_cast<const BYTE *>(data), data_size, 0))
+            {
+                DWORD cbHashSize = 0, dwCount = sizeof(DWORD);
+                if (CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE *)&cbHashSize, &dwCount, 0))
+                {
+                    std::vector<BYTE> buffer(cbHashSize);
+                    if (CryptGetHashParam(hHash, HP_HASHVAL, reinterpret_cast<BYTE*>(&buffer[0]), &cbHashSize, 0))
+                    {
+                        for (std::vector<BYTE>::const_iterator iter = buffer.begin(); iter != buffer.end(); ++iter)
+                        {
+                            oss.fill('0');
+                            oss.width(2);
+                            oss << std::hex << static_cast<const int>(*iter);
+                        }
+                    }
+                }
+            }
+
+            CryptDestroyHash(hHash);
+        }
+
+        CryptReleaseContext(hProv, 0);
     }
 
-    CryptDestroyHash(hHash);
-    CryptReleaseContext(hProv, 0);
     return oss.str();
 }
 
