85775d21caeea970638e60ef28fa08614a768a38
Stephen Houchen <shouchen@outlook.com>
2015-12-17 22:16:00 -0800

    Clean up solution to Day 18

--- Day18/AnimatedLights/AnimatedLights/AnimatedLights.cpp
+++ Day18/AnimatedLights/AnimatedLights/AnimatedLights.cpp
@@ -6,82 +6,110 @@
 #include <iostream>
 #include <algorithm>
 
-#define SIZE 100
-bool grid[SIZE][SIZE];
-
-void ReadInitialConfiguration(const char *filename)
+template<int SIZE>
+class Grid
 {
-    std::ifstream f(filename);
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++)
-        {
-            char c;
-            f >> c;
-            grid[i][j] = c == '#';
-        }
+public:
+    Grid()
+    {
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+                data[row][col] = false;
 
-    f.close();
-}
+        sticky = false;
+    }
 
-void StuckOn(bool grid[SIZE][SIZE])
-{
-    grid[0][0] = grid[0][SIZE - 1] = grid[SIZE - 1][0] = grid[SIZE - 1][SIZE - 1] = true;
-}
+    Grid &operator=(const Grid &rhs)
+    {
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+                data[row][col] = rhs.data[row][col];
 
-unsigned CountOnNeighbors(bool grid[SIZE][SIZE], int row, int col)
-{
-    unsigned count = 0;
-    for (int i = std::max(row - 1, 0); i <= std::min(row + 1, SIZE - 1); i++)
-        for (int j = std::max(col - 1, 0); j <= std::min(col + 1, SIZE - 1); j++)
-        {
-            if ((i != row || j != col) && grid[i][j])
-                count++;
-        }
+        sticky = rhs.sticky;
+    }
 
-    return count;
-}
+    void ReadInitialConfiguration(const char *filename)
+    {
+        std::ifstream f(filename);
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+            {
+                char c;
+                f >> c;
+                data[row][col] = c == '#';
+            }
 
-void Animate()
-{
-    bool prev[SIZE][SIZE];
-    memcpy(prev, grid, sizeof(prev));
+        UpdateStickyLights();
+    }
 
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++)
-        {
-            unsigned neighborsOn = CountOnNeighbors(prev, i, j);
-            if (prev[i][j])
-                grid[i][j] = (neighborsOn == 2 || neighborsOn == 3);
-            else
-                grid[i][j] = (neighborsOn == 3);
-        }
-}
+    void SetSticky()
+    {
+        sticky = true;
+    }
 
-unsigned Count()
-{
-    unsigned count = 0;
+    void Animate()
+    {
+        Grid prev = *this;
 
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++)
-        {
-            if (grid[i][j])
-                count++;
-        }
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+            {
+                unsigned neighborsOn = prev.CountOnNeighbors(row, col);
+                data[row][col] = data[row][col] ? (neighborsOn == 2 || neighborsOn == 3) : (neighborsOn == 3);
+            }
 
-    return count;
-}
+        UpdateStickyLights();
+    }
+
+    unsigned Count()
+    {
+        unsigned count = 0;
+
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+                if (data[row][col])
+                    count++;
+
+        return count;
+    }
+
+private:
+    bool data[SIZE][SIZE];
+    bool sticky;
+
+    unsigned CountOnNeighbors(int row, int col)
+    {
+        unsigned count = 0;
+
+        for (int i = std::max(row - 1, 0); i <= std::min(row + 1, SIZE - 1); i++)
+            for (int j = std::max(col - 1, 0); j <= std::min(col + 1, SIZE - 1); j++)
+                if ((i != row || j != col) && data[i][j])
+                    count++;
+
+        return count;
+    }
+
+    void UpdateStickyLights()
+    {
+        if (sticky)
+            data[0][0] = data[0][SIZE - 1] = data[SIZE - 1][0] = data[SIZE - 1][SIZE - 1] = true;
+    }
+};
 
 void _tmain(int argc, _TCHAR *argv[])
 {
-    ReadInitialConfiguration("Input.txt");
-    StuckOn(grid);
+    Grid<100> grid1;
+    grid1.ReadInitialConfiguration("Input.txt");
+
+    Grid<100> grid2 = grid1;
+    grid2.SetSticky();
 
     for (int i = 1; i <= 100; i++)
     {
-        std::cout << std::endl << "After " << i << std::endl;
-        Animate();
-        StuckOn(grid);
+        grid1.Animate();
+        grid2.Animate();
     }
 
-    std::cout << std::endl << Count() << std::endl;
+    std::cout << "part one: " << grid1.Count() << std::endl;
+    std::cout << "part two: " << grid2.Count() << std::endl;
 }
