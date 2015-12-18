9a5c9ed0ff115b8e1ae90640a89ba2091b972063
Stephen Houchen <shouchen@outlook.com>
2015-12-18 07:03:01 -0800

    Moved Grid class to its own file.

--- Day18/AnimatedLights/AnimatedLights/AnimatedLights.cpp
+++ Day18/AnimatedLights/AnimatedLights/AnimatedLights.cpp
@@ -4,97 +4,8 @@
 #include "stdafx.h"
 #include <fstream>
 #include <iostream>
-#include <algorithm>
+#include "Grid.h"
 
-template<int SIZE>
-class Grid
-{
-public:
-    Grid()
-    {
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
-                data[row][col] = false;
-
-        sticky = false;
-    }
-
-    Grid &operator=(const Grid &rhs)
-    {
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
-                data[row][col] = rhs.data[row][col];
-
-        sticky = rhs.sticky;
-    }
-
-    void ReadInitialConfiguration(const char *filename)
-    {
-        std::ifstream f(filename);
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
-            {
-                char c;
-                f >> c;
-                data[row][col] = c == '#';
-            }
-
-        UpdateStickyLights();
-    }
-
-    void SetSticky()
-    {
-        sticky = true;
-    }
-
-    void Animate()
-    {
-        Grid prev = *this;
-
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
-            {
-                unsigned neighborsOn = prev.CountOnNeighbors(row, col);
-                data[row][col] = data[row][col] ? (neighborsOn == 2 || neighborsOn == 3) : (neighborsOn == 3);
-            }
-
-        UpdateStickyLights();
-    }
-
-    unsigned Count()
-    {
-        unsigned count = 0;
-
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
-                if (data[row][col])
-                    count++;
-
-        return count;
-    }
-
-private:
-    bool data[SIZE][SIZE];
-    bool sticky;
-
-    unsigned CountOnNeighbors(int row, int col)
-    {
-        unsigned count = 0;
-
-        for (int i = std::max(row - 1, 0); i <= std::min(row + 1, SIZE - 1); i++)
-            for (int j = std::max(col - 1, 0); j <= std::min(col + 1, SIZE - 1); j++)
-                if ((i != row || j != col) && data[i][j])
-                    count++;
-
-        return count;
-    }
-
-    void UpdateStickyLights()
-    {
-        if (sticky)
-            data[0][0] = data[0][SIZE - 1] = data[SIZE - 1][0] = data[SIZE - 1][SIZE - 1] = true;
-    }
-};
 
 void _tmain(int argc, _TCHAR *argv[])
 {
@@ -104,7 +15,7 @@ void _tmain(int argc, _TCHAR *argv[])
     Grid<100> grid2 = grid1;
     grid2.SetSticky();
 
-    for (int i = 1; i <= 100; i++)
+    for (int i = 0; i < 100; i++)
     {
         grid1.Animate();
         grid2.Animate();
