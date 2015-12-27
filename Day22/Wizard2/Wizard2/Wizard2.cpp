a4b6299cf6e047ce5f9169baa183023453ef7b83
Stephen Houchen <shouchen@outlook.com>
2015-12-27 00:05:38 -0800

    Performance enhancements since algorithm is brute-force.

--- Day22/Wizard2/Wizard2/Wizard2.cpp
+++ Day22/Wizard2/Wizard2/Wizard2.cpp
@@ -2,156 +2,114 @@
 //
 
 #include "stdafx.h"
-#include <stdlib.h>
 #include <iostream>
 #include <chrono>
-#include <ctime>
 #include <cassert>
 
 using namespace std;
 
-enum class Spell { MagicMissile, Drain, Shield, Poison, Recharge };
+// Compiler ignoring requests to inline, so use this as a poor man's inline. Code needs to be
+// highly optimized since this brute force algorithm takes awhile to run.
 
-int bossdam, best, hardmode;
-
-bool Round(int bosshp, int playerhp, int mana, int cost, int s_shield, int s_poison, int s_recharge, Spell spell)
-{
-    int armor;
-
-    // player turn action
-    switch (spell)
-    {
-    case Spell::MagicMissile:
-        bosshp -= 4;
-        mana -= 53;
-        cost += 53;
-        break;
-    case Spell::Drain:
-        bosshp -= 2;
-        playerhp += 2;
-        mana -= 73;
-        cost += 73;
-        break;
-    case Spell::Shield:
-        if (s_shield > 0) return false;
-        s_shield = 6;
-        mana -= 113;
-        cost += 113;
-        break;
-    case Spell::Poison:
-        if (s_poison > 0) return false;
-        s_poison = 6;
-        mana -= 173;
-        cost += 173;
-        break;
-    case Spell::Recharge:
-        if (s_recharge > 0) return false;
-        s_recharge = 5;
-        mana -= 229;
-        cost += 229;
-        break;
-    }
-
-    if (bosshp <= 0)
-    {
-        if (cost < best) best = cost;
-        return true;
-    }
-
-    // boss turn
-    if (s_recharge > 0)
-    {
-        mana += 101;
-        s_recharge--;
-    }
-    if (s_poison > 0)
-    {
-        bosshp -= 3;
-        s_poison--;
-    }
-    if (s_shield > 0)
-    {
-        s_shield--;
-        armor = 7;
-    }
-    else
-        armor = 0;
-
-    if (bosshp > 0)
-    {
-        if ((bossdam - armor) < 1)
-            playerhp--;
-        else
-            playerhp -= (bossdam - armor);
-    }
-    else
-    {
-        if (cost < best) best = cost;
-        return true;
-    }
-
-    if (hardmode) playerhp--;
-    if (playerhp <= 0) return false;
-
-    // player turn begin
-    if (s_shield > 0) s_shield--;
-    if (s_recharge > 0)
-    {
-        mana += 101;
-        s_recharge--;
-    }
-    if (s_poison > 0)
-    {
-        bosshp -= 3;
-        s_poison--;
-    }
-    if (bosshp <= 0)
-    {
-        if (cost < best) best = cost;
-        return true;
-    }
-
-    if (mana >= 53) Round(bosshp, playerhp, mana, cost, s_shield, s_poison, s_recharge, Spell::MagicMissile);
-    if (mana >= 73) Round(bosshp, playerhp, mana, cost, s_shield, s_poison, s_recharge, Spell::Drain);
-    if (mana >= 113) Round(bosshp, playerhp, mana, cost, s_shield, s_poison, s_recharge, Spell::Shield);
-    if (mana >= 173) Round(bosshp, playerhp, mana, cost, s_shield, s_poison, s_recharge, Spell::Poison);
-    if (mana >= 229) Round(bosshp, playerhp, mana, cost, s_shield, s_poison, s_recharge, Spell::Recharge);
-
-    return false;
+#define DoEffects() { \
+    if (shieldTimer) { shieldTimer--; armor = 7; } \
+    if (poisonTimer) { hpBoss -= 3; poisonTimer--; } \
+    if (rechargeTimer) { mana += 101; rechargeTimer--; } \
 }
 
-int Solve(int bosshp, int bossdam, int playerhp, int mana, bool hardmode)
-{
-    ::bossdam = bossdam;
-    ::hardmode = hardmode;
-    ::best = INT_MAX;
-
-    if (hardmode) playerhp--;
-
-    Round(bosshp, playerhp, mana, 0, 0, 0, 0, Spell::MagicMissile);
-    Round(bosshp, playerhp, mana, 0, 0, 0, 0, Spell::Drain);
-    Round(bosshp, playerhp, mana, 0, 0, 0, 0, Spell::Shield);
-    Round(bosshp, playerhp, mana, 0, 0, 0, 0, Spell::Poison);
-    Round(bosshp, playerhp, mana, 0, 0, 0, 0, Spell::Recharge);
-
-    return ::best;
+#define DoBossDamageAgainstPlayer() { \
+    auto diff = s_damageBoss - armor; \
+    hpPlayer -= (diff > 1) ? diff : 1; \
 }
+
+#define ReturnIfPlayerWonAndUpdateBestCost() { \
+    if (hpBoss <= 0) { if (cost < s_lowestCost) s_lowestCost = cost; return; } \
+}
+
+#define ApplyHardModePointsAndReturnIfPlayerLost() { \
+    if ((hpPlayer -= s_hardModePoints) <= 0) return; \
+}
+
+#define ForkNewRoundForEachPossibleSpell() { \
+    if (mana >= 53) \
+        Round(hpBoss - 4, hpPlayer, mana - 53, cost + 53, shieldTimer, poisonTimer, rechargeTimer); \
+    if (mana >= 73) \
+        Round(hpBoss - 2, hpPlayer + 2, mana - 73, cost + 73, shieldTimer, poisonTimer, rechargeTimer); \
+    if (mana >= 113 && !shieldTimer) \
+        Round(hpBoss, hpPlayer, mana - 113, cost + 113, 6, poisonTimer, rechargeTimer); \
+    if (mana >= 173 && !poisonTimer) \
+        Round(hpBoss, hpPlayer, mana - 173, cost + 173, shieldTimer, 6, rechargeTimer); \
+    if (mana >= 229 && !rechargeTimer) \
+        Round(hpBoss, hpPlayer, mana - 229, cost + 229, shieldTimer, poisonTimer, 5); \
+}
+
+class Solver
+{
+public:
+    static int Solve(int hpBoss, int damageBoss, int hpPlayer, int mana, bool hardMode, double &time)
+    {
+        // Record time taken to solve (useful for performance tuning this algorithm).
+        auto start = chrono::system_clock::now();
+
+        // Save parameters that don't need to be on the stack (are constant the whole puzzle).
+        s_damageBoss = damageBoss;
+        s_hardModePoints = hardMode ? 1 : 0;
+        s_lowestCost = INT_MAX;
+
+        // Player's first turn--deduct hit points if hard mode, and check if it killed player.
+        hpPlayer -= s_hardModePoints;
+        if (hpPlayer <= 0) return s_lowestCost;
+
+        // First spell.
+        const auto shieldTimer = 0, poisonTimer = 0, rechargeTimer = 0, cost = 0;
+        ForkNewRoundForEachPossibleSpell();
+
+        // Figure out how long the whole thing took so we can report it out.
+        auto end = chrono::system_clock::now();
+        chrono::duration<double> seconds = end - start;
+        time = seconds.count();
+
+        return s_lowestCost;
+    }
+
+private:
+    static void Round(int hpBoss, int hpPlayer, int mana, int cost, int shieldTimer, int poisonTimer, int rechargeTimer)
+    {
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Start of Boss's turn--apply efects and see if that killed him.
+        int armor = 0;
+        DoEffects();
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Boss does his turn; check if it killed Player.
+        DoBossDamageAgainstPlayer();
+        ApplyHardModePointsAndReturnIfPlayerLost();
+
+        // Player's turn starting--first do effects and see if Boss gets killed.
+        DoEffects();
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Now fork for each possible spell the Player could choose, and try those, etc...
+        ForkNewRoundForEachPossibleSpell();
+    }
+
+    static int s_damageBoss, s_hardModePoints, s_lowestCost;
+};
+
+int Solver::s_damageBoss, Solver::s_lowestCost, Solver::s_hardModePoints;
+
 
 void main(int argc, char *argv[])
 {
-    auto start = chrono::system_clock::now();
-    auto answer = Solve(58, 9, 50, 500, false);
-    auto end = chrono::system_clock::now();
+    const int hpPlayer = 50, mana = 500, hpBoss = 58, damageBoss = 9;
+    double seconds = 0;
 
-    chrono::duration<double> seconds = end - start;
-    cout << "part one: " << answer << " found in " << seconds.count() << " seconds" << endl;
+    auto answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, false, seconds);
+    cout << "part one: " << answer << " found in " << seconds << " seconds" << endl;
     assert(answer == 1269);
 
-    start = chrono::system_clock::now();
-    answer = Solve(58, 9, 50, 500, true);
-    end = chrono::system_clock::now();
-
-    seconds = end - start;
-    cout << "part two: " << answer << " found in " << seconds.count() << " seconds" << endl;
+    answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, true, seconds);
+    cout << "part two: " << answer << " found in " << seconds << " seconds" << endl;
     assert(answer == 1309);
 }
