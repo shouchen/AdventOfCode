bca160c2fd506b284201b25140f4e0daf7faf826
Stephen Houchen <shouchen@outlook.com>
2015-12-28 08:05:06 -0800

    Nice clean-up for Day 22

--- Day22/Wizard/Wizard/Wizard.cpp
+++ Day22/Wizard/Wizard/Wizard.cpp
@@ -3,94 +3,88 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <chrono>
+#include <algorithm>
 #include <cassert>
-
-using namespace std;
 
 class Solver
 {
 public:
     static int Solve(int hpBoss, int damageBoss, int hpPlayer, int mana, bool hardMode)
     {
-        // Save parameters that don't need to be on the stack (are constant the whole puzzle).
+        // Initialize statics at the start of puzzle solving.
         s_damageBoss = damageBoss;
         s_hardModePoints = hardMode ? 1 : 0;
         s_lowestCost = INT_MAX;
 
-        // Player's first turn--deduct hit points if hard mode, and check if it killed player.
-        if (ApplyHardModePointsAndCheckIfPlayerLost(hpPlayer)) return s_lowestCost;
-
-        // First spell.
-        ForkNewRoundForEachPossibleSpell(hpBoss, hpPlayer, mana, 0, 0, 0, 0);
-
+        // Take the first player turn, and recurse through game tree.
+        DoPlayerTurn(State { hpBoss, hpPlayer, mana, 0, 0, 0, 0, 0 });
         return s_lowestCost;
     }
 
 private:
-    inline static void DoEffects(int &shieldTimer, int &poisonTimer, int &rechargeTimer, int &armor, int &hpBoss, int &mana)
+    struct State
     {
-        if (shieldTimer) { shieldTimer--; armor = 7; } \
-        if (poisonTimer) { hpBoss -= 3; poisonTimer--; } \
-        if (rechargeTimer) { mana += 101; rechargeTimer--; } \
+        int hpBoss, hpPlayer, mana, armor, cost, shieldTimer, poisonTimer, rechargeTimer;
+    };
+
+    static void DoPlayerTurn(State &state)
+    {
+        // Apply any hard mode points and check if Player has lost.
+        if ((state.hpPlayer -= s_hardModePoints) <= 0) return;
+
+        // Apply any active efects and see if they killed Boss.
+        if (DoEffectsAndCheckIfPlayerWon(state)) return;
+
+        // Cast each possible spell and recurse through resulting game tree.
+        if (state.mana < 53 || state.cost + 53 >= s_lowestCost) return;
+        DoBossTurn(State{ state.hpBoss - 4, state.hpPlayer, state.mana - 53, 0, state.cost + 53, state.shieldTimer, state.poisonTimer, state.rechargeTimer });
+
+        if (state.mana < 73 || state.cost + 73 >= s_lowestCost) return;
+        DoBossTurn(State{ state.hpBoss - 2, state.hpPlayer + 2, state.mana - 73, 0, state.cost + 73, state.shieldTimer, state.poisonTimer, state.rechargeTimer });
+
+        if (state.mana < 113 || state.cost + 113 >= s_lowestCost) return;
+        if (!state.shieldTimer)
+            DoBossTurn(State{ state.hpBoss, state.hpPlayer, state.mana - 113, 0, state.cost + 113, 6, state.poisonTimer, state.rechargeTimer });
+
+        if (state.mana < 173 || state.cost + 173 >= s_lowestCost) return;
+        if (!state.poisonTimer)
+            DoBossTurn(State{ state.hpBoss, state.hpPlayer, state.mana - 173, 0, state.cost + 173, state.shieldTimer, 6, state.rechargeTimer });
+
+        if (state.mana < 229 || state.cost + 229 >= s_lowestCost) return;
+        if (!state.rechargeTimer)
+            DoBossTurn(State{ state.hpBoss, state.hpPlayer, state.mana - 229, 0, state.cost + 229, state.shieldTimer, state.poisonTimer, 5 });
     }
 
-    inline static bool CheckIfPlayerWonAndUpdateLowestCost(int hpBoss, int cost)
+    static void DoBossTurn(State &state)
     {
-        if (hpBoss > 0) return false;
+        // Make sure Boss has not already lost.
+        if (CheckIfPlayerWonAndUpdateLowestCost(state)) return;
 
-        if (cost < s_lowestCost) s_lowestCost = cost; 
+        // Apply any active efects and see if they killed Boss.
+        if (DoEffectsAndCheckIfPlayerWon(state)) return;
+
+        // Now Boss takes his turn (check for Boss win happens at start of Player turn).
+        state.hpPlayer -= std::max(s_damageBoss - state.armor, 1);
+
+        // Now Player's turn again.
+        DoPlayerTurn(state);
+    }
+
+    static bool DoEffectsAndCheckIfPlayerWon(State &state)
+    {
+        if (state.shieldTimer) { state.shieldTimer--; state.armor = 7; }
+        if (state.poisonTimer) { state.hpBoss -= 3; state.poisonTimer--; }
+        if (state.rechargeTimer) { state.mana += 101; state.rechargeTimer--; }
+
+        return CheckIfPlayerWonAndUpdateLowestCost(state);
+    }
+
+    static bool CheckIfPlayerWonAndUpdateLowestCost(const State &state)
+    {
+        if (state.hpBoss > 0) return false;
+
+        s_lowestCost = std::min(state.cost, s_lowestCost);
         return true;
-    }
-
-    inline static void DoBossDamageAgainstPlayer(int armor, int &hpPlayer)
-    {
-        auto diff = s_damageBoss - armor;
-        hpPlayer -= (diff > 1) ? diff : 1;
-    }
-
-    inline static bool ApplyHardModePointsAndCheckIfPlayerLost(int &hpPlayer)
-    {
-        return ((hpPlayer -= s_hardModePoints) <= 0);
-    }
-
-    inline static void ForkNewRoundForEachPossibleSpell(int hpBoss, int hpPlayer, int mana, int cost, int shieldTimer, int poisonTimer, int rechargeTimer)
-    {
-        if (mana < 53 || cost + 53 >= s_lowestCost) return;
-        Round(hpBoss - 4, hpPlayer, mana - 53, cost + 53, shieldTimer, poisonTimer, rechargeTimer);
-
-        if (mana < 73 || cost + 73 >= s_lowestCost) return;
-        Round(hpBoss - 2, hpPlayer + 2, mana - 73, cost + 73, shieldTimer, poisonTimer, rechargeTimer);
-
-        if (mana < 113 || cost + 113 >= s_lowestCost) return;
-        if (!shieldTimer) Round(hpBoss, hpPlayer, mana - 113, cost + 113, 6, poisonTimer, rechargeTimer);
-
-        if (mana < 173 || cost + 173 >= s_lowestCost) return;
-        if (!poisonTimer) Round(hpBoss, hpPlayer, mana - 173, cost + 173, shieldTimer, 6, rechargeTimer);
-
-        if (mana < 229 || cost + 229 >= s_lowestCost) return;
-        if (!rechargeTimer) Round(hpBoss, hpPlayer, mana - 229, cost + 229, shieldTimer, poisonTimer, 5);
-    }
-
-    static void Round(int hpBoss, int hpPlayer, int mana, int cost, int shieldTimer, int poisonTimer, int rechargeTimer)
-    {
-        if (CheckIfPlayerWonAndUpdateLowestCost(hpBoss, cost)) return;
-
-        // Start of Boss's turn--apply efects and see if that killed him.
-        int armor = 0;
-        DoEffects(shieldTimer, poisonTimer, rechargeTimer, armor, hpBoss, mana);
-        if (CheckIfPlayerWonAndUpdateLowestCost(hpBoss, cost)) return;
-
-        // Boss does his turn; check if it killed Player.
-        DoBossDamageAgainstPlayer(armor, hpPlayer);
-        if (ApplyHardModePointsAndCheckIfPlayerLost(hpPlayer)) return;
-
-        // Player's turn starting--first do effects and see if Boss gets killed.
-        DoEffects(shieldTimer, poisonTimer, rechargeTimer, armor, hpBoss, mana);
-        if (CheckIfPlayerWonAndUpdateLowestCost(hpBoss, cost)) return;
-
-        // Now fork for each possible spell the Player could choose, and try those, etc...
-        ForkNewRoundForEachPossibleSpell(hpBoss, hpPlayer, mana, cost, shieldTimer, poisonTimer, rechargeTimer);
     }
 
     static int s_damageBoss, s_hardModePoints, s_lowestCost;
@@ -104,10 +98,10 @@ void main(int argc, char *argv[])
     const int hpPlayer = 50, mana = 500, hpBoss = 58, damageBoss = 9;
 
     auto answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, false);
-    cout << "part one: " << answer << endl;
+    std::cout << "part one: " << answer << std::endl;
     assert(answer == 1269);
 
     answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, true);
-    cout << "part two: " << answer << endl;
+    std::cout << "part two: " << answer << std::endl;
     assert(answer == 1309);
 }
