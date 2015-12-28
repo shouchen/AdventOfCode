8685110f0208da8bfc48005b8b34a58ec5442536
Stephen Houchen <shouchen@outlook.com>
2015-12-27 16:55:44 -0800

    Replacing queue-based approach with stack based one.

--- Day22/Wizard/Wizard/Wizard.cpp
+++ Day22/Wizard/Wizard/Wizard.cpp
@@ -3,231 +3,113 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <memory>
-#include <queue>
+#include <chrono>
 #include <cassert>
-#include <string>
 
 using namespace std;
 
-struct PlayerState
-{
-    int hits, armor, mana;
-};
+// Compiler ignoring requests to inline, so use this as a poor man's inline. Code needs to be
+// highly optimized since this brute force algorithm takes awhile to run.
 
-struct EnemyState
-{
-    int hits, damage;
-};
-
-class Game
-{
-public:
-    Game(PlayerState player, EnemyState enemy) :
-        player(player), enemy(enemy)
-    {
-    }
-
-    void DoMagicMissile()
-    {
-        debug += "m";
-        player.mana -= 53;
-        playerHasSpent += 53;
-
-        DoEffects();
-        enemy.hits -= 4;
-    }
-
-    void DoDrain()
-    {
-        debug += "d";
-        player.mana -= 73;
-        playerHasSpent += 73;
-
-        DoEffects();
-        enemy.hits -= 2;
-        player.hits += 2;
-    }
-
-    void DoShield()
-    {
-        debug += "s";
-        player.mana -= 113;
-        playerHasSpent += 113;
-
-        DoEffects();
-        player.armor += 7;
-        shieldTimer = 6;
-    }
-
-    void DoPoison()
-    {
-        debug += "p";
-        player.mana -= 173;
-        playerHasSpent += 173;
-
-        DoEffects();
-        poisonTimer = 6;
-    }
-
-    void DoRecharge()
-    {
-        debug += "r";
-        player.mana -= 229;
-        playerHasSpent += 229;
-
-        DoEffects();
-        rechargeTimer = 5;
-    }
-
-    void DoEnemyTurn()
-    {
-        DoEffects();
-
-        if (!PlayerHasWon())
-            player.hits -= max(enemy.damage - player.armor, 1);
-    }
-
-    bool CanDoMagicMissile(unsigned leastCostToWin) { return player.mana >= 53 && (playerHasSpent + 53 < leastCostToWin); }
-    bool CanDoDrain(unsigned leastCostToWin) { return player.mana >= 73 && (playerHasSpent + 73 < leastCostToWin); }
-    bool CanDoShield(unsigned leastCostToWin) { return player.mana >= 113 && !shieldTimer && (playerHasSpent + 113 < leastCostToWin); }
-    bool CanDoPoison(unsigned leastCostToWin) { return player.mana >= 173 && !poisonTimer && (playerHasSpent + 173 < leastCostToWin); }
-    bool CanDoRecharge(unsigned leastCostToWin) { return player.mana >= 229 && !rechargeTimer && (playerHasSpent + 229 < leastCostToWin); }
-
-    bool PlayerHasWon() const { return enemy.hits <= 0; }
-    bool PlayerHasLost() const { return player.hits <= 0; }
-    unsigned PlayerHasSpent() const { return playerHasSpent; }
-
-private:
-    void DoEffects()
-    {
-        if (shieldTimer)
-        {
-            if (--shieldTimer == 0)
-                player.armor -= 7;
-        }
-
-        if (poisonTimer)
-        {
-            enemy.hits -= 3;
-            --poisonTimer;
-        }
-
-        if (rechargeTimer)
-        {
-            player.mana += 101;
-            --rechargeTimer;
-        }
-    }
-
-    PlayerState player;
-    EnemyState enemy;
-    int shieldTimer = 0, poisonTimer = 0, rechargeTimer = 0;
-    unsigned playerHasSpent = 0;
-
-public:
-    string debug;
-
-    friend void DoTest1();
-    friend void DoTest2();
-    friend struct MyComparison;
-};
-
-#include "Tests.h"
-
-struct MyComparison
-{
-    bool operator()(const Game &lhs, const Game &rhs) const
-    {
-        if (lhs.player.hits - lhs.enemy.hits < rhs.player.hits - rhs.enemy.hits) return true;
-        if (lhs.player.hits - lhs.enemy.hits > rhs.player.hits - rhs.enemy.hits) return false;
-
-        if (lhs.PlayerHasSpent() > rhs.PlayerHasSpent()) return true;
-        if (lhs.PlayerHasSpent() < rhs.PlayerHasSpent()) return false;
-
-        return false;
-    }
-};
-
-priority_queue<Game, vector<Game>, MyComparison> queue;
-unsigned leastCostToWin = UINT_MAX;
-
-void DoPostMove(Game &nextGame)
-{
-    if (nextGame.PlayerHasSpent() < leastCostToWin)
-    {
-        if (nextGame.PlayerHasWon())
-            leastCostToWin = nextGame.PlayerHasSpent();
-        else
-        {
-            assert(!nextGame.PlayerHasLost());
-
-            nextGame.DoEnemyTurn();
-
-            if (nextGame.PlayerHasWon())
-                leastCostToWin = nextGame.PlayerHasSpent();
-            else if (!nextGame.PlayerHasLost())
-                ::queue.push(nextGame);
-        }
-    }
+#define DoEffects() { \
+if (shieldTimer) { shieldTimer--; armor = 7; } \
+if (poisonTimer) { hpBoss -= 3; poisonTimer--; } \
+if (rechargeTimer) { mana += 101; rechargeTimer--; } \
 }
 
-void _tmain(int argc, _TCHAR *argv[])
+#define DoBossDamageAgainstPlayer() { \
+    auto diff = s_damageBoss - armor; \
+    hpPlayer -= (diff > 1) ? diff : 1; \
+}
+
+#define ReturnIfPlayerWonAndUpdateBestCost() { \
+if (hpBoss <= 0) { if (cost < s_lowestCost) s_lowestCost = cost; return; } \
+}
+
+#define ApplyHardModePointsAndReturnIfPlayerLost() { \
+if ((hpPlayer -= s_hardModePoints) <= 0) return; \
+}
+
+#define ForkNewRoundForEachPossibleSpell() { \
+if (mana >= 53) \
+    Round(hpBoss - 4, hpPlayer, mana - 53, cost + 53, shieldTimer, poisonTimer, rechargeTimer); \
+if (mana >= 73) \
+    Round(hpBoss - 2, hpPlayer + 2, mana - 73, cost + 73, shieldTimer, poisonTimer, rechargeTimer); \
+if (mana >= 113 && !shieldTimer) \
+    Round(hpBoss, hpPlayer, mana - 113, cost + 113, 6, poisonTimer, rechargeTimer); \
+if (mana >= 173 && !poisonTimer) \
+    Round(hpBoss, hpPlayer, mana - 173, cost + 173, shieldTimer, 6, rechargeTimer); \
+if (mana >= 229 && !rechargeTimer) \
+    Round(hpBoss, hpPlayer, mana - 229, cost + 229, shieldTimer, poisonTimer, 5); \
+}
+
+class Solver
 {
-    DoTest1();
-    DoTest2();
-
-//    Game game(PlayerState{ 10, 0, 0, 250 }, EnemyState{ 13, 8 });
-//  Game game(PlayerState{ 10, 0, 0, 250 }, EnemyState{ 14, 8 });
-    Game game(PlayerState{ 50, 0, 500 }, EnemyState{ 58, 9 });
-    ::queue.push(game);
-
-    while (!::queue.empty())
+public:
+    static int Solve(int hpBoss, int damageBoss, int hpPlayer, int mana, bool hardMode, double &time)
     {
-        Game game = ::queue.top();
-        ::queue.pop();
+        // Record time taken to solve (useful for performance tuning this algorithm).
+        auto start = chrono::system_clock::now();
 
-        if (game.PlayerHasSpent() >= leastCostToWin) continue;
+        // Save parameters that don't need to be on the stack (are constant the whole puzzle).
+        s_damageBoss = damageBoss;
+        s_hardModePoints = hardMode ? 1 : 0;
+        s_lowestCost = INT_MAX;
 
-        //cout << game.PlayerHasSpent() << " " << game.debug << endl;
+        // Player's first turn--deduct hit points if hard mode, and check if it killed player.
+        hpPlayer -= s_hardModePoints;
+        if (hpPlayer <= 0) return s_lowestCost;
 
-        // Try all possible moves
-        if (game.CanDoPoison(leastCostToWin))
-        {
-            auto nextGame = game;
-            nextGame.DoPoison();
-            DoPostMove(nextGame);
-        }
+        // First spell.
+        const auto shieldTimer = 0, poisonTimer = 0, rechargeTimer = 0, cost = 0;
+        ForkNewRoundForEachPossibleSpell();
 
-        if (game.CanDoShield(leastCostToWin))
-        {
-            auto nextGame = game;
-            nextGame.DoShield();
-            DoPostMove(nextGame);
-        }
+        // Figure out how long the whole thing took so we can report it out.
+        auto end = chrono::system_clock::now();
+        chrono::duration<double> seconds = end - start;
+        time = seconds.count();
 
-        if (game.CanDoRecharge(leastCostToWin))
-        {
-            auto nextGame = game;
-            nextGame.DoRecharge();
-            DoPostMove(nextGame);
-        }
-
-        if (game.CanDoMagicMissile(leastCostToWin))
-        {
-            auto nextGame = game;
-            nextGame.DoMagicMissile();
-            DoPostMove(nextGame);
-        }
-
-        if (game.CanDoDrain(leastCostToWin))
-        {
-            auto nextGame = game;
-            nextGame.DoDrain();
-            DoPostMove(nextGame);
-        }
+        return s_lowestCost;
     }
 
-    assert(leastCostToWin == 1269); // part 2 = 1309
-    cout << "part one: " << leastCostToWin << endl;
+private:
+    static void Round(int hpBoss, int hpPlayer, int mana, int cost, int shieldTimer, int poisonTimer, int rechargeTimer)
+    {
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Start of Boss's turn--apply efects and see if that killed him.
+        int armor = 0;
+        DoEffects();
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Boss does his turn; check if it killed Player.
+        DoBossDamageAgainstPlayer();
+        ApplyHardModePointsAndReturnIfPlayerLost();
+
+        // Player's turn starting--first do effects and see if Boss gets killed.
+        DoEffects();
+        ReturnIfPlayerWonAndUpdateBestCost();
+
+        // Now fork for each possible spell the Player could choose, and try those, etc...
+        ForkNewRoundForEachPossibleSpell();
+    }
+
+    static int s_damageBoss, s_hardModePoints, s_lowestCost;
+};
+
+int Solver::s_damageBoss, Solver::s_lowestCost, Solver::s_hardModePoints;
+
+
+void main(int argc, char *argv[])
+{
+    const int hpPlayer = 50, mana = 500, hpBoss = 58, damageBoss = 9;
+    double seconds = 0;
+
+    auto answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, false, seconds);
+    cout << "part one: " << answer << " found in " << seconds << " seconds" << endl;
+    assert(answer == 1269);
+
+    answer = Solver::Solve(hpBoss, damageBoss, hpPlayer, mana, true, seconds);
+    cout << "part two: " << answer << " found in " << seconds << " seconds" << endl;
+    assert(answer == 1309);
 }
