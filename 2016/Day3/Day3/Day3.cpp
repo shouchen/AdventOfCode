472570ee5d8e3fb0b1bed5db2babccc552dc06bc
Stephen Houchen <shouchen@outlook.com>
2016-12-06 08:35:09 -0800

    Simplified Day3 code.

--- 2016/Day3/Day3/Day3.cpp
+++ 2016/Day3/Day3/Day3.cpp
@@ -1,59 +1,40 @@
-// Day3.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <cassert>
+
+int answer1 = 0, answer2 = 0;
 
 bool is_triangle(unsigned a, unsigned b, unsigned c)
 {
     return (a + b > c) && (a + c > b) && (b + c > a);
 }
 
-unsigned do_part1()
+void process_file(const char *filename)
 {
-    std::ifstream file;
-    file.open("input.txt");
-
-    unsigned a, b, c;
-    unsigned num_valid = 0;
-
-    while (file >> a >> b >> c)
-    {
-        if (is_triangle(a, b, c)) num_valid++;
-    }
-
-    return num_valid;
-}
-
-unsigned do_part2()
-{
-    std::ifstream file;
-    file.open("input.txt");
-
+    std::ifstream file(filename);
     unsigned a, b, c, d, e, f, g, h, i;
-    unsigned num_valid = 0;
 
     while (file >> a >> b >> c >> d >> e >> f >> g >> h >> i)
     {
-        if (is_triangle(a, d, g)) num_valid++;
-        if (is_triangle(b, e, h)) num_valid++;
-        if (is_triangle(c, f, i)) num_valid++;
-    }
+        if (is_triangle(a, b, c)) answer1++;
+        if (is_triangle(d, e, f)) answer1++;
+        if (is_triangle(g, h, i)) answer1++;
 
-    return num_valid;
+        if (is_triangle(a, d, g)) answer2++;
+        if (is_triangle(b, e, h)) answer2++;
+        if (is_triangle(c, f, i)) answer2++;
+    }
 }
 
 int main()
 {
-    auto num_valid = do_part1();
-    std::cout << "Part One: " << num_valid << std::endl;
-    assert(num_valid == 869);
+    process_file("input.txt");
 
-    num_valid = do_part2();
-    std::cout << "Part Two: " << num_valid << std::endl;
-    assert(num_valid == 1544);
+    std::cout << "Part One: " << answer1 << std::endl;
+    std::cout << "Part Two: " << answer2 << std::endl;
 
+    assert(answer1 == 869);
+    assert(answer2 == 1544);
     return 0;
 }
