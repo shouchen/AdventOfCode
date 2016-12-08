4dd1d745aa6632660152bbcafc0e36293f33031e
Stephen Houchen <shouchen@outlook.com>
2016-12-08 07:53:49 -0800

    Switched to more efficient rotates and cleaned up code.

--- 2016/Day8/Day8/Day8.cpp
+++ 2016/Day8/Day8/Day8.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -14,67 +15,69 @@ public:
     unsigned Count() const;
 
 private:
-    static const int NUM_ROWS = 6;
-    static const int NUM_COLUMNS = 50;
-    static const char on = '#';
-    static const char off = ' ';
-    char data[NUM_ROWS][NUM_COLUMNS];
+    void ReverseColumn(unsigned col, unsigned startRow, unsigned endRow);
+
+    static const int NumRows = 6;
+    static const int NumColumns = 50;
+    static const char On = '#';
+    static const char Off = ' ';
+    char data[NumRows][NumColumns];
 
     friend std::ostream &operator<<(std::ostream &o, const Board &b);
 };
 
 Board::Board()
 {
-    for (auto row = 0U; row < NUM_ROWS; row++)
-        for (auto column = 0U; column < NUM_COLUMNS; column++)
-            data[row][column] = off;
+    for (auto row = 0U; row < NumRows; row++)
+        for (auto &cell : data[row])
+            cell = Off;
 }
 
 void Board::Rect(unsigned a, unsigned b)
 {
     for (auto row = 0U; row < b; row++)
         for (auto column = 0U; column < a; column++)
-            data[row][column] = on;
+            data[row][column] = On;
 }
 
 void Board::RotateRow(unsigned a, unsigned b)
 {
-    while (b--)
-    {
-        auto temp = data[a][NUM_COLUMNS - 1];
-        for (auto j = NUM_COLUMNS - 1; j > 0; j--)
-            data[a][j] = data[a][j - 1];
-        data[a][0] = temp;
-    }
+    auto split = &data[a][NumColumns - b];
+    std::reverse(&data[a][0], split);
+    std::reverse(split, &data[a][NumColumns]);
+    std::reverse(&data[a][0], &data[a][NumColumns]);
 }
 
 void Board::RotateColumn(unsigned a, unsigned b)
 {
-    while (b--)
-    {
-        auto temp = data[NUM_ROWS - 1][a];
-        for (auto j = NUM_ROWS - 1; j > 0; j--)
-            data[j][a] = data[j - 1][a];
-        data[0][a] = temp;
-    }
+    auto split = NumRows - b;
+    ReverseColumn(a, 0, split - 1);
+    ReverseColumn(a, split, NumRows - 1);
+    ReverseColumn(a, 0, NumRows - 1);
 }
 
 unsigned Board::Count() const
 {
     auto count = 0U;
-    for (auto row = 0U; row < NUM_ROWS; row++)
-        for (auto column = 0U; column < NUM_COLUMNS; column++)
-            if (data[row][column] == on)
+    for (auto row = 0U; row < NumRows; row++)
+        for (auto cell : data[row])
+            if (cell == On)
                 count++;
 
     return count;
 }
 
+void Board::ReverseColumn(unsigned col, unsigned startRow, unsigned endRow)
+{
+    for (auto top = startRow, bottom = endRow; top < bottom; top++, bottom--)
+        std::swap(data[top][col], data[bottom][col]);
+}
+
 std::ostream &operator<<(std::ostream &o, const Board &bb)
 {
-    for (auto i = 0U; i < bb.NUM_ROWS; i++)
+    for (auto i = 0U; i < bb.NumRows; i++)
     {
-        for (auto j = 0U; j < bb.NUM_COLUMNS; j++)
+        for (auto j = 0U; j < bb.NumColumns; j++)
             o << bb.data[i][j];
         o << std::endl;
     }
@@ -82,7 +85,7 @@ std::ostream &operator<<(std::ostream &o, const Board &bb)
     return o;
 }
 
-void process_file(const std::string &filename, Board &board)
+void ProcessFile(const std::string &filename, Board &board)
 {
     std::ifstream f(filename);
     std::string word, by;
@@ -117,7 +120,7 @@ void process_file(const std::string &filename, Board &board)
 int main()
 {
     Board board;
-    process_file("input.txt", board);
+    ProcessFile("input.txt", board);
 
     auto count = board.Count();
 
