b6a6a2993a95af6474911160cbf1710149360c49
Stephen Houchen <shouchen@outlook.com>
2016-12-12 10:10:25 -0800

    Switching to an assembled implementation instead of an interpreted one (for perf).

--- 2016/Day12/Day12/Day12.cpp
+++ 2016/Day12/Day12/Day12.cpp
@@ -5,112 +5,175 @@
 #include <string>
 #include <cassert>
 
-using Program = std::vector<std::string>;
+
+class Computer;
+class Instruction;
+
+using Program = std::vector<Instruction *>;
+
+class Instruction
+{
+public:
+    virtual void Execute(Computer &computer) = 0;
+};
 
 class Computer
 {
 public:
-    Computer(int initialC = 0) : a(0), b(0), c(initialC), d(0) {}
+    Computer() { InitializeRegisters(); }
 
-    void ExecuteProgram(const Program &program)
+    void InitializeRegisters(int a = 0, int b = 0, int c = 0, int d = 0)
     {
-        while (pc < program.size())
-        {
-            std::string instruction = program[pc];
-            std::string::iterator pos = instruction.begin();
-
-            if (strncmp(instruction.c_str(), "cpy ", 4) == 0)
-            {
-                int value = 0;
-                pos += 4;
-                if (*pos >= 'a' && *pos <= 'd')
-                {
-                    value = GetRegister(*pos++);
-                }
-                else
-                {
-                    value = atoi(&*pos);
-                    while (*pos != ' ') pos++;
-                }
-                pos++;
-
-                DoCpy(value, GetRegister(*pos));
-            }
-            else if (strncmp(instruction.c_str(), "inc ", 4) == 0)
-            {
-                DoInc(GetRegister(*(pos + 4)));
-            }
-            else if (strncmp(instruction.c_str(), "dec ", 4) == 0)
-            {
-                DoDec(GetRegister(*(pos + 4)));
-            }
-            else if (strncmp(instruction.c_str(), "jnz ", 4) == 0)
-            {
-                int value = 0;
-                pos += 4;
-                if (*pos >= 'a' && *pos <= 'd')
-                {
-                    value = GetRegister(*pos++);
-                }
-                else
-                {
-                    value = atoi(&*pos);
-                    while (*pos != ' ') pos++;
-                }
-                pos++;
-
-                int offset = atoi(&*pos);
-
-                DoJnz(value, offset);
-            }
-        }
+        GetRegister('a') = a;
+        GetRegister('b') = b;
+        GetRegister('c') = c;
+        GetRegister('d') = d;
     }
 
-    int GetA() { return a; }
+    inline int &GetRegister(char name)
+    {
+        assert(name >= 'a' && name <= 'd');
+        return registers[name - 'a'];
+    }
+
+    inline void DoCpy(char from, char to) { DoCpy(GetRegister(from), to); }
+    inline void DoCpy(int value, char to) { GetRegister(to) = value; pc++; }
+    inline void DoInc(char r) { GetRegister(r)++; pc++; }
+    inline void DoDec(char r) { GetRegister(r)--; pc++; }
+    inline void DoJnz(char reg, int offset) { DoJnz(GetRegister(reg), offset); }
+    inline void DoJnz(int value, int offset) { pc += value ? offset : 1; }
+
+    void ExecuteProgram(const Program &Program)
+    {
+        for (pc = 0; pc < Program.size(); )
+            Program[pc]->Execute(*this);
+    }
 
 private:
-    int &GetRegister(char name) {
-        switch (name)
-        {
-        case 'a': return a;
-        case 'b': return b;
-        case 'c': return c;
-        case 'd': return d;
-        default: assert(false); return a;
-        }
-    }
-    void DoCpy(int value, int &r) { r = value; pc++; }
-    void DoInc(int &r) { r++; pc++; }
-    void DoDec(int &r) { r--; pc++; }
-    void DoJnz(int value, int offset) { pc += value ? offset : 1; }
-
-    int a, b, c, d;
+    int registers[4];
     unsigned pc = 0;
 };
 
-void ReadProgram(const std::string &filename, Program &program)
+class CpyRegisterInstruction : public Instruction
 {
-    std::ifstream f(filename);
-    for (std::string line; std::getline(f, line);)
-        program.push_back(line);
-}
+public:
+    CpyRegisterInstruction(char from, char to) : from(from), to(to) {}
+    void Execute(Computer &computer) override { computer.DoCpy(from, to); }
+private:
+    char from, to;
+};
+
+class CpyValueInstruction : public Instruction
+{
+public:
+    CpyValueInstruction(int value, char to) : value(value), to(to) {}
+    void Execute(Computer &computer) override { computer.DoCpy(value, to); }
+private:
+    int value;
+    char to;
+};
+
+class IncInstruction : public Instruction
+{
+public:
+    IncInstruction(char reg) : reg(reg) {}
+    void Execute(Computer &computer) override { computer.DoInc(reg); }
+private:
+    char reg;
+};
+
+class DecInstruction : public Instruction
+{
+public:
+    DecInstruction(char reg) : reg(reg) {}
+    void Execute(Computer &computer) override { computer.DoDec(reg); }
+private:
+    char reg;
+};
+
+class JnzRegisterInstruction : public Instruction
+{
+public:
+    JnzRegisterInstruction(char reg, int offset) : reg(reg), offset(offset) {}
+    void Execute(Computer &computer) override { computer.DoJnz(reg, offset); }
+private:
+    char reg;
+    int offset;
+};
+
+class JnzValueInstruction : public Instruction
+{
+public:
+    JnzValueInstruction(int value, int offset) : value(value), offset(offset) {}
+    void Execute(Computer &computer) override { computer.DoJnz(value, offset); }
+private:
+    int value, offset;
+};
+
+class Assembler
+{
+public:
+    static void AssembleProgram(const std::string &filename, Program &Program)
+    {
+        std::ifstream f(filename);
+        std::string opcode, op1, op2;
+
+        while (f >> opcode)
+        {
+            if (opcode == "cpy")
+            {
+                f >> op1 >> op2;
+                IsRegister(op1)
+                    ? Program.push_back(new CpyRegisterInstruction(op1[0], op2[0]))
+                    : Program.push_back(new CpyValueInstruction(atoi(op1.c_str()), op2[0]));
+            }
+            else if (opcode == "inc")
+            {
+                f >> op1;
+                Program.push_back(new IncInstruction(op1[0]));
+            }
+            else if (opcode == "dec")
+            {
+                f >> op1;
+                Program.push_back(new DecInstruction(op1[0]));
+            }
+            else if (opcode == "jnz")
+            {
+                f >> op1 >> op2;
+                IsRegister(op1)
+                    ? Program.push_back(new JnzRegisterInstruction(op1[0], atoi(op2.c_str())))
+                    : Program.push_back(new JnzValueInstruction(atoi(op1.c_str()), atoi(op2.c_str())));
+            }
+            else
+                assert(false);
+        }
+    }
+
+private:
+    inline static bool IsRegister(const std::string x)
+    {
+        return x.length() == 1 && x[0] >= 'a' && x[0] <= 'd';
+    }
+};
 
 int _tmain(int argc, _TCHAR *argv[])
 {
-    Program program;
-    ReadProgram("Input.txt", program);
+    Program Program;
+    Assembler::AssembleProgram("Input.txt", Program);
 
-    Computer computer1;
-    computer1.ExecuteProgram(program);
-    auto partOne = computer1.GetA();
+    Computer computer;
+    computer.ExecuteProgram(Program);
+
+    auto partOne = computer.GetRegister('a');
     std::cout << "Part One: " << partOne << std::endl;
 
-    Computer computer2(1);
-    computer2.ExecuteProgram(program);
-    auto partTwo = computer2.GetA();
+    computer.InitializeRegisters(0, 0, 1, 0);
+    computer.ExecuteProgram(Program);
+
+    auto partTwo = computer.GetRegister('a');
     std::cout << "Part Two: " << partTwo << std::endl;
 
-    assert(partOne == 318077); 
+    assert(partOne == 318077);
     assert(partTwo == 9227731);
     return 0;
 }
