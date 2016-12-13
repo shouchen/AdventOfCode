356c90440d5e853499533f042c39c1b55f86cf1e
Stephen Houchen <shouchen@outlook.com>
2016-12-13 07:06:00 -0800

    Initial Day 13 clean-up

--- 2016/Day13/Day13/Day13.cpp
+++ 2016/Day13/Day13/Day13.cpp
@@ -1,17 +1,13 @@
 #include "stdafx.h"
 #include <iostream>
 #include <climits>
-
-// Array:
-// -1 = wall
-// MAX_INT = never visited
+#include <cassert>
 
 #define MAX_Y 250
 #define MAX_X 250
-int board[MAX_Y][MAX_X];
 
-#define TARGET_X 31
-#define TARGET_Y 39
+int board[MAX_Y][MAX_X];
+int numReachable = 1;
 
 #define FAVORITE_NUMBER 1364
 
@@ -31,35 +27,18 @@ inline bool IsWall(int y, int x)
     return CountSetBits(x * x + 3 * x + 2 * x * y + y + y * y + FAVORITE_NUMBER) & 0x1;
 }
 
-void Dump()
-{
-    for (int i = 0; i < MAX_Y; i++)
-    {
-        for (int j = 0; j < MAX_X; j++)
-        {
-            if (board[i][j] == -1)
-                std::cout << "# ";
-            else if (board[i][j] == INT_MAX)
-                std::cout << ". ";
-            else
-                std::cout << char(board[i][j] + '@') << ' ';
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-bool Foo(int x, int y, unsigned gen)
+bool Foo(int x, int y, unsigned gen, unsigned targetX, unsigned targetY)
 {
     if (x >= 0 && x < MAX_X && y >=0 && y <= MAX_Y && board[y][x] == INT_MAX)
     {
         board[y][x] = gen;
-        return x == TARGET_X && y == TARGET_Y;
+        ++numReachable;
+        return x == targetX && y == targetY;
     }
     return false;
 }
 
-bool DoGeneration(unsigned gen)
+bool DoGeneration(unsigned gen, unsigned targetX, unsigned targetY)
 {
     for (int y = 0; y < MAX_Y; y++)
     {
@@ -67,10 +46,10 @@ bool DoGeneration(unsigned gen)
         {
             if (board[y][x] == gen - 1)
             {
-                if (Foo(x, y - 1, gen)) return true; // UP
-                if (Foo(x, y + 1, gen)) return true; // DOWN
-                if (Foo(x - 1, y, gen)) return true; // LEFT
-                if (Foo(x + 1, y, gen)) return true; // RIGHT
+                if (Foo(x, y - 1, gen, targetX, targetY)) return true; // UP
+                if (Foo(x, y + 1, gen, targetX, targetY)) return true; // DOWN
+                if (Foo(x - 1, y, gen, targetX, targetY)) return true; // LEFT
+                if (Foo(x + 1, y, gen, targetX, targetY)) return true; // RIGHT
             }
         }
     }
@@ -78,37 +57,33 @@ bool DoGeneration(unsigned gen)
     return false;
 }
 
-unsigned NumReachable()
-{
-    auto count = 0U;
-    for (int y = 0; y < MAX_Y; y++)
-        for (int x = 0; x < MAX_X; x++)
-            if (board[y][x] < INT_MAX && board[y][x] != -1)
-                count++;
-
-    return count;
-}
-
-int main()
+void Solve(unsigned startX, unsigned startY, unsigned targetX, unsigned targetY, unsigned favorite, unsigned part2Steps, unsigned &part1, unsigned &part2)
 {
     for (int y = 0; y < MAX_Y; y++)
         for (int x = 0; x < MAX_X; x++)
             board[y][x] = IsWall(y, x) ? -1 : INT_MAX;
 
-    board[1][1] = 0;
+    board[startY][startX] = 0;
 
-    // Part 1
-    //unsigned gen = 0;
-    //while (!DoGeneration(++gen));
+    part1 = 0, part2 = 0;
+    for (unsigned gen = 1; !part1 || !part2; gen++)
+    {
+        bool temp = DoGeneration(gen, targetX, targetY);
+        if (temp && !part1) part1 = gen;
+        if (gen == part2Steps) part2 = numReachable;
+    }
+}
 
-    // Part 2
-    for (int i = 1; i <= 50; i++)
-        DoGeneration(i);
+int main()
+{
+    unsigned part1 = 0, part2 = 0;
+    Solve(1, 1, 31, 39, 1364, 50, part1, part2);
 
-    auto part2 = NumReachable();
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
 
-    // Assert part 1 = 86
-    // Assert Part 2 = 127
+    assert(part1 == 86);
+    assert(part2 == 127);
 
     return 0;
 }
