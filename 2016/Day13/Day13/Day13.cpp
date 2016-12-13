c51df2791726a625d858111bee4804ceb7c002c3
Stephen Houchen <shouchen@outlook.com>
2016-12-13 07:58:12 -0800

    Final Day 13 clean-up.

--- 2016/Day13/Day13/Day13.cpp
+++ 2016/Day13/Day13/Day13.cpp
@@ -9,7 +9,7 @@ class CubeMaze
 public:
     CubeMaze(unsigned favorite) : favorite(favorite) {}
 
-    void Solve(unsigned startX, unsigned startY, unsigned targetX, unsigned targetY, unsigned part2Steps)
+    bool Solve(unsigned startX, unsigned startY, unsigned targetX, unsigned targetY, unsigned part2Steps)
     {
         for (auto y = 0; y < Y; y++)
             for (auto x = 0; x < X; x++)
@@ -24,36 +24,46 @@ public:
         part1 = 0, part2 = 0;
         for (auto gen = 1U; !part1 || !part2; gen++)
         {
-            DoGeneration(gen);
+            auto newReachable = DoGeneration(gen);
+            if (!newReachable) return false;
+
+            numReachable += newReachable;
+
             if (!part1 && board[targetY][targetX] == gen) part1 = gen;
             if (gen == part2Steps) part2 = numReachable;
         }
+
+        return true;
     }
 
     unsigned GetPart1() { return part1; }
     unsigned GetPart2() { return part2; }
 
 private:
-    void DoGeneration(unsigned gen)
+    unsigned DoGeneration(unsigned gen)
     {
+        auto newReachable = 0U;
+
         for (auto y = 0; y < Y; y++)
             for (auto x = 0; x < X; x++)
                 if (board[y][x] == gen - 1)
                 {
-                    TryReachNeighbor(x, y - 1, gen); // UP
-                    TryReachNeighbor(x, y + 1, gen); // DOWN
-                    TryReachNeighbor(x - 1, y, gen); // LEFT
-                    TryReachNeighbor(x + 1, y, gen); // RIGHT
+                    if (TryReachNeighbor(x, y - 1, gen)) newReachable++; // UP
+                    if (TryReachNeighbor(x, y + 1, gen)) newReachable++; // DOWN
+                    if (TryReachNeighbor(x - 1, y, gen)) newReachable++; // LEFT
+                    if (TryReachNeighbor(x + 1, y, gen)) newReachable++; // RIGHT
                 }
+
+        return newReachable;
     }
 
-    void TryReachNeighbor(int x, int y, unsigned gen)
+    bool TryReachNeighbor(int x, int y, unsigned gen)
     {
-        if (x >= 0 && x < X && y >= 0 && y <= Y && board[y][x] == INT_MAX)
-        {
-            board[y][x] = gen;
-            ++numReachable;
-        }
+        if (x < 0 || x >= X || y < 0 || y >= Y || board[y][x] != INT_MAX)
+            return false;
+
+        board[y][x] = gen;
+        return true;
     }
 
     inline bool IsWall(int x, int y)
@@ -79,12 +89,16 @@ private:
 int main()
 {
     CubeMaze<250, 250> cubeMaze(1364);
-    cubeMaze.Solve(1, 1, 31, 39, 50);
+    auto part1 = 0U, part2 = 0U;
 
-    auto part1 = cubeMaze.GetPart1(), part2 = cubeMaze.GetPart2();
+    if (cubeMaze.Solve(1, 1, 31, 39, 50))
+    {
+        part1 = cubeMaze.GetPart1();
+        part2 = cubeMaze.GetPart2();
 
-    std::cout << "Part One: " << cubeMaze.GetPart1() << std::endl;
-    std::cout << "Part Two: " << cubeMaze.GetPart2() << std::endl;
+        std::cout << "Part One: " << cubeMaze.GetPart1() << std::endl;
+        std::cout << "Part Two: " << cubeMaze.GetPart2() << std::endl;
+    }
 
     assert(part1 == 86);
     assert(part2 == 127);
