cb9b7cbcb082bd70c6badb1d9a0e403980cc7824
Stephen Houchen <shouchen@outlook.com>
2016-12-13 07:48:35 -0800

    Clean-up for Day 13.

--- 2016/Day13/Day13/Day13.cpp
+++ 2016/Day13/Day13/Day13.cpp
@@ -3,87 +3,90 @@
 #include <climits>
 #include <cassert>
 
-#define MAX_Y 250
-#define MAX_X 250
-
-int board[MAX_Y][MAX_X];
-int numReachable = 1;
-
-#define FAVORITE_NUMBER 1364
-
-inline unsigned CountSetBits(unsigned n)
+template<unsigned X, unsigned Y>
+class CubeMaze
 {
-    unsigned count = 0;
-    while (n)
+public:
+    CubeMaze(unsigned favorite) : favorite(favorite) {}
+
+    void Solve(unsigned startX, unsigned startY, unsigned targetX, unsigned targetY, unsigned part2Steps)
     {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
+        for (auto y = 0; y < Y; y++)
+            for (auto x = 0; x < X; x++)
+                board[y][x] = IsWall(x, y) ? -1 : INT_MAX;
 
-inline bool IsWall(int y, int x)
-{
-    return CountSetBits(x * x + 3 * x + 2 * x * y + y + y * y + FAVORITE_NUMBER) & 0x1;
-}
+        this->targetX = targetX;
+        this->targetY = targetY;
 
-bool Foo(int x, int y, unsigned gen, unsigned targetX, unsigned targetY)
-{
-    if (x >= 0 && x < MAX_X && y >=0 && y <= MAX_Y && board[y][x] == INT_MAX)
-    {
-        board[y][x] = gen;
-        ++numReachable;
-        return x == targetX && y == targetY;
-    }
-    return false;
-}
+        board[startY][startX] = 0;
+        numReachable = 1;
 
-bool DoGeneration(unsigned gen, unsigned targetX, unsigned targetY)
-{
-    for (int y = 0; y < MAX_Y; y++)
-    {
-        for (int x = 0; x < MAX_X; x++)
+        part1 = 0, part2 = 0;
+        for (auto gen = 1U; !part1 || !part2; gen++)
         {
-            if (board[y][x] == gen - 1)
-            {
-                if (Foo(x, y - 1, gen, targetX, targetY)) return true; // UP
-                if (Foo(x, y + 1, gen, targetX, targetY)) return true; // DOWN
-                if (Foo(x - 1, y, gen, targetX, targetY)) return true; // LEFT
-                if (Foo(x + 1, y, gen, targetX, targetY)) return true; // RIGHT
-            }
+            DoGeneration(gen);
+            if (!part1 && board[targetY][targetX] == gen) part1 = gen;
+            if (gen == part2Steps) part2 = numReachable;
         }
     }
 
-    return false;
-}
+    unsigned GetPart1() { return part1; }
+    unsigned GetPart2() { return part2; }
 
-void Solve(unsigned startX, unsigned startY, unsigned targetX, unsigned targetY, unsigned favorite, unsigned part2Steps, unsigned &part1, unsigned &part2)
-{
-    for (int y = 0; y < MAX_Y; y++)
-        for (int x = 0; x < MAX_X; x++)
-            board[y][x] = IsWall(y, x) ? -1 : INT_MAX;
-
-    board[startY][startX] = 0;
-
-    part1 = 0, part2 = 0;
-    for (unsigned gen = 1; !part1 || !part2; gen++)
+private:
+    void DoGeneration(unsigned gen)
     {
-        bool temp = DoGeneration(gen, targetX, targetY);
-        if (temp && !part1) part1 = gen;
-        if (gen == part2Steps) part2 = numReachable;
+        for (auto y = 0; y < Y; y++)
+            for (auto x = 0; x < X; x++)
+                if (board[y][x] == gen - 1)
+                {
+                    TryReachNeighbor(x, y - 1, gen); // UP
+                    TryReachNeighbor(x, y + 1, gen); // DOWN
+                    TryReachNeighbor(x - 1, y, gen); // LEFT
+                    TryReachNeighbor(x + 1, y, gen); // RIGHT
+                }
     }
-}
+
+    void TryReachNeighbor(int x, int y, unsigned gen)
+    {
+        if (x >= 0 && x < X && y >= 0 && y <= Y && board[y][x] == INT_MAX)
+        {
+            board[y][x] = gen;
+            ++numReachable;
+        }
+    }
+
+    inline bool IsWall(int x, int y)
+    {
+        return CountSetBits(x * x + 3 * x + 2 * x * y + y + y * y + favorite) & 0x1;
+    }
+
+    inline unsigned CountSetBits(unsigned n)
+    {
+        auto count = 0U;
+        while (n)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+    int board[Y][X];
+    unsigned targetX = 0, targetY = 0, favorite, numReachable = 0, part1 = 0, part2 = 0;
+};
 
 int main()
 {
-    unsigned part1 = 0, part2 = 0;
-    Solve(1, 1, 31, 39, 1364, 50, part1, part2);
+    CubeMaze<250, 250> cubeMaze(1364);
+    cubeMaze.Solve(1, 1, 31, 39, 50);
 
-    std::cout << "Part One: " << part1 << std::endl;
-    std::cout << "Part Two: " << part2 << std::endl;
+    auto part1 = cubeMaze.GetPart1(), part2 = cubeMaze.GetPart2();
+
+    std::cout << "Part One: " << cubeMaze.GetPart1() << std::endl;
+    std::cout << "Part Two: " << cubeMaze.GetPart2() << std::endl;
 
     assert(part1 == 86);
     assert(part2 == 127);
-
     return 0;
 }
