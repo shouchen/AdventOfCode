56e2b1a5321fcfb0780f9b2c7aebaf203690e002
Stephen Houchen <shouchen@outlook.com>
2016-12-14 11:06:20 -0800

    Perf for Day14 part 2

--- 2016/Day14/Day14/Day14.cpp
+++ 2016/Day14/Day14/Day14.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <sstream>
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -17,9 +16,10 @@ std::string salt = "zpqevtbw";
 const int extraTimes = 0; // CHANGE TO 0 FOR PART 1 OR 2016 FOR PART 2
 
 
-std::string GetHashText(HCRYPTPROV hProv, const void *data, const size_t dataSize)
+static const char hexdigit[] = "0123456789abcdef";
+
+void GetHashTextInternal(HCRYPTPROV hProv, const void *data, const size_t dataSize, char hashed[32])
 {
-    std::ostringstream oss;
     HCRYPTPROV hHash = NULL;
 
     if (CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
@@ -29,32 +29,34 @@ std::string GetHashText(HCRYPTPROV hProv, const void *data, const size_t dataSiz
         if (CryptHashData(hHash, static_cast<const BYTE *>(data), dataSize, 0) &&
             CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE *)&cbHashSize, &dwCount, 0))
         {
-            std::vector<BYTE> buffer(cbHashSize);
+            assert(cbHashSize == 16);
+            BYTE buffer[16];
 
             if (CryptGetHashParam(hHash, HP_HASHVAL, reinterpret_cast<BYTE *>(&buffer[0]), &cbHashSize, 0))
             {
-                for (std::vector<BYTE>::const_iterator iter = buffer.begin(); iter != buffer.end(); ++iter)
+                char *curr = hashed;
+                for (int i = 0; i < sizeof(buffer) / sizeof(buffer[0]); i++)
                 {
-                    oss << std::setfill('0') << std::setw(2) << std::hex << static_cast<const int>(*iter);
+                    auto x = buffer[i];
+                    *curr++ = hexdigit[(x >> 4) & 0xf];
+                    *curr++ = hexdigit[x & 0xf];
                 }
             }
         }
 
         CryptDestroyHash(hHash);
     }
-
-    return oss.str();
 }
 
-inline unsigned GetLengthLongestUnsigned()
+std::string GetHashText(HCRYPTPROV hProv, const void *data, const size_t dataSize)
 {
-    //std::ostringstream oss;
-    //oss << UINT_MAX;
-    //return oss.str().length();
-    return 10;
+    char hashed[33];
+    GetHashTextInternal(hProv, data, dataSize, hashed);
+    hashed[32] = '\0';
+    return hashed;
 }
 
-std::string Part2AdditionalHashing(const std::string &input, unsigned times)
+inline std::string Part2AdditionalHashing(const std::string &input, unsigned times)
 {
     std::string hashed = input;
 
@@ -77,7 +79,8 @@ std::string Foo(const std::string &input, unsigned suffix)
     HCRYPTPROV hProv = NULL;
     if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
     {
-        std::vector<char> buffer(input.length() + GetLengthLongestUnsigned() + 1);
+        // Longest Unsigned is 10 chars, add one for null terminator
+        std::vector<char> buffer(input.length() + 11);
 
         auto inputLength = sprintf_s(&buffer[0], buffer.capacity(), "%s", input.c_str());
         auto remainingBufferSize = buffer.capacity() - inputLength;
@@ -135,12 +138,12 @@ int main()
 
         if (mychar)
         {
-            bool lookForQuintuples = true;
-            for (int i = 1; i <= 1000 && lookForQuintuples; i++)
+            bool lookForQuintuple = true;
+            for (int i = 1; i <= 1000 && lookForQuintuple; i++)
             {
                 auto hash2 = GetHash(curr + i);
 
-                for (auto j = 0U; (j <= hash2.length() - 5) && lookForQuintuples; j++)
+                for (auto j = 0U; (j <= hash2.length() - 5) && lookForQuintuple; j++)
                 {
                     if (hash2[j] == mychar &&
                         hash2[j + 1] == mychar &&
@@ -148,7 +151,7 @@ int main()
                         hash2[j + 3] == mychar &&
                         hash2[j + 4] == mychar)
                     {
-                        lookForQuintuples = false;
+                        lookForQuintuple = false;
                         hashes++;
                         std::cout << "KEY #" << hashes << " is " << curr << std::endl;
                         if (hashes == 64)
