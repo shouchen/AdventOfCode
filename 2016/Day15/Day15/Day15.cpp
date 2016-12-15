066c90caa720c8a55c138bb26d9bb40c210a6fac
Stephen Houchen <shouchen@outlook.com>
2016-12-14 23:42:26 -0800

    Better solution to Day 15 (parts 1 & 2).

--- 2016/Day15/Day15/Day15.cpp
+++ 2016/Day15/Day15/Day15.cpp
@@ -4,66 +4,66 @@
 #include <string>
 #include <map>
 #include <cassert>
-
-int time;
+#include <climits>
 
 struct Disc
 {
-    Disc(int numPositions, int currPosition) : numPositions(numPositions), currPosition(currPosition) {}
-    int numPositions;
-    int currPosition;
+    Disc(unsigned long long numPositions, unsigned long long currPosition) : numPositions(numPositions), currPosition(currPosition) {}
+    unsigned long long numPositions;
+    unsigned long long currPosition;
 };
 
 std::map<int, Disc *> discs;
-
-void DoTick(int num = 1)
-{
-    time += num;
-    for (auto &d : discs)
-        d.second->currPosition = (d.second->currPosition + num) % d.second->numPositions;
-}
-
-bool PressButton(int time)
-{
-    DoTick(time);
-
-    DoTick();
-
-    if (discs[1]->currPosition != 0)
-        return false;
-
-    DoTick();
-
-    if (discs[2]->currPosition != 0)
-        return false;
-
-    return true;
-}
 
 void ReadFile(const std::string &filename)
 {
     std::ifstream f(filename);
     std::string disc, has, positions, at, time, it, is, position;
-    char pound, period, c1;
+    char pound, period;
     int d, p, startPos = 0;
 
     while (f >> disc >> pound >> d >> has >> p >> positions >> at >> time >> it >> is >> at >> position >> startPos >> period)
     {
-        assert(time == "time=0,");
         Disc *tempDisc = new Disc(p, startPos);
         discs[d] = tempDisc;
     }
+}
 
-//    Disc #1 has 5 positions; at time = 0, it is at position 4.
-
-
+inline bool IsSolution(unsigned long long time)
+{
+    for (auto p : discs)
+        if ((time + p.first + p.second->currPosition) % p.second->numPositions != 0)
+            return false;
+    return true;
 }
 
 int main()
 {
-    ReadFile("input-test.txt");
-    auto result = PressButton(5);
+    ReadFile("input.txt");
 
-    std::cout << result << std::endl;
+    auto part1 = 0ULL;
+    for (;;)
+    {
+        if (IsSolution(part1))
+            break;
+        part1++;
+    }
+
+    Disc *tempDisc = new Disc(11, 0);
+    discs[7] = tempDisc;
+
+    auto part2 = 0ULL;
+    for (;;)
+    {
+        if (IsSolution(part2))
+            break;
+        part2++;
+    }
+
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 122318);
+    assert(part2 == 3208583);
     return 0;
 }
