3c74a9628e4724275e0b36fb50a54b0b8bfb92de
Stephen Houchen <shouchen@outlook.com>
2016-12-16 06:57:06 -0800

    Day 16 optimizing checksum computation.

--- 2016/Day16/Day16/Day16.cpp
+++ 2016/Day16/Day16/Day16.cpp
@@ -3,70 +3,112 @@
 #include <algorithm>
 #include <cassert>
 
-std::string DoFirstPart(const std::string &a)
+unsigned ComputeGroupSize(const std::string &s)
 {
-    char *b = new char[a.length() + 1];
-    b[a.length()] = '\0';
+    // Round up to even length
+    auto temp = s.length();
+    if (temp & 0x1) temp++;
 
-    // Copy
-    for (auto i = 0U; i < a.length(); i++)
-        b[i] = a[i];
+    while ((temp & 0x1) == 0)
+        temp >>= 1;
 
-    // Invert
-    for (auto i = 0U, j = a.length() - 1; i < j; i++, j--)
-        std::swap(b[i], b[j]);
-    
-    // Toggle
-    for (auto i = 0U; i < a.length(); i++)
-        b[i] = static_cast<char>((b[i] == '1') ? '0' : '1');
-
-    auto retval = a + "0" + b;
-    delete[] b;
-    return retval;
+    return s.length() / temp;
 }
 
-std::string FillDisk(std::string initialState, unsigned diskLength)
+std::string FastFindChecksum(const std::string &s)
+{
+    auto groupSize = ComputeGroupSize(s);
+
+    std::string cs;
+    unsigned oneCount = 0;
+
+    for (auto i = 0U; i < s.length(); i++)
+    {
+        if (s[i] == '1') oneCount++;
+
+        // Emit checksum digit if time to (end of group or end of input)
+        if (((i + 1) % groupSize == 0) || (i + 1) == s.length())
+        {
+            cs.push_back((oneCount & 0x1) ? '0' : '1');
+            oneCount = 0;
+        }
+    }
+
+    return cs;
+}
+
+void ApplyModifiedDragonCurve(std::string &a)
+{
+    a.push_back('0');
+
+    for (int i = a.length() - 2; i >= 0; i--)
+        a.push_back((a[i] == '1') ? '0' : '1');
+}
+
+std::string FillDisk(std::string &initialState, unsigned diskLength)
 {
     while (initialState.length() < diskLength)
-    {
-        initialState = DoFirstPart(initialState);
-    }
+        ApplyModifiedDragonCurve(initialState);
 
     return initialState.substr(0, diskLength);
 }
 
-std::string FindChecksum(const std::string &s)
+std::string BruteForceFindChecksum(const std::string &s)
 {
     std::string cs;
 
     for (auto i = 0U; i < s.length() - 1; i += 2)
         cs.push_back((s[i] == s[i + 1]) ? '1' : '0');
 
-    return (cs.length() & 0x1) ? cs : FindChecksum(cs);
+    return (cs.length() & 0x1) ? cs : BruteForceFindChecksum(cs);
+}
+
+std::string BruteForceFindSolution(const std::string &s)
+{
+    auto fill = FillDisk(std::string("10011111011011001"), 272);
+    return BruteForceFindChecksum(fill);
+}
+
+std::string FastFindSolution(const std::string &s)
+{
+    auto fill = FillDisk(std::string("10011111011011001"), 272);
+    return FastFindChecksum(fill);
 }
 
 int main()
 {
     // Unit tests
-    assert(DoFirstPart("1") == "100");
-    assert(DoFirstPart("0") == "001");
-    assert(DoFirstPart("11111") == "11111000000");
-    assert(DoFirstPart("111100001010") == "1111000010100101011110000");
-    assert(FindChecksum("110010110100") == "100");
+    std::string test = "1";
+    ApplyModifiedDragonCurve(test);
+    assert(test == "100");
 
-    auto fill = FillDisk("10000", 20);
+    test = "0";
+    ApplyModifiedDragonCurve(test);
+    assert(test.compare("001") == 0);
+
+    test = "11111";
+    ApplyModifiedDragonCurve(test);
+    assert(test.compare("11111000000") == 0);
+
+    test = "111100001010";
+    ApplyModifiedDragonCurve(test);
+    assert(test.compare("1111000010100101011110000") == 0);
+
+    assert(FastFindChecksum("110010110100") == "100");
+
+    auto fill = FillDisk(std::string("10000"), 20);
     assert(fill == "10000011110010000111");
-    auto cs = FindChecksum(fill);
+    auto cs = FastFindChecksum(fill);
     assert(cs == "01100");
 
     // Part One
-    fill = FillDisk("10011111011011001", 272);
-    cs = FindChecksum(fill);
+    fill = FillDisk(std::string("10011111011011001"), 272);
+    cs = FastFindChecksum(fill);
     assert(cs == "10111110010110110");
 
     // Part Two
-    fill = FillDisk("10011111011011001", 35651584);
-    cs = FindChecksum(fill);
+    fill = FillDisk(std::string("10011111011011001"), 35651584);
+    cs = FastFindChecksum(fill);
     assert(cs == "01101100001100100");
 
     return 0;
