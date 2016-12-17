ae78538f1fed59ebee0c5efde6ead3c3f4e2d4a2
Stephen Houchen <shouchen@outlook.com>
2016-12-17 09:07:13 -0800

    Merged two solution finders.

--- 2016/Day17/Day17/Day17.cpp
+++ 2016/Day17/Day17/Day17.cpp
@@ -51,56 +51,14 @@ struct QueueNode
 {
     QueueNode(const std::string moves, unsigned x, unsigned y) : moves(moves), x(x), y(y) {}
     std::string moves;
-    unsigned x = 0, y = 0;
-
-    //bool operator<(const QueueNode &rhs) const
-    //{
-    //    auto leftAStar = moves.length() + (3 - x) + (3 - y);
-    //    auto rightAStar = rhs.moves.length() + (3 - rhs.x) + (3 - rhs.y);
-
-    //    if (leftAStar > rightAStar) return true;
-    //    if (leftAStar < rightAStar) return false;
-
-    //    if (moves > rhs.moves) return true;
-    //    if (moves < rhs.moves) return false;
-
-    //    if (x > rhs.x) return true;
-    //    if (x < rhs.x) return false;
-
-    //    if (y > rhs.y) return true;
-    //    if (y < rhs.y) return false;
-
-    //    return false;
-    //}
-
-    //bool operator>(const QueueNode &rhs) const
-    //{
-    //    auto leftAStar = moves.length() + (0 - x) + (0 - y);
-    //    auto rightAStar = rhs.moves.length() + (0 - rhs.x) + (0 - rhs.y);
-    //    
-    //    if (leftAStar < rightAStar) return true;
-    //    if (leftAStar > rightAStar) return false;
-    //    
-    //    if (moves < rhs.moves) return true;
-    //    if (moves > rhs.moves) return false;
-    //    
-    //    if (x < rhs.x) return true;
-    //    if (x > rhs.x) return false;
-    //    
-    //    if (y < rhs.y) return true;
-    //    if (y > rhs.y) return false;
-    //    
-    //    return false;
-    //}
+    unsigned x, y;
 };
 
 std::queue<QueueNode> queue;
-std::queue<QueueNode> queue2;
-
 std::string bestSolution;
-unsigned worstSolution = 0;
+unsigned worstSolution;
 
-bool InternalSolvePart1(const std::string &input)
+bool Solve(const std::string &input)
 {
     if (queue.empty())
         return true;
@@ -113,42 +71,6 @@ bool InternalSolvePart1(const std::string &input)
         if (node.moves.length() < bestSolution.length() || bestSolution.length() == 0)
             bestSolution = node.moves;
 
-        return false;
-    }
-
-    // If a better solution was already found, don't proceed.
-    if (bestSolution.length() && node.moves.length() >= bestSolution.length())
-        return false;
-
-    // Get hash of <input>+pathSoFar;
-    auto hash = HashInput(input + node.moves);
-
-    // Schedule that node to be processed if it works.
-    if (node.y > 0 && hash[0] > 'a')
-        queue.push(QueueNode(node.moves + "U", node.x, node.y - 1U ));
-
-    if (node.y < 3 && hash[1] > 'a')
-        queue.push(QueueNode(node.moves + "D", node.x, node.y + 1U ));
-
-    if (node.x > 0 && hash[2] > 'a')
-        queue.push(QueueNode(node.moves + "L", node.x - 1U, node.y ));
-
-    if (node.x < 3 && hash[3] > 'a')
-        queue.push(QueueNode(node.moves + "R", node.x + 1U, node.y ));
-
-    return false;
-}
-
-bool InternalSolvePart2(const std::string &input)
-{
-    if (queue2.empty())
-        return true;
-
-    QueueNode node = queue2.front();
-    queue2.pop();
-
-    if (node.x == 3 && node.y == 3)
-    {
         if (node.moves.length() > worstSolution)
             worstSolution = node.moves.length();
 
@@ -160,56 +82,33 @@ bool InternalSolvePart2(const std::string &input)
 
     // Schedule that node to be processed if it works.
     if (node.y > 0 && hash[0] > 'a')
-        queue2.push(QueueNode(node.moves + "U", node.x, node.y - 1U));
+        queue.push(QueueNode(node.moves + "U", node.x, node.y - 1U));
 
     if (node.y < 3 && hash[1] > 'a')
-        queue2.push(QueueNode(node.moves + "D", node.x, node.y + 1U));
+        queue.push(QueueNode(node.moves + "D", node.x, node.y + 1U));
 
     if (node.x > 0 && hash[2] > 'a')
-        queue2.push(QueueNode(node.moves + "L", node.x - 1U, node.y));
+        queue.push(QueueNode(node.moves + "L", node.x - 1U, node.y));
 
     if (node.x < 3 && hash[3] > 'a')
-        queue2.push(QueueNode(node.moves + "R", node.x + 1U, node.y));
+        queue.push(QueueNode(node.moves + "R", node.x + 1U, node.y));
 
     return false;
-}
-
-std::string SolvePart1(const std::string &input)
-{
-    queue.push(QueueNode("", 0, 0));
-    while (!InternalSolvePart1(input));
-
-    return bestSolution;
-}
-
-unsigned SolvePart2(const std::string &input)
-{
-    queue2.push(QueueNode("", 0, 0));
-    while (!InternalSolvePart2(input));
-
-    return worstSolution;
 }
 
 int main()
 {
     double startTime = clock();
 
-    std::string input("dmypynyp");
+    queue.push(QueueNode("", 0, 0));
+    while (!Solve("dmypynyp"));
 
-    auto part1 = SolvePart1(input);
-    auto part2 = SolvePart2(input);
+    std::cout << "Part One: " << bestSolution << std::endl;
+    std::cout << "Part Two: " << worstSolution << std::endl;
 
-    std::cout << "Part One: " << part1 << std::endl;
-    std::cout << "Part Two: " << part2 << std::endl;
-
-    assert(part1 == "RDRDUDLRDR");
-    assert(part2 == 386);
+    assert(bestSolution == "RDRDUDLRDR");
+    assert(worstSolution == 386);
 
     std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
-
     return 0;
 }
-
-// Merge the two solve methods
-
-// Other general clean-up
