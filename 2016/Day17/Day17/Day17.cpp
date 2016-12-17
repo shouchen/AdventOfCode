d52e72e9b76362d5c0bccd98a5bd70739fc0f4df
Stephen Houchen <shouchen@outlook.com>
2016-12-16 23:18:17 -0800

    Hacky solution for both parts of Day17.

--- 2016/Day17/Day17/Day17.cpp
+++ 2016/Day17/Day17/Day17.cpp
@@ -5,64 +5,46 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <functional>
+#include <ctime>
 #include <cassert>
 
-using Hash = BYTE[16];
-using HexHash = char[32];
-
-static void GetHashText(HCRYPTPROV hProv, const void *data, const DWORD dataSize, HexHash hashed)
+static std::string HashInput(const std::string &input)
 {
-    HCRYPTPROV hHash = NULL;
-
-    if (CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
-    {
-        if (CryptHashData(hHash, reinterpret_cast<const BYTE *>(data), dataSize, 0))
-        {
-            Hash buffer;
-            DWORD cbHashSize = sizeof(Hash);
-
-            if (CryptGetHashParam(hHash, HP_HASHVAL, buffer, &cbHashSize, 0))
-            {
-                static const char hexdigit[] = "0123456789abcdef";
-
-                char *curr = hashed;
-                for (auto i = 0U; i < cbHashSize; i++)
-                {
-                    auto x = buffer[i];
-                    *curr++ = hexdigit[(x >> 4) & 0xf];
-                    *curr++ = hexdigit[x & 0xf];
-                }
-            }
-        }
-
-        CryptDestroyHash(hHash);
-    }
-}
-
-static std::string HashInputWithPath(const std::string &input, const std::string &path)
-{
-    char hashed[33];
-    hashed[32] = '\0';
-
+    std::string retval;
     HCRYPTPROV hProv = NULL;
+
     if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
     {
-        std::string buffer = input + path;
+        HCRYPTPROV hHash = NULL;
 
-        //auto inputLength = sprintf_s(&inBuffer[0], inBuffer.capacity(), "%s", salt.c_str());
-        //auto remainingBufferSize = inBuffer.capacity() - inputLength;
-        //auto suffixStart = &inBuffer[0] + inputLength;
-        //auto numericSuffixSize = sprintf_s(suffixStart, remainingBufferSize, "%u", suffix);
+        if (CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+        {
+            if (CryptHashData(hHash, reinterpret_cast<const BYTE *>(input.c_str()), input.length(), 0))
+            {
+                BYTE buffer[16];
+                DWORD cbHashSize = sizeof(buffer);
 
-        GetHashText(hProv, buffer.c_str(), buffer.length(), hashed);
+                if (CryptGetHashParam(hHash, HP_HASHVAL, buffer, &cbHashSize, 0))
+                {
+                    static const char hexdigit[] = "0123456789abcdef";
 
-        //for (auto i = 0U; i < extraTimes; i++)
-        //    GetHashText(hProv, hashed, 32, hashed);
+                    for (auto i = 0U; i < cbHashSize; i++)
+                    {
+                        auto x = buffer[i];
+                        retval.push_back(hexdigit[(x >> 4) & 0xf]);
+                        retval.push_back(hexdigit[x & 0xf]);
+                    }
+                }
+            }
+
+            CryptDestroyHash(hHash);
+        }
 
         CryptReleaseContext(hProv, 0);
     }
 
-    return hashed;
+    return retval;
 }
 
 struct QueueNode
@@ -90,20 +72,41 @@ struct QueueNode
 
         return false;
     }
+
+    bool operator>(const QueueNode &rhs) const
+    {
+        auto leftAStar = moves.length() + (0 - x) + (0 - y);
+        auto rightAStar = rhs.moves.length() + (0 - rhs.x) + (0 - rhs.y);
+        
+        if (leftAStar < rightAStar) return true;
+        if (leftAStar > rightAStar) return false;
+        
+        if (moves < rhs.moves) return true;
+        if (moves > rhs.moves) return false;
+        
+        if (x < rhs.x) return true;
+        if (x > rhs.x) return false;
+        
+        if (y < rhs.y) return true;
+        if (y > rhs.y) return false;
+        
+        return false;
+    }
 };
 
 std::priority_queue<QueueNode> queue;
-std::string bestSolution;
+std::priority_queue<QueueNode, std::vector<QueueNode>, std::greater<QueueNode>> queue2;
 
-bool Solve(const std::string &input)
+std::string bestSolution;
+unsigned worstSolution = 0;
+
+bool InternalSolvePart1(const std::string &input)
 {
     if (queue.empty())
         return true;
 
     QueueNode node = queue.top();
     queue.pop();
-
-    //std::cout << "Working on (" << node.x << "," << node.y << ") " << node.moves << std::endl;
 
     if (node.x == 3 && node.y == 3)
     {
@@ -118,7 +121,7 @@ bool Solve(const std::string &input)
         return false;
 
     // Get hash of <input>+pathSoFar;
-    auto hash = HashInputWithPath(input, node.moves);
+    auto hash = HashInput(input + node.moves);
 
     // Schedule that node to be processed if it works.
     if (node.y > 0 && hash[0] > 'a')
@@ -136,16 +139,77 @@ bool Solve(const std::string &input)
     return false;
 }
 
+bool InternalSolvePart2(const std::string &input)
+{
+    if (queue2.empty())
+        return true;
+
+    QueueNode node = queue2.top();
+    queue2.pop();
+
+    if (node.x == 3 && node.y == 3)
+    {
+        if (node.moves.length() > worstSolution)
+            worstSolution = node.moves.length();
+
+        return false;
+    }
+
+    // Get hash of <input>+pathSoFar;
+    auto hash = HashInput(input + node.moves);
+
+    // Schedule that node to be processed if it works.
+    if (node.y > 0 && hash[0] > 'a')
+        queue2.push(QueueNode(node.moves + "U", node.x, node.y - 1U));
+
+    if (node.y < 3 && hash[1] > 'a')
+        queue2.push(QueueNode(node.moves + "D", node.x, node.y + 1U));
+
+    if (node.x > 0 && hash[2] > 'a')
+        queue2.push(QueueNode(node.moves + "L", node.x - 1U, node.y));
+
+    if (node.x < 3 && hash[3] > 'a')
+        queue2.push(QueueNode(node.moves + "R", node.x + 1U, node.y));
+
+    return false;
+}
+
+std::string SolvePart1(const std::string &input)
+{
+    queue.push(QueueNode("", 0, 0));
+    while (!InternalSolvePart1(input));
+
+    return bestSolution;
+}
+
+unsigned SolvePart2(const std::string &input)
+{
+    queue2.push(QueueNode("", 0, 0));
+    while (!InternalSolvePart2(input));
+
+    return worstSolution;
+}
+
 int main()
 {
+    double startTime = clock();
+
     std::string input("dmypynyp");
 
-    queue.push(QueueNode("", 0, 0));
-    while (!Solve(input));
+    auto part1 = SolvePart1(input);
+    auto part2 = SolvePart2(input);
 
-    std::cout << "Part One: " << bestSolution << std::endl;
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
 
-    assert(bestSolution == "RDRDUDLRDR");
+    assert(part1 == "RDRDUDLRDR");
+    assert(part2 == 386);
+
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
 
     return 0;
 }
+
+// Merge the two solve methods
+
+// Other general clean-up
