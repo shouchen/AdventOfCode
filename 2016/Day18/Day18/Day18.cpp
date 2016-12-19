74d4db0fc9b897fd221274c43d99e9063b93dbd3
Stephen Houchen <shouchen@outlook.com>
2016-12-18 22:30:22 -0800

    Code clean-up for Day 18.

--- 2016/Day18/Day18/Day18.cpp
+++ 2016/Day18/Day18/Day18.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <map>
+#include <algorithm>
+#include <ctime>
 #include <cassert>
 
-std::string FindNext(const std::string &input)
+inline void FindNext(std::string &input)
 {
     std::string next;
 
@@ -16,27 +19,46 @@ std::string FindNext(const std::string &input)
         next.push_back((left == right) ? '.' : '^');
     }
 
-    return next;
+    input = next;
 }
 
-unsigned count;
-
-int main()
+bool IsSafe(char c)
 {
-    std::string input = ".^^^^^.^^^..^^^^^...^.^..^^^.^^....^.^...^^^...^^^^..^...^...^^.^.^.......^..^^...^.^.^^..^^^^^...^.";
+    return c == '.';
+}
+
+void DoProblem(std::string input, unsigned &afterForty, unsigned &afterFourHundredThousand)
+{
+    auto count = 0U;
 
     for (auto i = 0U; i < 400000; i++)
     {
-        for (auto c : input)
-            if (c == '.')
-                count++;
+        count += std::count_if(input.begin(), input.end(), IsSafe);
 
-        input = FindNext(input);
+        if (i == 39)
+            afterForty = count;
+
+        FindNext(input);
     }
 
-    std::cout << count << std::endl;
+    afterFourHundredThousand = count;
+}
 
-    //assert(count == 1956);
-    //assert(count == 19995121);
+int main()
+{
+    double startTime = clock();
+
+    const std::string input = ".^^^^^.^^^..^^^^^...^.^..^^^.^^....^.^...^^^...^^^^..^...^...^^.^.^.......^..^^...^.^.^^..^^^^^...^.";
+    auto afterForty = 0U, afterFourHundredThousand = 0U;
+
+    DoProblem(input, afterForty, afterFourHundredThousand);
+
+    std::cout << "Part One: " << afterForty << std::endl;
+    std::cout << "Part Two: " << afterFourHundredThousand << std::endl;
+
+    assert(afterForty == 1956);
+    assert(afterFourHundredThousand == 19995121);
+
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
     return 0;
 }
