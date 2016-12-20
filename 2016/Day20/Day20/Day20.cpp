eafb4777e417bc210f72ce9e7786268cc767329b
Stephen Houchen <shouchen@outlook.com>
2016-12-19 23:49:46 -0800

    Day 20 clean-up

--- 2016/Day20/Day20/Day20.cpp
+++ 2016/Day20/Day20/Day20.cpp
@@ -9,90 +9,9 @@
 
 std::vector<std::pair<unsigned, unsigned>> ranges;
 
-unsigned Solve1()
+void ReadInput(const std::string &filename)
 {
-    unsigned curr = 0;
-
-    bool done = true;
-    do
-    {
-        done = true;
-        for (auto r : ranges)
-        {
-            // If inside a range, bump candidate to one past end of range.
-            if (curr >= r.first && curr <= r.second)
-            {
-                curr = r.second + 1;
-                done = false;
-            }
-        }
-    } while (!done);
-
-    return curr;
-}
-
-bool WayToSort(std::pair<unsigned, unsigned> &p1, std::pair<unsigned, unsigned> &p2)
-{
-    return p1.first < p2.first;
-}
-
-bool CoalesceRanges()
-{
-    std::sort(ranges.begin(), ranges.end(), WayToSort);
-
-    for (auto a = ranges.begin(); a != ranges.end(); a++)
-    {
-        for (auto b = a + 1; b != ranges.end(); b++)
-        {
-            // Case 1
-            if (a->first == b->first && b->second <= a->second)
-            {
-                ranges.erase(b);
-                return true;
-            }
-
-            // Case 2
-            if (a->first == b->first && b->second > a->second)
-            {
-                ranges.erase(a);
-                return true;
-            }
-
-            // Case 3
-            if (a->first <= b->first && a->second >= b->second)
-            {
-                ranges.erase(b);
-                return true;
-            }
-
-            // Case 4
-            if (a->first <= b->first && a->second >= b->first && a->second <= b->second)
-            {
-                a->second = b->second;
-                ranges.erase(b);
-                return true;
-            }
-
-            // Case 5
-            if (b->first == a->second + 1)
-            {
-                a->second = b->second;
-                ranges.erase(b);
-                return true;
-            }
-
-            // Case 6
-            if (b->first > a->second)
-                break;
-        }
-    }
-
-    return false;
-}
-
-void Part1Read()
-{
-    std::ifstream f("input.txt");
+    std::ifstream f(filename);
     unsigned start, end;
     char dash;
 
@@ -100,13 +19,47 @@ void Part1Read()
         ranges.push_back(std::pair<unsigned, unsigned>(start, end));
 }
 
-unsigned Solve2()
+bool WayToSort(std::pair<unsigned, unsigned> &p1, std::pair<unsigned, unsigned> &p2)
+{
+    return p1.first < p2.first;
+}
+
+// Returns true if something was coalesced, false if nothing more can be done.
+bool CoalesceOneRange()
+{
+    for (auto a = ranges.begin(); a != ranges.end(); a++)
+    {
+        // Try to merge qualifying B's with A.
+        auto lowestOverlappingBStart = (a->second == UINT_MAX) ? a->second : (a->second + 1);
+        for (auto b = a + 1; b != ranges.end() && b->first <= lowestOverlappingBStart; b++)
+        {
+            // In here, we know and B overlap, and that B's start is not before A's. We can
+            // remove B and merge it into A. This can only change A's end range, so the sort
+            // order won't be affected.
+
+            // Case 1: B overlaps A, but extends past it. Extend A to the new end and delete B.
+            // Case 2: B is wholly contained within A. Just delete B as redundant.
+
+            a->second = std::max(a->second, b->second);
+            ranges.erase(b);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+unsigned SolvePart1()
+{
+    return ranges[0].second + 1;
+}
+
+unsigned SolvePart2()
 {
     unsigned sizeOfRanges = 0;
     for (auto r : ranges)
     {
         auto thisSize = r.second - r.first + 1;
-        std::cout << r.first << "-" << r.second << " (" << thisSize << ")" << std::endl;
         sizeOfRanges += thisSize;
     }
 
@@ -115,16 +68,22 @@ unsigned Solve2()
 
 int main()
 {
-    Part1Read();
+    double startTime = clock();
 
-    while (CoalesceRanges());
+    ReadInput("input.txt");
 
-    auto part1 = Solve1();
-    auto part2 = Solve2();
+    std::sort(ranges.begin(), ranges.end(), WayToSort);
+    while (CoalesceOneRange());
 
+    auto part1 = SolvePart1();
+    std::cout << "Part One: " << part1 << std::endl;
     assert(part1 == 32259706);
+
+    auto part2 = SolvePart2();
+    std::cout << "Part Two: " << part2 << std::endl;
     assert(part2 == 113);
 
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
     return 0;
 }
 
