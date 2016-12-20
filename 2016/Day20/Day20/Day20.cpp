26ff5a43d6e0d00edbdbf44a7b72d675056ecc45
Stephen Houchen <shouchen@outlook.com>
2016-12-20 11:06:56 -0800

    Syntactical sugar for Day 20 solution.

--- 2016/Day20/Day20/Day20.cpp
+++ 2016/Day20/Day20/Day20.cpp
@@ -6,10 +6,10 @@
 #include <ctime>
 #include <cassert>
 
-// Summary:
-// Read the input then coalesce the blacklist ranges into a simpler representation. Part1 will
-// be the value after the lowest range ends. For part2, add up the size of all the ranges and
-// subtract this from the total address space.
+// Summary of Solution:
+// Read the input then coalesce blacklist ranges into a simpler representation.
+// Part1 will be the first value after the lowest range ends. For part2, add up
+// the size of all the ranges and subtract this from the total address space.
 
 std::vector<std::pair<unsigned, unsigned>> ranges;
 
@@ -26,25 +26,24 @@ void ReadInput(const std::string &filename)
 void CoalesceRanges()
 {
     // The logic in this function needs the input to be sorted by start value.
-    std::sort(
-        ranges.begin(), ranges.end(),
-        [](const std::pair<unsigned, unsigned> &p1, const std::pair<unsigned, unsigned> &p2) -> bool
+    std::sort(ranges.begin(), ranges.end(),
+              [](const auto &p1, const auto &p2) -> bool
     {
         return p1.first < p2.first;
     });
 
     for (auto a = ranges.begin(); a != ranges.end(); a++)
     {
-        // Look at next range to see if it needs to be merged with A. It's a loop because of the erase().
+        // See if next range needs to merge with A (loops because of the erase).
         for (auto b = a + 1; b != ranges.end(); b = a + 1)
         {
             // If this b doesn't qualify for merging, then no other B will.
-            auto lowestOverlappingBStart = (a->second == UINT_MAX) ? a->second : (a->second + 1);
-            if (b->first > lowestOverlappingBStart)
+            if (a->second < UINT_MAX && b->first > a->second + 1)
                 break;
 
-            // Here, we know that A and B overlap, and that B's start is not before A's. We can remove B
-            // and merge it into A. This can only change A's end range, so the sort order won't be affected.
+            // Here, we know A and B overlap, and B's start is not before A's.
+            // We can just remove B and adjust A. Since this can only change A's
+            // end range, the existing sort order won't be affected.
             a->second = std::max(a->second, b->second);
             ranges.erase(b);
         }
@@ -75,9 +74,9 @@ int main()
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
 
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
+
     assert(part1 == 32259706);
     assert(part2 == 113);
-
-    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
     return 0;
 }
