1c2f0a6b41fdd023f9fcf398cda7270f5e3707b4
Stephen Houchen <shouchen@outlook.com>
2016-12-21 08:14:14 -0800

    Day 21 code clean-up

--- 2016/Day21/Day21/Day21.cpp
+++ 2016/Day21/Day21/Day21.cpp
@@ -1,274 +1,172 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <ctime>
 #include <cassert>
 
-std::string input = "abcdefgh";
-
-std::string garbled = "fbgdceah";
-
-void SwapPositions(unsigned p1, unsigned p2)
+class Puzzle
 {
-    std::swap(input[p1], input[p2]);
-}
-
-void SwapLetters(char letter1, char letter2)
-{
-    std::vector<unsigned> p1, p2;
-
-    for (int i = 0; i < input.length(); i++)
-        if (input[i] == letter1)
-            p1.push_back(i);
-        else if (input[i] == letter2)
-            p2.push_back(i);
-
-    for (int i = 0; i < p1.size(); i++)
-        input[p1[i]] = letter2;
-
-    for (int i = 0; i < p2.size(); i++)
-        input[p2[i]] = letter1;
-}
-
-void Reverse(unsigned p1, unsigned p2)
-{
-    while (p1 < p2)
-        std::swap(input[p1++], input[p2--]);
-}
-
-void RotateLeft(unsigned n)
-{
-    while (n--)
+public:
+    Puzzle(const std::string &filename)
     {
-        char temp = input[0];
-        for (int i = 0; i < input.length() - 1; i++)
-            input[i] = input[i + 1];
-        input[input.length() - 1] = temp;
+        std::ifstream f(filename);
+        for (std::string line; getline(f, line);)
+            m_steps.push_back(line);
     }
-}
 
-void RotateRight(unsigned n)
-{
-    RotateLeft(input.length() - n);
-}
-
-void Move(unsigned p1, unsigned p2)
-{
-    char temp = input[p1];
-    input = input.substr(0, p1) + input.substr(p1 + 1);
-
-    input.insert(p2, 1, temp);
-}
-
-void RotateBasedOn(char letter1)
-{
-    unsigned pos = input.find(letter1);
-    RotateRight(1 + pos);
-
-    if (pos >= 4)
-        RotateRight(1);
-}
-
-void ReadInputPart1(const std::string &filename)
-{
-    std::ifstream f(filename);
-    std::string word, with, position, positions, letter, through, direction, step, to, on, of;
-    unsigned p1, p2, n;
-    char letter1, letter2;
-
-    while (f >> word)
+    void DoSteps(std::string &input)
     {
-        if (word == "swap")
-        {
-            f >> word;
-
-            if (word == "position")
-            {
-                f >> p1 >> with >> position >> p2;
-                SwapPositions(p1, p2);
-            }
-            else if (word == "letter")
-            {
-                f >> letter1 >> with >> letter >> letter2;
-                SwapLetters(letter1, letter2);
-            }
-            else
-                assert(false);
-        }
-        else if (word == "reverse")
-        {
-            f >> positions >> p1 >> through >> p2;
-            Reverse(p1, p2);
-        }
-        else if (word == "rotate")
-        {
-            f >> word;
-            if (word == "left")
-            {
-                f >> n >> step;
-                RotateLeft(n);
-            }
-            else if (word == "right")
-            {
-                f >> n >> step;
-                RotateRight(n);
-            }
-            else if (word == "based")
-            {
-                f >> on >> position >> of >> letter >> letter1;
-                RotateBasedOn(letter1);
-            }
-            else
-                assert(false);
-        }
-        else if (word == "move")
-        {
-            f >> position >> p1 >> to >> position >> p2;
-            Move(p1, p2);
-        }
-        else
-            assert(false);
+        for (auto step : m_steps)
+            PerformStep(step, input, false);
     }
-}
 
-
-void RotateLeftFoo(std::string &foo,unsigned n)
-{
-    while (n--)
+    void UndoSteps(std::string &input)
     {
-        char temp = foo[0];
-        for (int i = 0; i < foo.length() - 1; i++)
-            foo[i] = foo[i + 1];
-        foo[foo.length() - 1] = temp;
+        for (auto curr = m_steps.rbegin(); curr != m_steps.rend(); curr++)
+            PerformStep(*curr, input, true);
     }
-}
 
-void RotateRightFoo(std::string &foo, unsigned n)
-{
-    RotateLeftFoo(foo, foo.length() - n);
-}
-
-std::string RotateBasedOnLetterFoo(std::string foo, char letter1)
-{
-    unsigned pos = foo.find(letter1);
-    RotateRightFoo(foo,1 + pos);
-
-    if (pos >= 4)
-        RotateRightFoo(foo,1);
-
-    return foo;
-}
-
-void ReadInputPart2(const std::string &filename)
-{
-    std::ifstream f(filename);
-    std::string word, with, position, positions, letter, through, direction, step, to, on, of;
-    unsigned p1, p2, n;
-    char letter1, letter2;
-
-    while (f >> word)
+private:
+    void PerformStep(std::string step, std::string &input, bool undo)
     {
-        if (word == "swap")
-        {
-            f >> word;
+        std::stringstream ss(step);
+        std::string word, with, position, positions, letter, through, direction, steps, to, on, of;
+        unsigned p1, p2, n;
+        char letter1, letter2;
 
-            if (word == "position")
-            {
-                f >> p1 >> with >> position >> p2;
-                SwapPositions(p1, p2);
-            }
-            else if (word == "letter")
-            {
-                f >> letter1 >> with >> letter >> letter2;
-                SwapLetters(letter1, letter2);
-            }
-            else
-                assert(false);
-        }
-        else if (word == "reverse")
+        if (ss >> word)
         {
-            f >> positions >> p1 >> through >> p2;
-            Reverse(p1, p2);
-        }
-        else if (word == "rotate")
-        {
-            f >> word;
-            if (word == "left")
+            if (word == "swap")
             {
-                f >> n >> step;
-                RotateRight(n);
-            }
-            else if (word == "right")
-            {
-                f >> n >> step;
-                RotateLeft(n);
-            }
-            else if (word == "based")
-            {
-                f >> on >> position >> of >> letter >> letter1;
-                auto temp = input;
-                for (;;)
+                ss >> word;
+                if (word == "position")
                 {
-                    if (RotateBasedOnLetterFoo(temp, letter1) == input)
-                    {
-                        input = temp;
-                        break;
-                    }
-                    
-                    RotateLeftFoo(temp, 1);
+                    ss >> p1 >> with >> position >> p2;
+                    SwapPositions(input, p1, p2);
+                }
+                else if (word == "letter")
+                {
+                    ss >> letter1 >> with >> letter >> letter2;
+                    SwapLetters(input, letter1, letter2);
                 }
             }
-            else
-                assert(false);
+            else if (word == "reverse")
+            {
+                ss >> positions >> p1 >> through >> p2;
+                Reverse(input, p1, p2);
+            }
+            else if (word == "rotate")
+            {
+                ss >> word;
+                if (word == "left")
+                {
+                    ss >> n >> steps;
+                    undo ? RotateRight(input, n) : RotateLeft(input, n);
+                }
+                else if (word == "right")
+                {
+                    ss >> n >> steps;
+                    undo ? RotateLeft(input, n) : RotateRight(input, n);
+                }
+                else if (word == "based")
+                {
+                    ss >> on >> position >> of >> letter >> letter1;
+                    undo ? UnrotateBasedOnLetter(input, letter1) : RotateBasedOnLetter(input, letter1);
+                }
+            }
+            else if (word == "move")
+            {
+                ss >> position >> p1 >> to >> position >> p2;
+                undo ? Move(input, p2, p1) : Move(input, p1, p2);
+            }
         }
-        else if (word == "move")
-        {
-            f >> position >> p1 >> to >> position >> p2;
-            Move(p2, p1);
-        }
-        else
-            assert(false);
     }
-}
+
+    void SwapPositions(std::string &s, unsigned p1, unsigned p2)
+    {
+        std::swap(s[p1], s[p2]);
+    }
+
+    void SwapLetters(std::string &s, char letter1, char letter2)
+    {
+        for (auto &c : s)
+            if (c == letter1)
+                c = letter2;
+            else if (c == letter2)
+                c = letter1;
+    }
+
+    void Reverse(std::string &s, unsigned p1, unsigned p2)
+    {
+        std::reverse(s.begin() + p1, s.begin() + p2 + 1);
+    }
+
+    void RotateLeft(std::string &s, unsigned n)
+    {
+        std::rotate(s.begin(), s.begin() + n, s.end());
+    }
+
+    void RotateRight(std::string &s, unsigned n)
+    {
+        std::rotate(s.begin(), s.begin() + s.length() - n, s.end());
+    }
+
+    void Move(std::string &s, unsigned p1, unsigned p2)
+    {
+        auto temp = s[p1];
+        s = s.substr(0, p1) + s.substr(p1 + 1);
+        s.insert(p2, 1, temp);
+    }
+
+    void RotateBasedOnLetter(std::string &s, char letter1)
+    {
+        unsigned pos = s.find(letter1);
+        auto times = (pos + ((pos >= 4) ? 2 : 1)) % s.length();
+        RotateRight(s, times);
+    }
+
+    void UnrotateBasedOnLetter(std::string &s, char letter)
+    {
+        auto rotatedS = s;
+        for (;;)
+        {
+            auto rotatedBasedOnLetter = rotatedS;
+            RotateBasedOnLetter(rotatedBasedOnLetter, letter);
+            if (rotatedBasedOnLetter == s)
+            {
+                s = rotatedS;
+                break;
+            }
+
+            RotateLeft(rotatedS, 1);
+        }
+    }
+
+    std::vector<std::string> m_steps;
+};
 
 int main()
 {
     double startTime = clock();
 
-    //std::ifstream f("input.txt");
-    //std::string s;
-    //std::vector<std::string> line;
-    //while (f)
-    //{
-    //    char buffer[500];
-    //    f.getline(buffer, 500);
-    //    line.push_back(buffer);
-    //}
+    Puzzle puzzle("input.txt");
 
-    //std::ofstream f2("input2.txt");
-    //for (int i = line.size() - 1; i >= 0; i--)
-    //    f2 << line[i] << std::endl;;
+    std::string part1 = "abcdefgh";
+    puzzle.DoSteps(part1);
 
-    //ReadInputPart1("input.txt");
+    std::string part2 = "fbgdceah";
+    puzzle.UndoSteps(part2);
 
-    input = garbled;
-    ReadInputPart2("input2.txt");
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
 
-    //unsigned part2 = 0;
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
 
-    //auto part1 = Scramble("abcdefgh");
-
-    //std::cout << "Part One: " << part1 << std::endl;
-    //std::cout << "Part Two: " << part2 << std::endl;
-
-    //std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
-
-    //assert(part1 == 32259706);
-    //assert(part2 == 113);
-    return 0; // "gcedfahb" part1
+    assert(part1 == "gcedfahb");
+    assert(part2 == "hegbdcfa");
+    return 0;
 }
-// "hegbdcfa" part2
-
