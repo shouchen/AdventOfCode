0aa4cf969009dd1a44c84e282202b872be49d7e4
Stephen Houchen <shouchen@outlook.com>
2016-12-22 15:58:15 -0800

    Day 22 clean-up

--- 2016/Day22/Day22/Day22.cpp
+++ 2016/Day22/Day22/Day22.cpp
@@ -14,18 +14,21 @@ struct Node
     unsigned used, avail;
 };
 
+struct Coords
+{
+    int x, y;
+};
+
 #define X_SIZE 39
 #define Y_SIZE 25
-//#define X_SIZE 3
-//#define Y_SIZE 3
 Node grid[Y_SIZE][X_SIZE];
 auto goalX = X_SIZE - 1U, goalY = 0U;
 
 void Dump()
 {
-    for (int i = 0; i < Y_SIZE; i++)
+    for (auto i = 0U; i < Y_SIZE; i++)
     {
-        for (int j = 0; j < X_SIZE; j++)
+        for (auto j = 0U; j < X_SIZE; j++)
         {
             std::cout << std::setw(3) << grid[i][j].used << "/" << std::setw(3) << (grid[i][j].used + grid[i][j].avail);
             std::cout << ((i == goalY && j == goalX) ? "*" : " ");
@@ -59,160 +62,186 @@ void ReadInput(const std::string &filename)
         auto y = atoi(&path[path.find("-y") + 2]);
         grid[y][x].used = used;
         grid[y][x].avail = avail;
-
     }
+}
 
-#if 0
+unsigned DoPart1()
+{
     unsigned count = 0;
-    for (auto a = nodes.begin(); a != nodes.end(); a++)
-    {
-        if (a->used == 0) continue;
-
-        for (auto b = nodes.begin(); b != nodes.end(); b++)
+    for (auto y = 0; y < Y_SIZE; y++)
+        for (auto x = 0; x < X_SIZE; x++)
         {
-            if (a == b) 
-                continue;
-            if (a->used > b->avail) 
-                continue;
-            count++;
+            if (grid[y][x].used == 0) continue;
 
+            for (auto yy = 0; yy < Y_SIZE; yy++)
+                for (auto xx = 0; xx < X_SIZE; xx++)
+                {
+                    if (y == yy && x == xx)
+                        continue;
+                    if (grid[y][x].used > grid[yy][xx].avail)
+                        continue;
+                    count++;
+                }
         }
-    }
 
-    std::cout << count << std::endl; // 941 for part1
-#endif
+    return count;
 }
 
 unsigned numMoves;
+
+std::string *ExtendPath(std::string path[Y_SIZE][X_SIZE], const Coords &from, const Coords &to, unsigned generation, char c)
+{
+    if ((to.y < 0) || (to.y >= Y_SIZE) || (to.x < 0) || (to.x >= X_SIZE))
+        return nullptr;
+
+    if (!(path[to.y][to.x].length() > generation + 1 || path[to.y][to.x].length() == 0) ||
+        !(grid[to.y][to.x].avail + grid[to.y][to.x].used >= grid[from.y][from.x].used) ||
+        !(to.y != goalY || to.x != goalX))
+    {
+        return nullptr;
+    }
+
+    path[to.y][to.x] = path[from.y][from.x] + c;
+    
+    if (grid[to.y][to.x].used != 0)
+        return nullptr;
+
+    return &path[to.y][to.x];
+}
+
+/*
+inline void TryUpdateDistance(std::queue<Coords> &q, const Coords &coords, unsigned digit, unsigned newDistance)
+{
+    auto &oldDistance = distFromDigit[coords.row][coords.col][digit];
+
+    if (maze[coords.row][coords.col] != '#' && newDistance < oldDistance)
+    {
+        oldDistance = newDistance;
+        q.push(coords);
+    }
+}
+*/
+
 // Finds shortest path through the grid (xFrom, yFrom) to an empty disk but avoiding (goalX, goalY).
-// Returns the move string to it can be replayed.
+// Returns the move string so it can be replayed.
 std::string FindPath(unsigned xFrom, unsigned yFrom, unsigned spaceNeeded)
 {
     std::string path[Y_SIZE][X_SIZE];
     unsigned generation = 1;
+    std::string retval;
 
     path[yFrom][xFrom] = " ";
 
     for (;;)
     {
-        for (int y = 0; y < Y_SIZE; y++)
-            for (int x = 0; x < X_SIZE; x++)
+        for (auto y = 0; y < Y_SIZE; y++)
+        {
+            for (auto x = 0; x < X_SIZE; x++)
             {
                 if (x == goalX && y == goalY) continue;
 
                 if (path[y][x].length() == generation)
                 {
-                    if (y > 0 &&
-                        (path[y - 1][x].length() > generation + 1 || path[y - 1][x].length() == 0) &&
-                        (grid[y - 1][x].avail + grid[y - 1][x].used >= grid[y][x].used) &&
-                        (y - 1 != goalY || x != goalX))
+                    std::string *s;
+
+                    Coords from{ x, y };
+
+                    if ((s = ExtendPath(path, from, Coords{ x, y - 1 }, generation, 'U')) ||
+                        (s = ExtendPath(path, from, Coords{ x, y + 1 }, generation, 'D')) ||
+                        (s = ExtendPath(path, from, Coords{ x - 1, y }, generation, 'L')) ||
+                        (s = ExtendPath(path, from, Coords{ x + 1, y }, generation, 'R')))
                     {
-                        path[y - 1][x] = path[y][x] + 'U';
-                        if (grid[y - 1][x].used == 0)
-                            return path[y - 1][x];
-                    }
-                    if (y < Y_SIZE - 1 &&
-                        (path[y + 1][x].length() > generation + 1 || path[y + 1][x].length() == 0) &&
-                        (grid[y + 1][x].avail + grid[y + 1][x].used >= grid[y][x].used) &&
-                        (y + 1 != goalY || x != goalX))
-                    {
-                        path[y + 1][x] = path[y][x] + 'D';
-                        if (grid[y + 1][x].used == 0)
-                            return path[y + 1][x];
-                    }
-                    if (x > 0 &&
-                        (path[y][x - 1].length() > generation + 1 || path[y][x - 1].length() == 0) &&
-                        (grid[y][x - 1].avail + grid[y][x - 1].used >= grid[y][x].used) &&
-                        (y != goalY || x - 1 != goalX))
-                    {
-                        path[y][x - 1] = path[y][x] + 'L';
-                        if (grid[y][x - 1].used == 0)
-                            return path[y][x - 1];
-                    }
-                    if (x < X_SIZE - 1 &&
-                        (path[y][x + 1].length() > generation + 1 || path[y][x + 1].length() == 0) &&
-                        (grid[y][x + 1].avail + grid[y][x + 1].used >= grid[y][x].used) &&
-                        (y != goalY || x + 1 != goalX))
-                    {
-                        path[y][x + 1] = path[y][x] + 'R';
-                        if (grid[y][x + 1].used == 0)
-                            return path[y][x + 1];
+                        return *s;
                     }
                 }
             }
+        }
 
         generation++;
     }
 
-    return "";
+    return retval;
 }
 
-void MoveGoalLeft()
+/*
+void ComputeDistancesFromTargets()
 {
-    auto numToMove = grid[goalY][goalX].used;
-    if (grid[goalY][goalX - 1].avail < numToMove)
+    for (auto digit = 0U; digit < digitLocation.size(); digit++)
     {
-        auto path = FindPath(goalX - 1, goalY, grid[goalY][goalX].used);
-        assert(path.length() >= 1);
-        std::cout << "Doing" << path << " (" << path.length() - 1 << " steps)" << std::endl;
+        std::queue<Coords> q;
+        TryUpdateDistance(q, digitLocation[digit], digit, 0);
 
-        // Do the last step and cut it out of the string
-        while (path.length() > 1)
+        while (!q.empty())
         {
-            unsigned tempX = goalX - 1, tempY = goalY;
-            auto c = path.begin() + 1;
-            for (; c + 1 != path.end(); c++)
-            {
-                if (*c == 'U')
-                    tempY--;
-                if (*c == 'D')
-                    tempY++;
-                if (*c == 'L')
-                    tempX--;
-                if (*c == 'R')
-                    tempX++;
-            }
+            auto row = q.front().row, col = q.front().col;
+            auto newDistance = distFromDigit[row][col][digit] + 1;
 
-            // Do move from (tempX, tempY) -> (destX, destY)
-            unsigned destX = tempX, destY = tempY;
-            if (*c == 'L') destX = tempX - 1; else if (*c == 'R') destX = tempX + 1;
-            if (*c == 'U') destY = tempY - 1; else if (*c == 'D') destY = tempY + 1;
+            TryUpdateDistance(q, Coords{ row - 1, col }, digit, newDistance);
+            TryUpdateDistance(q, Coords{ row + 1, col }, digit, newDistance);
+            TryUpdateDistance(q, Coords{ row, col - 1 }, digit, newDistance);
+            TryUpdateDistance(q, Coords{ row, col + 1 }, digit, newDistance);
 
-            numToMove = grid[tempY][tempX].used;
-            assert(grid[destY][destX].avail >= numToMove);
-
-            grid[tempY][tempX].used = 0;
-            grid[tempY][tempX].avail += numToMove;
-            grid[destY][destX].used += numToMove;
-            grid[destY][destX].avail -= numToMove;
-            numMoves++;
-
-            path.pop_back();
-
-            //Dump();
+            q.pop();
         }
     }
+}
+*/
 
-    std::cout << "MoveGoalLeft(" << goalX << ", " << goalY << ") " << numMoves << std::endl;
+void MakeMove(unsigned fromX, unsigned fromY, unsigned toX, unsigned toY)
+{
+    auto numToMove = grid[fromY][fromX].used;
 
-    grid[goalY][goalX].used = 0;
-    grid[goalY][goalX].avail += numToMove;
-    goalX--;
-    grid[goalY][goalX].used += numToMove;
-    grid[goalY][goalX].avail -= numToMove;
+    grid[fromY][fromX].used = 0;
+    grid[fromY][fromX].avail += numToMove;
+    grid[toY][toX].used += numToMove;
+    grid[toY][toX].avail -= numToMove;
+
     numMoves++;
-
-    //Dump();
 }
 
-void Part2()
+void ApplyMoves(std::string path, unsigned numToMove)
 {
-    Dump();
+    // Do the last step and cut it out of the string
+    while (path.length() > 1)
+    {
+        unsigned tempX = goalX - 1, tempY = goalY;
+        auto c = path.begin() + 1;
+        for (; c + 1 != path.end(); c++)
+        {
+            if (*c == 'U') tempY--;
+            if (*c == 'D') tempY++;
+            if (*c == 'L') tempX--;
+            if (*c == 'R') tempX++;
+        }
 
+        // Do move from (tempX, tempY) -> (destX, destY)
+        unsigned destX = tempX, destY = tempY;
+        if (*c == 'L') destX = tempX - 1; else if (*c == 'R') destX = tempX + 1;
+        if (*c == 'U') destY = tempY - 1; else if (*c == 'D') destY = tempY + 1;
+
+        MakeMove(tempX, tempY, destX, destY);
+        path.pop_back();
+    }
+}
+
+unsigned DoPart2()
+{
     while (goalX != 0 || goalY != 0)
-        MoveGoalLeft();
+    {
+        auto numToMove = grid[goalY][goalX].used;
+        if (grid[goalY][goalX - 1].avail < numToMove)
+        {
+            auto path = FindPath(goalX - 1, goalY, grid[goalY][goalX].used);
+            assert(path.length() >= 1);
 
-    std::cout << "answer = " << numMoves << std::endl;
+            ApplyMoves(path, numToMove);
+        }
+
+        MakeMove(goalX, goalY, goalX - 1, goalY);
+        goalX--;
+    }
+
+    return numMoves;
 }
 
 int main()
@@ -220,19 +249,16 @@ int main()
     double startTime = clock();
 
     ReadInput("input.txt");
-    Part2();
 
-    //auto part1 = 0U, part2 = 0U;
-    //Solve(part1, part2);
+    auto part1 = DoPart1();
+    auto part2 = DoPart2();
 
-    //std::cout << "Part One: " << part1 << std::endl;
-    //std::cout << "Part Two: " << part2 << std::endl;
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
 
-    //std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
+    std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
 
-    //assert(part1 == 32259706);
-    //assert(part2 == 113);
+    assert(part1 == 941);
+    assert(part2 == 249);
     return 0;
 }
-
-// 249 was right for part 2!
