6922a9d9af236fc0b1c4a686e583ead5d82f3c88
Stephen Houchen <shouchen@outlook.com>
2016-12-24 00:39:46 -0800

    Correct solutions for Day 24, ugly code.

--- 2016/Day24/Day24/Day24.cpp
+++ 2016/Day24/Day24/Day24.cpp
@@ -1,198 +1,139 @@
 #include "stdafx.h"
 #include <fstream>
 #include <iostream>
-#include <iomanip>
-#include <sstream>
 #include <vector>
-#include <set>
-#include <queue>
-#include <map>
 #include <string>
-#include <memory>
 #include <algorithm>
-#include <climits>
 #include <ctime>
 #include <cassert>
 
-//using Program = std::vector<std::string>;
-//
-//class Computer
-//{
-//public:
-//    Computer() { InitializeRegisters(); }
-//
-//    void InitializeRegisters(long long a = 0, long long b = 0, long long c = 0, long long d = 0)
-//    {
-//        this->a = a;
-//        this->b = b;
-//        this->c = c;
-//        this->d = d;
-//    }
-//
-//    void ExecuteProgram(Program &program);
-//
-//    long long a, b, c, d;
-//
-//private:
-//    inline static bool IsRegister(const std::string x)
-//    {
-//        return x.length() == 1 && x[0] >= 'a' && x[0] <= 'd';
-//    }
-//
-//    inline long long &GetRegister(char name)
-//    {
-//        switch (name)
-//        {
-//        case 'a': return a;
-//        case 'b': return b;
-//        case 'c': return c;
-//        case 'd': return d;
-//        default:
-//            assert(false);
-//            return a;
-//        }
-//    }
-//
-//    void ExecuteInstruction(std::string instruction);
-//    void Optimize();
-//
-//    Program originalProgram, program;
-//    unsigned pc = 0;
-//};
-//
-//void Computer::ExecuteProgram(Program &program)
-//{
-//    this->originalProgram.clear();
-//
-//    for (auto i : program)
-//        this->originalProgram.push_back(i);
-//
-//    Optimize();
-//
-//    for (pc = 0U; pc < this->program.size(); )
-//        ExecuteInstruction(this->program[pc]);
-//}
-//
-//void Computer::Optimize()
-//{
-//    // Code self-modified; remove any optimizations and re-optimize
-//    program.clear();
-//    for (auto i : originalProgram)
-//        this->program.push_back(i);
-//
-//    // Replace slow multiplies of the form a += c * d
-//    for (auto i = 0U; i < program.size() - 4; i++)
-//    {
-//        if (program[i] == "inc a")
-//        {
-//            if ((program[i + 1] == "dec c" && program[i + 2] == "jnz c -2" && program[i + 3] == "dec d" && program[i + 4] == "jnz d -5") ||
-//                (program[i + 1] == "dec d" && program[i + 2] == "jnz d -2" && program[i + 3] == "dec c" && program[i + 4] == "jnz c -5"))
-//            {
-//                program[i] = "mpy c d a";
-//                program[i + 1] = "cpy 0 c";
-//                program[i + 2] = "cpy 0 d";
-//                program[i + 3] = "cpy 0 c";
-//                program[i + 4] = "cpy 0 d";
-//            }
-//        }
-//    }
-//}
-//
-//void Computer::ExecuteInstruction(std::string instruction)
-//{
-//    std::stringstream ss(instruction);
-//    std::string opcode, op1, op2, op3;
-//    ss >> opcode >> op1 >> op2 >> op3;
-//
-//    if (opcode == "cpy")
-//    {
-//        if (IsRegister(op2))
-//        {
-//            long long value = IsRegister(op1) ? GetRegister(op1[0]) : atoi(op1.c_str());
-//            GetRegister(op2[0]) = value;
-//        }
-//        pc++;
-//    }
-//    else if (opcode == "inc")
-//    {
-//        if (IsRegister(op1))
-//            GetRegister(op1[0])++;
-//
-//        pc++;
-//    }
-//    else if (opcode == "dec")
-//    {
-//        if (IsRegister(op1))
-//            GetRegister(op1[0])--;
-//
-//        pc++;
-//    }
-//    else if (opcode == "jnz")
-//    {
-//        long long value = IsRegister(op1) ? GetRegister(op1[0]) : atoi(op1.c_str());
-//        long long offset = IsRegister(op2) ? GetRegister(op2[0]) : atoi(op2.c_str());
-//
-//        auto newpc = pc + (value ? offset : 1);
-//        if (newpc >= 0 && newpc < program.size())
-//            pc = static_cast<unsigned>(newpc);
-//    }
-//    else if (opcode == "tgl")
-//    {
-//        long long offset = IsRegister(op1) ? GetRegister(op1[0]) : atoi(op1.c_str());
-//        if (pc + offset >= 0 && pc + offset < program.size())
-//        {
-//            auto instruction = originalProgram[pc + static_cast<int>(offset)];
-//            auto opcode = instruction.substr(0, 3);
-//
-//            if (opcode == "cpy") instruction.replace(0, 3, "jnz");
-//            else if (opcode == "jnz") instruction.replace(0, 3, "cpy");
-//            else if (opcode == "inc") instruction.replace(0, 3, "dec");
-//            else if (opcode == "dec" || opcode == "tgl") instruction.replace(0, 3, "inc");
-//            else assert(false);
-//
-//            originalProgram[pc + static_cast<int>(offset)] = instruction;
-//            Optimize();
-//        }
-//        pc++;
-//    }
-//    else if (opcode == "mpy")
-//    {
-//        if (IsRegister(op1) && IsRegister(op2) && IsRegister(op3))
-//            GetRegister(op3[0]) += GetRegister(op1[0]) * GetRegister(op2[0]);
-//
-//        pc++;
-//    }
-//    else
-//        assert(false);
-//}
+#define ROWS 39
+#define COLS 183
+#define DIGITS 8
+
+struct Coords
+{
+    unsigned row, col;
+};
+
+std::vector<std::string> maze;
+unsigned dists[ROWS][COLS][DIGITS];
+Coords targets[DIGITS];
+
+bool DoGeneration(unsigned generation)
+{
+    bool didSomething = false;
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            for (int k = 0; k < DIGITS; k++)
+                if (dists[i][j][k] == generation - 1)
+                {
+                    if (i > 0 && maze[i - 1][j] != '#' && dists[i - 1][j][k] > generation)
+                        dists[i - 1][j][k] = generation, didSomething = true;;
+
+                    if (i < ROWS - 1 && maze[i + 1][j] != '#' &&dists[i + 1][j][k] > generation)
+                        dists[i + 1][j][k] = generation, didSomething = true;;
+
+                    if (j > 0 && maze[i][j - 1] != '#' &&dists[i][j - 1][k] > generation)
+                        dists[i][j - 1][k] = generation, didSomething = true;;
+
+                    if (j < COLS - 1 && maze[i][j + 1] != '#' &&dists[i][j + 1][k] > generation)
+                        dists[i][j + 1][k] = generation, didSomething = true;;
+                }
+
+    return didSomething;
+}
+
+unsigned DeterminePathCost(const std::string path)
+{
+    unsigned cost = 0;
+    for (auto i = 0U; i < path.length() - 1; i++)
+    {
+        unsigned target1 = path[i] - '0', target2 = path[i + 1] - '0';
+        unsigned leg = dists[targets[target1].row][targets[target1].col][target2];
+        cost += leg;
+    }
+
+    return cost;
+}
+
+unsigned DoPart1()
+{
+    // Go through all permutations of 1..7 starting from zero
+    unsigned cheapest = UINT_MAX;
+    std::string perm = "01234567";
+
+    do
+    {
+        auto cost = DeterminePathCost(perm);
+        if (cost < cheapest)
+            cheapest = cost;
+    } while (std::next_permutation(perm.begin() + 1, perm.end()));
+
+    return cheapest;
+}
+
+unsigned DoPart2()
+{
+    // Go through all permutations of 1..7 starting from zero
+    unsigned cheapest = UINT_MAX;
+    std::string perm = "012345670";
+    do
+    {
+        auto cost = DeterminePathCost(perm);
+        if (cost < cheapest)
+            cheapest = cost;
+    } while (std::next_permutation(perm.begin() + 1, perm.end() - 1));
+
+    return cheapest;
+}
 
 int _tmain(int argc, _TCHAR *argv[])
 {
     double startTime = clock();
 
-    //Program program;
+    std::ifstream f("input.txt");
 
-    std::ifstream f("input-test.txt");
+    // Read maze
     std::string line;
     while (std::getline(f, line))
-        std::cout << line << std::endl;
+    {
+        maze.push_back(line);
+        // TODO: Figure out if 7 is correct
+    }
 
-    //Computer computer;
-    //computer.InitializeRegisters(7, 0, 0, 0);
-    //computer.ExecuteProgram(program);
+    // Initialize dist metrics and digit table
+    for (auto i = 0U; i < ROWS; i++)
+    {
+        for (auto j = 0U; j < COLS; j++)
+        {
+            for (auto k = 0U; k < DIGITS; k ++)
+                dists[i][j][k] = UINT_MAX;
 
-    //auto partOne = computer.a;
-    //std::cout << "Part One: " << partOne << std::endl;
+            if (isdigit(maze[i][j]))
+            {
+                unsigned target = maze[i][j] - '0';
+                dists[i][j][target] = 0;
+                targets[target] = { i, j };
+            }
+        }
+    }
 
-    //computer.InitializeRegisters(12, 0, 0, 0);
-    //computer.ExecuteProgram(program);
+    // Determine distances from any square to any number target
+    unsigned generation = 1;
+    while (DoGeneration(generation))
+        generation++;
 
-    //auto partTwo = computer.a;
-    //std::cout << "Part Two: " << partTwo << std::endl;
+    auto part1 = DoPart1();
+    std::cout << "Part One: " << part1 << std::endl;
+
+    // Similar thing for part 2
+    unsigned part2 = DoPart2();
+    std::cout << "Part Two: " << part2 << std::endl;
 
     std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
 
-    //assert(partOne == 13468);
-    //assert(partTwo == 479010028);
+    assert(part1 == 502);
+    assert(part2 == 724);
     return 0;
 }
