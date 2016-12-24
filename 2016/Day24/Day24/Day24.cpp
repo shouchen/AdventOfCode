e24d1b8a8542d26306f269ea14eb720e2668e94a
Stephen Houchen <shouchen@outlook.com>
2016-12-24 00:44:23 -0800

    Day 24: Replacing sucky generational cost calculator, other code clean-up

--- 2016/Day24/Day24/Day24.cpp
+++ 2016/Day24/Day24/Day24.cpp
@@ -2,11 +2,15 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
 #include <algorithm>
 #include <ctime>
 #include <cassert>
 
+using namespace std::string_literals;
+
+// TODO: Read these programmatically from file
 #define ROWS 39
 #define COLS 183
 #define DIGITS 8
@@ -20,115 +24,108 @@ std::vector<std::string> maze;
 unsigned dists[ROWS][COLS][DIGITS];
 Coords targets[DIGITS];
 
-bool DoGeneration(unsigned generation)
+void ReadMaze(const std::string filename)
 {
-    bool didSomething = false;
-    for (int i = 0; i < ROWS; i++)
-        for (int j = 0; j < COLS; j++)
-            for (int k = 0; k < DIGITS; k++)
-                if (dists[i][j][k] == generation - 1)
-                {
-                    if (i > 0 && maze[i - 1][j] != '#' && dists[i - 1][j][k] > generation)
-                        dists[i - 1][j][k] = generation, didSomething = true;;
+    std::ifstream f(filename);
+    std::string line;
 
-                    if (i < ROWS - 1 && maze[i + 1][j] != '#' &&dists[i + 1][j][k] > generation)
-                        dists[i + 1][j][k] = generation, didSomething = true;;
-
-                    if (j > 0 && maze[i][j - 1] != '#' &&dists[i][j - 1][k] > generation)
-                        dists[i][j - 1][k] = generation, didSomething = true;;
-
-                    if (j < COLS - 1 && maze[i][j + 1] != '#' &&dists[i][j + 1][k] > generation)
-                        dists[i][j + 1][k] = generation, didSomething = true;;
-                }
-
-    return didSomething;
+    while (std::getline(f, line))
+        maze.push_back(line);
 }
 
-unsigned DeterminePathCost(const std::string path)
+void InitializeMetrics()
+{
+    for (auto row = 0U; row < ROWS; row++)
+    {
+        for (auto col = 0U; col < COLS; col++)
+        {
+            for (auto digit = 0U; digit < DIGITS; digit++)
+                dists[row][col][digit] = UINT_MAX;
+
+            if (isdigit(maze[row][col]))
+            {
+                auto target = maze[row][col] - '0';
+                dists[row][col][target] = 0;
+                targets[target] = { row, col };
+            }
+        }
+    }
+}
+
+inline void TrySetNeighborDistance(std::queue<Coords> &q, unsigned row, unsigned col, unsigned digit, unsigned distance)
+{
+    if (maze[row][col] != '#' && distance < dists[row][col][digit])
+    {
+        dists[row][col][digit] = distance;
+        q.push(Coords{ row , col });
+    }
+}
+
+void ComputeDistancesFromTargets()
+{
+    for (auto digit = 0U; digit < DIGITS; digit++)
+    {
+        std::queue<Coords> q;
+        dists[targets[digit].row][targets[digit].col][digit] = 0;
+        q.push(targets[digit]);
+
+        while (!q.empty())
+        {
+            Coords coords = q.front();
+            q.pop();
+
+            auto row = coords.row, col = coords.col;
+            auto distance = dists[row][col][digit] + 1;
+
+            TrySetNeighborDistance(q, row - 1, col, digit, distance);
+            TrySetNeighborDistance(q, row + 1, col, digit, distance);
+            TrySetNeighborDistance(q, row, col - 1, digit, distance);
+            TrySetNeighborDistance(q, row, col + 1, digit, distance);
+        }
+    }
+}
+
+inline unsigned GetCost(unsigned fromDigit, unsigned toDigit)
+{
+    return dists[targets[fromDigit].row][targets[fromDigit].col][toDigit];
+}
+
+inline unsigned GetCost(const std::string &path)
 {
     unsigned cost = 0;
     for (auto i = 0U; i < path.length() - 1; i++)
-    {
-        unsigned target1 = path[i] - '0', target2 = path[i + 1] - '0';
-        unsigned leg = dists[targets[target1].row][targets[target1].col][target2];
-        cost += leg;
-    }
+        cost += GetCost(path[i] - '0', path[i + 1] - '0');
 
     return cost;
 }
 
-unsigned DoPart1()
+void Solve(const std::string &filename, unsigned &part1, unsigned &part2)
 {
-    // Go through all permutations of 1..7 starting from zero
-    unsigned cheapest = UINT_MAX;
-    std::string perm = "01234567";
+    ReadMaze(filename);
+    InitializeMetrics();
+    ComputeDistancesFromTargets();
+
+    part1 = part2 = UINT_MAX;
+    auto perm = "01234567"s;
 
     do
     {
-        auto cost = DeterminePathCost(perm);
-        if (cost < cheapest)
-            cheapest = cost;
+        auto cost1 = GetCost(perm);
+        auto cost2 = cost1 + GetCost(perm[perm.length() - 1] - '0', 0);
+
+        part1 = std::min(cost1, part1);
+        part2 = std::min(cost2, part2);
     } while (std::next_permutation(perm.begin() + 1, perm.end()));
-
-    return cheapest;
-}
-
-unsigned DoPart2()
-{
-    // Go through all permutations of 1..7 starting from zero
-    unsigned cheapest = UINT_MAX;
-    std::string perm = "012345670";
-    do
-    {
-        auto cost = DeterminePathCost(perm);
-        if (cost < cheapest)
-            cheapest = cost;
-    } while (std::next_permutation(perm.begin() + 1, perm.end() - 1));
-
-    return cheapest;
 }
 
 int _tmain(int argc, _TCHAR *argv[])
 {
     double startTime = clock();
 
-    std::ifstream f("input.txt");
+    auto part1 = UINT_MAX, part2 = UINT_MAX;
+    Solve("input.txt", part1, part2);
 
-    // Read maze
-    std::string line;
-    while (std::getline(f, line))
-    {
-        maze.push_back(line);
-        // TODO: Figure out if 7 is correct
-    }
-
-    // Initialize dist metrics and digit table
-    for (auto i = 0U; i < ROWS; i++)
-    {
-        for (auto j = 0U; j < COLS; j++)
-        {
-            for (auto k = 0U; k < DIGITS; k ++)
-                dists[i][j][k] = UINT_MAX;
-
-            if (isdigit(maze[i][j]))
-            {
-                unsigned target = maze[i][j] - '0';
-                dists[i][j][target] = 0;
-                targets[target] = { i, j };
-            }
-        }
-    }
-
-    // Determine distances from any square to any number target
-    unsigned generation = 1;
-    while (DoGeneration(generation))
-        generation++;
-
-    auto part1 = DoPart1();
     std::cout << "Part One: " << part1 << std::endl;
-
-    // Similar thing for part 2
-    unsigned part2 = DoPart2();
     std::cout << "Part Two: " << part2 << std::endl;
 
     std::cout << std::endl << "It took " << (clock() - startTime) / (CLOCKS_PER_SEC / 1000) << " ms." << std::endl;
