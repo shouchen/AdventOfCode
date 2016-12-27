29de1e228f6c8048789ae73f7aeb3e774d772921
Stephen Houchen <shouchen@outlook.com>
2016-12-27 15:14:49 -0800

    Day 22: Reworked the least-cost part

--- 2016/Day22/Day22/Day22.cpp
+++ 2016/Day22/Day22/Day22.cpp
@@ -1,10 +1,8 @@
 #include "stdafx.h"
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <vector>
-#include <set>
-#include <algorithm>
+#include <queue>
 #include <string>
 #include <ctime>
 #include <cassert>
@@ -17,27 +15,13 @@ struct Node
 struct Coords
 {
     int x, y;
+    bool operator==(const Coords &rhs) const { return x == rhs.x && y == rhs.y; }
+    bool operator!=(const Coords &rhs) const { return !operator==(rhs); }
 };
 
-#define X_SIZE 39
-#define Y_SIZE 25
-Node grid[Y_SIZE][X_SIZE];
-auto goalX = X_SIZE - 1U, goalY = 0U;
-
-void Dump()
-{
-    for (auto i = 0U; i < Y_SIZE; i++)
-    {
-        for (auto j = 0U; j < X_SIZE; j++)
-        {
-            std::cout << std::setw(3) << grid[i][j].used << "/" << std::setw(3) << (grid[i][j].used + grid[i][j].avail);
-            std::cout << ((i == goalY && j == goalX) ? "*" : " ");
-            std::cout << "|";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
+std::vector<std::vector<Node>> grid;
+Coords goal;
+unsigned numMoves;
 
 void ReadInput(const std::string &filename)
 {
@@ -49,196 +33,133 @@ void ReadInput(const std::string &filename)
     getline(f, line);
     getline(f, line);
 
-    std::vector<Node> nodes;
     while (f >> path >> size >> c >> used >> c >> avail >> c >> usedpct >> c)
     {
-        // Part1
-        nodes.push_back(Node{ used, avail });
+        unsigned x = atoi(&path[path.find("-x") + 2]);
+        unsigned y = atoi(&path[path.find("-y") + 2]);
 
-        auto temp = path.find("-y") + 2;
-        auto temp1 = &path[temp];
+        if (grid.size() < y + 1) grid.push_back(std::vector<Node>());
+        if (grid[y].size() < x + 1) grid[y].push_back(Node());
 
-        auto x = atoi(&path[path.find("-x") + 2]);
-        auto y = atoi(&path[path.find("-y") + 2]);
         grid[y][x].used = used;
         grid[y][x].avail = avail;
     }
+
+    goal = { static_cast<int>(grid[0].size()) - 1, 0 };
+}
+
+void MakeMove(const Coords &from, const Coords &to)
+{
+    auto numToMove = grid[from.y][from.x].used;
+
+    grid[from.y][from.x].used = 0;
+    grid[from.y][from.x].avail += numToMove;
+    grid[to.y][to.x].used += numToMove;
+    grid[to.y][to.x].avail -= numToMove;
+
+    numMoves++;
+}
+
+const Coords *ExtendPath(std::queue<Coords> &q, std::vector<std::vector<Coords>> &prev, const Coords &from, const Coords &to)
+{
+    // Make sure not off the board entirely.
+    if ((to.y < 0) || (to.y >= grid.size()) || (to.x < 0) || (to.x >= grid[0].size()))
+        return nullptr;
+
+    // Don't extend to the goal (it blocks).
+    if (to == goal)
+        return nullptr;
+
+    // To avoid loops, don't extend to an already-visited spot.
+    if (prev[to.y][to.x].x != -1 || prev[to.y][to.x].y != -1)
+        return nullptr;
+
+    // Make sure this intermediate square would have enough space for the entire source to pass through here.
+    if (!(grid[to.y][to.x].avail + grid[to.y][to.x].used >= grid[from.y][from.x].used))
+        return nullptr;
+
+    // Extend the path to this one by marking its prev as however we got here.
+    prev[to.y][to.x] = from;
+    q.push(to);
+    
+    // If this is not an empty one, have to keep going.
+    if (grid[to.y][to.x].used != 0)
+        return nullptr;
+
+    // This is the empty one; we're done.
+    prev[to.y][to.x] = from;
+    return &to;
+}
+
+// Finds shortest path through the grid from coords to an empty disk but avoiding goal in path.
+bool OpenPath(Coords coords)
+{
+    std::vector<std::vector<Coords>> prev;
+
+    for (auto y = 0U; y < grid.size(); y++)
+    {
+        prev.push_back(std::vector<Coords>());
+        for (auto x = 0U; x < grid[0].size(); x++)
+            prev[prev.size() - 1].push_back(Coords{ -1,-1 });
+    }
+
+    std::queue<Coords> q;
+    q.push(coords);
+
+    while (!q.empty())
+    {
+        Coords from = q.front();
+        q.pop();
+
+        const Coords *pathEnd;
+
+        if ((pathEnd = ExtendPath(q, prev, from, Coords{ from.x, from.y - 1 })) ||
+            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x, from.y + 1 })) ||
+            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x - 1, from.y })) ||
+            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x + 1, from.y })))
+        {
+            // Found shortest path to open disk; do the requisite moves and exit.
+            for (auto curr = *pathEnd; curr != coords; curr = prev[curr.y][curr.x])
+                MakeMove(prev[curr.y][curr.x], curr);
+
+            return true;
+        }
+    }
+
+    return false;
 }
 
 unsigned DoPart1()
 {
     unsigned count = 0;
-    for (auto y = 0; y < Y_SIZE; y++)
-        for (auto x = 0; x < X_SIZE; x++)
-        {
-            if (grid[y][x].used == 0) continue;
 
-            for (auto yy = 0; yy < Y_SIZE; yy++)
-                for (auto xx = 0; xx < X_SIZE; xx++)
-                {
-                    if (y == yy && x == xx)
-                        continue;
-                    if (grid[y][x].used > grid[yy][xx].avail)
-                        continue;
-                    count++;
-                }
-        }
+    for (auto y = 0; y < grid.size(); y++)
+        for (auto x = 0; x < grid[y].size(); x++)
+            if (grid[y][x].used)
+                for (auto yy = 0; yy < grid.size(); yy++)
+                    for (auto xx = 0; xx < grid[yy].size(); xx++)
+                        if ((y != yy || x != xx) &&
+                            (grid[y][x].used <= grid[yy][xx].avail))
+                        {
+                            count++;
+                        }
 
     return count;
 }
 
-unsigned numMoves;
-
-std::string *ExtendPath(std::string path[Y_SIZE][X_SIZE], const Coords &from, const Coords &to, unsigned generation, char c)
-{
-    if ((to.y < 0) || (to.y >= Y_SIZE) || (to.x < 0) || (to.x >= X_SIZE))
-        return nullptr;
-
-    if (!(path[to.y][to.x].length() > generation + 1 || path[to.y][to.x].length() == 0) ||
-        !(grid[to.y][to.x].avail + grid[to.y][to.x].used >= grid[from.y][from.x].used) ||
-        !(to.y != goalY || to.x != goalX))
-    {
-        return nullptr;
-    }
-
-    path[to.y][to.x] = path[from.y][from.x] + c;
-    
-    if (grid[to.y][to.x].used != 0)
-        return nullptr;
-
-    return &path[to.y][to.x];
-}
-
-/*
-inline void TryUpdateDistance(std::queue<Coords> &q, const Coords &coords, unsigned digit, unsigned newDistance)
-{
-    auto &oldDistance = distFromDigit[coords.row][coords.col][digit];
-
-    if (maze[coords.row][coords.col] != '#' && newDistance < oldDistance)
-    {
-        oldDistance = newDistance;
-        q.push(coords);
-    }
-}
-*/
-
-// Finds shortest path through the grid (xFrom, yFrom) to an empty disk but avoiding (goalX, goalY).
-// Returns the move string so it can be replayed.
-std::string FindPath(unsigned xFrom, unsigned yFrom, unsigned spaceNeeded)
-{
-    std::string path[Y_SIZE][X_SIZE];
-    unsigned generation = 1;
-    std::string retval;
-
-    path[yFrom][xFrom] = " ";
-
-    for (;;)
-    {
-        for (auto y = 0; y < Y_SIZE; y++)
-        {
-            for (auto x = 0; x < X_SIZE; x++)
-            {
-                if (x == goalX && y == goalY) continue;
-
-                if (path[y][x].length() == generation)
-                {
-                    std::string *s;
-
-                    Coords from{ x, y };
-
-                    if ((s = ExtendPath(path, from, Coords{ x, y - 1 }, generation, 'U')) ||
-                        (s = ExtendPath(path, from, Coords{ x, y + 1 }, generation, 'D')) ||
-                        (s = ExtendPath(path, from, Coords{ x - 1, y }, generation, 'L')) ||
-                        (s = ExtendPath(path, from, Coords{ x + 1, y }, generation, 'R')))
-                    {
-                        return *s;
-                    }
-                }
-            }
-        }
-
-        generation++;
-    }
-
-    return retval;
-}
-
-/*
-void ComputeDistancesFromTargets()
-{
-    for (auto digit = 0U; digit < digitLocation.size(); digit++)
-    {
-        std::queue<Coords> q;
-        TryUpdateDistance(q, digitLocation[digit], digit, 0);
-
-        while (!q.empty())
-        {
-            auto row = q.front().row, col = q.front().col;
-            auto newDistance = distFromDigit[row][col][digit] + 1;
-
-            TryUpdateDistance(q, Coords{ row - 1, col }, digit, newDistance);
-            TryUpdateDistance(q, Coords{ row + 1, col }, digit, newDistance);
-            TryUpdateDistance(q, Coords{ row, col - 1 }, digit, newDistance);
-            TryUpdateDistance(q, Coords{ row, col + 1 }, digit, newDistance);
-
-            q.pop();
-        }
-    }
-}
-*/
-
-void MakeMove(unsigned fromX, unsigned fromY, unsigned toX, unsigned toY)
-{
-    auto numToMove = grid[fromY][fromX].used;
-
-    grid[fromY][fromX].used = 0;
-    grid[fromY][fromX].avail += numToMove;
-    grid[toY][toX].used += numToMove;
-    grid[toY][toX].avail -= numToMove;
-
-    numMoves++;
-}
-
-void ApplyMoves(std::string path, unsigned numToMove)
-{
-    // Do the last step and cut it out of the string
-    while (path.length() > 1)
-    {
-        unsigned tempX = goalX - 1, tempY = goalY;
-        auto c = path.begin() + 1;
-        for (; c + 1 != path.end(); c++)
-        {
-            if (*c == 'U') tempY--;
-            if (*c == 'D') tempY++;
-            if (*c == 'L') tempX--;
-            if (*c == 'R') tempX++;
-        }
-
-        // Do move from (tempX, tempY) -> (destX, destY)
-        unsigned destX = tempX, destY = tempY;
-        if (*c == 'L') destX = tempX - 1; else if (*c == 'R') destX = tempX + 1;
-        if (*c == 'U') destY = tempY - 1; else if (*c == 'D') destY = tempY + 1;
-
-        MakeMove(tempX, tempY, destX, destY);
-        path.pop_back();
-    }
-}
-
 unsigned DoPart2()
 {
-    while (goalX != 0 || goalY != 0)
+    while (goal.x != 0)
     {
-        auto numToMove = grid[goalY][goalX].used;
-        if (grid[goalY][goalX - 1].avail < numToMove)
-        {
-            auto path = FindPath(goalX - 1, goalY, grid[goalY][goalX].used);
-            assert(path.length() >= 1);
+        auto newGoal = Coords{ goal.x - 1, goal.y };
 
-            ApplyMoves(path, numToMove);
-        }
+        // If the spot left of the goal is not open, free it up first
+        if (grid[newGoal.y][newGoal.x].avail < grid[goal.y][goal.x].used)
+            OpenPath(newGoal);
 
-        MakeMove(goalX, goalY, goalX - 1, goalY);
-        goalX--;
+        // Move goal left into the open spot
+        MakeMove(goal, newGoal);
+        goal = newGoal;
     }
 
     return numMoves;
