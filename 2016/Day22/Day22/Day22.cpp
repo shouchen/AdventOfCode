950c14eb3ee4e087053031c2de8e4d70f977a696
Stephen Houchen <shouchen@outlook.com>
2016-12-28 07:23:14 -0800

    Day 22: Removed grid size hard-coding

--- 2016/Day22/Day22/Day22.cpp
+++ 2016/Day22/Day22/Day22.cpp
@@ -14,14 +14,12 @@ struct Node
 
 struct Coords
 {
-    int x, y;
+    unsigned x, y;
     bool operator==(const Coords &rhs) const { return x == rhs.x && y == rhs.y; }
     bool operator!=(const Coords &rhs) const { return !operator==(rhs); }
 };
 
 std::vector<std::vector<Node>> grid;
-Coords goal;
-unsigned numMoves;
 
 void ReadInput(const std::string &filename)
 {
@@ -44,8 +42,6 @@ void ReadInput(const std::string &filename)
         grid[y][x].used = used;
         grid[y][x].avail = avail;
     }
-
-    goal = { static_cast<int>(grid[0].size()) - 1, 0 };
 }
 
 void MakeMove(const Coords &from, const Coords &to)
@@ -56,27 +52,28 @@ void MakeMove(const Coords &from, const Coords &to)
     grid[from.y][from.x].avail += numToMove;
     grid[to.y][to.x].used += numToMove;
     grid[to.y][to.x].avail -= numToMove;
-
-    numMoves++;
 }
 
-const Coords *ExtendPath(std::queue<Coords> &q, std::vector<std::vector<Coords>> &prev, const Coords &from, const Coords &to)
+// If this completes the path, the non-zero length of the path is returned.
+unsigned ExtendPath(
+    std::queue<Coords> &q,                   // BFS queue for shortest path
+    std::vector<std::vector<Coords>> &prev,  // table of previous for shortest path
+    const Coords &from,                      // previous grid location
+    const Coords &to,                        // potentially next grid location
+    const Coords &start,                     // the start for this entire path
+    const Coords &goal)                      // the goal (the "avoid" space on the grid
 {
-    // Make sure not off the board entirely.
-    if ((to.y < 0) || (to.y >= grid.size()) || (to.x < 0) || (to.x >= grid[0].size()))
-        return nullptr;
-
-    // Don't extend to the goal (it blocks).
-    if (to == goal)
-        return nullptr;
+    // Make sure not off the board entirely, and make sure path doesn't go through goal.
+    if (to.x >= grid[0].size() || to.y >= grid.size() || to == goal)
+        return 0;
 
     // To avoid loops, don't extend to an already-visited spot.
-    if (prev[to.y][to.x].x != -1 || prev[to.y][to.x].y != -1)
-        return nullptr;
+    if (prev[to.y][to.x].x != UINT_MAX || prev[to.y][to.x].y != UINT_MAX)
+        return 0;
 
     // Make sure this intermediate square would have enough space for the entire source to pass through here.
     if (!(grid[to.y][to.x].avail + grid[to.y][to.x].used >= grid[from.y][from.x].used))
-        return nullptr;
+        return 0;
 
     // Extend the path to this one by marking its prev as however we got here.
     prev[to.y][to.x] = from;
@@ -84,60 +81,64 @@ const Coords *ExtendPath(std::queue<Coords> &q, std::vector<std::vector<Coords>>
     
     // If this is not an empty one, have to keep going.
     if (grid[to.y][to.x].used != 0)
-        return nullptr;
+        return 0;
 
     // This is the empty one; we're done.
     prev[to.y][to.x] = from;
-    return &to;
+
+    // Found cheapest way to open a path; do the requisite moves.
+    auto numMoves = 0U;
+    for (auto curr = to; curr != start; curr = prev[curr.y][curr.x])
+    {
+        MakeMove(prev[curr.y][curr.x], curr);
+        numMoves++;
+    }
+
+    return numMoves;
 }
 
-// Finds shortest path through the grid from coords to an empty disk but avoiding goal in path.
-bool OpenPath(Coords coords)
+// Frees up grid at indicated spot by shifting data around (lowest cost). Returns numMoves.
+unsigned OpenPath(const Coords &at, const Coords &goal)
 {
+    auto numMoves = 0U;
     std::vector<std::vector<Coords>> prev;
 
     for (auto y = 0U; y < grid.size(); y++)
     {
         prev.push_back(std::vector<Coords>());
         for (auto x = 0U; x < grid[0].size(); x++)
-            prev[prev.size() - 1].push_back(Coords{ -1,-1 });
+            prev[prev.size() - 1].push_back(Coords{ UINT_MAX, UINT_MAX });
     }
 
     std::queue<Coords> q;
-    q.push(coords);
+    q.push(at);
 
     while (!q.empty())
     {
         Coords from = q.front();
         q.pop();
 
-        const Coords *pathEnd;
-
-        if ((pathEnd = ExtendPath(q, prev, from, Coords{ from.x, from.y - 1 })) ||
-            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x, from.y + 1 })) ||
-            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x - 1, from.y })) ||
-            (pathEnd = ExtendPath(q, prev, from, Coords{ from.x + 1, from.y })))
+        if ((ExtendPath(q, prev, from, Coords{ from.x, from.y - 1 }, at, goal)) ||
+            (ExtendPath(q, prev, from, Coords{ from.x, from.y + 1 }, at, goal)) ||
+            (ExtendPath(q, prev, from, Coords{ from.x - 1, from.y }, at, goal)) ||
+            (ExtendPath(q, prev, from, Coords{ from.x + 1, from.y }, at, goal)))
         {
-            // Found shortest path to open disk; do the requisite moves and exit.
-            for (auto curr = *pathEnd; curr != coords; curr = prev[curr.y][curr.x])
-                MakeMove(prev[curr.y][curr.x], curr);
-
-            return true;
+            return numMoves;
         }
     }
 
-    return false;
+    return 0;
 }
 
 unsigned DoPart1()
 {
     unsigned count = 0;
 
-    for (auto y = 0; y < grid.size(); y++)
-        for (auto x = 0; x < grid[y].size(); x++)
+    for (auto y = 0U; y < grid.size(); y++)
+        for (auto x = 0U; x < grid[y].size(); x++)
             if (grid[y][x].used)
-                for (auto yy = 0; yy < grid.size(); yy++)
-                    for (auto xx = 0; xx < grid[yy].size(); xx++)
+                for (auto yy = 0U; yy < grid.size(); yy++)
+                    for (auto xx = 0U; xx < grid[yy].size(); xx++)
                         if ((y != yy || x != xx) &&
                             (grid[y][x].used <= grid[yy][xx].avail))
                         {
@@ -149,16 +150,20 @@ unsigned DoPart1()
 
 unsigned DoPart2()
 {
+    auto numMoves = 0U;
+
+    Coords goal = { grid[0].size() - 1, 0 };
     while (goal.x != 0)
     {
         auto newGoal = Coords{ goal.x - 1, goal.y };
 
         // If the spot left of the goal is not open, free it up first
         if (grid[newGoal.y][newGoal.x].avail < grid[goal.y][goal.x].used)
-            OpenPath(newGoal);
+            numMoves += OpenPath(newGoal, goal);
 
         // Move goal left into the open spot
         MakeMove(goal, newGoal);
+        numMoves++;
         goal = newGoal;
     }
 
