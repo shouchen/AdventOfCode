43fdd234471cec4f556e1d166e60c67d31e0a561
Stephen Houchen <shouchen@outlook.com>
2017-12-02 08:06:10 -0800

    Day 2 code clean-up

--- 2017/Day2/Day2/Day2.cpp
+++ 2017/Day2/Day2/Day2.cpp
@@ -9,13 +9,13 @@
 
 typedef int(*Evaluator)(const std::vector<int> &data);
 
-int eval_row1(const std::vector<int> &data)
+int row_evaluator1(const std::vector<int> &data)
 {
     auto minmax = std::minmax_element(data.cbegin(), data.cend());
     return *minmax.second - *minmax.first;
 }
 
-int eval_row2(const std::vector<int> &data)
+int row_evaluator2(const std::vector<int> &data)
 {
     for (auto i = 0U; i < data.size(); i++)
     {
@@ -23,9 +23,9 @@ int eval_row2(const std::vector<int> &data)
         {
             if (i == j) continue;
 
-            auto div = data[i] / data[j];
-            if (div == (double)data[i] / data[j])
-                return div;
+            auto quotient = data[i] / data[j];
+            if (quotient == (double)data[i] / data[j])
+                return quotient;
         }
     }
 
@@ -54,11 +54,11 @@ int process_file(const std::string &filename, Evaluator evaluator)
 
 int main()
 {
-    assert(process_file("input-test.txt", eval_row1) == 18);
-    assert(process_file("input-test2.txt", eval_row2) == 9);
+    assert(process_file("input-test.txt", row_evaluator1) == 18);
+    assert(process_file("input-test2.txt", row_evaluator2) == 9);
 
-    auto part1 = process_file("input.txt", eval_row1);
-    auto part2 = process_file("input.txt", eval_row2);
+    auto part1 = process_file("input.txt", row_evaluator1);
+    auto part2 = process_file("input.txt", row_evaluator2);
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
