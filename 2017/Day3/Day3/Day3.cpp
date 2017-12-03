ee1c9235d4222a4e8d3b39f574e7aa6e7f896f61
Stephen Houchen <shouchen@outlook.com>
2017-12-02 22:10:48 -0800

    Day 3: Code clean-up

--- 2017/Day3/Day3/Day3.cpp
+++ 2017/Day3/Day3/Day3.cpp
@@ -3,9 +3,49 @@
 #include <iostream>
 #include <map>
 
-std::map<std::pair<int, int>, unsigned> board;
+unsigned do_part1(unsigned m)
+{
+    int top = 0, bottom = 0, left = 0, right = 0;
+    std::map<unsigned, int> dist;
 
-unsigned sum_neighbors(int x, int y)
+    dist[1] = 0;
+    int x = 0, y = 0;
+    unsigned n = 1;
+
+    for (;;)
+    {
+        // right
+        do { dist[n++] = abs(x++) + abs(y); } while (x <= right);
+        right++;
+
+        if (n > m) break;
+
+        // up
+        do { dist[n++] = abs(x) + abs(y--); } while (y >= top);
+        top--;
+
+        if (n > m) break;
+
+        // left
+        do { dist[n++] = abs(x--) + abs(y); } while (x >= left);
+        left--;
+
+        if (n > m) break;
+
+        // down
+        do { dist[n++] = abs(x) + abs(y++); } while (y <= bottom);
+        bottom++;
+
+        if (n > m) break;
+    }
+
+    return dist[m];
+}
+
+std::map<std::pair<int, int>, unsigned> board;
+unsigned part2 = 0;
+
+unsigned sum_neighbors(int x, int y, unsigned input)
 {
     unsigned sum = 0;
  
@@ -26,111 +66,60 @@ unsigned sum_neighbors(int x, int y)
     if (board.find({ x + 1, y + 1}) != board.end())
         sum += board[{x + 1, y + 1}];
 
-    std::cout << "(" << x << "," << y << ") " << sum << std::endl;
-
-    if (sum > 347991)
-        std::cout << std::endl; // 349975
+    if (!part2 && sum > input)
+        part2 = sum;
 
     return sum;
 }
 
-void do_part2()
+unsigned do_part2(unsigned input)
 {
     int top = 0, bottom = 0, left = 0, right = 1;
-
-    board[{0, 0}] = 1;
     int x = 1, y = 0;
+    board[{0, 0}] = 1;
 
     for (;;)
     {
         // up
-        do {
-            board[{x, y}] = sum_neighbors(x, y);
-        } while (--y >= top);
+        do { board[{x, y}] = sum_neighbors(x, y, input); } while (--y >= top);
         top--;
 
+        if (part2) return part2;
+
         // left
-        do {
-            board[{x, y}] = sum_neighbors(x, y);
-        } while (--x >= left);
+        do { board[{x, y}] = sum_neighbors(x, y, input); } while (--x >= left);
         left--;
 
+        if (part2) return part2;
+
         // down
-        do {
-            board[{x, y}] = sum_neighbors(x, y);
-        } while (++y <= bottom);
+        do { board[{x, y}] = sum_neighbors(x, y, input); } while (++y <= bottom);
         bottom++;
 
-        // right
-        do {
-            board[{x, y}] = sum_neighbors(x, y);
-        } while (++x <= right);
-        right++;
-    }
+        if (part2) return part2;
 
-    //std::cout << "Part Two: " << part2 << std::endl;
+        // right
+        do { board[{x, y}] = sum_neighbors(x, y, input); } while (++x <= right);
+        right++;
+
+        if (part2) return part2;
+    }
 }
 
 int main()
 {
-    do_part2();
-    return 0;
+    assert(do_part1(1) == 0);
+    assert(do_part1(12) == 3);
+    assert(do_part1(23) == 2);
+    assert(do_part1(1024) == 31);
 
-
-    int top = 0, bottom = 0, left = 0, right = 0;
-    std::map<unsigned, int> dist;
-
-    dist[1] = 0;
-    int x = 0, y = 0;
-    unsigned n = 1;
-
-    for (;;)
-    {
-        // right
-        do {
-            dist[n++] = abs(x++) + abs(y);
-        } while (x <= right);
-        right++;
-
-        if (n > 347991) break;
-
-        // up
-        do {
-            dist[n++] = abs(x) + abs(y--);
-        } while (y >= top);
-        top--;
-
-        if (n > 347991) break;
-
-        // left
-        do {
-            dist[n++] = abs(x--) + abs(y);
-        } while (x >= left);
-        left--;
-
-        if (n > 347991) break;
-
-        // down
-        do {
-            dist[n++] = abs(x) + abs(y++);
-        } while (y <= bottom);
-        bottom++;
-
-        if (n > 347991) break;
-    }
-    
-    assert(dist[1] == 0);
-    assert(dist[12] == 3);
-    assert(dist[23] == 2);
-    assert(dist[1024] == 31);
-
-    auto part1 = dist[347991];
-    auto part2 = 347991;
+    auto part1 = do_part1(347991);
+    auto part2 = do_part2(347991);
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 480);
-    assert(part2 == 347991);
+    assert(part2 == 349975);
     return 0;
 }
