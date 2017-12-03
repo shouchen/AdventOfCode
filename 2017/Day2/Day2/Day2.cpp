ec47465839b901b0c6d680b2c41a06b67043597b
Stephen Houchen <shouchen@outlook.com>
2017-12-02 16:12:53 -0800

    Day 2: Made a spreadsheet class

--- 2017/Day2/Day2/Day2.cpp
+++ 2017/Day2/Day2/Day2.cpp
@@ -7,24 +7,70 @@
 #include <vector>
 #include <algorithm>
 
-typedef int(*Evaluator)(const std::vector<int> &data);
-
-int row_evaluator1(const std::vector<int> &data)
+class Spreadsheet
 {
-    auto minmax = std::minmax_element(data.cbegin(), data.cend());
+public:
+    Spreadsheet(const std::string &filename);
+    unsigned ComputeChecksum1();
+    unsigned ComputeChecksum2();
+
+private:
+    static unsigned RowEvaluator1(const std::vector<unsigned> row);
+    static unsigned RowEvaluator2(const std::vector<unsigned> row);
+
+    std::vector<std::vector<unsigned>> data;
+};
+
+Spreadsheet::Spreadsheet(const std::string &filename)
+{
+    std::ifstream f(filename);
+    std::string input;
+
+    while (std::getline(f, input))
+    {
+        data.push_back(std::vector<unsigned>());
+        std::stringstream ss(input);
+        unsigned n;
+
+        while (ss >> n)
+            data[data.size() - 1].push_back(n);
+    }
+}
+
+unsigned Spreadsheet::ComputeChecksum1()
+{
+    auto sum = 0U;
+    for (auto row : data)
+        sum += RowEvaluator1(row);
+
+    return sum;
+}
+
+unsigned Spreadsheet::ComputeChecksum2()
+{
+    auto sum = 0U;
+    for (auto row : data)
+        sum += RowEvaluator2(row);
+
+    return sum;
+}
+
+unsigned Spreadsheet::RowEvaluator1(const std::vector<unsigned> row)
+{
+    auto minmax = std::minmax_element(row.cbegin(), row.cend());
     return *minmax.second - *minmax.first;
 }
 
-int row_evaluator2(const std::vector<int> &data)
+unsigned Spreadsheet::RowEvaluator2(const std::vector<unsigned> row)
 {
-    for (auto i = 0U; i < data.size(); i++)
+    for (auto i = 0U; i < row.size(); i++)
     {
-        for (auto j = 0U; j < data.size(); j++)
+        for (auto j = 0U; j < row.size(); j++)
         {
             if (i == j) continue;
 
-            auto quotient = data[i] / data[j];
-            if (quotient == (double)data[i] / data[j])
+            auto quotient = row[i] / row[j];
+            if (quotient == (double)row[i] / row[j])
                 return quotient;
         }
     }
@@ -32,33 +78,17 @@ int row_evaluator2(const std::vector<int> &data)
     return 0;
 }
 
-int process_file(const std::string &filename, Evaluator evaluator)
-{
-    std::ifstream f(filename);
-    std::string input;
-    int n, sum = 0;
-
-    while (std::getline(f, input))
-    {
-        std::stringstream ss(input);
-        std::vector<int> data;
-
-        while (ss >> n)
-            data.push_back(n);
-
-        sum += evaluator(data);
-    }
-
-    return sum;
-}
-
 int main()
 {
-    assert(process_file("input-test.txt", row_evaluator1) == 18);
-    assert(process_file("input-test2.txt", row_evaluator2) == 9);
+    Spreadsheet input_test("input-test.txt");
+    assert(input_test.ComputeChecksum1() == 18);
 
-    auto part1 = process_file("input.txt", row_evaluator1);
-    auto part2 = process_file("input.txt", row_evaluator2);
+    Spreadsheet input_test2("input-test2.txt");
+    assert(input_test2.ComputeChecksum2() == 9);
+
+    Spreadsheet input("input.txt");
+    auto part1 = input.ComputeChecksum1();
+    auto part2 = input.ComputeChecksum2();
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
