48d5c7a11307be7993247d4f8ff58b9e98cda12d
Stephen Houchen <shouchen@outlook.com>
2017-12-04 21:46:58 -0800

    Day 5: Code clean-up

--- 2017/Day5/Day5/Day5.cpp
+++ 2017/Day5/Day5/Day5.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <vector>
 
-unsigned do_part1(const std::string &filename)
+unsigned process_file(const std::string &filename, bool for_part2)
 {
     std::ifstream f(filename);
     int offset;
@@ -14,60 +14,24 @@ unsigned do_part1(const std::string &filename)
     while (f >> offset)
         data.push_back(offset);
 
-    int curr = 0;
-    unsigned count = 0;
-    for (;;)
+    auto count = 0U;
+
+    for (auto curr = 0; curr >= 0 && curr < (int)data.size(); curr += offset, count++)
     {
         offset = data[curr];
-        data[curr]++;
-
-        curr += offset;
-        count++;
-
-        if (curr < 0 || curr >= (int)data.size())
-        {
-            return count;
-        }
+        data[curr] += (offset >= 3 && for_part2) ? -1 : 1;
     }
-}
 
-unsigned do_part2(const std::string &filename)
-{
-    std::ifstream f(filename);
-    int offset;
-    std::vector<int> data;
-
-    while (f >> offset)
-        data.push_back(offset);
-
-    int curr = 0;
-    unsigned count = 0;
-    for (;;)
-    {
-        offset = data[curr];
-
-        if (offset >= 3)
-            data[curr]--;
-        else
-            data[curr]++;
-
-        curr += offset;
-        count++;
-
-        if (curr < 0 || curr >= (int)data.size())
-        {
-            return count;
-        }
-    }
+    return count;
 }
 
 int main()
 {
-    assert(do_part1("input-test.txt") == 5);
-    assert(do_part2("input-test.txt") == 10);
+    assert(process_file("input-test.txt", false) == 5);
+    assert(process_file("input-test.txt", true) == 10);
 
-    auto part1 = do_part1("input.txt");
-    auto part2 = do_part2("input.txt");
+    auto part1 = process_file("input.txt", false);
+    auto part2 = process_file("input.txt", true);
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
