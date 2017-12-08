9e83d33d73ffbc95e513e1fc0e98efcadd6d1810
Stephen Houchen <shouchen@outlook.com>
2017-12-07 22:30:57 -0800

    Day 8: Code clean-up

--- aoc2017/Day8/Day8.cpp
+++ aoc2017/Day8/Day8.cpp
@@ -4,81 +4,58 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <map>
-#include <set>
 #include <algorithm>
 
-std::map<std::string, int> registers;
-
-int main()
+void foo(const std::string &filename, int &part1, int &part2)
 {
-    std::ifstream f("input.txt");
-    std::string line;
+    part1 = part2 = INT_MIN;
 
-    int part2 = INT_MIN;
+    std::map<std::string, int> registers;
+
+    std::ifstream f(filename);
+    std::string line;
 
     while (std::getline(f, line))
     {
         std::stringstream ss(line);
-        std::string reg1, reg2, inc_dec, if_, cond;
+        std::string reg1, inc_dec, if_, reg2, cond;
         int amount1, amount2;
 
         ss >> reg1 >> inc_dec >> amount1 >> if_ >> reg2 >> cond >> amount2;
 
-        bool process = false;
-        if (cond == ">")
-        {
-            process = registers[reg2] > amount2;
-        }
-        else if (cond == "<")
-        {
-            process = registers[reg2] < amount2;
-        }
-        else if (cond == "==")
-        {
-            process = registers[reg2] == amount2;
-        }
-        else if (cond == "<=")
-        {
-            process = registers[reg2] <= amount2;
-        }
-        else if (cond == ">=")
-        {
-            process = registers[reg2] >= amount2;
-        }
-        else if (cond == "!=")
-        {
-            process = registers[reg2] != amount2;
-        }
-        else
-        {
-            assert(false);
-        }
+        auto reg2_value = registers[reg2];
 
-        if (process)
+        if ((cond == ">") && (reg2_value > amount2) ||
+            (cond == "<") && (reg2_value < amount2) ||
+            (cond == "==") && (reg2_value == amount2) ||
+            (cond == "<=") && (reg2_value <= amount2) ||
+            (cond == ">=") && (reg2_value >= amount2) ||
+            (cond == "!=") && (reg2_value != amount2))
         {
-            if (inc_dec == "inc")
-            {
-                registers[reg1] += amount1;
-            }
-            else
-            {
-                registers[reg1] -= amount1;
-            }
-
-            if (registers[reg1] > part2)
-                part2 = registers[reg1];
+            registers[reg1] += (inc_dec == "inc") ? amount1 : -amount1;
+            part2 = std::max(registers[reg1], part2);
         }
     }
 
-    auto max = INT_MIN;
     for (auto item : registers)
-    {
-        if (item.second > max)
-            max = item.second;
-    }
+        part1 = std::max(part1, item.second);
+}
 
+int main()
+{
+    auto part1 = 0, part2 = 0;
+    foo("input-test.txt", part1, part2);
+
+    assert(part1 == 1);
+    assert(part2 == 10);
+
+    foo("input.txt", part1, part2);
+
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 7296);
+    assert(part2 == 8186);
     return 0;
 }
-//7296, 8186
