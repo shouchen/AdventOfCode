8f98479ee84739a85fa1dc15039643ff909f28ad
Stephen Houchen <shouchen@outlook.com>
2018-12-07 00:22:15 -0800

    Day 6 clean-up

--- aoc2018/Day7/Day7.cpp
+++ aoc2018/Day7/Day7.cpp
@@ -5,16 +5,14 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <queue>
-#include <list>
 #include <cassert>
-
-std::map<char, std::vector<char>> graph;
-std::map<char, std::vector<char>> graph_back;
-std::set<char> all;
 
 std::string do_part1()
 {
+    std::map<char, std::vector<char>> graph;
+    std::map<char, std::vector<char>> graph_back;
+    std::set<char> all;
+
     std::ifstream file("input.txt");
 
     auto line = 'A', comma = ',';
@@ -22,7 +20,6 @@ std::string do_part1()
     std::string step, must, be, finished, before, can, begin;
     char first_step, second_step;
 
-    //Step C must be finished before step A can begin.
     while (file >> step >> first_step >> must >> be >> finished >> before >> step >> second_step >> can >> begin)
     {
         graph[first_step].push_back(second_step);
@@ -67,7 +64,6 @@ std::string do_part1()
         // Follow the lowest available one
         path.push_back(lowest);
         next.erase(lowest);
-        std::cout << lowest << std::endl;
 
         // Add all the next as options from the current one visited
         auto curr = path[path.length() - 1];
@@ -86,11 +82,11 @@ std::string do_part1()
 
 struct LetterTime
 {
-    char letter = ' ';
+    char letter = '.';
     unsigned time = 0;
 };
 
-std::string do_part2()
+unsigned do_part2()
 {
     std::ifstream file("input.txt");
 
@@ -99,55 +95,44 @@ std::string do_part2()
     std::string step, must, be, finished, before, can, begin;
     char first_step, second_step;
 
-    //Step C must be finished before step A can begin.
+    std::map<char, std::vector<char>> prerequisites;
+    std::set<char> steps;
+
     while (file >> step >> first_step >> must >> be >> finished >> before >> step >> second_step >> can >> begin)
     {
-        graph[first_step].push_back(second_step);
-        graph_back[second_step].push_back(first_step);
-        all.insert(first_step);
-        all.insert(second_step);
+        prerequisites[second_step].push_back(first_step);
+        steps.insert(first_step);
+        steps.insert(second_step);
     }
 
-    //std::set<char> next;
+    // Now, process the data that was read
+
     std::string path;
 
-    //for (auto item : all)
-    //{
-    //    if (graph_back.find(item) == graph_back.end())
-    //        next.insert(item);
-    //}
-
-    int seconds = 0;
-    LetterTime worker[2];
+    auto seconds = 0U;
+    LetterTime worker_status[5];
     std::set<char> taken;
 
     for (;;)
     {
-        for (int w = 0; w < 2; w++)
+        for (int w = 0; w < 5; w++)
         {
-            if (worker[w].time > 0)
+            if (worker_status[w].time > 0)
             {
-                worker[w].time--;
-                if (worker[w].time == 0)
+                worker_status[w].time--;
+                if (worker_status[w].time == 0)
                 {
-                    path.push_back(worker[w].letter);
-                    std::cout << path << std::endl;
-
-                    //// Add all the next as options from the current one visited
-                    //auto curr = worker[w].letter;
-                    //for (auto &item : graph[curr])
-                    //{
-                    //    next.insert(item);
-                    //}
+                    path.push_back(worker_status[w].letter);
+                    worker_status[w].letter = '.';
                 }
                 else
                     continue;
             }
 
-            for (char mynext = 'A'; mynext <= 'F'; mynext++)
+            for (char mynext = 'A'; mynext <= 'Z'; mynext++)
             {
                 bool prereqs = true;
-                for (auto pre : graph_back[mynext])
+                for (auto pre : prerequisites[mynext])
                 {
                     if (path.find(pre) == -1)
                     {
@@ -163,33 +148,29 @@ std::string do_part2()
                     continue;
 
                 taken.insert(mynext);
-                //next.erase(mynext);
 
-                worker[w].letter = mynext;
-                worker[w].time = /*60 +*/ (mynext - 'A' + 1);
-                std::cout << "@second " << seconds << " worker " << w << " takes " << mynext << std::endl;
+                worker_status[w].letter = mynext;
+                worker_status[w].time = 61 + mynext - 'A';
                 break;
             }
         }
-        // Stop when no more
-        if (path.length() == 6)
-            break;
+
+        if (path.length() == 26)
+            return seconds;
 
         seconds++;
     }
-
-    return path;
 }
 
 int main()
 {
-    //auto part1 = do_part1();
+    auto part1 = do_part1();
     auto part2 = do_part2();
 
-    //std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
 
-    //assert(part1 == "BITRAQVSGUWKXYHMZPOCDLJNFE");
-    assert(part2 == "BITRAQVSGUWKXYHMZPOCDLJNFE"); // 1847 too high
+    assert(part1 == "BITRAQVSGUWKXYHMZPOCDLJNFE");
+    assert(part2 == 869);
     return 0;
 }
