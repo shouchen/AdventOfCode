9d3a10a1d1644ecdc415e06855601c4de58da2b3
Stephen Houchen <shouchen@outlook.com>
2018-12-08 08:08:02 -0800

    Final Day 8 code.

--- aoc2018/Day8/Day8.cpp
+++ aoc2018/Day8/Day8.cpp
@@ -1,34 +1,37 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include <vector>
 #include <numeric>
 #include <cassert>
 
 struct Node
 {
-    std::vector<Node *> children;
+    std::vector<std::unique_ptr<Node>> children;
     std::vector<unsigned> metadata;
-} *tree;
+};
 
-unsigned total_metadata = 0;
+std::unique_ptr<Node> tree;
 
-Node *read_node(std::ifstream &file)
+std::unique_ptr<Node> read_node_and_sum_metadata(std::istream &file, unsigned &total_metadata)
 {
+    std::unique_ptr<Node> node;
     auto num_children = 0U, num_metadata = 0U;
-
-    Node *node = NULL;
 
     if (file >> num_children >> num_metadata)
     {
-        node = new Node();
+        node = std::make_unique<Node>();
 
-        for (auto i = 0U; i < num_children; i++)
-            node->children.push_back(read_node(file));
-
-        auto metadata = 0U;
-        for (auto i = 0U; i < num_metadata; i++)
+        while (num_children--)
         {
+            auto child = read_node_and_sum_metadata(file, total_metadata);
+            node->children.push_back(std::move(child));
+        }
+
+        while (num_metadata--)
+        {
+            auto metadata = 0U;
             file >> metadata;
 
             node->metadata.push_back(metadata);
@@ -42,7 +45,9 @@ Node *read_node(std::ifstream &file)
 unsigned do_part1(const std::string &filename)
 {
     std::ifstream file(filename);
-    tree = read_node(file);
+    auto total_metadata = 0U;
+
+    tree = read_node_and_sum_metadata(file, total_metadata);
     return total_metadata;
 }
 
@@ -51,17 +56,17 @@ unsigned do_part2(Node *node)
     if (!node->children.size())
         return std::accumulate(node->metadata.begin(), node->metadata.end(), 0);
 
-    auto total = 0U;
+    auto value = 0U;
     for (auto item : node->metadata)
         if (item > 0 && item <= node->children.size())
-            total += do_part2(node->children[item - 1]);
-    return total;
+            value += do_part2(node->children[item - 1].get());
+    return value;
 }
 
 int main()
 {
     auto part1 = do_part1("input.txt");
-    auto part2 = do_part2(tree);
+    auto part2 = do_part2(tree.get());
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
