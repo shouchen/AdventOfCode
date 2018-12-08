751df5eb1be8d296f8a469472e88c5b7ccc5883a
Stephen Houchen <shouchen@outlook.com>
2018-12-08 07:19:26 -0800

    Clean-up Day 8.

--- aoc2018/Day8/Day8.cpp
+++ aoc2018/Day8/Day8.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
 #include <cassert>
 
 struct Node
@@ -38,41 +39,29 @@ Node *read_node(std::ifstream &file)
     return node;
 }
 
-unsigned do_part1()
+unsigned do_part1(const std::string &filename)
 {
-    std::ifstream file("input.txt");
+    std::ifstream file(filename);
     tree = read_node(file);
     return total_metadata;
 }
 
-unsigned get_value(Node *node)
+unsigned do_part2(Node *node)
 {
+    if (!node->children.size())
+        return std::accumulate(node->metadata.begin(), node->metadata.end(), 0);
+
     auto total = 0U;
-
-    if (node->children.size())
-    {
-        for (auto item : node->metadata)
-            if (item > 0 && item <= node->children.size())
-                total += get_value(node->children[item - 1]);
-    }
-    else
-    {
-        for (auto item : node->metadata)
-            total += item;
-    }
-
+    for (auto item : node->metadata)
+        if (item > 0 && item <= node->children.size())
+            total += do_part2(node->children[item - 1]);
     return total;
-}
-
-unsigned do_part2()
-{
-    return get_value(tree);
 }
 
 int main()
 {
-    auto part1 = do_part1();
-    auto part2 = do_part2();
+    auto part1 = do_part1("input.txt");
+    auto part2 = do_part2(tree);
 
     std::cout << "Part One: " << part1 << std::endl;
     std::cout << "Part Two: " << part2 << std::endl;
