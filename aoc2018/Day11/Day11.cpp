99d2875bec20e78cea0fcd6c2940d6f34c99afe2
Stephen Houchen <shouchen@outlook.com>
2018-12-10 21:26:45 -0800

    Quick and dirty Day 11, both parts.

--- aoc2018/Day11/Day11.cpp
+++ aoc2018/Day11/Day11.cpp
@@ -28,19 +28,39 @@ int get_power(unsigned x, unsigned y, int gsn)
     return power;
 }
 
+void populate_grid(int gsn)
+{
+    for (int y = 1; y <= 300; y++)
+        for (int x = 1; x <= 300; x++)
+        {
+            grid[y][x] = get_power(x, y, gsn);
+        }
+}
+
 int get_power_of_3x3(unsigned x, unsigned y, int gsn)
 {
     int power = 0;
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
         {
-            power += get_power(x + j, y + i, gsn);
+            power += grid[y + i][x + j];
+        }
+    return power;
+}
+
+int get_power_of_nxn(unsigned x, unsigned y, unsigned size, int gsn)
+{
+    int power = 0;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+        {
+            power += grid[y + i][x + j];
         }
     return power;
 }
 
 std::pair<unsigned, unsigned> part1;
-void do_part1()
+void do_part1(int gsn)
 {
     int highest_total_power = std::numeric_limits<int>::min();
     unsigned best_x = 0, best_y = 0;
@@ -48,7 +68,7 @@ void do_part1()
     for (int y = 1; y < 298; y++)
         for (int x = 1; x < 298; x++)
         {
-            auto power = get_power_of_3x3(x, y, 9306);
+            auto power = get_power_of_3x3(x, y, gsn);
             if (power > highest_total_power)
             {
                 highest_total_power = power;
@@ -57,101 +77,38 @@ void do_part1()
             }
         }
 
-    std::cout << best_x << "," << best_y << std::endl;
+    std::cout << "Answer: " << best_x << "," << best_y << std::endl;
 }
 
-
-
-auto left = std::numeric_limits<int>::max(), right = std::numeric_limits<int>::min();
-auto top = std::numeric_limits<int>::max(), bottom = std::numeric_limits<int>::min();
-
-void read_input(const std::string &filename)
+void do_part2(int gsn)
 {
-    std::ifstream file(filename);
-    std::string line;
+    int highest_total_power = std::numeric_limits<int>::min();
+    unsigned best_x = 0, best_y = 0, best_size = 0;
 
-    while (std::getline(file, line))
+    for (int i = 1; i <= 300; i++)
     {
-        auto p = strchr(line.c_str(), '<') + 1;
-
-        auto x = atoi(p);
-        auto y = atoi(p = strchr(p, ',') + 1);
-        auto vx = atoi(p = strchr(p, '<') + 1);
-        auto vy = atoi(strchr(p, ',') + 1);
-
-        locations.push_back(std::make_pair(x, y));
-        velocities.push_back(std::make_pair(vx, vy));
-
-        left = std::min(left, x);
-        right = std::max(right, x);
-        top = std::min(top, y);
-        bottom = std::max(bottom, y);
-    }
-}
-
-void output_grid()
-{
-    for (auto y = top; y <= bottom; y++)
-    {
-        for (auto x = left; x <= right; x++)
-            std::cout << (std::find(locations.begin(), locations.end(), std::make_pair(x, y)) == locations.end() ? '.' : '#');
-
-        std::cout << std::endl;
+        for (int y = 1; y < 301 - i; y++)
+            for (int x = 1; x < 301 - i; x++)
+            {
+                auto power = get_power_of_nxn(x, y, i, gsn);
+                if (power > highest_total_power)
+                {
+                    highest_total_power = power;
+                    best_x = x;
+                    best_y = y;
+                    best_size = i;
+                    std::cout << x << "," << y << "," << i << std::endl;
+                }
+            }
     }
 
-    std::cout << std::endl;
-}
-
-unsigned long long update_locations_and_bounds_and_compute_area()
-{
-    left = top = std::numeric_limits<int>::max(), right = bottom = std::numeric_limits<int>::min();
-
-    for (auto i = 0; i < locations.size(); i++)
-    {
-        auto x = locations[i].first += velocities[i].first;
-        auto y = locations[i].second += velocities[i].second;
-
-        left = std::min(left, x);
-        right = std::max(right, x);
-        top = std::min(top, y);
-        bottom = std::max(bottom, y);
-    }
-
-    unsigned long long width = right - left, height = bottom - top;
-    return width * height;
+    std::cout << "Answer: " << best_x << "," << best_y << "," << best_size << std::endl;
 }
 
 int main()
 {
-    do_part1();
+    populate_grid(9306);
+    do_part2(9306);
 
-    //read_input("input.txt");
-
-    //auto time = 0U;
-    //auto area = std::numeric_limits<unsigned long long>::max();
-
-    //for (;;)
-    //{
-    //    if (right - left < 100 && bottom - top < 100)
-    //        output_grid();
-
-    //    auto new_area = update_locations_and_bounds_and_compute_area();
-    //    if (new_area > area)
-    //        break;
-
-    //    area = new_area;
-    //    time++;
-    //}
-
-    //std::cout << "After " << time << " seconds" << std::endl;
-
-    //auto part1 = do_part1();
-    //auto part2 = do_part2();
-
-    //std::cout << "Part One: " << part1 << std::endl;
-    //std::cout << "Part Two: " << part2 << std::endl;
-
-    //assert(part1 == 561);
-    //assert(part2 == 563);
-    return 0;  //235,38
+    return 0;  //235,38 (part1) //233,146,13 (part2)
 }
