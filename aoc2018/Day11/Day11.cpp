1164dbc0471e8dc49e9ef5258c9050837cf17cd0
Stephen Houchen <shouchen@outlook.com>
2018-12-10 22:50:14 -0800

    Day 11 clean-up

--- aoc2018/Day11/Day11.cpp
+++ aoc2018/Day11/Day11.cpp
@@ -4,7 +4,9 @@
 #include <numeric>
 #include <cassert>
 
-int grid[301][301];
+const unsigned grid_size = 300;
+
+int grid[grid_size + 1][grid_size + 1];
 
 int compute_power(unsigned x, unsigned y, int gsn)
 {
@@ -19,74 +21,44 @@ int compute_power(unsigned x, unsigned y, int gsn)
 
 void populate_grid(int gsn)
 {
-    for (auto y = 1; y <= 300; y++)
-        for (auto x = 1; x <= 300; x++)
+    for (auto y = 1; y <= grid_size; y++)
+        for (auto x = 1; x <= grid_size; x++)
             grid[y][x] = compute_power(x, y, gsn);
 }
 
-//int get_power_of_3x3(unsigned x, unsigned y)
-//{
-//    auto power = 0;
-//    for (auto i = 0; i < 3; i++)
-//        for (auto j = 0; j < 3; j++)
-//            power += grid[y + i][x + j];
-//
-//    return power;
-//}
-
-int get_power_of_nxn(unsigned x, unsigned y, unsigned size)
+int get_power_of_square(unsigned x, unsigned y, unsigned n)
 {
     auto power = 0;
-    for (auto i = 0U; i < size; i++)
-        for (auto j = 0U; j < size; j++)
+    for (auto i = 0U; i < n; i++)
+        for (auto j = 0U; j < n; j++)
             power += grid[y + i][x + j];
 
     return power;
 }
 
-std::string do_part1()
+std::string find_square_with_highest_power(unsigned min_n, unsigned max_n)
 {
     auto highest_total_power = std::numeric_limits<int>::min();
-    auto best_x = 0U, best_y = 0U;
+    auto best_x = 0U, best_y = 0U, best_n = 0U;
 
-    for (auto y = 1; y < 298; y++)
-        for (auto x = 1; x < 298; x++)
-        {
-            auto power = get_power_of_nxn(x, y, 3);
-            if (power > highest_total_power)
+    for (auto n = min_n; n <= max_n; n++)
+        for (auto y = 1U; y < grid_size + 1 - n; y++)
+            for (auto x = 1U; x < grid_size + 1 - n; x++)
             {
-                highest_total_power = power;
-                best_x = x;
-                best_y = y;
-            }
-        }
-
-    std::ostringstream o;
-    o << best_x << "," << best_y;
-    return o.str();
-}
-
-std::string do_part2()
-{
-    auto highest_total_power = std::numeric_limits<int>::min();
-    auto best_x = 0U, best_y = 0U, best_size = 0U;
-
-    for (auto size = 1; size <= 300; size++)
-        for (auto y = 1; y < 301 - size; y++)
-            for (auto x = 1; x < 301 - size; x++)
-            {
-                auto power = get_power_of_nxn(x, y, size);
+                auto power = get_power_of_square(x, y, n);
                 if (power > highest_total_power)
                 {
                     highest_total_power = power;
                     best_x = x;
                     best_y = y;
-                    best_size = size;
+                    best_n = n;
                 }
             }
 
     std::ostringstream o;
-    o << best_x << "," << best_y << "," << best_size;
+    o << best_x << "," << best_y;
+    if (min_n != max_n)
+        o << "," << best_n;
     return o.str();
 }
 
@@ -95,8 +67,8 @@ int main()
     const int input = 9306;
     populate_grid(input);
 
-    auto part1 = do_part1();
-    auto part2 = do_part2();
+    auto part1 = find_square_with_highest_power(3, 3);
+    auto part2 = find_square_with_highest_power(1, 100);
 
     std::cout << "Part 1: " << part1 << std::endl;
     std::cout << "Part 2: " << part2 << std::endl;
