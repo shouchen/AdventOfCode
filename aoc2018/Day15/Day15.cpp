5f952a3f75c107f79477032b94082d22db99968e
Stephen Houchen <shouchen@outlook.com>
2018-12-15 08:30:48 -0800

    Day 15 movement correct

--- aoc2018/Day15/Day15.cpp
+++ aoc2018/Day15/Day15.cpp
@@ -12,91 +12,191 @@
 #include <algorithm>
 #include <cassert>
 
-//std::vector<std::pair<int, int>> locations;
-//std::vector<std::pair<int, int>> velocities;
-//
-//auto left = std::numeric_limits<int>::max(), right = std::numeric_limits<int>::min();
-//auto top = std::numeric_limits<int>::max(), bottom = std::numeric_limits<int>::min();
-//
-//void read_input(const std::string &filename)
-//{
-//    std::ifstream file(filename);
-//    std::string line;
-//
-//    while (std::getline(file, line))
-//    {
-//        auto p = strchr(line.c_str(), '<') + 1;
-//
-//        auto x = atoi(p);
-//        auto y = atoi(p = strchr(p, ',') + 1);
-//        auto vx = atoi(p = strchr(p, '<') + 1);
-//        auto vy = atoi(strchr(p, ',') + 1);
-//
-//        locations.push_back(std::make_pair(x, y));
-//        velocities.push_back(std::make_pair(vx, vy));
-//
-//        left = std::min(left, x);
-//        right = std::max(right, x);
-//        top = std::min(top, y);
-//        bottom = std::max(bottom, y);
-//    }
-//}
-//
-//void output_grid()
-//{
-//    for (auto y = top; y <= bottom; y++)
-//    {
-//        for (auto x = left; x <= right; x++)
-//            std::cout << (std::find(locations.begin(), locations.end(), std::make_pair(x, y)) == locations.end() ? '.' : '#');
-//
-//        std::cout << std::endl;
-//    }
-//
-//    std::cout << std::endl;
-//}
-//
-//unsigned long long update_locations_and_bounds_and_compute_area()
-//{
-//    left = top = std::numeric_limits<int>::max(), right = bottom = std::numeric_limits<int>::min();
-//
-//    for (auto i = 0; i < locations.size(); i++)
-//    {
-//        auto x = locations[i].first += velocities[i].first;
-//        auto y = locations[i].second += velocities[i].second;
-//
-//        left = std::min(left, x);
-//        right = std::max(right, x);
-//        top = std::min(top, y);
-//        bottom = std::max(bottom, y);
-//    }
-//
-//    unsigned long long width = right - left, height = bottom - top;
-//    return width * height;
-//}
+struct Unit
+{
+    int row, col;
+    int hit_points = 200;
+    char type;
+    Unit(int row, int col, char type, int hit_points) : row(row), col(col), type(type), hit_points(hit_points) {}
+};
+
+std::vector<std::string> grid;
+std::vector<Unit *> units;
+
+void dump()
+{
+    for (int row = 0; row < grid.size(); row++)
+    {
+        for (int col = 0; col < grid[row].size(); col++)
+            std::cout << grid[row][col];
+
+        bool first = true;
+        for (int col = 0; col < grid[row].size(); col++)
+            for (auto unit : units)
+                if (unit->row == row && unit->col == col)
+                {
+                    std::cout << (first ? " " : ", ") << unit->type << "(" << unit->hit_points << ")";
+                    first = false;
+                }
+ 
+        std::cout << std::endl;
+    }
+}
+
+void read_input(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (file >> line)
+        grid.push_back(line);
+
+    for (int row = 0; row < grid.size(); row++)
+        for (int col = 0; col < grid[row].size(); col++)
+        {
+            if (grid[row][col] == 'E' || grid[row][col] == 'G')
+                units.push_back(new Unit(row, col, grid[row][col], 200));
+        }
+}
+
+std::vector<std::pair<int, int>> best_path;
+
+void get_next_step(int row, int col, int &shortest, std::vector<std::pair<int, int>> &path)
+{
+    //std::cout << "STEP" << std::endl;
+    //dump();
+
+    if (path.size() >= shortest)
+        return;
+
+    if (grid[row][col] == '?')
+    {
+        shortest = path.size();
+        best_path.clear();
+        for (auto &item : path)
+            best_path.push_back(item);
+        return;
+    }
+
+    if (grid[row - 1][col] == '.' || grid[row - 1][col] == '?')
+    {
+        char temp = grid[--row][col];
+        if (temp == '.')
+            grid[row][col] = 'x';
+        path.push_back(std::make_pair(row, col));
+        get_next_step(row, col, shortest, path);
+        path.pop_back();
+        grid[row++][col] = temp;
+    }
+    if (grid[row][col - 1] == '.' || grid[row][col - 1] == '?')
+    {
+        char temp = grid[row][--col];
+        if (temp == '.')
+            grid[row][col] = 'x';
+        path.push_back(std::make_pair(row, col));
+        get_next_step(row, col, shortest, path);
+        path.pop_back();
+        grid[row][col++] = temp;
+    }
+    if (grid[row][col + 1] == '.' || grid[row][col + 1] == '?')
+    {
+        char temp = grid[row][++col];
+        if (temp == '.')
+            grid[row][col] = 'x';
+        path.push_back(std::make_pair(row, col));
+        get_next_step(row, col, shortest, path);
+        path.pop_back();
+        grid[row][col--] = temp;
+    }
+    if (grid[row + 1][col] == '.' || grid[row + 1][col] == '?')
+    {
+        char temp = grid[++row][col];
+        if (temp == '.')
+            grid[row][col] = 'x';
+        path.push_back(std::make_pair(row, col));
+        get_next_step(row, col, shortest, path);
+        path.pop_back();
+        grid[row--][col] = temp;
+    }
+}
+
+bool do_turn(Unit *unit)
+{
+    // If already in a target square, don't move
+    if (unit->type == 'E' && grid[unit->row - 1][unit->col] == 'G' || unit->type == 'G' && grid[unit->row - 1][unit->col] == 'E') return false;
+    if (unit->type == 'E' && grid[unit->row][unit->col - 1] == 'G' || unit->type == 'G' && grid[unit->row][unit->col - 1] == 'E') return false;
+    if (unit->type == 'E' && grid[unit->row][unit->col + 1] == 'G' || unit->type == 'G' && grid[unit->row][unit->col + 1] == 'E') return false;
+    if (unit->type == 'E' && grid[unit->row + 1][unit->col] == 'G' || unit->type == 'G' && grid[unit->row + 1][unit->col] == 'E') return false;
+
+    // Identify all open squares in range of each target
+    for (auto other : units)
+    {
+        if (other->type != unit->type)
+        {
+            if (grid[other->row - 1][other->col] == '.') grid[other->row - 1][other->col] = '?';
+            if (grid[other->row][other->col - 1] == '.') grid[other->row][other->col - 1] = '?';
+            if (grid[other->row][other->col + 1] == '.') grid[other->row][other->col + 1] = '?';
+            if (grid[other->row + 1][other->col] == '.') grid[other->row + 1][other->col] = '?';
+        }
+    }
+
+    // Get shortest distance to a target
+    std::vector<std::pair<int, int>> path;
+    int shortest = std::numeric_limits<int>::max();
+    best_path.clear();
+    get_next_step(unit->row, unit->col, shortest, path);
+
+    // Remove all the ?'s that were added
+    for (int row = 0; row < grid.size(); row++)
+        for (int col = 0; col < grid[row].size(); col++)
+            if (grid[row][col] == '?')
+                grid[row][col] = '.';
+
+    // Take the best step (if any)
+    if (!best_path.empty())
+    {
+        grid[unit->row][unit->col] = '.';
+        unit->row = best_path[0].first;
+        unit->col = best_path[0].second;
+        grid[unit->row][unit->col] = unit->type;
+        return true;
+    }
+
+    return false;
+}
+
+bool do_round()
+{
+    // Sort units
+    std::sort(units.begin(), units.end(), [&](const Unit *lhs, const Unit *rhs) -> bool {
+        return std::make_pair(lhs->row, rhs->col) < std::make_pair(rhs->row, rhs->col);
+    });
+
+    bool move_made = false;
+    for (auto unit : units)
+    {
+        move_made |= do_turn(unit);
+    }
+
+    return move_made;
+}
 
 int main()
 {
-    //read_input("input.txt");
+    read_input("input.txt");
+    int round = 0;
 
-    //auto time = 0U;
-    //auto area = std::numeric_limits<unsigned long long>::max();
+    std::cout << "Initially: " << std::endl;
+    dump();
+    std::cout << std::endl;
 
-    //for (;;)
-    //{
-    //    if (right - left < 100 && bottom - top < 100)
-    //        output_grid();
+    while (do_round())
+    {
+        round++;
+        std::cout << "After " << round << " round(s)" << std::endl;
+        dump();
+        std::cout << std::endl;
+    }
 
-    //    auto new_area = update_locations_and_bounds_and_compute_area();
-    //    if (new_area > area)
-    //        break;
-
-    //    area = new_area;
-    //    time++;
-    //}
-
-    //std::cout << "After " << time << " seconds" << std::endl;
-
-    //// Visually, you should see "GPEPPPEJ" for this input data.
-    //assert(time == 10101);
     return 0;
 }
