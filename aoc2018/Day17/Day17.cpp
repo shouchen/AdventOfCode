5e4f736087d62cfce6c39d58c2497c3340aae67f
Stephen Houchen <shouchen@outlook.com>
2018-12-17 07:16:19 -0800

    Day 17 correct solution for both parts.

--- aoc2018/Day17/Day17.cpp
+++ aoc2018/Day17/Day17.cpp
@@ -1,17 +1,9 @@
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 #include <fstream>
-#include <vector>
-#include <set>
-#include <queue>
-#include <list>
-#include <stack>
-#include <map>
-#include <string>
-#include <numeric>
 #include <algorithm>
 #include <cassert>
-
 
 char grid[1820][700];
 int min_depth = std::numeric_limits<int>::max();
@@ -19,31 +11,28 @@ int max_depth = std::numeric_limits<int>::min();
 int min_x = std::numeric_limits<int>::max();
 int max_x = std::numeric_limits<int>::min();
 
-unsigned dump()
+void dump()
 {
-    unsigned count = 0;
-    for (auto row = min_depth; row <= max_depth; row++)
+    auto count = 0U, count2 = 0U;
+    for (auto row = 0; row <= max_depth + 2; row++)
     {
-        for (auto col = min_x; col <= max_x; col++)
+        std::cout << std::setw(4) << row << " ";
+        for (auto col = min_x - 1; col <= max_x + 1; col++)
         {
             auto c = grid[row][col];
             std::cout << (c ? c : '.');
-            if (c == '|' || c == '~') count++;
+            if ((c == '|' || c == '~') && row >= min_depth) count++;
+            if (c == '~' && row >= min_depth) count2++;
         }
-        std::cout << std::endl;
+        std::cout << " " << std::setw(5) << count << " " << count2 << std::endl;
     }
-
-    std::string s;
-    std::cout << count << " =============================================" << std::endl;
-    std::getline(std::cin, s);
-    return count;
 }
 
 void read_input(const std::string &filename)
 {
     std::ifstream file(filename);
-    char var1, equals = '=', var2, comma = ',', dot = '.';
-    int dim1, dim2a, dim2b;
+    auto var1 = 'x', equals = '=', var2 = 'y', comma = ',', dot = '.';
+    auto dim1 = 0, dim2a = 0, dim2b = 0;
 
     grid[0][500] = '+';
 
@@ -51,7 +40,7 @@ void read_input(const std::string &filename)
     {
         if (var1 == 'x')
         {
-            for (int row = dim2a; row <= dim2b; row++)
+            for (auto row = dim2a; row <= dim2b; row++)
                 grid[row][dim1] = '#';
             min_depth = std::min(min_depth, dim2a);
             max_depth = std::max(max_depth, dim2b);
@@ -60,7 +49,7 @@ void read_input(const std::string &filename)
         }
         else
         {
-            for (int col = dim2a; col <= dim2b; col++)
+            for (auto col = dim2a; col <= dim2b; col++)
                 grid[dim1][col] = '#';
             min_depth = std::min(min_depth, dim1);
             max_depth = std::max(max_depth, dim1);
@@ -70,113 +59,64 @@ void read_input(const std::string &filename)
     }
 }
 
-void drip(int row, int col)
+void seep(int row, int col)
 {
-    auto loc = std::make_pair(row, col);
+    if (row > max_depth || col < min_x - 1 || col > max_x + 1)
+        return;
 
-    while (++loc.first <= max_depth)
+    if (grid[row][col])
+        return;
+
+    grid[row][col] = '|';
+
+    seep(row + 1, col);
+
+    if (grid[row + 1][col] != '#' && grid[row + 1][col] != '~')
+        return;
+
+    seep(row, col - 1);
+
+    if (grid[row][col + 1] == '#')
     {
-        if (!grid[loc.first][loc.second])
-        {
-            grid[loc.first][loc.second] = '|';
-            dump();
-        }
-        else if (grid[loc.first][loc.second] == '#')
-        {
-            for (;;)
-            {
-                loc.first--;
+        auto test_col = col;
+        while (grid[row][test_col] == '|') --test_col;
 
-                grid[loc.first][loc.second] = '|';
-                dump();
-
-                int left_wall = -1, right_wall = -1;
-
-                // fill left
-                auto left_loc = loc;
-
-                for (;;)
-                {
-                    --left_loc.second;
-                    if (!grid[left_loc.first][left_loc.second])
-                    {
-                        grid[left_loc.first][left_loc.second] = '|';
-                        dump();
-
-                        auto under = left_loc;
-                        under.first++;
-
-                        if (!grid[under.first][under.second])
-                        {
-                            drip(left_loc.first, left_loc.second);
-                            break;
-                        }
-                    }
-                    else if (grid[left_loc.first][left_loc.second] == '#')
-                    {
-                        left_wall = left_loc.second + 1;
-                        break;
-                    }
-                }
-
-                // fill right
-                auto right_loc = loc;
-
-                for (;;)
-                {
-                    right_loc.second++;
-                    if (!grid[right_loc.first][right_loc.second])
-                    {
-                        grid[right_loc.first][right_loc.second] = '|';
-                        dump();
-
-                        auto under = right_loc;
-                        under.first++;
-
-                        if (!grid[under.first][under.second])
-                        {
-                            drip(right_loc.first, right_loc.second);
-                            break;
-                        }
-                    }
-                    else if (grid[right_loc.first][right_loc.second] == '#')
-                    {
-                        right_wall = right_loc.second - 1;
-                        break;
-                    }
-                }
-
-                // convert whole row into ~
-                if (right_wall != -1 && left_wall != -1)
-                {
-                    for (auto col = left_wall; col <= right_wall; col++)
-                        grid[loc.first][col] = '~';
-                    dump();
-                }
-                else
-                    return;
-            }
-        }
+        if (grid[row][test_col] == '#')
+            while (grid[row][col] == '|')
+                grid[row][col--] = '~';
     }
+    else
+        seep(row, col + 1);
 }
 
-void do_part1()
+void do_parts1_and_2(unsigned &part1, unsigned &part2)
 {
-    drip(0, 500);
+    seep(1, 500);
+
+    part1 = 0, part2 = 0;
+    for (auto row = min_depth; row <= max_depth; row++)
+    {
+        for (auto col = min_x - 1; col <= max_x + 1; col++)
+        {
+            auto c = grid[row][col];
+            if (c == '|' || c == '~') part1++;
+            if (c == '~') part2++;
+        }
+    }
 }
 
 int main()
 {
     read_input("input.txt");
 
-    dump();
-    do_part1();
-    std::cout << dump() << std::endl;
+    auto part1 = 0U, part2 = 0U;
+    do_parts1_and_2(part1, part2);
 
-    //assert(time == 10101);
+    std::cout << "Part 1: " << part1 << std::endl;
+    std::cout << "Part 2: " << part2 << std::endl;
+
+    assert(part1 == 31953);
+    assert(part2 == 26410);
 
     return 0;
-
-
 }
-// 146921 was too high
