deaa72e939d4391a2e971826771bf6c36c6c34ef
Stephen Houchen <shouchen@outlook.com>
2018-12-18 15:23:27 -0800

    Day 18 clean-up

--- aoc2018/enc_temp_folder/372d20dfcb052a41cda79fe834397d/Day18.cpp
+++ aoc2018/enc_temp_folder/372d20dfcb052a41cda79fe834397d/Day18.cpp
@@ -137,8 +137,60 @@ int main()
     // linearly, decreasing linearly, or cyclic. Data shows that it's a cycle
     // of period 28. 1,000,000,000 mod 28 is 20, so we can extrapolate to that
     // future generation value by taking any other generation where mod 28 is
-    // equal to 20. For example, use "After 16008 generations = 174420" to get
-    // the answer to Part 2, which is 174420.
+    // equal to 20. 
+    //
+    // Here, this would be 26410.
+
+    //auto part1 = do_part1(grid, 10);
+    //std::cout << "Part 1: " << part1 << std::endl;
+
+    ////auto part2 = do_part2(10);
+    ////std::cout << "Part 2: " << part2 << std::endl;
+
+    //assert(part1 == 394420);
+    ////assert(part2 == 26410);
 
     return 0;
 }
+
+/*
+
+Run the second part for long enough and it gets into a cyclic pattern:
+
+After 15646 times = 182590    22         cycle = 28 (numbers to the left show iteration % 28)
+After 15647 times = 189630    23
+After 15648 times = 196452    24
+After 15649 times = 205000    25
+After 15650 times = 210789    26
+After 15651 times = 218240    27
+After 15652 times = 222824    0
+After 15653 times = 231336    1
+After 15654 times = 236155    2
+After 15655 times = 240482    3
+After 15656 times = 237636    4
+After 15657 times = 237082    5
+After 15658 times = 234938    6
+After 15659 times = 237900    7
+After 15660 times = 232232    8
+After 15661 times = 228274    9
+After 15662 times = 220584   10
+After 15663 times = 216752   11
+After 15664 times = 206702   12
+After 15665 times = 199824   13
+After 15666 times = 178100   14
+After 15667 times = 175587   15
+After 15668 times = 169880   16
+After 15669 times = 173014   17
+After 15670 times = 171248   18
+After 15671 times = 174782   19
+After 15672 times = 174420   20
+After 15673 times = 179800   21
+After 15674 times = 182590   22
+
+So, 1 billion % 28 = 20 --> 174420 is the answer.
+
+A better solution for part2 would be looking for these cycles and inferring it after some point.
+But it's also possible that it's not cyclic but diverging, in which case, would also need to infer
+that as well and find a linear growing or shrinking score and do that inference as well.
+
+*/
