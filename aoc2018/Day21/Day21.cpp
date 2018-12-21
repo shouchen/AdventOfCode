ae6abf4a3f93b084e9c3ea2eb3afb697f29c742e
Stephen Houchen <shouchen@outlook.com>
2018-12-21 08:19:12 -0800

    Cleaned-up Day 21.

--- aoc2018/Day21/Day21.cpp
+++ aoc2018/Day21/Day21.cpp
@@ -3,11 +3,39 @@
 #include <set>
 #include <cassert>
 
-auto do_part1()
-{
-    auto r0 = 0, r2 = 0, r3 = 0;
+// Reverse-engineering the input code, it is doing the equivalent of this:
+//
+//   do
+//   {
+//       r2 = r3 | 0x10000;
+//       r3 = 832312;
+//
+//       for (;;)
+//       {
+//           r3 = (r3 + (r2 & 0xff)) & 0xffffff;
+//           r3 = (r3 * 65899) & 0xffffff;
+//           if (r2 < 256)
+//               break;
+//
+//           r2 >>= 8;
+//       }
+//   } while (r3 != r0);
+//
+// The key is that r0 is only ever read from as a condition to exit the program;
+// its value is never modified. The outer loop cycles through set of 3-byte
+// values in r3 until r3 matches r0. To find the least number of instructions to
+// make the program exit (part 1), choose the value for r0 that makes the outer
+// loop run only a single time. For part2, choose the value for r0 that makes
+// the outer loop run the most times before the r3 cycle repeats itself.
 
-    do
+void do_both_parts(int &part1, int &part2)
+{
+    std::set<int> r3_seen;
+    auto previous_iteration_r3 = 0;
+
+    auto r2 = 0, r3 = 0;
+
+    for (;;)
     {
         r2 = r3 | 0x10000;
         r3 = 832312;
@@ -16,51 +44,30 @@ auto do_part1()
         {
             r3 = (r3 + (r2 & 0xff)) & 0xffffff;
             r3 = (r3 * 65899) & 0xffffff;
-            if (r2 < 256) break;
+            if (r2 < 256)
+                break;
 
             r2 >>= 8;
         }
 
-        // Ending after 1 iteration is best case, return that.
-        return r3;
-    } while (r3 != r0);
-}
-
-auto do_part2()
-{
-    std::set<int> seen;
-    auto previous = -1;
-
-    auto r0 = 0, r2 = 0, r3 = 0;
-
-    do
-    {
-        r2 = r3 | 0x10000;
-        r3 = 832312;
-
-        for (;;)
+        if (r3_seen.empty())
         {
-            r3 = (r3 + (r2 & 0xff)) & 0xffffff;
-            r3 = (r3 * 65899) & 0xffffff;
-            if (r2 < 256) break;
-
-            r2 >>= 8;
+            part1 = r3;
         }
-
-        // When do we get a repeat?
-        if (seen.find(r3) != seen.end())
+        else if (r3_seen.find(r3) != r3_seen.end())
+        {
+            part2 = previous_iteration_r3;
             break;
-        seen.insert(r3);
-        previous = r3;
-    } while (r3 != r0);
+        }
 
-    return previous;
+        r3_seen.insert(previous_iteration_r3 = r3);
+    }
 }
 
-int main()
+auto main()
 {
-    auto part1 = do_part1();
-    auto part2 = do_part2();
+    auto part1 = 0, part2 = 0;
+    do_both_parts(part1, part2);
 
     std::cout << "Part 1: " << part1 << std::endl;
     std::cout << "Part 2: " << part2 << std::endl;
