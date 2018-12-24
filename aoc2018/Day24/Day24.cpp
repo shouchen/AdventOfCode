1c294932b663e90d51f62e0b3a20d12cbb2007b9
Stephen Houchen <shouchen@outlook.com>
2018-12-24 13:10:48 -0800

    Day 24 code clean-up (still not parsing input file).

--- aoc2018/Day24/Day24.cpp
+++ aoc2018/Day24/Day24.cpp
@@ -2,16 +2,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <set>
 #include <map>
-#include <string>
-#include <numeric>
 #include <algorithm>
 #include <cassert>
 
-//#define DEBUG_OUTPUT
-
-enum Type { Immune, Infection };
+enum Type { ImmuneSystem, Infection };
 enum AttackType { Radiation = 0x01, Bludgeoning = 0x02, Fire = 0x04, Slashing = 0x08, Cold = 0x10 };
 
 struct Group
@@ -38,7 +33,7 @@ struct Group
         if (weakness & other.attack_type) damage *= 2;
         return damage;
     }
-    bool receive_attack(Group &other) // returns true is attack actually changed anything
+    bool receive_attack(Group &other) // returns true if attack actually changed anything
     {
         auto damage = other.attack_damage * other.num_units;
         if (immunities & other.attack_type) return false;
@@ -48,10 +43,6 @@ struct Group
         if (num_units_destroyed > num_units) num_units_destroyed = num_units;
         num_units -= num_units_destroyed;
         return num_units_destroyed > 0;
-
-#ifdef DEBUG_OUTPUT
-        std::cout << ((other.type == Infection) ? "Infection" : "Immune System") << " group " << other.group_num << " attacks defending group " << group_num << ", killing " << num_units_destroyed << " unit(s)" << std::endl;
-#endif
     }
 };
 
@@ -79,11 +70,6 @@ bool do_fight() // returns false if nothing changed
             if (other->type == g->type) continue; // group can't attack its own kind (nor itself)
             if (attacked_by.find(other) != attacked_by.end()) continue; // same group can't be attacked by multiple groups
             if (other->immunities & g->attack_type) continue; // this group has immunity, don't attack it
-
-#ifdef DEBUG_OUTPUT
-            std::cout << ((g->type == Infection) ? "Infection" : "Immune System") << " group " << g->group_num
-                << " would deal defending group " << other->group_num << " " << other->damage_would_be_dealt(*g) << " damage" << std::endl;
-#endif
             potential_damage[other] = other->damage_would_be_dealt(*g);
         }
 
@@ -108,9 +94,6 @@ bool do_fight() // returns false if nothing changed
             attacked_by[best_target] = g;
         }
     }
-#ifdef DEBUG_OUTPUT
-    std::cout << std::endl;
-#endif
 
     // 2. Attacking
     // Build list of descending order of initiative to launch attacks
@@ -140,9 +123,6 @@ bool do_fight() // returns false if nothing changed
                 attacks_had_effect |= attackee->receive_attack(*attacker);
         }
     }
-#ifdef DEBUG_OUTPUT
-    std::cout << std::endl;
-#endif
 
     // Prune dead groups
     groups.erase(
@@ -152,44 +132,62 @@ bool do_fight() // returns false if nothing changed
     return attacks_had_effect;
 }
 
-unsigned do_part1or2(bool &combat_ended)
+void populate_data(int boost = 0)
 {
-    combat_ended = false;
+    while (!groups.empty())
+    {
+        delete groups.back();
+        groups.pop_back();
+    }
+
+    // Problem input
+    groups.push_back(new Group(ImmuneSystem, 1, 2743, 4149, 13 + boost, Radiation, 14, 0, 0));
+    groups.push_back(new Group(ImmuneSystem, 2, 8829, 7036, 7 + boost, Fire, 15, 0, 0));
+    groups.push_back(new Group(ImmuneSystem, 3, 1928, 10700, 50 + boost, Slashing, 3, Cold, Fire | Radiation | Slashing));
+    groups.push_back(new Group(ImmuneSystem, 4, 6051, 11416, 15 + boost, Bludgeoning, 20, 0, 0));
+    groups.push_back(new Group(ImmuneSystem, 5, 895, 10235, 92 + boost, Bludgeoning, 10, Bludgeoning, Slashing));
+    groups.push_back(new Group(ImmuneSystem, 6, 333, 1350, 36 + boost, Radiation, 12, 0, 0));
+    groups.push_back(new Group(ImmuneSystem, 7, 2138, 8834, 35 + boost, Cold, 11, Bludgeoning, 0));
+    groups.push_back(new Group(ImmuneSystem, 8, 4325, 1648, 3 + boost, Bludgeoning, 8, Cold | Fire, 0));
+    groups.push_back(new Group(ImmuneSystem, 9, 37, 4133, 1055 + boost, Radiation, 1, 0, Radiation | Slashing));
+    groups.push_back(new Group(ImmuneSystem, 10, 106, 3258, 299 + boost, Cold, 13, 0, Slashing | Radiation));
+
+    groups.push_back(new Group(Infection, 1, 262, 8499, 45, Cold, 6, Cold, 0));
+    groups.push_back(new Group(Infection, 2, 732, 47014, 127, Bludgeoning, 17, Cold | Bludgeoning, 0));
+    groups.push_back(new Group(Infection, 3, 4765, 64575, 20, Radiation, 18, 0, 0));
+    groups.push_back(new Group(Infection, 4, 3621, 19547, 9, Cold, 5, 0, Radiation | Cold));
+    groups.push_back(new Group(Infection, 5, 5913, 42564, 14, Slashing, 9, 0, Radiation | Bludgeoning | Fire));
+    groups.push_back(new Group(Infection, 6, 7301, 51320, 11, Fire, 2, Radiation | Fire, Bludgeoning));
+    groups.push_back(new Group(Infection, 7, 3094, 23713, 14, Radiation, 19, Slashing | Fire, 0));
+    groups.push_back(new Group(Infection, 8, 412, 36593, 177, Slashing, 16, Radiation | Bludgeoning, 0));
+    groups.push_back(new Group(Infection, 9, 477, 35404, 146, Cold, 7, 0, 0));
+    groups.push_back(new Group(Infection, 10, 332, 11780, 70, Slashing, 4, Fire, 0));
+}
+
+unsigned do_part1(bool *combat_ended = nullptr, unsigned boost = 0)
+{
+    populate_data(boost);
+
+    if (combat_ended)
+        *combat_ended = false;
 
     for (;;)
     {
-#ifdef DEBUG_OUTPUT
-        auto count = 0;
-        std::cout << "Immune System:" << std::endl;
-        for (auto &g : groups)
-        {
-            if (g->type == Immune)
-                std::cout << "Group " << ++count << " contains " << g->num_units << " unit(s)" << std::endl;
-        }
-
-        count = 0;
-        std::cout << "Infection:" << std::endl;
-        for (auto &g : groups)
-        {
-            if (g->type == Infection)
-                std::cout << "Group " << ++count << " contains " << g->num_units << " unit(s)" << std::endl;
-        }
-        std::cout << std::endl;
-#endif
         // Stalemate
         if (!do_fight())
             return 0;
 
-        bool has_immune = false, has_infection = false;
+        bool has_immune_system = false, has_infection = false;
         for (auto &g : groups)
         {
-            if (g->type == Immune) has_immune = true;
+            if (g->type == ImmuneSystem) has_immune_system = true;
             if (g->type == Infection) has_infection = true;
         }
 
-        if (!has_immune || !has_infection)
+        if (!has_immune_system || !has_infection)
         {
-            combat_ended = true;
+            if (combat_ended)
+                *combat_ended = true;
             break;
         }
     }
@@ -202,96 +200,23 @@ unsigned do_part1or2(bool &combat_ended)
     return total;
 }
 
-// Type type; int units;  int hit_points; int attack_damage; int attack_type; int initiative; int weakness; int immunities;
-
-/*
-Immune System:
-17 units each with 5390 hit points (weak to radiation, bludgeoning) with
-an attack that does 4507 fire damage at initiative 2
-989 units each with 1274 hit points (immune to fire; weak to bludgeoning,
-slashing) with an attack that does 25 slashing damage at initiative 3
-
-Infection:
-801 units each with 4706 hit points (weak to radiation) with an attack
-that does 116 bludgeoning damage at initiative 1
-4485 units each with 2961 hit points (immune to radiation; weak to fire,
-cold) with an attack that does 12 slashing damage at initiative 4
-*/
-
-void populate_data(int boost = 0)
+unsigned do_part2()
 {
-    while (!groups.empty())
+    bool combat_ended = false;
+    auto boost = 1, retval = 0;
+    for (;;)
     {
-        delete groups.back();
-        groups.pop_back();
+        retval = do_part1(&combat_ended, boost);
+        if (combat_ended && groups[0]->type == ImmuneSystem)
+            return retval;
+        boost++;
     }
-
-    // Example input
-    //groups.push_back(new Group(Immune, 1, 17, 5390, 4507, Fire, 2, Radiation | Bludgeoning, 0));
-    //groups.push_back(new Group(Immune, 2, 989, 1274, 25, Slashing, 3, Bludgeoning | Slashing, Fire));
-    //groups.push_back(new Group(Infection, 1, 801, 4706, 116, Bludgeoning, 1, Radiation, 0));
-    //groups.push_back(new Group(Infection, 2, 4485, 2961, 12, Slashing, 4, Fire | Cold, Radiation));
-
-    // Real input
-    // 2743 units each with 4149 hit points with an attack that does 13 radiation damage at initiative 14
-    groups.push_back(new Group(Immune, 1, 2743, 4149, 13 + boost, Radiation, 14, 0, 0));
-    // 8829 units each with 7036 hit points with an attack that does 7 fire damage at initiative 15
-    groups.push_back(new Group(Immune, 2, 8829, 7036, 7 + boost, Fire, 15, 0, 0));
-    // 1928 units each with 10700 hit points (weak to cold; immune to fire, radiation, slashing) with an attack that does 50 slashing damage at initiative 3
-    groups.push_back(new Group(Immune, 3, 1928, 10700, 50 + boost, Slashing, 3, Cold, Fire | Radiation | Slashing));
-    // 6051 units each with 11416 hit points with an attack that does 15 bludgeoning damage at initiative 20
-    groups.push_back(new Group(Immune, 4, 6051, 11416, 15 + boost, Bludgeoning, 20, 0, 0));
-    // 895 units each with 10235 hit points (immune to slashing; weak to bludgeoning) with an attack that does 92 bludgeoning damage at initiative 10
-    groups.push_back(new Group(Immune, 5, 895, 10235, 92 + boost, Bludgeoning, 10, Bludgeoning, Slashing));
-    // 333 units each with 1350 hit points with an attack that does 36 radiation damage at initiative 12
-    groups.push_back(new Group(Immune, 6, 333, 1350, 36 + boost, Radiation, 12, 0, 0));
-    // 2138 units each with 8834 hit points (weak to bludgeoning) with an attack that does 35 cold damage at initiative 11
-    groups.push_back(new Group(Immune, 7, 2138, 8834, 35 + boost, Cold, 11, Bludgeoning, 0));
-    // 4325 units each with 1648 hit points (weak to cold, fire) with an attack that does 3 bludgeoning damage at initiative 8
-    groups.push_back(new Group(Immune, 8, 4325, 1648, 3 + boost, Bludgeoning, 8, Cold | Fire, 0));
-    // 37 units each with 4133 hit points (immune to radiation, slashing) with an attack that does 1055 radiation damage at initiative 1
-    groups.push_back(new Group(Immune, 9, 37, 4133, 1055 + boost, Radiation, 1, 0, Radiation | Slashing));
-    // 106 units each with 3258 hit points (immune to slashing, radiation) with an attack that does 299 cold damage at initiative 13
-    groups.push_back(new Group(Immune, 10, 106, 3258, 299 + boost, Cold, 13, 0, Slashing | Radiation));
-
-    // 262 units each with 8499 hit points (weak to cold) with an attack that does 45 cold damage at initiative 6
-    groups.push_back(new Group(Infection, 1, 262, 8499, 45, Cold, 6, Cold, 0));
-    // 732 units each with 47014 hit points (weak to cold, bludgeoning) with an attack that does 127 bludgeoning damage at initiative 17
-    groups.push_back(new Group(Infection, 2, 732, 47014, 127, Bludgeoning, 17, Cold | Bludgeoning, 0));
-    // 4765 units each with 64575 hit points with an attack that does 20 radiation damage at initiative 18
-    groups.push_back(new Group(Infection, 3, 4765, 64575, 20, Radiation, 18, 0, 0));
-    // 3621 units each with 19547 hit points (immune to radiation, cold) with an attack that does 9 cold damage at initiative 5
-    groups.push_back(new Group(Infection, 4, 3621, 19547, 9, Cold, 5, 0, Radiation | Cold));
-    // 5913 units each with 42564 hit points (immune to radiation, bludgeoning, fire) with an attack that does 14 slashing damage at initiative 9
-    groups.push_back(new Group(Infection, 5, 5913, 42564, 14, Slashing, 9, 0, Radiation | Bludgeoning | Fire));
-    // 7301 units each with 51320 hit points (weak to radiation, fire; immune to bludgeoning) with an attack that does 11 fire damage at initiative 2
-    groups.push_back(new Group(Infection, 6, 7301, 51320, 11, Fire, 2, Radiation | Fire, Bludgeoning));
-    // 3094 units each with 23713 hit points (weak to slashing, fire) with an attack that does 14 radiation damage at initiative 19
-    groups.push_back(new Group(Infection, 7, 3094, 23713, 14, Radiation, 19, Slashing | Fire, 0));
-    // 412 units each with 36593 hit points (weak to radiation, bludgeoning) with an attack that does 177 slashing damage at initiative 16
-    groups.push_back(new Group(Infection, 8, 412, 36593, 177, Slashing, 16, Radiation | Bludgeoning, 0));
-    // 477 units each with 35404 hit points with an attack that does 146 cold damage at initiative 7
-    groups.push_back(new Group(Infection, 9, 477, 35404, 146, Cold, 7, 0, 0));
-    // 332 units each with 11780 hit points (weak to fire) with an attack that does 70 slashing damage at initiative 4*/
-    groups.push_back(new Group(Infection, 10, 332, 11780, 70, Slashing, 4, Fire, 0));
 }
 
 int main()
 {
-    populate_data();
-
-    bool combat_ended = false;
-    auto part1 = do_part1or2(combat_ended);
-
-    auto boost = 1, part2 = 0;
-    for (;;)
-    {
-        populate_data(boost);
-        part2 = do_part1or2(combat_ended);
-        if (combat_ended && groups[0]->type == Immune)
-            break;
-        boost++;
-    }
+    auto part1 = do_part1();
+    auto part2 = do_part2();
 
     std::cout << "Part 1: " << part1 << std::endl;
     std::cout << "Part 2: " << part2 << std::endl;
