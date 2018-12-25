143216ae1b752aad8855362d87f85d0a09131df3
Stephen Houchen <shouchen@outlook.com>
2018-12-24 21:29:05 -0800

    Correct Day 25.

---
+++ aoc2018/Day25/Day25.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cassert>
+
+struct Coords
+{
+    int x, y, z, t;
+};
+
+std::vector<Coords> stars;
+
+auto get_mahnattan(const Coords &p1, const Coords &p2)
+{
+    auto x = p1.x - p2.x; if (x < 0) x = -x;
+    auto y = p1.y - p2.y; if (y < 0) y = -y;
+    auto z = p1.z - p2.z; if (z < 0) z = -z;
+    auto t = p1.t - p2.t; if (t < 0) t = -t;
+
+    return x + y + z + t;
+}
+
+// recur
+std::map<int, std::vector<int>> neighbors;
+std::set<int> stars_merged;
+void walk_neighbors(int index)
+{
+    if (stars_merged.find(index) == stars_merged.end())
+    {
+        stars_merged.insert(index);
+        for (auto i = 0; i < neighbors[index].size(); i++)
+            walk_neighbors(neighbors[index][i]);
+    }
+}
+
+int main()
+{
+    std::ifstream file("input.txt");
+    auto x = 0, y = 0, z = 0, t = 0;
+    auto comma = ',';
+
+    while (file >> x >> comma >> y >> comma >> z >> comma >> t)
+        stars.push_back(Coords{ x, y, z, t });
+
+    for (auto i = 0; i < stars.size(); i++)
+    {
+        for (auto j = 0; j < i; j++)
+        {
+            if (get_mahnattan(stars[i], stars[j]) <= 3)
+            {
+                neighbors[i].push_back(j);
+                neighbors[j].push_back(i);
+            }
+        }
+    }
+
+    auto part1 = 0;
+    for (auto i = 0; i < stars.size(); i++)
+    {
+        if (stars_merged.find(i) != stars_merged.end())
+            continue;
+
+        part1++;
+        walk_neighbors(i);
+    }
+
+    std::cout << "Part 1: " << part1 << std::endl;
+
+    assert(part1 == 375);
+    return 0;
+}
