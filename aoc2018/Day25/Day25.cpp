ed4cc534fd7634fc3abd9403259bc07f5d723441
Stephen Houchen <shouchen@outlook.com>
2018-12-25 07:13:30 -0800

    Final clean-up Day 25

--- aoc2018/Day25/Day25.cpp
+++ aoc2018/Day25/Day25.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <map>
 #include <set>
 #include <cassert>
 
@@ -12,27 +11,19 @@ struct Coords
 };
 
 std::vector<Coords> stars;
-std::map<int, std::vector<int>> neighbors;
-std::set<int> stars_merged;
+std::set<const Coords *> visited;
 
-auto get_manhattan(const Coords &p1, const Coords &p2)
+auto get_dist(const Coords *p1, const Coords *p2)
 {
-    auto x = p1.x - p2.x; if (x < 0) x = -x;
-    auto y = p1.y - p2.y; if (y < 0) y = -y;
-    auto z = p1.z - p2.z; if (z < 0) z = -z;
-    auto t = p1.t - p2.t; if (t < 0) t = -t;
-
-    return x + y + z + t;
+    return abs(p1->x - p2->x) + abs(p1->y - p2->y) + abs(p1->z - p2->z) + abs(p1->t - p2->t);
 }
 
-void walk_neighbors(int index)
+void visit(const Coords *star)
 {
-    if (stars_merged.find(index) == stars_merged.end())
-    {
-        stars_merged.insert(index);
-        for (auto i = 0; i < neighbors[index].size(); i++)
-            walk_neighbors(neighbors[index][i]);
-    }
+    visited.insert(star);
+    for (const auto &other : stars)
+        if (get_dist(star, &other) <= 3 && visited.find(&other) == visited.end())
+            visit(&other);
 }
 
 int main()
@@ -44,30 +35,15 @@ int main()
     while (file >> x >> comma >> y >> comma >> z >> comma >> t)
         stars.push_back(Coords{ x, y, z, t });
 
-    for (auto i = 0; i < stars.size(); i++)
-    {
-        for (auto j = 0; j < i; j++)
-        {
-            if (get_manhattan(stars[i], stars[j]) <= 3)
-            {
-                neighbors[i].push_back(j);
-                neighbors[j].push_back(i);
-            }
-        }
-    }
-
     auto num_constellations = 0;
-    for (auto i = 0; i < stars.size(); i++)
-    {
-        if (stars_merged.find(i) == stars_merged.end())
+    for (const auto &star : stars)
+        if (visited.find(&star) == visited.end())
         {
             num_constellations++;
-            walk_neighbors(i);
+            visit(&star);
         }
-    }
 
     std::cout << "Part 1: " << num_constellations << std::endl;
-
     assert(num_constellations == 375);
     return 0;
 }
