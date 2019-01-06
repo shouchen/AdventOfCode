b947bcd5afbfbfa4e22dadb6028a65793e26fe47
Stephen Houchen <shouchen@outlook.com>
2019-01-05 23:11:15 -0800

    More Day 12 clean-up.

--- aoc2018/Day12/Day12.cpp
+++ aoc2018/Day12/Day12.cpp
@@ -1,39 +1,29 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <numeric>
-#include <stack>
-#include <queue>
 #include <string>
-#include <list>
+#include <vector>
 #include <map>
-#include <deque>
-#include <set>
-#include <algorithm>
 #include <cassert>
 
 using namespace std::string_literals;
+using Pots = std::map<int, char>;
 using Rules = std::vector<std::pair<std::string, char>>;
 
-#define SIZE 20000
-
-void transform(std::map<int, char> &current, const Rules &rules)
+void transform(Pots &current, const Rules &rules)
 {
+    auto min_index = current.begin()->first, max_index = current.rbegin()->first;
     std::map<int, char> next;
 
-    for (auto i = -SIZE; i < SIZE; i++)
-        next[i] = '.';
-
-    for (auto i = -SIZE; i < (SIZE - 1) - 4; i++)
+    for (auto i = min_index - 3; i < max_index; i++)
     {
         for (auto rule : rules)
         {
-            if ((current[i] == '#') == (rule.first[0] == '#') &&
-                current[i + 1] == rule.first[1] &&
-                current[i + 2] == rule.first[2] &&
-                current[i + 3] == rule.first[3] &&
-                current[i + 4] == rule.first[4])
+            if ((current[i + 0] == '#') == (rule.first[0] == '#') &&
+                (current[i + 1] == '#') == (rule.first[1] == '#') &&
+                (current[i + 2] == '#') == (rule.first[2] == '#') &&
+                (current[i + 3] == '#') == (rule.first[3] == '#') &&
+                (current[i + 4] == '#') == (rule.first[4] == '#'))
             {
                 next[i + 2] = rule.second;
                 break;
@@ -59,11 +49,7 @@ void read_input(const std::string &filename, Rules &rules, std::string &initial_
 
 void do_parts(const Rules &rules, const std::string &initial_state, unsigned long long &part1, unsigned long long &part2)
 {
-    std::map<int, char> state;
-
-    for (auto i = -SIZE; i < SIZE; i++)
-        state[i] = '.';
-
+    Pots state;
     for (auto i = 0; i < initial_state.length(); i++)
         state[i] = initial_state[i];
 
@@ -71,25 +57,25 @@ void do_parts(const Rules &rules, const std::string &initial_state, unsigned lon
 
     for (auto i = 0; i <= 120; i++)
     {
-        auto count = 0;
+        auto sum = 0;
         for (auto item : state)
             if (item.second == '#')
-                count += item.first;
+                sum += item.first;
 
         if (i == 20)
-            part1 = count;
+            part1 = sum;
 
-        std::cout << i << ": " << " count = " << count << ", diff from prev = " << (count - prev) << std::endl;
+        std::cout << i << ": " << " sum = " << sum << ", diff from prev = " << (sum - prev) << std::endl;
 
         transform(state, rules);
-        prev = count;
+        prev = sum;
     }
     std::cout << std::endl;
 
     // Observe that for this program input, the count progression soon
     // stabilizes to just adding 62 each generation. Based on this observation,
     // a simple formula can be made to extrapolate the counts for much later
-    // generations (which are otherwise computationally infeasible):
+    // generations:
     //
     // f(x) = 62x + 655, where x >= 98
 
