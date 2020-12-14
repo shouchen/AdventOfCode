3dd152d9a8e2fb47d5a377939e08a776db97eba8
Stephen Houchen <shouchen@outlook.com>
2020-12-13 23:36:51 -0800

    Day 14 solution

---
+++ aoc2020/Day14/Day14.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <cassert>
+
+std::map<unsigned long long, unsigned long long> mem;
+
+void write_memory(unsigned long long addr, unsigned long long value, unsigned long long floats = 0)
+{
+    for (auto i = 1ULL; floats; i <<= 1)
+    {
+        if (i & floats)
+        {
+            floats &= ~i;
+
+            write_memory(addr & ~i, value, floats);
+            write_memory(addr | i, value, floats);
+        }
+    }
+
+    mem[addr] = value;
+}
+
+auto do_part(const std::string &filename, bool part2)
+{
+    mem.clear();
+
+    std::ifstream file(filename);
+    std::string s;
+    auto zeros = 0ULL, ones = 0ULL, exes = 0ULL;
+
+    while (std::getline(file, s))
+    {
+        if (s.substr(0, 7) == "mask = ")
+        {
+            zeros = ones = exes = 0ULL;
+
+            for (auto b = s.begin() + 7; b != s.end(); b++)
+            {
+                zeros = (zeros << 1) | (*b == '0' ? 1 : 0);
+                ones = (ones << 1) | (*b == '1' ? 1 : 0);
+                exes = (exes << 1) | (*b == 'X' ? 1 : 0);
+            }
+        }
+        else
+        {
+            auto addr = atoi(s.c_str() + 4);
+            auto value = atoi(s.c_str() + s.find(" = ") + 3);
+
+            if (part2)
+                write_memory(addr | ones, value, exes);
+            else
+                write_memory(addr, value & ~zeros | ones);
+        }
+    }
+
+    auto sum = 0ULL;
+    for (auto &a : mem)
+        sum += a.second;
+
+    return sum;
+}
+
+int main()
+{
+    auto part1 = do_part("input.txt", false);
+    std::cout << "Part One: " << part1 << std::endl;
+
+    auto part2 = do_part("input.txt", true);
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 11612740949946);
+    assert(part2 == 3394509207186);
+    return 0;
+}
