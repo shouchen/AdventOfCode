664653691ae0a8c3e4ab348f66f5e4d5fa9e446c
Stephen Houchen <shouchen@outlook.com>
2021-12-03 00:20:48 -0800

    Day 3 solutions

---
+++ aoc2021/Day3/Day3.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+auto read_data(const std::string &filename)
+{
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
+    std::string s;
+
+    while (file >> s)
+        lines.push_back(s);
+
+    return lines;
+}
+
+auto do_ocr_or_csr_reduction_step(const std::vector<std::string> &lines, int pos, bool ocr)
+{
+    std::vector<std::string> retval;
+    auto ones = 0, zeros = 0;
+
+    for (auto &s : lines)
+        if (s[pos] == '1') ones++; else zeros++;
+
+    for (auto &line : lines)
+    {
+        if ((line[pos] == '1' && (ocr ^ (ones < zeros))) ||
+            (line[pos] == '0' && (ocr ^ (ones >= zeros))))
+        {
+            retval.push_back(line);
+        }
+    }
+    
+    return retval;
+}
+
+auto do_part1(const std::vector<std::string> &lines)
+{
+    std::string gamma, epsilon;
+
+    for (auto i = 0U; i < lines[0].length(); i++)
+    {
+        auto ones = 0, zeros = 0;
+        for (auto &s : lines)
+            if (s[i] == '1') ones++; else zeros++;
+
+        gamma.push_back((ones > zeros) ? '1' : '0');
+        epsilon.push_back((ones < zeros) ? '1' : '0');
+    }
+
+    return stoi(gamma, nullptr, 2) * stoi(epsilon, nullptr, 2);
+}
+
+auto do_part2(std::vector<std::string> lines, bool ocr)
+{
+    for (int i = 0; lines.size() > 1; i++)
+        lines = do_ocr_or_csr_reduction_step(lines, i, ocr);
+
+    return stoi(lines[0], nullptr, 2);
+}
+
+int main()
+{
+    auto lines = read_data("input.txt");
+
+    auto part1 = do_part1(lines);
+    std::cout << "Part One: " << part1 << std::endl;
+
+    auto part2 = do_part2(lines, true) * do_part2(lines, false);
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 749376);
+    assert(part2 == 2372923);
+    return 0;
+}
