ce64fda383cef8506947d0cefe39e2ee42897edd
Stephen Houchen <shouchen@outlook.com>
2021-12-12 22:57:19 -0800

    Cleaned up Day 13 solutions.

--- aoc2021/Day13/Day13.cpp
+++ aoc2021/Day13/Day13.cpp
@@ -1,82 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <map>
+#include <set>
+#include <vector>
 #include <string>
 #include <cassert>
 
-std::map<std::pair<int, int>, char> grid;
-int max_x = INT_MIN, max_y = INT_MIN;
+using Dots = std::set<std::pair<int, int>>;
 
-void fold_up(int axis)
-{
-    int from = axis + 1, to = axis - 1;
-    while (to >= 0)
-    {
-        for (int x = 0; x <= max_x; x++)
-            if (grid[std::make_pair(x, from)] == '#')
-                grid[std::make_pair(x, to)] = '#';
-        from++;
-        to--;
-    }
-
-    for (int y = axis; y <= max_y; y++)
-        for (int x = 0; x <= max_x; x++)
-            grid.erase(std::make_pair(x, y));
-
-    max_y = axis - 1;
-}
-
-void fold_left(int axis)
-{
-    int from = axis + 1, to = axis - 1;
-    while (to >= 0)
-    {
-        for (int y = 0; y <= max_y; y++)
-            if (grid[std::make_pair(from, y)] == '#')
-                grid[std::make_pair(to, y)] = '#';
-        from++;
-        to--;
-    }
-
-    for (int x = axis; x <= max_x; x++)
-        for (int y = 0; y <= max_y; y++)
-            grid.erase(std::make_pair(x, y));
-
-    max_x = axis - 1;
-}
-
-void dump()
-{
-    for (int y = 0; y <= max_y; y++)
-    {
-        for (int x = 0; x <= max_x; x++)
-            std::cout << ((grid[std::make_pair(x, y)] == '#') ? '#' : '.');
-
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-int count()
-{
-    int count = 0;
-    for (int y = 0; y <= max_y; y++)
-    {
-        for (int x = 0; x <= max_x; x++)
-        {
-            if (grid[std::make_pair(x, y)] == '#')
-                count++;
-        }
-    }
-    return count;
-}
+Dots dots;
 
 auto process_input(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line, fold, along;
-    auto part1 = -1, x = 0, y = 0, amount = 0;
+    auto part1 = -1, x = 0, y = 0, crease = 0;
     auto comma = ',', axis = 'x', equals = '=';
 
     while (getline(file, line) && line.length())
@@ -84,27 +22,57 @@ auto process_input(const std::string &filename)
         std::istringstream ss(line);
         ss >> x >> comma >> y;
 
-        grid[std::make_pair(x, y)] = '#';
-
-        if (x > max_x) max_x = x;
-        if (y > max_y) max_y = y;
+        dots.insert(std::make_pair(x, y));
     }
 
     while (getline(file, line))
     {
         std::istringstream ss(line);
-        ss >> fold >> along >> axis >> equals >> amount;
+        ss >> fold >> along >> axis >> equals >> crease;
 
+        Dots next;
         if (axis == 'y')
-            fold_up(amount);
+        {
+            for (auto &dot : dots)
+                if (dot.second < crease)
+                    next.insert(std::make_pair(dot.first, dot.second));
+                else
+                    next.insert(std::make_pair(dot.first, crease * 2 - dot.second));
+        }
         else
-            fold_left(amount);
+        {
+            for (auto &dot : dots)
+                if (dot.first < crease)
+                    next.insert(std::make_pair(dot.first, dot.second));
+                else
+                    next.insert(std::make_pair(crease * 2 - dot.first, dot.second));
+        }
+        dots = next;
 
         if (part1 == -1)
-            part1 = count();
+            part1 = int(dots.size());
     }
 
     return part1;
+}
+
+void dump()
+{
+    auto max_x = INT_MIN, max_y = INT_MIN;
+    for (auto &dot : dots)
+    {
+        if (dot.first > max_x) max_x = dot.first;
+        if (dot.second > max_y) max_y = dot.second;
+    }
+
+    for (auto y = 0; y <= max_y; y++)
+    {
+        for (auto x = 0; x <= max_x; x++)
+            std::cout << (dots.find(std::make_pair(x, y)) == dots.end() ? '.' : '#');
+
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 int main()
