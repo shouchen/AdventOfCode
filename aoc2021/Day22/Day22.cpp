552fa05dd44bc05412a5d2871cb88db340edde72
Stephen Houchen <shouchen@outlook.com>
2021-12-22 00:27:34 -0800

    Some Day 22 clean-up.

--- aoc2021/Day22/Day22.cpp
+++ aoc2021/Day22/Day22.cpp
@@ -1,124 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <set>
+#include <vector>
 #include <algorithm>
 #include <cassert>
-#include "..\Utils\Utils.h"
-
-set<tuple<int, int, int>> grid;
-
-auto do_part1(const std::string &filename)
-{
-    std::ifstream file("input.txt");
-    std::string player, n, starting, position, line, on_off;
-    auto input = std::make_pair(0, 0);
-    char c;
-
-    int x1, x2, y1, y2, z1, z2;
-    while (file >> on_off >> c >> c >> x1 >> c >> c >> x2 >> c >> c >> c >> y1 >> c >> c >> y2 >> c >> c >> c >> z1 >> c >> c >> z2)
-    {
-        if (x1 < -50) continue;
-        if (y1 < -50) continue;
-        if (z1 < -50) continue;
-        if (x1 > 50) continue;
-        if (y1 > 50) continue;
-        if (z1 > 50) continue;
-
-        for (int x = x1 ; x <= x2; x++)
-            for (int y = y1; y <= y2; y++)
-                for (int z = z1; z <= z2; z++)
-                {
-                    if (on_off == "on")
-                        grid.insert(make_tuple(x, y, z));
-                    else if (on_off == "off")
-                        grid.erase(make_tuple(x, y, z));
-                    else assert(false);
-                }
-    }
-
-    return grid.size();
-}
-
-struct Cuboid
-{
-    pair<long long, long long> x, y, z;
-};
 
 struct InputStep
 {
     bool on;
-    int xmin, xmax, ymin, ymax, zmin, zmax;
+    int x_min, x_max, y_min, y_max, z_min, z_max;
 };
 
+std::vector<InputStep> steps;
 
-long long on_count = 0;
-
-vector<Cuboid> cuboids;
-
-bool contains(Cuboid &a, Cuboid &b)
+void read_input(const std::string &filename)
 {
-    return
-        a.x.first <= b.x.first && a.x.second >= b.x.second &&
-        a.y.first <= b.y.first && a.y.second >= b.y.second &&
-        a.z.first <= b.z.first && a.z.second >= b.z.second;
-}
-
-bool intersects(Cuboid &a, Cuboid &b)
-{
-    return
-        a.x.first <= b.x.second && a.x.second >= b.x.first &&
-        a.y.first <= b.y.second && a.y.second >= b.y.first &&
-        a.z.first <= b.z.second && a.z.second >= b.z.first;
-}
-
-// if second < first, not an intersection
-pair<int, int> range_intersection(const pair<int, int> &r1, const pair<int, int> &r2)
-{
-    pair<int, int> retval = make_pair(std::max(r1.first, r2.first), std::min(r1.second, r2.second));
-    return retval;
-}
-
-long long cuboid_intersection(const Cuboid &c1, const Cuboid &c2)
-{
-    auto ri_x = range_intersection(c1.x, c2.x);
-    auto ri_y = range_intersection(c1.y, c2.y);
-    auto ri_z = range_intersection(c1.z, c2.z);
-
-    if (ri_x.first <= ri_x.second && ri_y.first <= ri_y.second && ri_z.first <= ri_z.second)
-        return (ri_x.second - ri_x.first + 1) * (ri_y.second - ri_y.first + 1) * (ri_z.second - ri_z.first + 1);
-    else
-        return 0;
-}
-
-auto count()
-{
-    long long retval = 0;
-    for (auto &c : cuboids)
-        retval += (c.x.second - c.x.first + 1) * (c.y.second - c.y.first + 1) * (c.z.second - c.z.first + 1);
-
-    return retval;
-}
-
-void merge_on_cuboid(Cuboid cuboid)
-{
-    if (cuboids.size() == 0)
-    {
-        on_count += (cuboid.x.second - cuboid.x.first + 1) * (cuboid.y.second - cuboid.y.first + 1) * (cuboid.z.second - cuboid.z.first + 1);
-        cuboids.push_back(cuboid);
-        cout << count() << endl;
-    }
-    else
-    {
-        for (auto &c : cuboids)
-        {
-            auto result = (cuboid_intersection(cuboid, c));
-        }
-    }
-}
-
-auto do_part2(const std::string &filename)
-{
-    std::vector<InputStep> steps;
     std::vector<int> x_seam, y_seam, z_seam;
 
     std::string s, on_off;
@@ -127,47 +24,75 @@ auto do_part2(const std::string &filename)
     std::ifstream file(filename);
 
     InputStep step;
-    while (file >> on_off >> x >> equals >> step.xmin >> dot >> dot >> step.xmax >> comma >>
-        y >> equals >> step.ymin >> dot >> dot >> step.ymax >> comma >>
-        z >> equals >> step.zmin >> dot >> dot >> step.zmax)
+    while (file >> on_off >> x >> equals >> step.x_min >> dot >> dot >> step.x_max >> comma >>
+        y >> equals >> step.y_min >> dot >> dot >> step.y_max >> comma >>
+        z >> equals >> step.z_min >> dot >> dot >> step.z_max)
     {
         step.on = on_off == "on";
         steps.push_back(step);
+    }
+}
 
-        x_seam.push_back(step.xmin); x_seam.push_back(step.xmax + 1);
-        y_seam.push_back(step.ymin); y_seam.push_back(step.ymax + 1);
-        z_seam.push_back(step.zmin); z_seam.push_back(step.zmax + 1);
+auto do_part1(void)
+{
+    std::set<std::tuple<int, int, int>> grid;
+    for (auto &step : steps)
+        if (std::abs(step.x_min) <= 50 && std::abs(step.y_min) <= 50 && std::abs(step.z_min) <= 50)
+            for (auto x = step.x_min; x <= step.x_max; x++)
+                for (auto y = step.y_min; y <= step.y_max; y++)
+                    for (auto z = step.z_min; z <= step.z_max; z++)
+                        if (step.on)
+                            grid.insert(std::make_tuple(x, y, z));
+                        else
+                            grid.erase(std::make_tuple(x, y, z));
+
+    return grid.size();
+}
+
+auto do_part2()
+{
+    // Find all the "seams" for each axis (x, y, z boundaries where on/off could possibly change)
+    std::vector<int> x_seam, y_seam, z_seam;
+    for (auto &step : steps)
+    {
+        x_seam.push_back(step.x_min); x_seam.push_back(step.x_max + 1);
+        y_seam.push_back(step.y_min); y_seam.push_back(step.y_max + 1);
+        z_seam.push_back(step.z_min); z_seam.push_back(step.z_max + 1);
     }
 
+    // Sort the seams on each axis
     sort(x_seam.begin(), x_seam.end());
     sort(y_seam.begin(), y_seam.end());
     sort(z_seam.begin(), z_seam.end());
 
-    std::vector<std::vector<std::vector<int>>> cubettes;
-    cubettes.resize(x_seam.size(), std::vector<std::vector<int>>(y_seam.size(), std::vector<int>(z_seam.size())));
+    // Use the seams to partition 3D space, where each point in the partition has same on/off
+    std::vector<std::vector<std::vector<bool>>> cubettes(
+        x_seam.size(),
+        std::vector<
+            std::vector<bool>>(
+                y_seam.size(),
+                std::vector<bool>(
+                    z_seam.size())));
 
-    //std::map<std::tuple<int, int, int>, bool> region;
-
+    // Play back the input steps to determine the on/off for each partition
     for (auto &step : steps)
     {
-        auto x_min = lower_bound(x_seam.begin(), x_seam.end(), step.xmin) - x_seam.begin();
-        auto x_max = lower_bound(x_seam.begin(), x_seam.end(), step.xmax + 1) - x_seam.begin();
+        auto x_min = lower_bound(x_seam.begin(), x_seam.end(), step.x_min) - x_seam.begin();
+        auto x_max = lower_bound(x_seam.begin(), x_seam.end(), step.x_max + 1) - x_seam.begin();
 
-        auto y_min = lower_bound(y_seam.begin(), y_seam.end(), step.ymin) - y_seam.begin();
-        auto y_max = lower_bound(y_seam.begin(), y_seam.end(), step.ymax + 1) - y_seam.begin();
+        auto y_min = lower_bound(y_seam.begin(), y_seam.end(), step.y_min) - y_seam.begin();
+        auto y_max = lower_bound(y_seam.begin(), y_seam.end(), step.y_max + 1) - y_seam.begin();
 
-        auto z_min = lower_bound(z_seam.begin(), z_seam.end(), step.zmin) - z_seam.begin();
-        auto z_max = lower_bound(z_seam.begin(), z_seam.end(), step.zmax + 1) - z_seam.begin();
+        auto z_min = lower_bound(z_seam.begin(), z_seam.end(), step.z_min) - z_seam.begin();
+        auto z_max = lower_bound(z_seam.begin(), z_seam.end(), step.z_max + 1) - z_seam.begin();
 
         for (auto i = x_min; i < x_max; i++)
             for (auto j = y_min; j < y_max; j++)
                 for (auto k = z_min; k < z_max; k++)
-                {
                     cubettes[i][j][k] = step.on;
-                    //region[make_tuple(i, j, k)] = step.on;
-                }
     }
 
+    // For all partitions that are "on", sum the volume of points they contain
     auto retval = 0LL;
     for (auto i = 0; i < x_seam.size() - 1; i++)
         for (auto j = 0; j < y_seam.size() - 1; j++)
@@ -180,10 +105,12 @@ auto do_part2(const std::string &filename)
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    read_input("input.txt");
+
+    auto part1 = do_part1();
     std::cout << "Part 1: " << part1 << std::endl;
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = do_part2();
     std::cout << "Part 2: " << part2 << std::endl;
 
     assert(part1 == 587097);
