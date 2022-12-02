a1f8d7ed6b7b2e8cce7f5a6d63b485cf1061a461
Stephen Houchen <shouchen@outlook.com>
2022-12-02 07:04:55 -0800

    Final Day 2

--- aoc2022/Day2/Day2.cpp
+++ aoc2022/Day2/Day2.cpp
@@ -2,37 +2,17 @@
 #include <fstream>
 #include <cassert>
 
-enum class Option { Rock = 1, Paper = 2, Scissors = 3 };
-enum class Outcome { Lose = 0, Draw = 3, Win = 6 };
+enum Play { Rock = 1, Paper = 2, Scissors = 3 };
+enum Outcome { Lose = 0, Draw = 3, Win = 6 };
 
-auto map_ABC_to_option(char abc)
+auto get_outcome(Play them, Play me)
 {
-    static const Option options[] = { Option::Rock, Option::Paper, Option::Scissors };
-    return options[abc - 'A'];
+    return Outcome((2 * them + me + 1) % 3 * 3);
 }
 
-auto map_XYZ_to_option(char xyz)
+auto get_play(Play them, Outcome outcome)
 {
-    static const Option option[] = { Option::Rock, Option::Paper, Option::Scissors };
-    return option[xyz - 'X'];
-}
-
-auto map_XYZ_to_outcome(char xyz)
-{
-    static const Outcome outcome[] = { Outcome::Lose, Outcome::Draw, Outcome::Win };
-    return outcome[xyz - 'X'];
-}
-
-auto get_outcome(Option they_play, Option i_play)
-{
-    auto x = (2 * unsigned(they_play) + unsigned(i_play) + 1) % 3 * 3;
-    return Outcome(x);
-}
-
-auto get_my_option(Option they_play, Outcome outcome)
-{
-    auto x = (unsigned(they_play) + unsigned(outcome) / 3 + 1) % 3 + 1;
-    return Option(x);
+    return Play((them + outcome / 3 + 1) % 3 + 1);
 }
 
 void process_input(const std::string &filename, unsigned &part1, unsigned &part2)
@@ -40,17 +20,14 @@ void process_input(const std::string &filename, unsigned &part1, unsigned &part2
     std::ifstream file(filename);
     auto abc = ' ', xyz = ' ';
     
-    part1 = part2 = 0;
-
     while (file >> abc >> xyz)
     {
-        auto they_play = map_ABC_to_option(abc), i_play = map_XYZ_to_option(xyz);
-        auto outcome = get_outcome(they_play, i_play);
-        part1 += unsigned(i_play) + unsigned(outcome);
+        auto them = Play(abc - 'A' + 1);
+        auto me = Play(xyz - 'X' + 1);
+        part1 += me + get_outcome(them, me);
 
-        outcome = map_XYZ_to_outcome(xyz);
-        they_play = map_ABC_to_option(abc), i_play = get_my_option(they_play, outcome);
-        part2 += unsigned(i_play) + unsigned(outcome);
+        auto outcome = Outcome((xyz - 'X') * 3);
+        part2 += get_play(them, outcome) + outcome;
     }
 }
 
