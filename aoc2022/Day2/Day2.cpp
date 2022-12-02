bf618729ac4535566e1394f32304bed60fbbf2c0
Stephen Houchen <shouchen@outlook.com>
2022-12-02 06:38:03 -0800

    Day 2 fun with math

--- aoc2022/Day2/Day2.cpp
+++ aoc2022/Day2/Day2.cpp
@@ -23,58 +23,16 @@ auto map_XYZ_to_outcome(char xyz)
     return outcome[xyz - 'X'];
 }
 
-auto get_my_outcome(Option they_play, Option i_play)
+auto get_outcome(Option they_play, Option i_play)
 {
-    switch (they_play)
-    {
-    case Option::Rock:
-        if (i_play == Option::Paper)
-            return Outcome::Win;
-        if (i_play == Option::Scissors)
-            return Outcome::Lose;
-        break;
-    case Option::Paper:
-        if (i_play == Option::Scissors)
-            return Outcome::Win;
-        if (i_play == Option::Rock)
-            return Outcome::Lose;
-        break;
-    case Option::Scissors:
-        if (i_play == Option::Rock)
-            return Outcome::Win;
-        if (i_play == Option::Paper)
-            return Outcome::Lose;
-        break;
-    }
-
-    return Outcome::Draw;
+    auto x = (2 * unsigned(they_play) + unsigned(i_play) + 1) % 3 * 3;
+    return Outcome(x);
 }
 
 auto get_my_option(Option they_play, Outcome outcome)
 {
-    switch (they_play)
-    {
-    case Option::Rock:
-        if (outcome == Outcome::Draw)
-            return Option::Rock;
-        if (outcome == Outcome::Win)
-            return Option::Paper;
-        break;
-    case Option::Paper:
-        if (outcome == Outcome::Lose)
-            return Option::Rock;
-        if (outcome == Outcome::Draw)
-            return Option::Paper;
-        break;
-    case Option::Scissors:
-        if (outcome == Outcome::Win)
-            return Option::Rock;
-        if (outcome == Outcome::Lose)
-            return Option::Paper;
-        break;
-    }
-
-    return Option::Scissors;
+    auto x = (unsigned(they_play) + unsigned(outcome) / 3 + 1) % 3 + 1;
+    return Option(x);
 }
 
 void process_input(const std::string &filename, unsigned &part1, unsigned &part2)
@@ -87,7 +45,7 @@ void process_input(const std::string &filename, unsigned &part1, unsigned &part2
     while (file >> abc >> xyz)
     {
         auto they_play = map_ABC_to_option(abc), i_play = map_XYZ_to_option(xyz);
-        auto outcome = get_my_outcome(they_play, i_play);
+        auto outcome = get_outcome(they_play, i_play);
         part1 += unsigned(i_play) + unsigned(outcome);
 
         outcome = map_XYZ_to_outcome(xyz);
@@ -100,6 +58,9 @@ int main()
 {
     auto part1 = 0U, part2 = 0U;
     process_input("input.txt", part1, part2);
+
+    std::cout << "Part One: " << part1 << std::endl;
+    std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 10718);
     assert(part2 == 14652);
