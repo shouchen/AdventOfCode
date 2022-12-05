18f525a2cf57ffa4d90894404a605cbbae32bae9
Stephen Houchen <shouchen@outlook.com>
2022-12-05 07:17:35 -0800

    Day 5 clean-up

--- aoc2022/Day5/Day5.cpp
+++ aoc2022/Day5/Day5.cpp
@@ -2,72 +2,54 @@
 #include <fstream>
 #include <cassert>
 #include <string>
-#include <vector>
+#include <array>
 #include <queue>
+#include <algorithm>
 
-typedef void (MoveFunc)(std::vector<std::deque<char>> &data, int quantity, int from, int to);
-
-void move_part1(std::vector<std::deque<char>> &data, int quantity, int from, int to)
+auto process_data(const std::string &filename, bool part2)
 {
-    for (auto i = 0; i < quantity; i++)
-    {
-        auto temp = data[from].front();
-        data[from].pop_front();
-        data[to].push_front(temp);
-    }
-}
-
-void move_part2(std::vector<std::deque<char>> &data, int quantity, int from, int to)
-{
-    std::string qqq;
-
-    for (auto i = 0; i < quantity; i++)
-    {
-        auto temp = data[from].front();
-        data[from].pop_front();
-        qqq.push_back(temp);
-    }
-
-    for (auto j = qqq.rbegin(); j != qqq.rend(); j++)
-        data[to].push_front(*j);
-}
-
-auto process_data(const std::string &filename, MoveFunc move_func)
-{
-    std::vector<std::deque<char>> data;
-    for (auto i = 0; i < 1000; i++)
-        data.push_back(std::deque<char>());
-
+    std::array<std::deque<char>, 10> data;
     std::ifstream file(filename);
     std::string line;
 
     while (std::getline(file, line) && line[1] != '1')
-        for (auto i = 0; i < line.length(); i += 4)
-            if (line[i] == '[')
-            {
-                char l = line[i + 1];
-                data[i / 4 + 1].push_back(l);
-            }
+        for (auto i = 1, stack = 1; i < line.length(); i += 4, stack++)
+            if (line[i] != ' ')
+                data[stack].push_back(line[i]);
 
     std::string move, from, to;
-    int quantity = 0, from_stack = 0, to_stack = 0;
+    auto quantity = 0, from_stack = 0, to_stack = 0;
+
     while (file >> move >> quantity >> from >> from_stack >> to >> to_stack)
-        move_func(data, quantity, from_stack, to_stack);
+    {
+        std::string removed_creates;
+
+        for (auto i = 0; i < quantity; i++)
+        {
+            removed_creates.push_back(data[from_stack].front());
+            data[from_stack].pop_front();
+        }
+
+        if (part2)
+            std::reverse(removed_creates.begin(), removed_creates.end());
+
+        for (auto j = removed_creates.begin(); j != removed_creates.end(); j++)
+            data[to_stack].push_front(*j);
+    }
 
     std::string retval;
-    for (auto i = 1; i < 100; i++)
-        if (!data[i].empty())
-            retval += data[i].front();
+    for (auto i = 1; i < data.size(); i++)
+        retval.push_back(data[i].front());
 
     return retval;
 }
 
 int main()
 {
-    auto part1 = process_data("input.txt", move_part1);
+    auto part1 = process_data("input.txt", false);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = process_data("input.txt", move_part2);
+    auto part2 = process_data("input.txt", true);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == "HBTMTBSDC");
