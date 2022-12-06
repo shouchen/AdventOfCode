0fb5c9ed03764c3dcbe4f37fd03305ee6d34daaf
Stephen Houchen <shouchen@outlook.com>
2022-12-05 21:49:52 -0800

    Day 6 solutions, created day 25 as a template.

---
+++ aoc2022/Day25/Day25.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <string>
+#include <array>
+#include <queue>
+#include <map>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+
+using namespace std;
+
+auto process_data(const std::string &filename, bool part2)
+{
+    std::array<std::deque<char>, 10> data;
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line) && line[1] != '1')
+        for (auto i = 1, stack = 1; i < line.length(); i += 4, stack++)
+            if (line[i] != ' ')
+                data[stack].push_back(line[i]);
+
+    std::string move, from, to;
+    auto quantity = 0, from_stack = 0, to_stack = 0;
+
+    while (file >> move >> quantity >> from >> from_stack >> to >> to_stack)
+    {
+        std::deque<char> removed_crates;
+
+        for (auto i = 0; i < quantity; i++)
+        {
+            auto crate = data[from_stack].front();
+            data[from_stack].pop_front();
+
+            part2 ? removed_crates.push_front(crate) : removed_crates.push_back(crate);
+        }
+
+        for (auto i = removed_crates.begin(); i != removed_crates.end(); i++)
+            data[to_stack].push_front(*i);
+    }
+
+    std::string retval;
+    for (auto i = 1; i < data.size(); i++)
+        retval.push_back(data[i].front());
+
+    return retval;
+}
+
+auto read_file_line_by_line(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (getline(file, line))
+    {
+        cout << line << endl;
+
+        stringstream ss(line);
+        string move;
+
+        if (ss >> move && move == "move")
+            cout << "YES!" << endl;
+    }
+}
+
+int main()
+{
+    read_file_line_by_line("input.txt");
+
+    auto part1 = process_data("input.txt", false);
+    std::cout << "Part One: " << part1 << std::endl;
+
+    auto part2 = process_data("input.txt", true);
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == "HBTMTBSDC");
+    assert(part2 == "PQTJRSHWS");
+    return 0;
+}
---
+++ aoc2022/Day6/Day6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+
+auto any_repeats(const std::string &s, int start, int end)
+{
+    if (start >= end)
+        return false;
+
+    for (auto other = start + 1; other <= end; other++)
+        if (s[start] == s[other])
+            return true;
+
+    return any_repeats(s, start + 1, end);
+}
+
+auto process_data(const std::string &filename, int n)
+{
+    std::ifstream file(filename);
+    std::string line;
+    file >> line;
+
+    auto start = 0, end = n - 1;
+    while (any_repeats(line, start, end))
+        start++, end++;
+
+    return end + 1;
+}
+
+int main()
+{
+    auto part1 = process_data("input.txt", 4);
+    std::cout << "Part One: " << part1 << std::endl;
+
+    auto part2 = process_data("input.txt", 14);
+    std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 1042);
+    assert(part2 == 2980);
+    return 0;
+}
