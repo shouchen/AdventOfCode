25a74d5603a515ee020cdf4ae8ec5f8b40d10efc
Stephen Houchen <shouchen@outlook.com>
2022-12-09 22:19:47 -0800

    Day 10 clean-up

--- aoc2022/Day10/Day10.cpp
+++ aoc2022/Day10/Day10.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cassert>
-#include <string>
 
-auto x = 1;
-
-auto inc_cycle_and_check(int &cycle)
+void update_ss(int &ss, int cycle, int x)
 {
-    ++cycle;
-    return ((cycle + 20) % 40 == 0);
+    if ((cycle + 20) % 40 == 0)
+        ss += cycle * x;
 }
 
 auto do_part1(const std::string &filename)
 {
     std::ifstream file(filename);
-    std::string line, instr;
-    auto operand = 0, cycle = 0, ss = 0;
+    std::string instruction;
+    auto operand = 0, ss = 0, cycle = 1, x = 1;
 
-    while (file >> instr)
+    while (file >> instruction)
     {
-        if (inc_cycle_and_check(cycle))
-            ss += cycle * x;
-
-        if (instr == "noop")
-            continue;
-
-        if (instr == "addx")
+        update_ss(ss, cycle++, x);
+        if (instruction == "addx")
         {
+            update_ss(ss, cycle++, x);
             file >> operand;
-            if (inc_cycle_and_check(cycle))
-                ss += cycle * x;
-
             x += operand;
         }
     }
@@ -38,49 +29,47 @@ auto do_part1(const std::string &filename)
     return ss;
 }
 
-std::string screen = "................................................................................................................................................................................................................................................";
-
-void inc_crt_and_check(int &crt, int cycle)
+void update_screen(std::string &screen, int crt, int x)
 {
-    cycle++;
-
     auto temp = crt % 40;
 
     if (x == temp - 1 || x == temp || x == temp + 1)
         screen[crt] = '#';
-
-    if (++crt == 240) crt = 0;
 }
 
-void do_part2(const std::string &filename)
+auto format_screen(const std::string &screen)
 {
-    std::ifstream file(filename);
-    std::string line, instr;
-    auto operand = 0, crt = 0, cycle = 0;
-
-    x = 1;
-
-    while (file >> instr)
-    {
-        if (instr == "noop")
-        {
-            inc_crt_and_check(crt, cycle);
-        }
-        else if (instr == "addx")
-        {
-            file >> operand;
-            inc_crt_and_check(crt, cycle);
-            inc_crt_and_check(crt, cycle);
-            x += operand;
-        }
-    }
+    std::stringstream ss;
 
     for (auto i = 0; i < 240; i++)
     {
         if (i % 40 == 0)
-            std::cout << std::endl;
-        std::cout << screen[i];
+            ss << std::endl;
+        ss << screen[i];
     }
+
+    return ss.str();
+}
+
+auto do_part2(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::string instruction;
+    auto operand = 0, crt = 0, x = 1;
+    std::string screen(240, '.');
+
+    while (file >> instruction)
+    {
+        update_screen(screen, crt++, x);
+        if (instruction == "addx")
+        {
+            update_screen(screen, crt++, x);
+            file >> operand;
+            x += operand;
+        }
+    }
+
+    return format_screen(screen);
 }
 
 int main()
@@ -88,10 +77,10 @@ int main()
     auto part1 = do_part1("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
 
-    std::cout << "Part Two: ";
-    do_part2("input.txt");
+    auto part2 = do_part2("input.txt");
+    std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 13180);
-    // part2 displays "EZFCHJAB"
+    // part2 displays "EZFCHJAB" in ASCII art
     return 0;
 }
