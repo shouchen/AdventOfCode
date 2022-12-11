f7a11a0ab8cb5970f13e7629fe43783bcd2dd3a5
Stephen Houchen <shouchen@outlook.com>
2022-12-10 22:14:12 -0800

    Day 11: Merge part1 and part2 into one solution

--- aoc2022/Day11/Day11.cpp
+++ aoc2022/Day11/Day11.cpp
@@ -1,177 +1,90 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <cassert>
 #include <string>
-#include <array>
-#include <queue>
 #include <map>
-#include <stack>
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <numeric>
-#include <limits>
-
-using namespace std;
 
 struct Monkey
 {
-    int id;
-    vector<long long> items;
+    std::vector<long long> items;
     char operation;
-    string operand;
+    std::string operand;
     long long divisible_by;
     int true_throw_to, false_throw_to;
 };
 
-auto do_part1(const std::string &filename)
+auto process_input(const std::string &filename, bool divide_by_three, int rounds)
 {
-    vector<Monkey> monkeys;
-    map<int, long long> monkey_count;
-
     std::ifstream file(filename);
-    std::string monkey_str, starting, items_colon, temp, new_str, old, test_colon, if_str, true_colon, false_colon, throw_str, to, divisible, by;
-    int id;
-    char colon, equals;
+    std::string monkey, starting, items_colon, s, new_str, old, test_colon, divisible, by, if_str, true_colon, false_colon, throw_str, to;
+    auto id = 0;
+    auto colon = ':', equals = '=';
+    std::vector<Monkey> monkeys;
 
-    while (file >> monkey_str >> id >> colon)
+    while (file >> monkey >> id >> colon >> starting >> items_colon)
     {
-        Monkey monkey{ id };
+        Monkey m;
 
-        file >> starting >> items_colon;
-        while (file >> temp)
-        {
-            if (temp == "Operation:")
-                break;
+        while (file >> s && s != "Operation:")
+            m.items.push_back(atoi(s.c_str()));
 
-            monkey.items.push_back(atoi(temp.c_str()));
-        }
+        file >> new_str >> equals >> old >> m.operation >> m.operand;
+        file >> test_colon >> divisible >> by >> m.divisible_by;
+        file >> if_str >> true_colon >> throw_str >> to >> monkey >> m.true_throw_to;
+        file >> if_str >> false_colon >> throw_str >> to >> monkey >> m.false_throw_to;
 
-        file >> new_str >> equals >> old >> monkey.operation >> monkey.operand;
-        file >> test_colon >> divisible >> by >> monkey.divisible_by;
-        file >> if_str >> true_colon >> throw_str >> to >> monkey_str >> monkey.true_throw_to;
-        file >> if_str >> false_colon >> throw_str >> to >> monkey_str >> monkey.false_throw_to;
-
-        monkeys.push_back(monkey);
+        monkeys.push_back(m);
     }
 
-    for (int round = 1; round <= 20; round++)
-    {
-        for (Monkey &m : monkeys)
-        {
-            for (auto &i : m.items)
-            {
-                monkey_count[m.id]++;
-
-                auto temp = i;
-                if (m.operation == '*')
-                    temp *= ((m.operand == "old") ? temp : atoi(m.operand.c_str()));
-                else if (m.operation == '+')
-                    temp += ((m.operand == "old") ? temp : atoi(m.operand.c_str()));
-                else
-                    assert(false);
-
-                temp /= 3;
-                if ((temp % m.divisible_by) == 0)
-                    monkeys[m.true_throw_to].items.push_back(temp);
-                else
-                    monkeys[m.false_throw_to].items.push_back(temp);
-            }
-
-            m.items.clear();
-        }
-    }
-
-    vector<long long> counts;
-    for (auto &i : monkey_count)
-        counts.push_back(i.second);
-
-    sort(counts.begin(), counts.end());
-
-    auto part1 = counts[counts.size() - 1] * counts[counts.size() - 2];
-    return part1;
-}
-
-auto do_part2(const std::string &filename)
-{
-    vector<Monkey> monkeys;
-    map<int, long long> monkey_count;
-
-    std::ifstream file(filename);
-    std::string monkey_str, starting, items_colon, temp, new_str, old, test_colon, if_str, true_colon, false_colon, throw_str, to, divisible, by;
-    int id;
-    char colon, equals;
-
-    while (file >> monkey_str >> id >> colon)
-    {
-        Monkey monkey{ id };
-
-        file >> starting >> items_colon;
-        while (file >> temp)
-        {
-            if (temp == "Operation:")
-                break;
-
-            monkey.items.push_back(atoi(temp.c_str()));
-        }
-
-        file >> new_str >> equals >> old >> monkey.operation >> monkey.operand;
-        file >> test_colon >> divisible >> by >> monkey.divisible_by;
-        file >> if_str >> true_colon >> throw_str >> to >> monkey_str >> monkey.true_throw_to;
-        file >> if_str >> false_colon >> throw_str >> to >> monkey_str >> monkey.false_throw_to;
-
-        monkeys.push_back(monkey);
-    }
-
-    long long modulo = 1;
+    auto modulo = 1LL;
     for (Monkey &m : monkeys)
         modulo *= m.divisible_by;
 
-    for (int round = 1; round <= 10000; round++)
+    std::map<int, long long> monkey_count;
+
+    while (rounds--)
     {
-        for (Monkey &m : monkeys)
+        for (auto i = 0; i < monkeys.size(); i++)
         {
-            for (auto &i : m.items)
+            Monkey &m = monkeys[i];
+
+            for (auto &j : m.items)
             {
-                monkey_count[m.id]++;
+                monkey_count[i]++;
 
-                auto temp = i;
-                if (m.operation == '*')
-                    temp *= ((m.operand == "old") ? temp : atoi(m.operand.c_str()));
-                else if (m.operation == '+')
-                    temp += ((m.operand == "old") ? temp : atoi(m.operand.c_str()));
-                else
-                    assert(false);
+                auto worry = j;
+                auto operand = ((m.operand == "old") ? worry : atoi(m.operand.c_str()));
+                worry = (m.operation == '*') ? (worry * operand) : (worry + operand);
 
-                temp %= modulo;
+                if (divide_by_three)
+                    worry /= 3;
 
-                if ((temp % m.divisible_by) == 0)
-                    monkeys[m.true_throw_to].items.push_back(temp);
-                else
-                    monkeys[m.false_throw_to].items.push_back(temp);
+                worry %= modulo;
+
+                auto throw_to = (worry % m.divisible_by) ? m.false_throw_to : m.true_throw_to;
+                monkeys[throw_to].items.push_back(worry);
             }
 
             m.items.clear();
         }
     }
 
-    vector<long long> counts;
+    std::vector<long long> counts;
     for (auto &i : monkey_count)
         counts.push_back(i.second);
 
     sort(counts.begin(), counts.end());
-
-    auto part2 = counts[counts.size() - 1] * counts[counts.size() - 2];
-    return part2;
+    return counts.back() * counts[counts.size() - 2];
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = process_input("input.txt", true, 20);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = process_input("input.txt", false, 10000);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 95472);
