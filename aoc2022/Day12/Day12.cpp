f818cdc73ba3dc83396988f1d7096c9cb8f568a7
Stephen Houchen <shouchen@outlook.com>
2022-12-12 06:58:59 -0800

    Day 12: More brute force clean-up

--- aoc2022/Day12/Day12.cpp
+++ aoc2022/Day12/Day12.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
 #include <array>
 #include <vector>
-#include <set>
+#include <map>
 #include <cassert>
 
-static const std::array<std::pair<int, int>, 4> deltas{
+using RowCol = std::pair<int, int>;
+
+static const std::array<RowCol, 4> deltas{
     std::make_pair(-1, 0), std::make_pair(1, 0), std::make_pair(0, -1), std::make_pair(0, 1)
 };
 
-bool can_move_back(const std::vector<std::string> &grid, std::map<std::pair<int, int>, int> &dist, const std::pair<int, int> &pos1, const std::pair<int, int> &pos2)
-{
-    return
-        pos2.first >= 0 && pos2.first < grid.size() && pos2.second >= 0 && pos2.second < grid[0].length() &&
-        grid[pos1.first][pos1.second] - grid[pos2.first][pos2.second] <= 1;
-}
+std::vector<std::string> grid;
+RowCol startpos, endpos;
 
-auto process_input(const std::string &filename, bool part2)
+void read_input(const std::string &filename)
 {
-    std::vector<std::string> grid;
-    std::set<std::pair<int, int>> visited;
-    std::pair<int, int> startpos, endpos;
-
     std::ifstream file(filename);
     std::string line;
 
@@ -45,14 +38,21 @@ auto process_input(const std::string &filename, bool part2)
 
         grid.push_back(line);
     }
+}
 
-    std::map<std::pair<int, int>, int> dist;
+auto do_part(bool part2)
+{
+    std::map<RowCol, int> dist;
+
     for (auto i = 0; i < grid.size(); i++)
         for (auto j = 0; j < grid[i].length(); j++)
             dist[std::make_pair(i, j)] = INT_MAX;
     dist[endpos] = 0;
 
     for (auto d = 0; ; d++)
+    {
+        auto next_dist = d + 1;
+
         for (auto i = 0; i < grid.size(); i++)
             for (auto j = 0; j < grid[i].length(); j++)
             {
@@ -63,22 +63,27 @@ auto process_input(const std::string &filename, bool part2)
                 for (auto &delta : deltas)
                 {
                     auto next = std::make_pair(i + delta.first, j + delta.second);
-                    if (can_move_back(grid, dist, curr, next) && dist[next] > d + 1)
+
+                    if (next.first >= 0 && next.first < grid.size() && next.second >= 0 && next.second < grid[0].length() &&
+                        grid[curr.first][curr.second] - grid[next.first][next.second] <= 1 && dist[next] > next_dist)
                     {
-                        dist[next] = d + 1;
                         if (part2 && grid[next.first][next.second] == 'a' || next == startpos)
-                            return d + 1;
+                            return next_dist;
+                        dist[next] = next_dist;
                     }
                 }
             }
+    }
 }
 
 int main()
 {
-    auto part1 = process_input("input.txt", false);
+    read_input("input.txt");
+
+    auto part1 = do_part(false);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = process_input("input.txt", true);
+    auto part2 = do_part(true);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 408);
