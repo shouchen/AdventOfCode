f6df89bbe8cdc029d8582fef0108893e863245c1
Stephen Houchen <shouchen@outlook.com>
2022-12-11 23:16:14 -0800

    Day 12: Merged solutions for two parts

--- aoc2022/Day12/Day12.cpp
+++ aoc2022/Day12/Day12.cpp
@@ -6,83 +6,7 @@
 #include <set>
 #include <cassert>
 
-auto do_part1(const std::string &filename)
-{
-    std::vector<std::string> grid;
-    std::set<std::pair<int, int>> visited;
-    std::pair<int, int> startpos, endpos;
-
-    std::ifstream file(filename);
-    std::string line;
-
-    while (getline(file, line))
-    {
-        auto temp = line.find('S');
-        if (temp != -1)
-        {
-            line[temp] = 'a';
-            startpos = std::make_pair(grid.size(), temp);
-        }
-
-        temp = line.find('E');
-        if (temp != -1)
-        {
-            line[temp] = 'z';
-            endpos = std::make_pair(grid.size(), temp);
-        }
-
-        grid.push_back(line);
-    }
-
-    std::map<std::pair<int, int>, int> dist;
-    for (int i = 0; i < grid.size(); i++)
-        for (int j = 0; j < grid[i].length(); j++)
-            dist[std::make_pair(i,j)] = INT_MAX;
-    dist[startpos] = 0;
-
-    for (int d = 0; d < 100000; d++)
-    {
-        for (int i = 0; i < grid.size(); i++)
-            for (int j = 0; j < grid[i].length(); j++)
-            {
-                auto temp = dist[std::make_pair(i, j)];
-                if (temp == d)
-                {
-                    if (i > 0 &&
-                        dist[std::make_pair(i - 1, j)] > d + 1 &&
-                        grid[i - 1][j] - grid[i][j] <= 1)
-                    {
-                        dist[std::make_pair(i - 1, j)] = d + 1;
-                    }
-                    if (i < grid.size() - 1 &&
-                        dist[std::make_pair(i + 1, j)] > d + 1 &&
-                        grid[i + 1][j] - grid[i][j] <= 1)
-                    {
-                        dist[std::make_pair(i + 1, j)] = d + 1;
-                    }
-                    if (j > 0 &&
-                        dist[std::make_pair(i, j - 1)] > d + 1 &&
-                        grid[i][j - 1] - grid[i][j] <= 1)
-                    {
-                        dist[std::make_pair(i, j - 1)] = d + 1;
-                    }
-                    if (j < grid[i].length() - 1 &&
-                        dist[std::make_pair(i, j + 1)] > d + 1 &&
-                        grid[i][j + 1] - grid[i][j] <= 1)
-                    {
-                        dist[std::make_pair(i, j + 1)] = d + 1;
-                    }
-                }
-            }
-
-        if (dist[endpos] != INT_MAX)
-            return dist[endpos];
-    }
-
-    return -1;
-}
-
-auto do_part2(const std::string &filename)
+auto process_input(const std::string &filename, bool part2)
 {
     std::vector<std::string> grid;
     std::set<std::pair<int, int>> visited;
@@ -116,7 +40,7 @@ auto do_part2(const std::string &filename)
             dist[std::make_pair(i, j)] = INT_MAX;
     dist[endpos] = 0;
 
-    for (int d = 0; d < 100000; d++)
+    for (int d = 0; ; d++)
     {
         for (int i = 0; i < grid.size(); i++)
             for (int j = 0; j < grid[i].length(); j++)
@@ -129,7 +53,7 @@ auto do_part2(const std::string &filename)
                         grid[i][j] - grid[i - 1][j] <= 1)
                     {
                         dist[std::make_pair(i - 1, j)] = d + 1;
-                        if (grid[i - 1][j] == 'a')
+                        if (part2 && grid[i - 1][j] == 'a' || std::make_pair(i - 1, j) == startpos)
                             return d + 1;
                     }
                     if (i < grid.size() - 1 &&
@@ -137,7 +61,7 @@ auto do_part2(const std::string &filename)
                         grid[i][j] - grid[i + 1][j] <= 1)
                     {
                         dist[std::make_pair(i + 1, j)] = d + 1;
-                        if (grid[i + 1][j] == 'a')
+                        if (part2 && grid[i + 1][j] == 'a' || std::make_pair(i + 1, j) == startpos)
                             return d + 1;
                     }
                     if (j > 0 &&
@@ -145,7 +69,7 @@ auto do_part2(const std::string &filename)
                         grid[i][j] - grid[i][j - 1] <= 1)
                     {
                         dist[std::make_pair(i, j - 1)] = d + 1;
-                        if (grid[i][j - 1] == 'a')
+                        if (part2 && grid[i][j - 1] == 'a' || std::make_pair(i, j - 1) == startpos)
                             return d + 1;
                     }
                     if (j < grid[i].length() - 1 &&
@@ -153,26 +77,20 @@ auto do_part2(const std::string &filename)
                         grid[i][j] - grid[i][j + 1] <= 1)
                     {
                         dist[std::make_pair(i, j + 1)] = d + 1;
-                        if (grid[i][j + 1] == 'a')
+                        if (part2 && grid[i][j + 1] == 'a' || std::make_pair(i, j + 1) == startpos)
                             return d + 1;
                     }
                 }
             }
-
-        // part1
-        if (dist[startpos] != INT_MAX)
-            return dist[startpos];
     }
-
-    return -1;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = process_input("input.txt", false);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = process_input("input.txt", true);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 408);
