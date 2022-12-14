312e60a04a33f65b977e996bf14e3b6ae315ed7a
Stephen Houchen <shouchen@outlook.com>
2022-12-13 22:26:30 -0800

    Day 14 clean-up

--- aoc2022/Day14/Day14.cpp
+++ aoc2022/Day14/Day14.cpp
@@ -5,26 +5,8 @@
 #include <map>
 #include <cassert>
 
-using namespace std;
-
-map<pair<int, int>, char> grid;
-int min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
-
-void dump()
-{
-    for (int i = min_y; i <= max_y; i++)
-    {
-        for (int j = min_x; j <= max_x; j++)
-            if (grid.find(make_pair(j, i)) != grid.end())
-            {
-                cout << grid[make_pair(j, i)];
-            }
-            else
-                cout << ".";
-        cout << endl;
-    }
-    cout << endl;
-}
+std::map<std::pair<int, int>, char> grid;
+auto min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
 
 auto do_part1(const std::string &filename)
 {
@@ -36,74 +18,66 @@ auto do_part1(const std::string &filename)
 
     while (std::getline(file, line))
     {
-        stringstream ss(line);
-        int a, b, c, d;
-        string arrow;
-        char comma;
+        std::stringstream ss(line);
+        std::pair<int, int> from, to;
+        std::string arrow;
+        auto comma = ',';
 
-        ss >> a >> comma >> b;
-        while (ss >> arrow >> c >> comma >> d)
+        ss >> from.first >> comma >> from.second;
+        while (ss >> arrow >> to.first >> comma >> to.second)
         {
-            if (a == c)
+            if (from.first == to.first)
             {
-                if (b < d)
-                    for (int i = b; i <= d; i++)
-                        grid[make_pair(a, i)] = '#';
+                if (from.second < to.second)
+                    for (auto i = from.second; i <= to.second; i++)
+                        grid[std::make_pair(from.first, i)] = '#';
                 else
-                    for (int i = b; i >= d; i--)
-                        grid[make_pair(a, i)] = '#';
+                    for (auto i = from.second; i >= to.second; i--)
+                        grid[std::make_pair(from.first, i)] = '#';
             }
             else
             {
-                if (a < c)
-                    for (int i = a; i <= c; i++)
-                        grid[make_pair(i, b)] = '#';
+                if (from.first < to.first)
+                    for (auto i = from.first; i <= to.first; i++)
+                        grid[std::make_pair(i, from.second)] = '#';
                 else
-                    for (int i = a; i >= c; i--)
-                        grid[make_pair(i, b)] = '#';
+                    for (auto i = from.first; i >= to.first; i--)
+                        grid[std::make_pair(i, from.second)] = '#';
             }
 
-            a = c, b = d;
+            from = to;
         }
     }
 
     for (auto &i : grid)
     {
-        min_x = min(min_x, i.first.first);
-        max_x = max(max_x, i.first.first);
-        min_y = min(min_y, i.first.second);
-        max_y = max(max_y, i.first.second);
+        min_x = std::min(min_x, i.first.first);
+        max_x = std::max(max_x, i.first.first);
+        min_y = std::min(min_y, i.first.second);
+        max_y = std::max(max_y, i.first.second);
     }
 
-    dump();
-
-    for (int i = 0; i < 10000; i++)
+    auto units = 0;
+    for (;;)
     {
-        auto sand = make_pair(500, 0);
+        auto sand = std::make_pair(500, 0);
         for (;;)
         {
-            if (grid.find(make_pair(sand.first, sand.second + 1)) == grid.end())
-            {
+            if (grid.find(std::make_pair(sand.first, sand.second + 1)) == grid.end())
                 sand.second++;
-            }
-            else if (grid.find(make_pair(sand.first - 1, sand.second + 1)) == grid.end())
-            {
+            else if (grid.find(std::make_pair(sand.first - 1, sand.second + 1)) == grid.end())
                 sand.first--, sand.second++;
-            }
-            else if (grid.find(make_pair(sand.first + 1, sand.second + 1)) == grid.end())
-            {
+            else if (grid.find(std::make_pair(sand.first + 1, sand.second + 1)) == grid.end())
                 sand.first++, sand.second++;
-            }
             else
                 break;
 
             if (sand.first < min_x || sand.first > max_x)
-                return i;
+                return units;
         }
 
         grid[sand] = 'o';
-
-        //dump();
+        units++;
     }
 }
 
@@ -114,80 +88,73 @@ auto do_part2(const std::string &filename)
 
     std::ifstream file(filename);
     std::string line;
+    std::pair<int, int> from, to;
 
     while (getline(file, line))
     {
-        stringstream ss(line);
+        std::stringstream ss(line);
         int a, b, c, d;
-        string arrow;
-        char comma;
+        std::string arrow;
+        auto comma = ',';
 
-        ss >> a >> comma >> b;
-        while (ss >> arrow >> c >> comma >> d)
+        ss >> from.first >> comma >> from.second;
+        while (ss >> arrow >> to.first >> comma >> to.second)
         {
-            if (a == c)
+            if (from.first == to.first)
             {
-                if (b < d)
-                    for (int i = b; i <= d; i++)
-                        grid[make_pair(a, i)] = '#';
+                if (from.second < to.second)
+                    for (auto i = from.second; i <= to.second; i++)
+                        grid[std::make_pair(from.first, i)] = '#';
                 else
-                    for (int i = b; i >= d; i--)
-                        grid[make_pair(a, i)] = '#';
+                    for (auto i = from.second; i >= to.second; i--)
+                        grid[std::make_pair(from.first, i)] = '#';
             }
             else
             {
-                if (a < c)
-                    for (int i = a; i <= c; i++)
-                        grid[make_pair(i, b)] = '#';
+                if (from.first < to.first)
+                    for (auto i = from.first; i <= to.first; i++)
+                        grid[std::make_pair(i, from.second)] = '#';
                 else
-                    for (int i = a; i >= c; i--)
-                        grid[make_pair(i, b)] = '#';
+                    for (auto i = from.first; i >= to.first; i--)
+                        grid[std::make_pair(i, from.second)] = '#';
             }
 
-            a = c, b = d;
+            from = to;
         }
     }
 
     for (auto &i : grid)
     {
-        min_x = min(min_x, i.first.first);
-        max_x = max(max_x, i.first.first);
-        min_y = min(min_y, i.first.second);
-        max_y = max(max_y, i.first.second);
+        min_x = std::min(min_x, i.first.first);
+        max_x = std::max(max_x, i.first.first);
+        min_y = std::min(min_y, i.first.second);
+        max_y = std::max(max_y, i.first.second);
     }
 
-    dump();
+    for (auto i = -10000; i <= 10000; i++)
+        grid[std::make_pair(i, max_y + 2)] = '#';
 
-    for (int i = -10000; i <= 10000; i++)
-        grid[make_pair(i, max_y + 2)] = '#';
-
-    for (int i = 0; i < 100000; i++)
+    auto units = 0;
+    for (;;)
     {
-        auto sand = make_pair(500, 0);
+        auto sand = std::make_pair(500, 0);
         for (;;)
         {
-            if (grid.find(make_pair(sand.first, sand.second + 1)) == grid.end())
-            {
+            if (grid.find(std::make_pair(sand.first, sand.second + 1)) == grid.end())
                 sand.second++;
-            }
-            else if (grid.find(make_pair(sand.first - 1, sand.second + 1)) == grid.end())
-            {
+            else if (grid.find(std::make_pair(sand.first - 1, sand.second + 1)) == grid.end())
                 sand.first--, sand.second++;
-            }
-            else if (grid.find(make_pair(sand.first + 1, sand.second + 1)) == grid.end())
-            {
+            else if (grid.find(std::make_pair(sand.first + 1, sand.second + 1)) == grid.end())
                 sand.first++, sand.second++;
-            }
             else
                 break;
         }
 
-        if (sand.first == 500 && sand.second == 0)
-            return i + 1;
-
         grid[sand] = 'o';
+        units++;
 
-        //dump();
+        if (sand.first == 500 && sand.second == 0)
+            return units;
     }
 }
 
