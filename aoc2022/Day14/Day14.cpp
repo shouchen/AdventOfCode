22f64e9b82455650bb378a00b37d1e3b6324d18b
Stephen Houchen <shouchen@outlook.com>
2022-12-13 22:34:41 -0800

    Day 14: More clean-up

--- aoc2022/Day14/Day14.cpp
+++ aoc2022/Day14/Day14.cpp
@@ -7,8 +7,9 @@
 
 std::map<std::pair<int, int>, char> grid;
 auto min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
+auto source = std::make_pair(500, 0);
 
-auto do_part1(const std::string &filename)
+void read_input(const std::string &filename)
 {
     grid.clear();
     min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
@@ -56,11 +57,16 @@ auto do_part1(const std::string &filename)
         min_y = std::min(min_y, i.first.second);
         max_y = std::max(max_y, i.first.second);
     }
+}
 
+auto do_part1(const std::string &filename)
+{
+    read_input(filename);
     auto units = 0;
+
     for (;;)
     {
-        auto sand = std::make_pair(500, 0);
+        auto sand = source;
         for (;;)
         {
             if (grid.find(std::make_pair(sand.first, sand.second + 1)) == grid.end())
@@ -83,61 +89,15 @@ auto do_part1(const std::string &filename)
 
 auto do_part2(const std::string &filename)
 {
-    grid.clear();
-    min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
-
-    std::ifstream file(filename);
-    std::string line;
-    std::pair<int, int> from, to;
-
-    while (getline(file, line))
-    {
-        std::stringstream ss(line);
-        int a, b, c, d;
-        std::string arrow;
-        auto comma = ',';
-
-        ss >> from.first >> comma >> from.second;
-        while (ss >> arrow >> to.first >> comma >> to.second)
-        {
-            if (from.first == to.first)
-            {
-                if (from.second < to.second)
-                    for (auto i = from.second; i <= to.second; i++)
-                        grid[std::make_pair(from.first, i)] = '#';
-                else
-                    for (auto i = from.second; i >= to.second; i--)
-                        grid[std::make_pair(from.first, i)] = '#';
-            }
-            else
-            {
-                if (from.first < to.first)
-                    for (auto i = from.first; i <= to.first; i++)
-                        grid[std::make_pair(i, from.second)] = '#';
-                else
-                    for (auto i = from.first; i >= to.first; i--)
-                        grid[std::make_pair(i, from.second)] = '#';
-            }
-
-            from = to;
-        }
-    }
-
-    for (auto &i : grid)
-    {
-        min_x = std::min(min_x, i.first.first);
-        max_x = std::max(max_x, i.first.first);
-        min_y = std::min(min_y, i.first.second);
-        max_y = std::max(max_y, i.first.second);
-    }
+    read_input(filename);
+    auto units = 0;
 
     for (auto i = -10000; i <= 10000; i++)
         grid[std::make_pair(i, max_y + 2)] = '#';
 
-    auto units = 0;
     for (;;)
     {
-        auto sand = std::make_pair(500, 0);
+        auto sand = source;
         for (;;)
         {
             if (grid.find(std::make_pair(sand.first, sand.second + 1)) == grid.end())
@@ -153,7 +113,7 @@ auto do_part2(const std::string &filename)
         grid[sand] = 'o';
         units++;
 
-        if (sand.first == 500 && sand.second == 0)
+        if (sand == source)
             return units;
     }
 }
