0fe45035a33d55c841cf8198e3179669ba4d6a66
Stephen Houchen <shouchen@outlook.com>
2022-12-21 11:24:19 -0800

    Final Day 17

--- aoc2022/Day17/Day17.cpp
+++ aoc2022/Day17/Day17.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 #include <vector>
+#include <map>
 #include <cassert>
 
-std::vector<int> tower;
+using Tower = std::vector<int>;
+using Piece = std::vector<int>;
 
-//vector<int> minus_piece{ 0x1e };
-//vector<int> plus_piece{ 0x08, 0x1c, 0x08 };
-//vector<int> l_piece{ 0x04, 0x04, 0x1c };
-//vector<int> i_piece{ 0x20, 0x20, 0x20, 0x20 };
-//vector<int> box_piece{ 0x30, 0x30 };
-
-std::vector<std::vector<int>> shapes =
+const std::vector<Piece> shapes =
 {
     { 0x1e },
     { 0x08, 0x1c, 0x08 },
@@ -20,140 +17,161 @@ std::vector<std::vector<int>> shapes =
     { 0x18, 0x18 }
 };
 
-std::vector<int> curr_piece;
-int curr_piece_row;
-
-
-void dump()
+struct Key
 {
-    auto height = tower.size();
-    if (curr_piece_row > -1)
-        height = std::max(height, curr_piece_row + curr_piece.size());
+    std::array<char, 7> hash;
+    char rock_type;
+    int jet_index;
+};
 
-    for (int i = height - 1; i >= 0; i--)
-    {
-        std::cout << '|';
-        for (auto bit = 0x40; bit; bit >>= 1)
-        {
-            if (curr_piece_row != -1 && i >= curr_piece_row && i < curr_piece_row + curr_piece.size() && (curr_piece[i - curr_piece_row] & bit))
-                std::cout << '@';
-            else
-                std::cout << ((i < tower.size() && (tower[i] & bit)) ? '#' : '.');
-        }
-        std::cout << '|' << std::endl;
-    }
-    std::cout << "+-------+" << std::endl << std::endl;
+struct Value
+{
+    long long num_rocks, tower_height;
+};
+
+auto operator<(const Key &lhs, const Key &rhs)
+{
+    if (lhs.hash < rhs.hash) return true;
+    if (lhs.hash > rhs.hash) return false;
+    if (lhs.rock_type < rhs.rock_type) return true;
+    if (lhs.rock_type > rhs.rock_type) return false;
+    return lhs.jet_index < rhs.jet_index;
 }
 
-auto collides()
+auto collides(Tower &tower, Piece &curr_piece, int curr_piece_row)
 {
     if (curr_piece_row < 0)
         return true;
 
     for (auto i = 0; i < curr_piece.size(); i++)
-    {
         if ((tower.size() >= curr_piece_row + i + 1) &&
             (curr_piece[i] & tower[curr_piece_row + i]))
             return true;
-    }
 
     return false;
 }
 
-void apply_jet(char jet)
+void apply_jet(Tower &tower, const std::string &jets, int &jet, Piece &curr_piece, int curr_piece_row)
 {
-    switch (jet)
+    auto j = jets[jet];
+    if (++jet == jets.length())
+        jet = 0;
+
+    Piece new_piece;
+
+    for (auto i = 0; i < curr_piece.size(); i++)
     {
-    case '<':
-        for (int i = 0; i < curr_piece.size(); i++)
-            if (curr_piece[i] & 0x40) // at extreme left
+        if (j == '<')
+        {
+            if (curr_piece[i] & 0x40) // at extreme left, don't move
                 return;
-
-        for (int i = 0; i < curr_piece.size(); i++)
-            curr_piece[i] <<= 1;
-        if (collides())
-            for (int i = 0; i < curr_piece.size(); i++)
-                curr_piece[i] >>= 1;
-        break;
-
-    case '>':
-        for (int i = 0; i < curr_piece.size(); i++)
-            if (curr_piece[i] & 0x01) // at extreme right
+            new_piece.push_back(curr_piece[i] << 1);
+        }
+        else
+        {
+            if (curr_piece[i] & 0x01) // at extreme right, don't move
                 return;
-
-        for (int i = 0; i < curr_piece.size(); i++)
-            curr_piece[i] >>= 1;
-        if (collides())
-            for (int i = 0; i < curr_piece.size(); i++)
-                curr_piece[i] <<= 1;
-        break;
-
-    default:
-        assert(false);
+            new_piece.push_back(curr_piece[i] >> 1);
+        }
     }
+
+    if (!collides(tower, new_piece, curr_piece_row))
+        curr_piece = new_piece;
 }
 
-auto do_part1(const std::string &filename)
+void place_piece_at_rest(Tower &tower, const Piece &piece, int curr_piece_row)
+{
+    while (tower.size() < curr_piece_row + piece.size())
+        tower.push_back(0);
+    for (auto i = 0; i < piece.size(); i++)
+        tower[curr_piece_row + i] |= piece[i];
+}
+
+auto hash_tops(Tower &tower)
+{
+    auto retval = std::array<char, 7>();
+    auto index = 0;
+
+    for (auto bit = 0x40; bit; bit >>= 1)
+    {
+        auto depth = 0;
+        for (int i = tower.size() - 1; i >= 0; i--)
+            if (tower[i] & bit)
+                break;
+            else
+                depth++;
+        retval[index++] = depth;
+    }
+    return retval;
+}
+
+auto process_input(const std::string &filename, long long times)
 {
     std::ifstream file(filename);
     std::string jets;
-
     file >> jets;
 
-    auto floor = 0, curr_top = 0, curr_bottom = 0, curr_left = 0, curr_right = 0;
+    Tower tower;
+    Piece curr_piece;
+    auto curr_piece_row = -1, curr_shape = 0, jet = 0;
 
-    int curr_shape = 0;
-    int jet = 0;
-    int rock_count = 0;
+    std::map<Key, Value> seen;
+    auto skip_ahead = -1;
+    auto future_full_cycles = -1LL, added_height_per_period = -1LL;
 
-    for (;;)
+    for (auto rock_count = 1LL; rock_count <= times; rock_count++)
     {
-        if (rock_count == 2022)
-            return tower.size();
-
         curr_piece = shapes[curr_shape];
-        if (++curr_shape == shapes.size()) curr_shape = 0;
-        curr_piece_row = tower.size() + 3;
-        //dump();
-        std::cout << std::endl;
 
-        for (;;)
+        for (curr_piece_row = tower.size() + 3; ; --curr_piece_row)
         {
-            apply_jet(jets[jet]);
-            if (++jet == jets.size()) jet = 0;
-            //dump();
+            apply_jet(tower, jets, jet, curr_piece, curr_piece_row);
 
-            curr_piece_row--;
-            if (collides())
+            if (collides(tower, curr_piece, curr_piece_row - 1))
             {
-                curr_piece_row++;
+                place_piece_at_rest(tower, curr_piece, curr_piece_row);
                 break;
             }
-            //dump();
         }
 
-        while (tower.size() < curr_piece_row + curr_piece.size())
-            tower.push_back(0);
-        for (int i = 0; i < curr_piece.size(); i++)
-            tower[curr_piece_row + i] |= curr_piece[i];
-        curr_piece_row = -1;
+        if (++curr_shape == shapes.size())
+            curr_shape = 0;
 
-        rock_count++;
+        if (skip_ahead > -1)
+        {
+            if (rock_count == skip_ahead)
+                return tower.size() + future_full_cycles * added_height_per_period;
+        }
+        else
+        {
+            Key key{ hash_tops(tower), curr_shape, jet };
+            auto value = seen.find(key);
 
-        //dump();
-        std::cout << std::endl;
+            if (value != seen.end())
+            {
+                auto period = rock_count - value->second.num_rocks;
+                added_height_per_period = tower.size() - value->second.tower_height;
+
+                future_full_cycles = (times - rock_count) / period;
+                auto modulo = (times - rock_count) % period;
+
+                skip_ahead = rock_count + modulo;
+            }
+            else
+                seen[key] = Value{ rock_count, (int)tower.size() };
+        }
     }
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = process_input("input.txt", 2022);
     std::cout << "Part One: " << part1 << std::endl;
 
-    //auto part2 = process_data("test.txt");
-    //std::cout << "Part Two: " << part2 << std::endl;
+    auto part2 = process_input("input.txt", 1000000000000);
+    std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 3071);
-    //assert(part2 == "PQTJRSHWS");
+    assert(part2 == 1523615160362);
     return 0;
 }
