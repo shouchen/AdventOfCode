ea748b60b3b6d2b1f4f4f32bbbed25da340052eb
Stephen Houchen <shouchen@outlook.com>
2022-12-26 23:00:34 -0800

    Day 22 clean-up

--- aoc2022/Day22/Day22.cpp
+++ aoc2022/Day22/Day22.cpp
@@ -4,17 +4,19 @@
 #include <vector>
 #include <cassert>
 
+using MoveFunc = void(*)(int &, int &, int &, int &, int);
+
 std::vector<std::string> grid;
 
-void move(int &row, int &col, int dr, int dc, int count)
+void move1(int &row, int &col, int &dr, int &dc, int count)
 {
     auto next_row = row, next_col = col;
 
-    for (auto i = 0; i < count; i++)
+    while (count--)
     {
-        next_row += dr;
-        next_col += dc;
+        next_row += dr, next_col += dc;
 
+        // part1 wrapping
         if (next_row < 0)
             next_row = int(grid.size()) - 1;
         else if (next_row == grid.size())
@@ -40,93 +42,15 @@ void move(int &row, int &col, int dr, int dc, int count)
     }
 }
 
-void turn(int &dr, int &dc, char dir)
-{
-    if (dr == -1 && dc == 0)
-    {
-        if (dir == 'L') dr = 0, dc = -1;
-        else dr = 0, dc = 1;
-    }
-    else if (dr == 1 && dc == 0)
-    {
-        if (dir == 'L') dr = 0, dc = 1;
-        else dr = 0, dc = -1;
-    }
-    else if (dr == 0 && dc == -1)
-    {
-        if (dir == 'L') dr = 1, dc = 0;
-        else dr = -1, dc = 0;
-    }
-    else if (dr == 0 && dc == 1)
-    {
-        if (dir == 'L') dr = -1, dc = 0;
-        else dr = 1, dc = 0;
-    }
-}
-
-auto do_part1(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
-
-    auto max_line = 0;
-    while (getline(file, line) && !line.empty())
-    {
-        grid.push_back(line);
-        if (line.length() > max_line)
-            max_line = int(line.length());
-    }
-
-    for (auto &i : grid)
-        while (i.length() < max_line)
-            i.push_back(' ');
-
-    auto curr_row = 0, curr_col = 0, dr = 0, dc = 1;
-    while (grid[curr_row][curr_col] != '.')
-        curr_col++;
-
-    std::string number;
-    auto c = ' ';
-
-    while (file >> c)
-    {
-        if (isdigit(c))
-        {
-            number.push_back(c);
-        }
-        else
-        {
-            move(curr_row, curr_col, dr, dc, atoi(number.c_str()));
-            turn(dr, dc, c);
-
-            number.clear();
-        }
-    }
-
-    if (!number.empty())
-        move(curr_row, curr_col, dr, dc, atoi(number.c_str()));
-
-    auto retval = 1000 * (curr_row + 1) + 4 * (curr_col + 1);
-    if (dr == -1 && dc == 0)
-        retval += 3;
-    else if (dr == 1 && dc == 0)
-        retval += 1;
-    else if (dr == 0 && dc == -1)
-        retval += 2;
-    else if (dr ==0 && dc == 1)
-        retval += 0;
-    return retval;
-}
-
 void move2(int &row, int &col, int &dr, int &dc, int count)
 {
     auto next_row = row, next_col = col, next_dr = dr, next_dc = dc;
 
-    for (int i = 0; i < count; i++)
+    while (count--)
     {
         next_row += dr, next_col += dc;
 
-        // special wrapping
+        // part2 wrapping
         if (next_row == -1 && next_col >= 50 && next_col < 100)
             next_row = next_col + 100, next_col = 0, next_dr = 0, next_dc = 1; // 1 out top
         else if (next_col == 49 && next_row >= 0 && next_row < 50)
@@ -157,63 +81,86 @@ void move2(int &row, int &col, int &dr, int &dc, int count)
             next_col += 100, next_row = 0; // 6 out bottom
 
         if (grid[next_row][next_col] == '#')
-            return;
+            break;
 
         row = next_row, col = next_col;
         dr = next_dr, dc = next_dc;
     }
 }
 
-auto do_part2(const std::string &filename)
+void turn(int &dr, int &dc, char dir)
 {
-    std::ifstream file(filename);
-    std::string line, number;
+    if (dc == 0)
+        dc = (dir == 'L') ? dr : -dr, dr = 0;
+    else
+        dr = (dir == 'L') ? -dc : dc, dc = 0;
+}
 
-    while (getline(file, line) && !line.empty())
-        grid.push_back(line);
-
-    auto curr_row = 0, curr_col = 0, dr = 0, dc = 1;
-    while (grid[curr_row][curr_col] != '.')
-        curr_col++;
-
-    auto c = ' ';
-
-    while (file >> c)
-    {
-        if (isdigit(c))
-        {
-            number.push_back(c);
-        }
-        else
-        {
-            move2(curr_row, curr_col, dr, dc, atoi(number.c_str()));
-            turn(dr, dc, c);
-
-            number.clear();
-        }
-    }
-
-    if (!number.empty())
-        move2(curr_row, curr_col, dr, dc, atoi(number.c_str()));
-
+auto compute_passcode(int curr_row, int curr_col, int dr, int dc)
+{
     auto retval = 1000 * (curr_row + 1) + 4 * (curr_col + 1);
+
     if (dr == -1 && dc == 0)
         retval += 3;
     else if (dr == 1 && dc == 0)
         retval += 1;
     else if (dr == 0 && dc == -1)
         retval += 2;
-    else if (dr == 0 && dc == 1)
-        retval += 0;
+
     return retval;
+}
+
+auto do_part(const std::string &filename, MoveFunc move)
+{
+    std::ifstream file(filename);
+    std::string line;
+    auto max_line = 0;
+
+    while (getline(file, line) && !line.empty())
+    {
+        grid.push_back(line);
+        if (line.length() > max_line)
+            max_line = int(line.length());
+    }
+
+    for (auto &i : grid)
+        while (i.length() < max_line)
+            i.push_back(' ');
+
+    auto curr_row = 0, curr_col = 0, dr = 0, dc = 1;
+    while (grid[curr_row][curr_col] != '.')
+        curr_col++;
+
+    auto c = ' ';
+    auto number = 0;
+
+    while (file >> c)
+    {
+        if (isdigit(c))
+        {
+            number = number * 10 + c - '0';
+        }
+        else
+        {
+            move(curr_row, curr_col, dr, dc, number);
+            turn(dr, dc, c);
+
+            number = 0;
+        }
+    }
+
+    if (number > 0)
+        move(curr_row, curr_col, dr, dc, number);
+
+    return compute_passcode(curr_row, curr_col, dr, dc);
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = do_part("input.txt", move1);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = do_part("input.txt", move2);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 131052);
