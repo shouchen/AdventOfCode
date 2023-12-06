3eb675c0dbc15bb3abe7067361ec78b832e5c6c2
Stephen Houchen <shouchen@outlook.com>
2023-12-06 13:06:27 -0800

    Better Day 5 solution that doesn't take 5 minutes to run.

--- aoc2023/Day05/Day05.cpp
+++ aoc2023/Day05/Day05.cpp
@@ -4,26 +4,72 @@
 #include <vector>
 #include <cassert>
 
-struct Mapping
-{
-    long long dest, source, len;
-};
+struct Range { long long start, len; };
+struct Mapping { Range range; long long dest; };
 
 std::vector<Mapping> mapping[7];
+std::string seeds_line;
 
-auto follow_map(int map, long long value)
+long long recur(int level, Range input)
 {
-    for (auto i = 0; i < mapping[map].size(); i++)
-        if (value>= mapping[map][i].source && value < mapping[map][i].source + mapping[map][i].len)
-            return value - mapping[map][i].source + mapping[map][i].dest;
+    auto min = LLONG_MAX;
 
-    return value;
+    if (input.len == 0)
+        return min;
+
+    if (level == 4)
+    {
+        std::cout << "debug" << std::endl;
+    }
+
+    //  if no move levels, return the lowest beginning range of all
+    if (level == 7)
+    {
+        std::cout << "terminus " << input.start << std::endl;
+        return input.start;
+    }
+
+    for (auto &test : mapping[level])
+    {
+        auto input_end = input.start + input.len - 1, test_end = test.range.start + test.range.len - 1;
+
+        auto overlap_start = std::max(input.start, test.range.start);
+        auto overlap_end = std::min(input_end, test_end);
+        if (overlap_start <= overlap_end)
+        {
+            auto overlap = Range{ overlap_start, overlap_end - overlap_start + 1 };
+            overlap.start += test.dest - test.range.start;
+            auto temp = recur(level + 1, overlap);
+            min = std::min(min, temp);
+
+            if (input.start < overlap_start)
+            {
+                auto preoverlap = Range{ input.start, test.range.start - input.start };
+                auto preoverlap_end = std::min(input_end, test_end);
+                auto temp = recur(level, preoverlap);
+                min = std::min(min, temp);
+            }
+
+            if (input_end > overlap_end)
+            {
+                auto postoverlap = Range{ test_end + 1, input_end - test_end };
+                auto postoverlap_end = std::min(input_end, test_end);
+                auto temp = recur(level, postoverlap);
+                min = std::min(min, temp);
+            }
+
+            return min;
+        }
+    }
+
+    auto temp = recur(level + 1, input);
+    return std::min(min, temp);
 }
 
-auto do_part(const std::string &filename, bool part2)
+void read_input(const std::string &filename)
 {
     std::ifstream file(filename);
-    std::string seeds_line, line;
+    std::string line;
 
     std::getline(file, seeds_line);
     std::getline(file, line); // blank line after seeds
@@ -36,30 +82,27 @@ auto do_part(const std::string &filename, bool part2)
             std::istringstream is(line);
             Mapping m;
 
-            is >> m.dest >> m.source >> m.len;
+            is >> m.dest >> m.range.start >> m.range.len;
             mapping[i].push_back(m);
         }
     }
+}
 
+auto do_part(bool part2)
+{
     auto n = 0LL, len = 1LL, min = LLONG_MAX;
     std::string seeds_colon;
     std::istringstream is(seeds_line);
 
     is >> seeds_colon;
+
     while (is >> n)
     {
         if (part2)
             is >> len;
 
-        std::cout << n << std::endl; // debug progress
-        for (auto j = 0; j < len; j++)
-        {
-            auto curr = n + j;
-            for (auto level = 0; level < 7; level++)
-                curr = follow_map(level, curr);
-
-            min = std::min(min, curr);
-        }
+        auto temp = recur(0, Range{ n, len });
+        min = std::min(min, temp);
     }
 
     return min;
@@ -67,11 +110,13 @@ auto do_part(const std::string &filename, bool part2)
 
 int main()
 {
-    auto part1 = do_part("input.txt", false);
+    read_input("input.txt");
+
+    auto part1 = do_part(false);
     std::cout << "Part One: " << part1 << std::endl;
     assert(part1 == 484023871);
 
-    auto part2 = do_part("input.txt", true);
+    auto part2 = do_part(true);
     std::cout << "Part Two: " << part2 << std::endl;
     assert(part2 == 46294175);
     return 0;
