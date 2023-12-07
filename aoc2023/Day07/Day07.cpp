d3db221de4721641d242e7fb7297c77fa16b7994
Stephen Houchen <shouchen@outlook.com>
2023-12-07 08:58:51 -0800

    Removed recursion to simplify.

--- aoc2023/Day07/Day07.cpp
+++ aoc2023/Day07/Day07.cpp
@@ -18,67 +18,85 @@ enum Type
 
 struct HandBid
 {
+    HandBid(const std::string &hand, int bid);
+
     std::string hand;
-    Type type = HighCard;
-    int bid = 0;
+    Type type1, type2;
+    int bid;
 };
 
-std::string order1 = "AKQJT98765432", order2 = "AKQT98765432J";
-
-Type get_type(std::string hand)
+HandBid::HandBid(const std::string &hand, int bid) : hand(hand), bid(bid)
 {
-    std::map<char, int> m;
+    std::map<char, int> card_to_count;
     for (auto c : hand)
-        m[c]++;
+        card_to_count[c]++;
 
-    std::map<int, int> s2;
-    for (auto &mm : m)
-        s2[mm.second]++;
+    std::map<int, int> count_of_counts;
+    for (auto &mm : card_to_count)
+        count_of_counts[mm.second]++;
 
-    if (s2.find(5) != s2.end())
-        return FiveOfAKind;
-    if (s2.find(4) != s2.end())
-        return FourOfAKind;
-    if (s2.find(3) != s2.end() && s2.find(2) != s2.end())
-        return FullHouse;
-    if (s2.find(3) != s2.end())
-        return ThreeOfAKind;
-    if (s2[2] == 2 && s2[1] == 1)
-        return TwoPair;
-    if (s2[2] == 1)
-        return OnePair;
+    if (count_of_counts[5] == 1)
+        type1 = FiveOfAKind;
+    else if (count_of_counts[4] == 1)
+        type1 = FourOfAKind;
+    else if (count_of_counts[3] == 1 && count_of_counts[2] == 1)
+        type1 = FullHouse;
+    else if (count_of_counts[3] == 1)
+        type1 = ThreeOfAKind;
+    else if (count_of_counts[2] == 2)
+        type1 = TwoPair;
+    else if (count_of_counts[2] == 1)
+        type1 = OnePair;
+    else
+        type1 = HighCard;
 
-    return HighCard;
-}
+    type2 = type1;
+    auto jokers = card_to_count['J'];
 
-Type get_best_type_with_jokers(std::string hand)
-{
-    Type best = get_type(hand);
-
-    auto pos = hand.find('J');
-    if (pos != std::string::npos)
+    switch (type2)
     {
-        for (int i = 0; i < order2.size() - 1; i++)
-        {
-            hand[pos] = order2[i];
-            auto temp = get_best_type_with_jokers(hand);
-            if (temp < best) best = temp;
-        }
+    case FourOfAKind:
+    case FullHouse:
+        if (jokers) type2 = FiveOfAKind;
+        break;
+    case ThreeOfAKind:
+        if (jokers) type2 = FourOfAKind;
+        break;
+    case TwoPair:
+        if (jokers == 2) type2 = FourOfAKind;
+        else if (jokers == 1) type2 = FullHouse;
+        break;
+    case OnePair:
+        if (jokers) type2 = ThreeOfAKind;
+        break;
+    case HighCard:
+        if (jokers) type2 = OnePair;
+        break;
     }
-
-    return best;
 }
 
-bool compare_hands(const HandBid &hb1, const HandBid &hb2, const std::string &order)
+using HandBidComparator = bool (*)(const HandBid &hb1, const HandBid &hb2);
+
+std::vector<HandBid> hbv;
+
+auto read_data(const std::string &filename)
 {
-    auto h1 = hb1.hand, h2 = hb2.hand;
+    std::ifstream file(filename);
+    std::string hand;
+    auto bid = 0;
 
-    if (hb1.type < hb2.type) return true;
-    if (hb2.type < hb1.type) return false;
+    while (file >> hand >> bid)
+        hbv.push_back(HandBid(hand, bid));
+}
 
-    for (int i = 0; i < 5; i++)
+bool compare_hands(const HandBid &hb1, const HandBid &hb2, Type HandBid:: *type, std::string order)
+{
+    if (hb1.*type < hb2.*type) return true;
+    if (hb2.*type < hb1.*type) return false;
+
+    for (auto i = 0; i < hb1.hand.length(); i++)
     {
-        auto pos1 = order.find(h1[i]), pos2 = order.find(h2[i]);
+        auto pos1 = order.find(hb1.hand[i]), pos2 = order.find(hb2.hand[i]);
         if (pos1 < pos2) return true;
         if (pos2 < pos1) return false;
     }
@@ -88,28 +106,19 @@ bool compare_hands(const HandBid &hb1, const HandBid &hb2, const std::string &or
 
 bool compare_hands1(const HandBid &hb1, const HandBid &hb2)
 {
-    return compare_hands(hb1, hb2, order1);
+    return compare_hands(hb1, hb2, &HandBid::type1, "AKQJT98765432");
 }
 
 bool compare_hands2(const HandBid &hb1, const HandBid &hb2)
 {
-    return compare_hands(hb1, hb2, order2);
+    return compare_hands(hb1, hb2, &HandBid::type2, "AKQT98765432J");
 }
 
-auto do_part(const std::string &filename, bool part2)
+auto do_part(HandBidComparator cmp)
 {
-    std::ifstream file(filename);
-    std::vector<HandBid> hbv;
-    HandBid hb;
     auto retval = 0LL;
 
-    while (file >> hb.hand >> hb.bid)
-    {
-        hb.type = part2 ? get_best_type_with_jokers(hb.hand) : get_type(hb.hand);
-        hbv.push_back(hb);
-    }
-
-    sort(hbv.begin(), hbv.end(), part2 ? compare_hands2 : compare_hands1);
+    sort(hbv.begin(), hbv.end(), cmp);
 
     for (auto i = 0; i < hbv.size(); i++)
     {
@@ -122,10 +131,12 @@ auto do_part(const std::string &filename, bool part2)
 
 int main()
 {
-    auto part1 = do_part("input.txt", false);
+    read_data("input.txt");
+
+    auto part1 = do_part(compare_hands1);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = do_part("input.txt", true);
+    auto part2 = do_part(compare_hands2);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 251058093);
