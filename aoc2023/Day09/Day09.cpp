c19ff4af40a1ccdde134504f29960f595fcd8b97
Stephen Houchen <shouchen@outlook.com>
2023-12-08 22:02:37 -0800

    Day 9 clean-up

--- aoc2023/Day09/Day09.cpp
+++ aoc2023/Day09/Day09.cpp
@@ -5,33 +5,16 @@
 #include <algorithm>
 #include <cassert>
 
-using Series = std::vector<int>;
-
-auto predict(const std::vector<int> &v)
+int predict(const std::vector<int> &curr)
 {
-    std::vector<Series> pyr;
-    pyr.push_back(v);
+    std::vector<int> next;
+    for (auto i = 0; i < curr.size() - 1; i++)
+        next.push_back(curr[i + 1] - curr[i]);
 
-    for (int j = 1; j < 1000; j++)
-    {
-        Series next;
-        for (int i = 0; i < pyr[j-1].size() - 1; i++)
-            next.push_back(pyr[j-1][i + 1] - pyr[j-1][i]);
-
-        pyr.push_back(next);
-        if (next.size() == 1)
-            break;
-    }
-
-    pyr.back().push_back(pyr.back().front());
-
-    for (auto i = int(pyr.size()) - 2; i >= 0; --i)
-        pyr[i].push_back(pyr[i].back() + pyr[i + 1].back());
-
-    return pyr[0].back();
+    return (next.size() == 1) ? next.front() : (curr.back() + predict(next));
 }
 
-auto do_part1(const std::string &filename)
+auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
@@ -41,7 +24,7 @@ auto do_part1(const std::string &filename)
     {
         std::istringstream is(line);
         auto n = 0;
-        Series series;
+        std::vector<int> series;
 
         while (is >> n)
             series.push_back(n);
@@ -57,7 +40,7 @@ auto do_part1(const std::string &filename)
 
 int main()
 {
-    auto answer = do_part1("input.txt");
+    auto answer = solve("input.txt");
     std::cout << "Part One: " << answer.first << std::endl;
     std::cout << "Part Two: " << answer.second << std::endl;
 
