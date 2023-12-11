f5057dcd143115a4112e2a7d53821de07df0556d
Stephen Houchen <shouchen@outlook.com>
2023-12-11 06:38:10 -0800

    Last finessing on Day 11.

--- aoc2023/Day11/Day11.cpp
+++ aoc2023/Day11/Day11.cpp
@@ -7,33 +7,15 @@
 std::vector<std::pair<int, int>> galaxies;
 std::vector<bool> row_has_galaxies, col_has_galaxies;
 
-auto get_dists(std::pair<int, int> &g1, std::pair<int, int> &g2)
+void add_dist(int from, int to, std::vector<bool> &has_galaxies, std::pair<long long, long long> &dists)
 {
-    auto dist1 = 0LL, dist2 = 0LL;
+    auto delta = (from < to) ? 1 : -1;
 
-    if (g1.first != g2.first)
-    {
-        auto delta = (g1.first < g2.first) ? 1 : -1;
-
-        for (auto row = g1.first; row != g2.first; row += delta)
-            if (row_has_galaxies[row])
-                dist1++, dist2++;
-            else
-                dist1 += 2, dist2 += 1000000;
-    }
-
-    if (g1.second != g2.second)
-    {
-        auto delta = (g1.second < g2.second) ? 1 : -1;
-
-        for (auto col = g1.second; col != g2.second; col += delta)
-            if (col_has_galaxies[col])
-                dist1++, dist2++;
-            else
-                dist1 += 2, dist2 += 1000000;
-    }
-
-    return std::make_pair(dist1, dist2);
+    for (auto rc = from; rc != to; rc += delta)
+        if (has_galaxies[rc])
+            dists.first++, dists.second++;
+        else
+            dists.first += 2, dists.second += 1000000;
 }
 
 auto solve(const std::string &filename)
@@ -56,17 +38,16 @@ auto solve(const std::string &filename)
             }
     }
 
-    auto part1 = 0LL, part2 = 0LL;
+    auto retval = std::make_pair(0LL, 0LL);
 
     for (auto g1 = galaxies.begin(); g1 != galaxies.end(); g1++)
         for (auto g2 = g1 + 1; g2 != galaxies.end(); g2++)
         {
-            auto dists = get_dists(*g1, *g2);
-            part1 += dists.first;
-            part2 += dists.second;
+            add_dist(g1->first, g2->first, row_has_galaxies, retval);
+            add_dist(g1->second, g2->second, col_has_galaxies, retval);
         }
 
-    return std::make_pair(part1, part2);
+    return retval;
 }
 
 int main()
