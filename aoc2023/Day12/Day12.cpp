92b41051804f8063b41f4cba37bb3d33dfd23ef5
Stephen Houchen <shouchen@outlook.com>
2023-12-12 17:40:32 -0800

    Added nice recursive lambda to Day 12

--- aoc2023/Day12/Day12.cpp
+++ aoc2023/Day12/Day12.cpp
@@ -3,61 +3,61 @@
 #include <vector>
 #include <cassert>
 
-using Memo = std::vector<std::vector<std::vector<long long>>>;
-
-long long recur(int s_index, int v_index, int curr_run_len, std::string &s, std::vector<int> &v, Memo &memo)
-{
-    if (s_index >= s.size())
-        return (v_index == v.size()) ? 1 : 0;
-
-    auto &retval = memo[s_index][v_index][curr_run_len];
-    if (retval != -1)
-        return retval;
-
-    retval = 0;
-
-    if ((s[s_index] == '.' || s[s_index] == '?') && curr_run_len == 0)
-        retval += recur(s_index + 1, v_index, 0, s, v, memo);
-
-    if ((s[s_index] == '#' || s[s_index] == '?') && v_index < v.size())
-    {
-        if (curr_run_len + 1 < v[v_index])
-            retval += recur(s_index + 1, v_index, curr_run_len + 1, s, v, memo);
-        else if (s_index + 1 == s.size() || s[s_index + 1] != '#')
-            retval += recur(s_index + 2, v_index + 1, 0, s, v, memo);
-    }
-
-    return retval;
-}
-
 auto solve(std::string s, std::vector<int> v)
 {
-    auto max_run_len = *max_element(v.begin(), v.end());
-    Memo memo(
+    auto max_run_len = *std::max_element(v.begin(), v.end());
+    auto memo = std::vector<std::vector<std::vector<long long>>>(
         s.size(),
         std::vector<std::vector<long long>>(
             v.size() + 1,
             std::vector<long long>(max_run_len + 1, -1)));
 
-    return recur(0, 0, 0, s, v, memo);
+    auto recur = [&](auto &self, int s_index, int v_index, int curr_run_len) -> long long
+    {
+        if (s_index >= s.size())
+            return (v_index == v.size()) ? 1 : 0;
+
+        auto &retval = memo[s_index][v_index][curr_run_len];
+        if (retval != -1)
+            return retval;
+
+        retval = 0;
+        auto curr = s[s_index], next = s[s_index + 1];
+
+        if ((curr == '.' || curr == '?') && curr_run_len == 0)
+            retval += self(self, s_index + 1, v_index, 0);
+
+        if ((curr == '#' || curr == '?') && v_index < v.size())
+        {
+            if (curr_run_len < v[v_index] - 1)
+                retval += self(self, s_index + 1, v_index, curr_run_len + 1);
+            else if (s_index == s.size() - 1 || next != '#')
+                retval += self(self, s_index + 2, v_index + 1, 0);
+        }
+
+        return retval;
+    };
+    
+    return recur(recur, 0, 0, 0);
 }
 
 auto do_part(const std::string &filename, bool part2)
 {
     std::ifstream file(filename);
+    std::string s;
     auto total = 0LL;
-    std::string data;
 
-    while (file >> data)
+    while (file >> s)
     {
-        std::vector<int> blocks;
+        std::vector<int> v;
         auto comma = ',';
         auto n = 0;
 
         for (;;)
         {
             file >> n;
-            blocks.push_back(n);
+            v.push_back(n);
+
             if (file.peek() == ',')
                 file >> comma;
             else
@@ -66,16 +66,14 @@ auto do_part(const std::string &filename, bool part2)
 
         if (part2)
         {
-            auto temp = data;
-            data = temp + "?" + temp + "?" + temp + "?" + temp + "?" + temp;
+            s = s + "?" + s + "?" + s + "?" + s + "?" + s;
 
-            auto bb = blocks;
+            auto original = v;
             for (auto i = 0; i < 4; i++)
-                for (auto b : bb)
-                    blocks.push_back(b);
+                v.insert(v.end(), original.begin(), original.end());
         }
 
-        total += solve(data, blocks);
+        total += solve(s, v);
     }
 
     return total;
