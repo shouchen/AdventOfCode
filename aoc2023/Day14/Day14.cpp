12f4d3540e9bea8c08591009df5a31d6bbc70c79
Stephen Houchen <shouchen@outlook.com>
2023-12-13 23:38:09 -0800

    Better Day 14 code

--- aoc2023/Day14/Day14.cpp
+++ aoc2023/Day14/Day14.cpp
@@ -16,9 +16,8 @@ void read_grid(const std::string &filename)
         grid.push_back(line);
 }
 
-auto do_part1()
+void tilt_N()
 {
-    // tilt N
     for (auto col = 0; col < grid[0].size(); col++)
     {
         for (;;)
@@ -37,15 +36,90 @@ auto do_part1()
                 break;
         }
     }
+}
 
+void tilt_W()
+{
+    for (auto row = 0; row < grid.size(); row++)
+    {
+        for (;;)
+        {
+            auto moved = false;
+            for (auto col = 0; col < grid[0].size() - 1; col++)
+            {
+                if (grid[row][col] == '.' && grid[row][col + 1] == 'O')
+                {
+                    grid[row][col] = 'O', grid[row][col + 1] = '.';
+                    moved = true;
+                }
+            }
+
+            if (!moved)
+                break;
+        }
+    }
+}
+
+void tilt_S()
+{
+    for (auto col = 0; col < grid[0].size(); col++)
+    {
+        for (;;)
+        {
+            auto moved = false;
+            for (auto row = 0; row < grid.size() - 1; row++)
+            {
+                if (grid[row][col] == 'O' && grid[row + 1][col] == '.')
+                {
+                    grid[row][col] = '.', grid[row + 1][col] = 'O';
+                    moved = true;
+                }
+            }
+
+            if (!moved)
+                break;
+        }
+    }
+}
+
+void tilt_E()
+{
+    for (auto row = 0; row < grid.size(); row++)
+    {
+        for (;;)
+        {
+            auto moved = false;
+            for (auto col = 0; col < grid[0].size() - 1; col++)
+            {
+                if (grid[row][col] == 'O' && grid[row][col + 1] == '.')
+                {
+                    grid[row][col] = '.', grid[row][col + 1] = 'O';
+                    moved = true;
+                }
+            }
+
+            if (!moved)
+                break;
+        }
+    }
+}
+
+auto get_load()
+{
     auto total = 0LL;
 
     for (auto i = 0; i < grid.size(); i++)
         for (auto j = 0; j < grid[0].size(); j++)
             if (grid[i][j] == 'O')
                 total += grid.size() - i;
-
+    
     return total;
+}
+
+auto do_part1()
+{
+    tilt_N();
+    return get_load();
 }
 
 std::map<std::vector<std::string>, int> seen;
@@ -53,96 +127,12 @@ int period_start = -1, period_len = -1;
 
 auto do_part2()
 {
-    auto cycle = 1;
-    for (cycle = 1; ; cycle++)
+    for (auto cycle = 1; ; cycle++)
     {
-        // tilt N
-        for (auto col = 0; col < grid[0].size(); col++)
-        {
-            for (;;)
-            {
-                auto moved = false;
-                for (auto row = 0; row < grid.size() - 1; row++)
-                {
-                    if (grid[row][col] == '.' && grid[row + 1][col] == 'O')
-                    {
-                        grid[row][col] = 'O', grid[row + 1][col] = '.';
-                        moved = true;
-                    }
-                }
-
-                if (!moved)
-                    break;
-            }
-        }
-
-
-        // tilt W
-        for (auto row = 0; row < grid.size(); row++)
-        {
-            for (;;)
-            {
-                auto moved = false;
-                for (auto col = 0; col < grid[0].size() - 1; col++)
-                {
-                    if (grid[row][col] == '.' && grid[row][col + 1] == 'O')
-                    {
-                        grid[row][col] = 'O', grid[row][col + 1] = '.';
-                        moved = true;
-                    }
-                }
-
-                if (!moved)
-                    break;
-            }
-        }
-
-        // tilt S
-        for (auto col = 0; col < grid[0].size(); col++)
-        {
-            for (;;)
-            {
-                auto moved = false;
-                for (auto row = 0; row < grid.size() - 1; row++)
-                {
-                    if (grid[row][col] == 'O' && grid[row + 1][col] == '.')
-                    {
-                        grid[row][col] = '.', grid[row + 1][col] = 'O';
-                        moved = true;
-                    }
-                }
-
-                if (!moved)
-                    break;
-            }
-        }
-
-        // tilt E
-        for (auto row = 0; row < grid.size(); row++)
-        {
-            for (;;)
-            {
-                auto moved = false;
-                for (auto col = 0; col < grid[0].size() - 1; col++)
-                {
-                    if (grid[row][col] == 'O' && grid[row][col + 1] == '.')
-                    {
-                        grid[row][col] = '.', grid[row][col + 1] = 'O';
-                        moved = true;
-                    }
-                }
-
-                if (!moved)
-                    break;
-            }
-        }
-
-        auto total = 0LL;
-
-        for (auto i = 0; i < grid.size(); i++)
-            for (auto j = 0; j < grid[0].size(); j++)
-                if (grid[i][j] == 'O')
-                    total += grid.size() - i;
+        tilt_N();
+        tilt_W();
+        tilt_S();
+        tilt_E();
 
         if (seen.find(grid) == seen.end())
         {
@@ -159,23 +149,13 @@ auto do_part2()
     auto index = (1000000000 - period_start) % period_len + period_start;
 
     for (auto &i : seen)
-    {
         if (i.second == index)
         {
             grid = i.first;
-
-            auto total = 0LL;
-
-            for (auto i = 0; i < grid.size(); i++)
-                for (auto j = 0; j < grid[0].size(); j++)
-                    if (grid[i][j] == 'O')
-                        total += grid.size() - i;
-
-            return total;
+            break;
         }
-    }
 
-    return -1LL;
+    return get_load();;
 }
 
 int main()
