7ff91a221fe1f6e682db7505e99defcf1b55b3e8
Stephen Houchen <shouchen@outlook.com>
2023-12-20 06:49:20 -0800

    Some Day 20 clean-up

--- aoc2023/Day20/Day20.cpp
+++ aoc2023/Day20/Day20.cpp
@@ -7,17 +7,17 @@
 #include <numeric>
 #include <cassert>
 
-struct Mod
+struct Module
 {
     enum Type { FlipFlop, Conjunction, Broadcaster } type;
     std::string name;
     std::vector<std::string> next;
     bool ff_on = false;
     std::map<std::string, int> remember;
-    std::map<std::string, int> first_high;
 };
 
-std::map<std::string, Mod> mods;
+std::map<std::string, Module> mods;
+std::string rx_pred;
 
 void read_input_lines(const std::string &filename)
 {
@@ -29,15 +29,16 @@ void read_input_lines(const std::string &filename)
         std::istringstream is(line);
         is >> name >> arrow;
 
-        auto type = Mod::Type::Broadcaster;
+        auto type = Module::Type::Broadcaster;
 
         if (name != "broadcaster")
         {
-            type = (name.front() == '%') ? Mod::Type::FlipFlop : Mod::Type::Conjunction;
+            type = (name.front() == '%')
+                ? Module::Type::FlipFlop : Module::Type::Conjunction;
             name = name.substr(1);
         }
 
-        auto mod = Mod{ type, name };
+        auto mod = Module{ type, name };
 
         std::string name;
         while (is >> name)
@@ -50,20 +51,32 @@ void read_input_lines(const std::string &filename)
     }
 
     for (auto &i : mods)
+    {
+        auto &from = i.second;
         for (auto &j : i.second.next)
-            mods[j].remember[i.second.name] = 0;
+            mods[j].remember[from.name] = 0;
+
+        if (from.next.size() == 1 && from.next.front() == "rx")
+        {
+            assert(rx_pred.empty());
+            rx_pred = from.name;
+        }
+    }
+
+    assert(!rx_pred.empty());
 }
 
-struct Elem
-{
-    std::string src, dest;
-    int pulse;
-};
-
-auto count_high = 0LL, count_low = 0LL, button_presses = 0LL;
+auto count_high = 0, count_low = 0, button_presses = 0;
+std::map<std::string, int> first_high;
 
 void press_button()
 {
+    struct Elem
+    {
+        std::string src, dest;
+        int pulse;
+    };
+
     button_presses++;
 
     std::queue<Elem> q;
@@ -76,18 +89,18 @@ void press_button()
         elem.pulse ? count_high++ : count_low++;
         auto &mod = mods[elem.dest];
 
-        if (elem.dest == "ll" && elem.pulse == 1)
-            if (mod.first_high.find(elem.src) == mod.first_high.end())
-                mod.first_high[elem.src] = button_presses;
+        if (elem.dest == rx_pred && elem.pulse == 1)
+            if (first_high.find(elem.src) == first_high.end())
+                first_high[elem.src] = button_presses;
 
         switch (mod.type)
         {
-        case Mod::Type::Broadcaster:
+        case Module::Type::Broadcaster:
             for (auto &a : mod.next)
                 q.push(Elem{ mod.name, a, elem.pulse });
             break;
 
-        case Mod::Type::FlipFlop:
+        case Module::Type::FlipFlop:
             if (elem.pulse == 0)
             {
                 mod.ff_on = !mod.ff_on;
@@ -97,7 +110,7 @@ void press_button()
             }
             break;
 
-        case Mod::Type::Conjunction:
+        case Module::Type::Conjunction:
             mod.remember[elem.src] = elem.pulse;
             bool all_high = true;
             for (auto &i : mod.remember)
@@ -122,18 +135,19 @@ auto do_part1()
     return count_low * count_high;
 }
 
+// Observing the configuration of the input data, rx is fed only by ll, which is
+// a conjunction module. It, in turn, is fed by four modules (kl, vm, kv, and
+// vb). Looking at the input and the debug output as the program runs, it's
+// evident that these are counters that hit 1 periodically. The answer for part
+// 2 will be the lcm of all of these since that would cause "rx" to receive a
+// low signal for the first time.
 auto do_part2()
 {
-    // Observing the configuration of the input data, rx is fed only by ll, which is a conjunction module. It,
-    // in turn, is fed by four modules (kl, vm, kv, and vb). Looking at the input and the debug output as the
-    // program runs, it's evident that these are counters that hit 1 periodically. The answer for part 2 will
-    // be the lcm of all of these since that would cause "rx" to receive a low signal for the first time.
-
-    while (mods["ll"].first_high.size() < mods["ll"].remember.size())
+    while (first_high.size() < mods[rx_pred].remember.size())
         press_button();
 
     auto retval = 1LL;
-    for (auto i : mods["ll"].first_high)
+    for (auto i : first_high)
         retval = std::lcm(retval, i.second);
     return retval;
 }
