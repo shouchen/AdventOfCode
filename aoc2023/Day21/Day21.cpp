37b4468f9f583ee26f0490470192bcdc298a2c96
Stephen Houchen <shouchen@outlook.com>
2023-12-22 13:30:28 -0800

    Day 21 clean-up plus some Day 22 clean-up

--- aoc2023/Day21/Day21.cpp
+++ aoc2023/Day21/Day21.cpp
@@ -7,7 +7,7 @@
 #include <cassert>
 
 std::vector<std::string> grid;
-auto s_row = -1, s_col = -1;
+auto s_row = 0, s_col = 0;
 
 void read_grid(const std::string &filename)
 {
@@ -16,129 +16,113 @@ void read_grid(const std::string &filename)
 
     while (std::getline(file, line))
     {
-        auto p = line.find('S');
+        auto p = int(line.find('S'));
         if (p != std::string::npos)
-            s_row = grid.size(), s_col = p;
+            s_row = int(grid.size()), s_col = p;
 
         grid.push_back(line);
     }
 }
 
-auto do_part1(const std::string &filename, int steps)
+auto solve(int steps, bool multiple = false)
 {
-    struct Elem
-    {
-        int row, col, dist;
-    };
+    struct Elem { int steps, row, col; };
 
-    std::set<std::pair<int, int>> reachable;
-    std::set<std::pair<std::pair<int, int>, int>> seen;
+    static const int dx[] = { 0, 1, 0, -1 }, dy[] = { 1, 0, -1, 0 };
 
+    auto n = int(grid.size()), m = int(grid[0].size());
+
+    std::set<std::pair<int, int>> seen;
+    std::vector<std::pair<int, int>> ret;
     std::queue<Elem> q;
-    q.push({ s_row, s_col, 0 });
+
+    q.push({ steps, s_row, s_col });
+    seen.insert(std::make_pair(s_row, s_col));
 
     while (!q.empty())
     {
-        auto elem = q.front(); q.pop();
-        auto row = elem.row, col = elem.col, dist = elem.dist;
+        steps = q.front().steps;
+        auto x = q.front().row, y = q.front().col;
+        q.pop();
 
-        if (seen.find(std::make_pair(std::make_pair(row, col), dist)) != seen.end())
+        if (steps % 2 == 0)
+            ret.push_back(std::make_pair(x, y));
+
+        if (steps == 0)
             continue;
-        seen.insert(std::make_pair(std::make_pair(row, col), dist));
 
-        if (grid[row][col] == '#')
-            continue;
-
-        if (dist == steps)
+        for (auto i = 0; i < 4; i++)
         {
-            reachable.insert({ row, col });
-            continue;
-        }
+            auto nx = x + dx[i], ny = y + dy[i];
+            auto nnx = nx, nny = ny;
 
-        if (row > 0 && grid[row - 1][col] != '#')
-            q.push({ row - 1, col, dist + 1 });
-        if (row < grid.size() - 1 && grid[row + 1][col] != '#')
-            q.push({ row + 1, col, dist + 1 });
-        if (col > 0 && grid[row][col - 1] != '#')
-            q.push({ row, col - 1, dist + 1 });
-        if (col < grid[0].size() - 1 && grid[row][col + 1] != '#')
-            q.push({ row, col + 1, dist + 1 });
+            if (multiple)
+            {
+                while (nx < 0) nx += n;
+                while (ny < 0) ny += m;
+
+                nx = (nx + n) % n;
+                ny = (ny + m) % m;
+            }
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+
+            if (grid[nx][ny] == '#')
+                continue;
+
+            if (seen.find(std::make_pair(nnx, nny)) != seen.end())
+                continue;
+
+            seen.insert(std::make_pair(nnx, nny));
+            q.push({ steps - 1, nnx, nny });
+        }
     }
 
-    return reachable.size();
+    return ret.size();
 }
 
-char get_cell(long long row, long long col)
+long long do_part2(long long unsigned target)
 {
-    auto rows = grid.size(), cols = grid[0].size();
-    row %= rows, col %= cols;
-    if (row < 0) row += rows;
-    if (col < 0) col += cols;
-    return grid[row][col];
-}
-
-int dx[] = { 0, -1, 0, 1 };
-int dy[] = { -1, 0, 1, 0 };
-
-long long do_part2(long long target)
-{
-    auto grid_size = grid.size();
-    auto grids = target / grid.size();
-    auto rem = target % grid.size();
+    // assert grid is square
+    // asserts starts in middle
+    // assume init row, col is otherwise free
+    // assume not dense maze
 
     // Looking at the specific input data, make a few assumptions: (1) input
     // grid is square, (2) straight vertical and horizontal lines are open from
     // S position. So the result should be quadratic: 
     //     (rem), (rem + grid_size), (rem + 2 * grid_size), ...
-    std::vector<long long> sequence;
-    std::set<std::pair<long long, long long>> work;
-    auto steps = 0;
 
-    work.insert({ s_row, s_col });
+    auto sol = 0LLU;
+
+    auto steps = target - 65LLU;
 
     // Three numbers in the series is enough to extrapolate to large vales if
     // this is quadratic. Similar to part1.
-    for (auto n = 0; n < 3; n++)
-    {
-        for (; steps < n * grid_size + rem; steps++)
-        {
-            std::set<std::pair<long long, long long>> next;
-
-            for (auto coord : work)
-                for (auto dir = 0; dir < 4; dir++)
-                {
-                    auto dest = std::make_pair(coord.first + dy[dir], coord.second + dx[dir]);
-                    if (get_cell(dest.first, dest.second) != '#')
-                        next.insert(dest);
-                }
-
-            work = next;
-        }
-
-        sequence.push_back(work.size());
-    }
+    auto a0 = solve(0 * 131 + 65, true);
+    auto a1 = solve(1 * 131 + 65, true);
+    auto a2 = solve(2 * 131 + 65, true);
 
     // Solve for the quadratic coefficients using method of Day 9
-    auto c = sequence[0];
-    auto a_plus_b = sequence[1] - c;
-    auto four_a_plus_two_b = sequence[2] - c;
-    auto two_a = four_a_plus_two_b - (2 * a_plus_b);
-    auto a = two_a / 2;
-    auto b = a_plus_b - a;
+    auto b0 = a0;
+    auto b1 = a1 - a0;
+    auto b2 = a2 - a1;
+
+    auto x = steps / 131;
 
     // Extrapolate
-    auto n = grids;
-    return a * (n * n) + b * n + c;
+    return ((b0 + b1 * x + (x * (x - 1) / 2) * (b2 - b1)));
 }
 
 int main()
 {
     read_grid("input.txt");
 
-    auto part1 = do_part1("input.txt", 64);
+    auto part1 = solve(64);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = do_part2(26501365LL);
+    auto part2 = do_part2(26501365LLU);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 3677);
