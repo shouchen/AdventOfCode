1d424f5b9bb02dc4321de3270f1e96c7ae7a4bba
Stephen Houchen <shouchen@outlook.com>
2023-12-27 07:35:33 -0800

    Day 25 - removed hard-coded max, getting from input

--- aoc2023/Day25/Day25.cpp
+++ aoc2023/Day25/Day25.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <array>
 #include <map>
 #include <random>
 #include <cassert>
@@ -49,15 +48,15 @@ struct DSU
     }
 };
 
-auto rng = std::mt19937(unsigned(time(nullptr)));
-std::vector<std::vector<std::pair<int, int>>> adj; // 2D grid of pairs (second is edge name)
-std::vector<std::array<int, 2>> edges; // first, second are the vertex indices (could just be pair)
+std::vector<std::vector<std::pair<int, int>>> adj;
+std::vector<std::pair<int, int>> edges;
 auto V = 0, E = 0;
+auto rng = std::mt19937(unsigned(time(nullptr)));
 
 void read_input(const std::string &filename)
 {
     std::ifstream file("input.txt");
-    adj.resize(1500); // must be large enough for all nodes (which is more than input lines)
+//    adj.resize(1500); // must be large enough for all vertices (which is more than input lines)
 
     std::string line, s, t;
     std::map<std::string, int> name_to_index;
@@ -68,18 +67,17 @@ void read_input(const std::string &filename)
 
         ss >> s;
         s.pop_back();
-
-        if (!name_to_index.count(s))
-            name_to_index[s] = V++;
+        auto s_index = name_to_index.count(s) ? name_to_index[s] : (name_to_index[s] = V++);
 
         while (ss >> t)
         {
-            if (!name_to_index.count(t))
-                name_to_index[t] = V++;
+            auto t_index = name_to_index.count(t) ? name_to_index[t] : (name_to_index[t] = V++);
+            while (adj.size() < name_to_index.size())
+                adj.push_back(std::vector<std::pair<int, int>>());
 
-            adj[name_to_index[s]].emplace_back(name_to_index[t], int(edges.size()));
-            adj[name_to_index[t]].emplace_back(name_to_index[s], int(edges.size()));
-            edges.push_back({ name_to_index[s], name_to_index[t] });
+            adj[s_index].push_back({ t_index, int(edges.size()) });
+            adj[t_index].push_back({ s_index, int(edges.size()) });
+            edges.push_back({ s_index, t_index });
         }
     }
 
@@ -91,7 +89,7 @@ auto do_karger()
     DSU dsu(V);
 
     auto distinct = [&](int e) {
-        return dsu.Find(edges[e][0]) != dsu.Find(edges[e][1]);
+        return dsu.Find(edges[e].first) != dsu.Find(edges[e].second);
     };
 
     auto getE = [&]() -> int {
@@ -106,7 +104,7 @@ auto do_karger()
     for (auto n = V; n > 2; --n)
     {
         auto temp = edges[getE()];
-        dsu.Union(temp[0], temp[1]);
+        dsu.Union(temp.first, temp.second);
     }
 
     std::vector<int> ans;
