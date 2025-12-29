37d9aee325cb7877cbfe34231f1d1dc5a26d84cf
Stephen Houchen <shouchen@outlook.com>
2025-12-29 12:20:08 -0800

    Day 4 both parts, clean solution

--- aoc2025/Day04/Day04.cpp
+++ aoc2025/Day04/Day04.cpp
@@ -4,85 +4,72 @@
 #include <string>
 #include <cassert>
 
-auto is_roll_accessible(const std::vector<std::string> &grid, int row, int col)
+std::vector<std::string> grid;
+
+auto read_data(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line))
+        grid.push_back(line);
+}
+
+auto is_roll_accessible(int row, int col)
 {
     if (grid[row][col] != '@')
         return false;
 
     auto adjacents = 0;
 
-    for (int r = row - 1; r <= row + 1; r++)
-        for (int c = col - 1; c <= col + 1; c++)
-        {
-            if (r == row && c == col)
-                continue;
+    auto min_row = std::max(0, row - 1), max_row = std::min(static_cast<int>(grid.size()) - 1, row + 1);
+    auto min_col = std::max(0, col - 1), max_col = std::min(static_cast<int>(grid[0].size()) - 1, col + 1);
 
-            if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size())
-                continue;
-
-            if (grid[r][c] == '@')
+    for (auto r = min_row; r <= max_row; r++)
+        for (auto c = min_col; c <= max_col; c++)
+            if ((r != row || c != col) && grid[r][c] == '@')
                 adjacents++;
-        }
 
     return adjacents < 4;
 }
 
-auto solve(const std::string &filename)
+auto solve(bool remove)
 {
-    std::ifstream file(filename);
-    std::vector<std::string> grid;
-    std::string line;
+    auto any_removed = false;
     auto retval = 0;
 
-    while (std::getline(file, line))
-        grid.push_back(line);
-
-    for (auto row = 0; row < grid.size(); row++)
-        for (auto col = 0; col < grid[0].size(); col++)
-            if (is_roll_accessible(grid, row, col))
-                retval++;
-
-    return retval;
-}
-
-auto solve2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::vector<std::string> grid;
-    std::string line;
-    auto retval = 0;
-
-    while (std::getline(file, line))
-        grid.push_back(line);
-
-    for (;;)
+    do
     {
-        bool any_removed = false;
+        any_removed = false;
+
         for (auto row = 0; row < grid.size(); row++)
             for (auto col = 0; col < grid[0].size(); col++)
-                if (is_roll_accessible(grid, row, col))
+                if (is_roll_accessible(row, col))
                 {
-                    grid[row][col] = '.';
                     retval++;
-                    any_removed = true;
-                }
 
-        if (!any_removed)
-            break;
-    }
+                    if (remove)
+                    {
+                        grid[row][col] = '.';
+                        any_removed = true;
+                    }
+                }
+    } while (any_removed);
 
     return retval;
 }
 
 int main()
 {
-    auto part1 = solve("input.txt");
+    read_data("input.txt");
+
+    auto part1 = solve(false);
     std::cout << "Part One: " << part1 << std::endl;
 
-    auto part2 = solve2("input.txt");
+    auto part2 = solve(true);
     std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 1464);
-    assert(part2 == 8409 );
+    assert(part2 == 8409);
     return 0;
 }
