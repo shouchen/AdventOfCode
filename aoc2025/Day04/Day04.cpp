510bbcac898a669cc96637a99f1f4194aaae738e
Stephen Houchen <shouchen@outlook.com>
2025-12-29 11:50:37 -0800

    Dirty solution to Day 4, part one

---
+++ aoc2025/Day04/Day04.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+auto is_roll_accessible(const std::vector<std::string> &grid, int row, int col)
+{
+    if (grid[row][col] != '@')
+        return false;
+
+    auto adjacents = 0;
+
+    for (int r = row - 1; r <= row + 1; r++)
+        for (int c = col - 1; c <= col + 1; c++)
+        {
+            if (r == row && c == col)
+                continue;
+
+            if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size())
+                continue;
+
+            if (grid[r][c] == '@')
+                adjacents++;
+        }
+
+    return adjacents < 4;
+}
+
+auto solve(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::vector<std::string> grid;
+    std::string line;
+    auto retval = 0;
+
+    while (std::getline(file, line))
+        grid.push_back(line);
+
+    for (auto row = 0; row < grid.size(); row++)
+        for (auto col = 0; col < grid[0].size(); col++)
+            if (is_roll_accessible(grid, row, col))
+                retval++;
+
+    return retval;
+}
+
+int main()
+{
+    auto part1 = solve("input.txt");
+    std::cout << "Part One: " << part1 << std::endl;
+
+    //auto part2 = solve("input.txt");
+    //std::cout << "Part Two: " << part2 << std::endl;
+
+    assert(part1 == 1464);
+    //assert(part2 == );
+    return 0;
+}
