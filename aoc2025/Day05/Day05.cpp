4424147e25772b1811f33f1d1cb732451d450ffb
Stephen Houchen <shouchen@outlook.com>
2025-12-29 14:50:31 -0800

    Simplified Day 5 code

--- aoc2025/Day05/Day05.cpp
+++ aoc2025/Day05/Day05.cpp
@@ -2,25 +2,50 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <string>
 #include <cassert>
 
 std::vector<std::pair<unsigned long long, unsigned long long>> ranges;
+
+auto coalesce_range()
+{
+    for (auto a = 0; a < ranges.size(); a++)
+        for (auto b = 0; b < ranges.size(); b++)
+        {
+            if (a == b)
+                continue;
+
+            // Extend A left and delete B
+            if (ranges[b].first < ranges[a].first && ranges[b].second >= ranges[a].first && ranges[b].second <= ranges[a].second)
+            {
+                ranges[a].first = ranges[b].first;
+                ranges.erase(ranges.begin() + b);
+                return true;
+            }
+
+            // Just delete A
+            if (ranges[b].first <= ranges[a].first && ranges[b].second >= ranges[a].second)
+            {
+                ranges.erase(ranges.begin() + a);
+                return true;
+            }
+        }
+
+    return false;
+}
 
 auto is_fresh(unsigned long long ingredient)
 {
     for (const auto &range : ranges)
         if (ingredient >= range.first && ingredient <= range.second)
             return true;
- 
+
     return false;
 }
 
-auto solve1(const std::string &filename)
+auto do_part1(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
-    auto retval = 0;
 
     while (std::getline(file, line) && line.length())
     {
@@ -32,75 +57,21 @@ auto solve1(const std::string &filename)
         ranges.push_back(std::make_pair(from, to));
     }
 
+    while(coalesce_range());
+
+    auto retval = 0;
     auto ingredient = 0ULL;
+
     while (file >> ingredient)
-    {
         if (is_fresh(ingredient))
             retval++;
-    }
 
     return retval;
 }
 
-auto condense_ranges()
+auto do_part2()
 {
-    for (auto a = 0; a < ranges.size(); a++)
-        for (auto b = a + 1; b < ranges.size(); b++)
-        {
-            // Extend A left and delete B
-            if (ranges[b].first < ranges[a].first && ranges[b].second >= ranges[a].first && ranges[b].second <= ranges[a].second)
-            {
-                ranges[a].first = ranges[b].first;
-                ranges.erase(ranges.begin() + b);
-                return true;
-            }
-
-            // Extend A right and delete B
-            if (ranges[b].second > ranges[a].second && ranges[b].first >= ranges[a].first && ranges[b].first <= ranges[a].second)
-            {
-                ranges[a].second = ranges[b].second;
-                ranges.erase(ranges.begin() + b);
-                return true;
-            }
-
-            // Just delete A
-            if (ranges[b].first <= ranges[a].first && ranges[b].second >= ranges[a].second)
-            {
-                ranges.erase(ranges.begin() + a);
-                return true;
-            }
-
-            // Just delete B
-            if (ranges[b].first >= ranges[a].first && ranges[b].first <= ranges[a].second &&
-                ranges[b].second >= ranges[a].first && ranges[b].second <= ranges[a].second)
-            {
-                ranges.erase(ranges.begin() + b);
-                return true;
-            }
-        }
-
-    return false;
-}
-
-auto solve2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
     auto retval = 0LL;
-
-    ranges.clear();
-
-    while (std::getline(file, line) && line.length())
-    {
-        std::stringstream ss(line);
-        auto from = 0ULL, to = 0ULL;
-        auto dash = '-';
-
-        ss >> from >> dash >> to;
-        ranges.push_back(std::make_pair(from, to));
-    }
-
-    while (condense_ranges());
 
     for (const auto &range : ranges)
         retval += range.second - range.first + 1;
@@ -110,11 +81,11 @@ auto solve2(const std::string &filename)
 
 int main()
 {
-    auto part1 = solve1("input.txt");
+    auto part1 = do_part1("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
     assert(part1 == 611);
 
-    auto part2 = solve2("input.txt");
+    auto part2 = do_part2();
     std::cout << "Part Two: " << part2 << std::endl;
     assert(part2 == 345995423801866);
 
