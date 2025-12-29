144fe66825aa77b12be22ebcda92df8749a9da89
Stephen Houchen <shouchen@outlook.com>
2025-12-29 11:57:44 -0800

    Quick and dirty Day 4 (both parts)

--- aoc2025/Day04/Day04.cpp
+++ aoc2025/Day04/Day04.cpp
@@ -45,15 +45,44 @@ auto solve(const std::string &filename)
     return retval;
 }
 
+auto solve2(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::vector<std::string> grid;
+    std::string line;
+    auto retval = 0;
+
+    while (std::getline(file, line))
+        grid.push_back(line);
+
+    for (;;)
+    {
+        bool any_removed = false;
+        for (auto row = 0; row < grid.size(); row++)
+            for (auto col = 0; col < grid[0].size(); col++)
+                if (is_roll_accessible(grid, row, col))
+                {
+                    grid[row][col] = '.';
+                    retval++;
+                    any_removed = true;
+                }
+
+        if (!any_removed)
+            break;
+    }
+
+    return retval;
+}
+
 int main()
 {
     auto part1 = solve("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
 
-    //auto part2 = solve("input.txt");
-    //std::cout << "Part Two: " << part2 << std::endl;
+    auto part2 = solve2("input.txt");
+    std::cout << "Part Two: " << part2 << std::endl;
 
     assert(part1 == 1464);
-    //assert(part2 == );
+    assert(part2 == 8409 );
     return 0;
 }
