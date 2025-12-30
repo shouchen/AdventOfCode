99d928ac8965a50026f979e21b718a3b96592ce0
Stephen Houchen <shouchen@outlook.com>
2025-12-29 23:55:18 -0800

    Day 7 Part 2 Solution

--- aoc2025/Day07/Day07.cpp
+++ aoc2025/Day07/Day07.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
+#include <map>
 #include <cassert>
-
-std::vector<std::string> grid;
-
-void dump_grid()
-{
-    std::cout << std::endl;
-
-    for (auto &line : grid)
-        std::cout << line << std::endl;
-}
 
 auto do_part1(const std::string &filename)
 {
+    std::vector<std::string> grid;
     std::ifstream file(filename);
     std::string line;
     auto retval = 0;
 
     while (file >> line)
         grid.push_back(line);
-
-    //dump_grid();
 
     for (auto row = 1; row < grid.size(); row++)
         for (auto col = 0; col < grid[0].size(); col++)
@@ -35,9 +24,39 @@ auto do_part1(const std::string &filename)
                     grid[row][col - 1] = grid[row][col + 1] = '|', retval++;
             }
 
-    //dump_grid();
-
     return retval;
+}
+
+std::map<std::pair<int, int>, unsigned long long> memo;
+
+auto recur(const std::vector<std::string> &grid, int row, int col)
+{
+    if (row >= grid.size())
+        return 1ULL;
+
+    auto x = memo.find({ row, col });
+    if (x != memo.end())
+        return x->second;
+
+    auto retval = (grid[row][col] == '^')
+        ? recur(grid, row + 1, col - 1) + recur(grid, row + 1, col + 1)
+        : recur(grid, row + 1, col);
+
+    memo[{row, col}] = retval;
+    return retval;
+}
+
+auto do_part2(const std::string &filename)
+{
+    std::vector<std::string> grid;
+    std::ifstream file(filename);
+    std::string line;
+
+    while (file >> line)
+        grid.push_back(line);
+
+    auto row = 0, col = int(grid[0].find('S'));
+    return recur(grid, row, col);
 }
 
 int main()
@@ -46,9 +65,9 @@ int main()
     std::cout << "Part One: " << part1 << std::endl;
     assert(part1 == 1533);
 
-    //auto part2 = do_part2("input.txt");
-    //std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == );
+    auto part2 = do_part2("input.txt");
+    std::cout << "Part Two: " << part2 << std::endl;
+    assert(part2 == 10733529153890);
 
     return 0;
 }
