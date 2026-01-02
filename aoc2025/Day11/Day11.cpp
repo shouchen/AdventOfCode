341250a10d430472ac8983310e8ff1af02cd0ed8
Stephen Houchen <shouchen@outlook.com>
2026-01-02 00:18:12 -0800

    Cleaned up Day 11 solutions

--- aoc2025/Day11/Day11.cpp
+++ aoc2025/Day11/Day11.cpp
@@ -12,62 +12,48 @@ struct Node
     std::vector<Node *> adjacents;
 };
 
-auto graph = std::map<std::string, Node *>();
+std::map<std::string, Node *> graph;
+std::map<std::pair<Node *, Node *>, unsigned long long> memo;
 
 auto read_data(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string token;
     Node *from = nullptr, *to = nullptr;
-    auto retval = 0;
 
     while (file >> token)
     {
         if (token.back() == ':')
         {
             token.pop_back();
-            if (graph.find(token) == graph.end())
-                graph[token] = from = new Node{ token };
-            else
-                from = graph[token];
+
+            from = graph.find(token) == graph.end()
+                ? graph[token] = new Node{ token }
+                : graph[token];
         }
         else
         {
-            if (graph.find(token) == graph.end())
-                graph[token] = to = new Node{ token };
-            else
-                to = graph[token];
+            to = graph.find(token) == graph.end()
+                ? graph[token] = new Node{ token }
+                : graph[token];
 
-            if (from != nullptr)
-                from->adjacents.push_back(to);
+            from->adjacents.push_back(to);
         }
     }
-
-    return retval;
 }
-
-std::set<Node *> visited;
-std::map<std::pair<Node *, Node *>, unsigned long long> memo;
 
 auto recur(Node *curr, Node *dest)
 {
     if (curr == dest)
-        return (visited.find(graph["dac"]) != visited.end() && visited.find(graph["fft"]) != visited.end()) ? 1ULL : 1ULL;
+        return 1ULL;
 
     auto temp = memo.find({ curr, dest });
     if (temp != memo.end())
         return temp->second;
 
-    if (visited.find(curr) != visited.end())
-        return 0ULL;
-
-    visited.insert(curr);
-
     auto retval = 0ULL;
     for (auto next : curr->adjacents)
         retval += recur(next, dest);
-
-    visited.erase(curr);
 
     memo.insert({ { curr, dest }, retval });
     return retval;
@@ -75,36 +61,20 @@ auto recur(Node *curr, Node *dest)
 
 auto do_part1()
 {
-    Node *you = graph["you"];
-    Node *out = graph["out"];
-
-    return recur(you, out);
+    return recur(graph["you"], graph["out"]);
 }
 
 auto do_part2()
 {
-    Node *svr = graph["svr"];
-    Node *out = graph["out"];
-    Node *dac = graph["dac"];
-    Node *fft = graph["fft"];
+    Node *svr = graph["svr"], *out = graph["out"];
+    Node *dac = graph["dac"], *fft = graph["fft"];
 
     auto fft_dac = recur(fft, dac);
     auto dac_fft = recur(dac, fft);
 
-    if (fft_dac)
-    {
-        auto svr_fft = recur(svr, fft);
-        auto dac_out = recur(dac, out);
-
-        return svr_fft * fft_dac * dac_out;
-    }
-    else
-    {
-        auto svr_dac = recur(svr, dac);
-        auto fft_out = recur(fft, out);
-
-        return svr_dac * dac_fft * fft_out;
-    }
+    return fft_dac
+        ? recur(svr, fft) * fft_dac * recur(dac, out)
+        : recur(svr, dac) * dac_fft * recur(fft, out);
 }
 
 int main()
