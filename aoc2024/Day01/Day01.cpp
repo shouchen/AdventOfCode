67b1b1ac5a9c8f058a6affb8476e8eecf07bbe73
Stephen Houchen <shouchen@outlook.com>
2026-01-15 22:51:56 -0800

    Day 1 clean-up

--- aoc2024/Day01/Day01.cpp
+++ aoc2024/Day01/Day01.cpp
@@ -5,62 +5,40 @@
 #include <algorithm>
 #include <cassert>
 
-auto do_part1(const std::string &filename)
+auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
-    std::vector<int> list1, list2;
-    auto n1 = 0, n2 = 0;
-
-    while (file >> n1 >> n2)
-    {
-        list1.push_back(n1);
-        list2.push_back(n2);
-    }
-
-    std::sort(list1.begin(), list1.end());
-    std::sort(list2.begin(), list2.end());
-
-    auto total = 0LL;
-
-    for (auto i = 0; i < list1.size(); i++)
-    {
-        auto dist = std::abs(list1[i] - list2[i]);
-        total += dist;
-    }
-
-    return total;
-}
-
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::vector<int> left;
-    std::map<int, int> right;
+    std::vector<int> left, right;
+    std::map<int, int> right_freq;
+    std::pair<int, int> retval;
     auto n1 = 0, n2 = 0;
 
     while (file >> n1 >> n2)
     {
         left.push_back(n1);
-        right[n2]++;
+        right.push_back(n2);
+        right_freq[n2]++;
     }
 
-    auto total = 0;
-
     for (auto i : left)
-        total += i * right[i];
+        retval.second += i * right_freq[i];
 
-    return total;
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    for (auto i = 0; i < left.size(); i++)
+        retval.first += std::abs(left[i] - right[i]);
+
+    return retval;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
-    std::cout << "Part One: " << part1 << std::endl;
+    auto answer = solve("input.txt");
+    std::cout << "Part One: " << answer.first << std::endl;
+    std::cout << "Part Two: " << answer.second << std::endl;
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-
-    assert(part1 == 936063);
-    assert(part2 == 23150395);
+    assert(answer.first == 936063);
+    assert(answer.second == 23150395);
     return 0;
 }
