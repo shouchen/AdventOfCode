5e88e40b7558fe9b276a95c97d0e6fb7cd730922
Stephen Houchen <shouchen@outlook.com>
2026-01-17 06:46:27 -0800

    Unifying Part 1 and Part 2 approaches for Day 2

--- aoc2024/Day02/Day02.cpp
+++ aoc2024/Day02/Day02.cpp
@@ -5,48 +5,41 @@
 #include <string>
 #include <cassert>
 
-auto is_safe(const std::vector<int> &report)
+bool is_safe(const std::vector<int> &report, bool allow_skip, int start = 0, int prev_val = -1, int dirn = 0)
 {
-    int previous = report[0];
-    bool increases = report[1] > report[0];
+    if (start > report.size() - 1)
+        return true;
 
-    for (auto i = 1; i < report.size(); i++)
+    // Case 1: Not skipping this element
+    if (prev_val == -1) // First element
     {
-        auto delta = report[i] - previous;
-        if (increases)
+        if (is_safe(report, allow_skip, start + 1, report[start], dirn))
+            return true;
+    }
+    else
+    {
+        auto delta = report[start] - prev_val, save_dirn = dirn;
+
+        if (dirn == 0) // Second element (no direction yet)
         {
-            if (delta < 1 || delta > 3)
-                return false;
-        }
-        else
-        {
-            if (delta > -1 || delta < -3)
-                return false;
+            if (delta > 0)
+                dirn = 1;
+            else if (delta < 0)
+                dirn = -1;
         }
 
-        previous = report[i];
+        if ((dirn == 1 && delta >= 1 && delta <= 3) ||
+            (dirn == -1 && delta <= -1 && delta >= -3))
+        {
+            if (is_safe(report, allow_skip, start + 1, report[start], dirn))
+                return true;
+        }
+
+        dirn = save_dirn; // undo direction if we're going to skipped this one 
     }
 
-    return true;
-}
-
-auto is_safe2(const std::vector<int> &report, int skip)
-{
-    std::vector<int> new_report;
-    for (int i = 0; i < report.size(); i++)
-        if (i != skip)
-            new_report.push_back(report[i]);
-
-    return is_safe(new_report);
-}
-
-auto is_safe2(const std::vector<int> &report)
-{
-    for (auto i = 0; i < report.size(); i++)
-        if (is_safe2(report, i))
-            return true;
-
-    return false;
+    // Case 2: Skipping this element
+    return allow_skip && is_safe(report, false, start + 1, prev_val, dirn);
 }
 
 auto solve(const std::string &filename)
@@ -64,10 +57,10 @@ auto solve(const std::string &filename)
         while (ss >> n)
             report.push_back(n);
 
-        if (is_safe(report))
+        if (is_safe(report, false))
             retval.first++;
 
-        if (is_safe2(report))
+        if (is_safe(report, true))
             retval.second++;
     }
 
