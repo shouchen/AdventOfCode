999d50df9a399be58184e12ad259d9737d92e43b
Stephen Houchen <shouchen@outlook.com>
2026-01-20 02:59:23 -0800

    Some Day 6 clean-up

--- aoc2024/Day06/Day06.cpp
+++ aoc2024/Day06/Day06.cpp
@@ -7,24 +7,29 @@
 
 std::vector<std::string> grid;
 std::set<std::pair<int, int>> visited;
+auto init_guard_row = 0, init_guard_col = 0;
 
-auto do_part1(const std::string &filename)
+auto read_input(const std::string &filename)
 {
-    std::ifstream file(filename); // TODO: Just read the file once
+    std::ifstream file(filename);
     std::string line;
-    auto guard_row = 0, guard_col = 0, row_dir = -1, col_dir = 0;
 
     while (std::getline(file, line))
     {
         auto pos = line.find('^');
         if (pos != std::string::npos)
         {
-            guard_row = int(grid.size()), guard_col = int(pos);
+            init_guard_row = int(grid.size()), init_guard_col = int(pos);
             line[pos] = '.';
         }
 
         grid.push_back(line);
     }
+}
+
+auto do_part1()
+{
+    auto guard_row = init_guard_row, guard_col = init_guard_col, row_dir = -1, col_dir = 0;
 
     for (;;)
     {
@@ -84,49 +89,36 @@ auto has_loop(int guard_row, int guard_col, int row_dir, int col_dir)
     }
 }
 
-auto do_part2(const std::string &filename)
+auto do_part2()
 {
-    grid.clear();
-
-    std::ifstream file(filename);
-    std::string line;
-    auto guard_row = 0, guard_col = 0, row_dir = -1, col_dir = 0;
-
-    while (std::getline(file, line))
-    {
-        auto pos = line.find('^');
-        if (pos != std::string::npos)
-        {
-            guard_row = int(grid.size()), guard_col = int(pos);
-            line[pos] = '.';
-        }
-
-        grid.push_back(line);
-    }
-
     auto retval = 0;
 
-    for (auto obs_row = 0; obs_row < int(grid.size()); obs_row++)
-        for (auto obs_col = 0; obs_col < int(grid[obs_row].size()); obs_col++)
-            if (grid[obs_row][obs_col] == '.' && visited.find({ obs_row, obs_col }) != visited.end())
-            {
-                grid[obs_row][obs_col] = '#';
-                if (has_loop(guard_row, guard_col, row_dir, col_dir))
-                    retval++;
+    for (auto &v : visited)
+    {
+        auto obs_row = v.first, obs_col = v.second;
 
-                grid[obs_row][obs_col] = '.';
-            }
+        if (obs_row == init_guard_row && obs_col == init_guard_col)
+            continue;
+
+        grid[obs_row][obs_col] = '#';
+        if (has_loop(init_guard_row, init_guard_col, -1, 0))
+            retval++;
+
+        grid[obs_row][obs_col] = '.';
+    }
 
     return retval;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    read_input("input.txt");
+
+    auto part1 = do_part1();
     std::cout << "Part One: " << part1 << std::endl;
     assert(part1 == 4967);
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = do_part2();
     std::cout << "Part Two: " << part2 << std::endl;
     assert(part2 == 1789);
 
