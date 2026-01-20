4c4b9d14b1029b93fce6d546f66dc9c03ba2bbe7
Stephen Houchen <shouchen@outlook.com>
2026-01-20 01:33:39 -0800

    Cleaned up Day 5, both days, final version.

--- aoc2024/Day05/Day05.cpp
+++ aoc2024/Day05/Day05.cpp
@@ -8,25 +8,18 @@
 
 std::vector<std::pair<int,int>> rules;
 
-auto in_order(const std::vector<int> &numbers)
+auto compare(int lhs, int rhs)
 {
     for (auto &rule : rules)
-    {
-        auto lesser = rule.first, greater = rule.second;
-
-        if (std::find(numbers.begin(), numbers.end(), lesser) == numbers.end() ||
-            std::find(numbers.begin(), numbers.end(), greater) == numbers.end())
-            continue;
-
-        if (std::find(numbers.begin(), numbers.end(), lesser) >
-            std::find(numbers.begin(), numbers.end(), greater))
+        if (rule.first == lhs && rule.second == rhs)
+            return true;
+        else if (rule.first == rhs && rule.second == lhs)
             return false;
-    }
 
-    return true;
+    return false;
 }
 
-auto do_part1(const std::string &filename)
+auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
@@ -38,8 +31,8 @@ auto do_part1(const std::string &filename)
         rules.push_back({ lesser, greater });
     }
 
-    auto n = 0, retval = 0;
-    auto comma = ',';
+    std::pair<int, int> retval;
+    auto comma = ',';    
 
     while (std::getline(file, line))
     {
@@ -47,62 +40,19 @@ auto do_part1(const std::string &filename)
 
         std::stringstream ss(line);
         std::vector<int> numbers;
+        auto n = 0;
 
         while (ss >> n >> comma)
             numbers.push_back(n);
 
-        if (in_order(numbers))
-            retval += numbers[numbers.size() / 2];
-    }
-
-    return retval;
-}
-
-auto sort_numbers(std::vector<int> &numbers)
-{
-    std::sort(numbers.begin(), numbers.end(), [](int a, int b) {
-        for (auto &rule : rules)
-            if (rule.first == a && rule.second == b)
-                return true;
-            else if (rule.first == b && rule.second == a)
-                return false;
-
-        assert(false);
-        return false;
-    });
-}
-
-auto do_part2(const std::string &filename)
-{
-    rules.clear();
-
-    std::ifstream file(filename);
-    std::string line;
-
-    while (std::getline(file, line) && !line.empty())
-    {
-        auto lesser = atoi(line.c_str());
-        auto greater = atoi(line.c_str() + line.find('|') + 1);
-        rules.push_back({ lesser, greater });
-    }
-
-    auto n = 0, retval = 0;
-    auto comma = ',';
-
-    while (std::getline(file, line))
-    {
-        line.push_back(comma);
-
-        std::stringstream ss(line);
-        std::vector<int> numbers;
-
-        while (ss >> n >> comma)
-            numbers.push_back(n);
-
-        if (!in_order(numbers))
+        if (std::is_sorted(numbers.begin(), numbers.end(), compare))
         {
-            sort_numbers(numbers);
-            retval += numbers[numbers.size() / 2];
+            retval.first += numbers[numbers.size() / 2];
+        }
+        else
+        {
+            std::sort(numbers.begin(), numbers.end(), compare);
+            retval.second += numbers[numbers.size() / 2];
         }
     }
 
@@ -111,13 +61,11 @@ auto do_part2(const std::string &filename)
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
-    std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == 6498);
+    auto answer = solve("input.txt");
+    std::cout << "Part One: " << answer.first << std::endl;
+    std::cout << "Part Two: " << answer.second << std::endl;
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == );
-
+    assert(answer.first == 6498);
+    assert(answer.second == 5017);
     return 0;
 }
