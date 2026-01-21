63d3d3d9a3f99675ffb2c253e310b48b17fc735c
Stephen Houchen <shouchen@outlook.com>
2026-01-21 12:08:19 -0800

    Final Day 8 cleanup

--- aoc2024/Day08/Day08.cpp
+++ aoc2024/Day08/Day08.cpp
@@ -11,7 +11,7 @@ using CoordinateList = std::vector<Coordinates>;
 using CoordinateSet = std::set<Coordinates>;
 using AntennaMap = std::map<char, CoordinateList>;
 
-auto add_antinodes(CoordinateList &same_freq_antennae, int height, int width, bool part2, CoordinateSet &antinodes)
+auto add_antinodes(CoordinateList &same_freq_antennae, int max_row, int max_col, bool part2, CoordinateSet &antinodes)
 {
     for (auto i = 0; i < same_freq_antennae.size(); i++)
         for (auto j = i + 1; j < same_freq_antennae.size(); j++)
@@ -19,26 +19,20 @@ auto add_antinodes(CoordinateList &same_freq_antennae, int height, int width, bo
             auto a = same_freq_antennae[i], b = same_freq_antennae[j];
             auto row_dir = b.first - a.first, col_dir = b.second - a.second;
 
-            auto new_antinode_row = part2 ? a.first : (a.first - row_dir);
-            auto new_antinode_col = part2 ? a.second : (a.second - col_dir);
-
-            while (new_antinode_row >= 0 && new_antinode_row < height && new_antinode_col >= 0 && new_antinode_col < width)
+            auto row = a.first - (part2 ? 0 : row_dir), col = a.second - (part2 ? 0 : col_dir);
+            while (row >= 0 && row < max_row && col >= 0 && col < max_col)
             {
-                antinodes.insert({ new_antinode_row, new_antinode_col });
-                new_antinode_row -= row_dir;
-                new_antinode_col -= col_dir;
+                antinodes.insert({ row, col });
+                row -= row_dir, col -= col_dir;
 
                 if (!part2) break;
             }
 
-            new_antinode_row = part2 ? b.first : (b.first + row_dir);
-            new_antinode_col = part2 ? b.second : (b.second + col_dir);
-
-            while (new_antinode_row >= 0 && new_antinode_row < height && new_antinode_col >= 0 && new_antinode_col < width)
+            row = b.first + (part2 ? 0 : row_dir), col = b.second + (part2 ? 0 : col_dir);
+            while (row >= 0 && row < max_row && col >= 0 && col < max_col)
             {
-                antinodes.insert({ new_antinode_row, new_antinode_col });
-                new_antinode_row += row_dir;
-                new_antinode_col += col_dir;
+                antinodes.insert({ row, col });
+                row += row_dir, col += col_dir;
 
                 if (!part2) break;
             }
@@ -50,35 +44,33 @@ auto solve(const std::string &filename)
     std::ifstream file(filename);
     std::string line;
     AntennaMap antennae;
-    auto height = 0, width = 0;
+    auto max_row = 0, max_col= 0;
 
     while (file >> line)
     {
         for (auto i = 0; i < line.size(); i++)
             if (line[i] != '.')
-                antennae[line[i]].push_back({ height, i });
+                antennae[line[i]].push_back({ max_row, i });
 
-        height++;
-        width = int(line.length());
+        max_row++;
+        max_col = int(line.length());
     }
 
     CoordinateSet antinodes;
 
     for (auto test : antennae)
-        add_antinodes(test.second, height, width, false, antinodes);
+        add_antinodes(test.second, max_row, max_col, false, antinodes);
 
-    auto retval = std::make_pair(antinodes.size(), 0ULL);
+    auto part1 = antinodes.size();
 
     for (auto test : antennae)
-        add_antinodes(test.second, height, width, true, antinodes);
+        add_antinodes(test.second, max_row, max_col, true, antinodes);
 
-    retval.second = antinodes.size();
-    return retval;
+    return std::make_pair(part1, antinodes.size());
 }
 
 int main()
 {
-
     auto answer = solve("input.txt");
     std::cout << "Part One: " << answer.first << std::endl;
     std::cout << "Part Two: " << answer.second << std::endl;
