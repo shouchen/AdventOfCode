f00fe7fa95808db233f9256f8d91f69f0543b415
Stephen Houchen <shouchen@outlook.com>
2026-01-22 10:47:37 -0800

    Final touches to Day 10

--- aoc2024/Day10/Day10.cpp
+++ aoc2024/Day10/Day10.cpp
@@ -5,19 +5,18 @@
 #include <string>
 #include <cassert>
 
+using Coordinates = std::pair<int, int>;
 using Grid = std::vector<std::string>;
-using LocationList = std::vector<std::pair<int, int>>;
-using LocationSet = std::set<std::pair<int, int>>;
-using DirectionList = std::vector<std::pair<int, int>>;
+using CoordinateList = std::vector<Coordinates>;
+using CoordinateSet = std::set<Coordinates>;
 
-const DirectionList dirs = { {-1,0}, {1,0}, {0,-1}, {0,1} };
+const std::pair<int, int> dirs[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-// DFS - builds up the location set
-auto recur(const Grid &grid, int row, int col, LocationSet &reachable_nines, unsigned long long &num_paths)
+auto recur(const Grid &grid, int row, int col, CoordinateSet &reachable_nines, int &num_paths)
 {
     if (grid[row][col] == '9')
     {
-        reachable_nines.insert({ row,col });
+        reachable_nines.insert({ row, col });
         num_paths++;
         return;
     }
@@ -26,23 +25,21 @@ auto recur(const Grid &grid, int row, int col, LocationSet &reachable_nines, uns
     {
         auto new_row = row + d.first, new_col = col + d.second;
 
-        if (new_row < 0 || new_row >= grid.size() ||
-            new_col < 0 || new_col >= grid[new_row].size())
-            continue;
-
-        if (grid[new_row][new_col] == grid[row][col] + 1)
+        if (new_row >= 0 && new_row < grid.size() &&
+            new_col >= 0 && new_col < grid[new_row].size() &&
+            grid[new_row][new_col] == grid[row][col] + 1)
+        {
             recur(grid, new_row, new_col, reachable_nines, num_paths);
+        }
     }
 }
 
-// first = score, second = rating
-auto score_trailhead(const Grid &grid, int start_row, int start_col)
+auto score_and_rate_trail(const Grid &grid, const Coordinates &trailhead)
 {
-    LocationSet reachable_nines;
-    auto num_paths = 0ULL;
+    CoordinateSet reachable_nines;
+    auto num_paths = 0;
 
-    recur(grid, start_row, start_col, reachable_nines, num_paths);
-
+    recur(grid, trailhead.first, trailhead.second, reachable_nines, num_paths);
     return std::make_pair(reachable_nines.size(), num_paths);
 }
 
@@ -55,18 +52,18 @@ auto solve(const std::string &filename)
     while (file >> line)
         grid.push_back(line);
 
-    LocationList trailheads;
+    CoordinateList trailheads;
     for (auto row = 0; row < int(grid.size()); row++)
-        for (int col = 0; col < int(grid[row].size()); col++)
+        for (auto col = 0; col < int(grid[row].size()); col++)
             if (grid[row][col] == '0')
-                trailheads.push_back({ row, col });
+                trailheads.push_back(Coordinates(row, col));
 
     auto retval = std::make_pair(0ULL, 0ULL);
     for (auto &th : trailheads)
     {
-        auto temp = score_trailhead(grid, th.first, th.second);
-        retval.first += temp.first;
-        retval.second += temp.second;
+        auto score_rating = score_and_rate_trail(grid, th);
+        retval.first += score_rating.first;
+        retval.second += score_rating.second;
     }
 
     return retval;
