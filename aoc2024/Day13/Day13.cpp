62f441151834d9014b3810cf91e9d7cde5aa940b
Stephen Houchen <shouchen@outlook.com>
2026-01-26 08:54:42 -0800

    Day 13 both parts, rewritten for perf to use Cramer's Rule.

--- aoc2024/Day13/Day13.cpp
+++ aoc2024/Day13/Day13.cpp
@@ -1,77 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include <queue>
 #include <cassert>
 
-auto solve_machine(int ax_diff, int ay_diff, int bx_diff, int by_diff, int x_goal, int y_goal)
+auto solve_machine(long long ax, long long ay, long long bx, long long by, long long x, long long y)
 {
-    auto min_cost = ULLONG_MAX;
+    auto determinant = ax * by - ay * bx;
+    assert(determinant); // If zero, there are more possibilities to check - this wasn't in puzzle data.
 
-    auto x = 0, y = 0, cost = 0;
-    for (auto a = 1; a <= 100; a++)
-    {
-        x += ax_diff, y += ay_diff, cost += 3;
+    // We have a single real solution; use Cramer's rule.
+    auto numerator1 = x * by - bx * y, numerator2 = ax * y - x * ay;
+    if (numerator1 % determinant || numerator2 % determinant)
+        return ULLONG_MAX; // No integral solution.
 
-        if (x > x_goal)
-            break;
-
-        auto temp_x = x_goal - x, temp_y = y_goal - y;
-        if ((temp_x % bx_diff != 0) || (temp_y % by_diff != 0))
-            continue;
-
-        auto b = temp_x / bx_diff;
-        if (x + b * bx_diff == x_goal && y + b * by_diff == y_goal)
-        {
-            unsigned long long total_cost = cost + b;
-            min_cost = std::min(total_cost, min_cost);
-        }
-    }
-
-    return min_cost;
+    auto a = numerator1 / determinant, b = numerator2 / determinant;
+    return 3ULL * a + b;
 }
 
-auto do_part1(const std::string &filename)
+auto solve(const std::string &filename, unsigned long long additional_xy = 0)
 {
     std::ifstream file(filename);
-    std::string line;
-    std::string button, a_or_b_colon, prize_colon;
-    auto x = 'X', y = 'Y', plus = '+', comma = ',', equals = '=';
-    auto ax_diff = 0, ay_diff = 0, bx_diff = 0, by_diff = 0, x_goal = 0, y_goal = 0;
-    auto retval = 0;
+    std::string line, button, a_or_b_colon, prize_colon;
+    auto x_char = 'X', y_char = 'Y', plus = '+', comma = ',', equals = '=';
+    auto ax = 0ULL, ay = 00ULL, bx = 00ULL, by = 00ULL, x = 00ULL, y = 00ULL, retval = 0ULL;
 
-    while (file >> button >> a_or_b_colon >> x >> plus >> ax_diff >> comma >> y >> plus >> ay_diff)
+    while (file >> button >> a_or_b_colon >> x_char >> plus >> ax >> comma >> y_char >> plus >> ay)
     {
-        file >> button >> a_or_b_colon >> x >> plus >> bx_diff >> comma >> y >> plus >> by_diff;
-        file >> prize_colon >> x >> equals >> x_goal >> comma >> y >> equals >> y_goal;
+        file >> button >> a_or_b_colon >> x_char >> plus >> bx >> comma >> y_char >> plus >> by;
+        file >> prize_colon >> x_char >> equals >> x >> comma >> y_char >> equals >> y;
 
-        auto cost = solve_machine(ax_diff, ay_diff, bx_diff, by_diff, x_goal, y_goal);
-        if (cost < ULLONG_MAX)
-            retval += solve_machine(ax_diff, ay_diff, bx_diff, by_diff, x_goal, y_goal);
+        auto tokens = solve_machine(ax, ay, bx, by, x + additional_xy, y + additional_xy);
+        if (tokens < ULLONG_MAX)
+            retval += tokens;
     }
 
     return retval;
 }
 
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
-
-    while (file >> line)
-        ;
-
-    return -1;
-}
-
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = solve("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == 36250);
+    assert(part1 == 36250);
 
-    auto part2 = do_part2("input.txt");
+    auto part2 = solve("input.txt", 10000000000000);
     std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == );
+    assert(part2 == 83232379451012);
 
     return 0;
 }
