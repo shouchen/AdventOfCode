bad10df1e548833498974902c01ffb66e964cf94
Stephen Houchen <shouchen@outlook.com>
2026-01-28 08:20:27 -0800

    Start of Day clean-up

--- aoc2024/Day15/Day15.cpp
+++ aoc2024/Day15/Day15.cpp
@@ -4,20 +4,26 @@
 #include <string>
 #include <cassert>
 
+struct Location { int row = 0, col = 0; };
+struct Direction { int row, col; Direction(char move); };
+
 std::vector<std::string> grid;
+Location robot;
 
-auto robot_row = 0, robot_col = 0;
-
-auto dump()
+Direction::Direction(char move)
 {
-    for (auto &row : grid)
-        std::cout << row << std::endl;
-    std::cout << std::endl;
+    switch (move)
+    {
+    case '^': row = -1, col =  0; break;
+    case 'v': row =  1, col =  0; break;
+    case '<': row =  0, col = -1; break;
+    case '>': row =  0, col =  1; break;
+    }
 }
 
 auto get_total_for_config()
 {
-    auto retval = 0ULL;
+    auto retval = 0;
     for (auto row = 0; row < grid.size(); row++)
         for (auto col = 0; col < grid[row].size(); col++)
             if (grid[row][col] == 'O' || grid[row][col] == '[')
@@ -25,22 +31,15 @@ auto get_total_for_config()
     return retval;
 }
 
-auto do_move(char move)
+auto do_move(Direction dir)
 {
-    auto d_row = 0, d_col = 0;
-
-    if (move == '^') d_row = -1;
-    else if (move == 'v') d_row = 1;
-    else if (move == '<') d_col = -1;
-    else if (move == '>') d_col = 1;
-
     // Find next empty space or wall in direction of travel.
-    auto row = robot_row + d_row, col = robot_col + d_col;
+    auto row = robot.row + dir.row, col = robot.col + dir.col;
     while (grid[row][col] != '#' && grid[row][col] != '.')
-        row += d_row, col += d_col;
+        row += dir.row, col += dir.col;
 
     // Find next space in direction of travel.
-    auto next_row = robot_row + d_row, next_col = robot_col + d_col;
+    auto next_row = robot.row + dir.row, next_col = robot.col + dir.col;
 
     // Blocked?
     if (grid[row][col] == '#')
@@ -49,50 +48,23 @@ auto do_move(char move)
     // Simple Move?
     if (grid[next_row][next_col] == '.')
     {
-        grid[robot_row][robot_col] = '.';
-        grid[robot_row + d_row][robot_col + d_col] = '@';
-        robot_row = next_row, robot_col = next_col;
+        grid[robot.row][robot.col] = '.';
+        grid[robot.row + dir.row][robot.col + dir.col] = '@';
+        robot.row = next_row, robot.col = next_col;
         return true;
     }
 
     // Else, it's a push
-    grid[robot_row][robot_col] = '.';
-    grid[robot_row + d_row][robot_col + d_col] = '@';
-    robot_row = next_row, robot_col = next_col;
+    grid[robot.row][robot.col] = '.';
+    grid[robot.row + dir.row][robot.col + dir.col] = '@';
+    robot.row = next_row, robot.col = next_col;
     grid[row][col] = 'O';
     return  true;
 }
 
-auto do_part1(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
-
-    while (std::getline(file, line) && line.length())
-    {
-        auto at_pos = line.find('@');
-        if (at_pos != std::string::npos)
-            robot_row = int(grid.size()), robot_col = int(at_pos);
-        grid.push_back(line);
-    }
-
-    //std::cout << "Initial State:" << std::endl;
-    //dump();
-
-    auto move = ' ';
-    while (file >> move)
-    {
-        do_move(move);
-        //std::cout << "Move " << move << ":" << std::endl;
-        //dump();
-    }
-
-    return get_total_for_config();
-}
-
 // See if it will be possible to move the object at (row, col) in the specified direction
 // without actually performing the move.
-auto can_move2(int row, int col, char move)
+auto can_move2(int row, int col, Direction dir)
 {
     if (grid[row][col] == '.')
         return true;
@@ -104,25 +76,18 @@ auto can_move2(int row, int col, char move)
     if (grid[row][col] == ']')
         col--;
 
-    auto d_row = 0, d_col = 0;
-
-    if (move == '^') d_row = -1;
-    else if (move == 'v') d_row = 1;
-    else if (move == '<') d_col = -1;
-    else if (move == '>') d_col = 1;
-
-    auto next_row = row + d_row, next_col = col + d_col;
-    if (grid[row][col] == '[' && move == '>') next_col++;
+    auto next_row = row + dir.row, next_col = col + dir.col;
+    if (grid[row][col] == '[' && dir.col == 1) next_col++;
     return
-        can_move2(next_row, next_col, move) &&
-        (grid[row][col] != '[' || (move != '^' && move != 'v') || can_move2(next_row, next_col + 1, move));
+        can_move2(next_row, next_col, dir) &&
+        (grid[row][col] != '[' || dir.row == 0 || can_move2(next_row, next_col + 1, dir));
 }
 
 // Move the object at the location in the direction indicated. Should first call can_move2() 
 // to see whether the way is open or not. Otherwise, this function would have to back out
 // partial changes. TODO: Make this one function with a "test" bool parameter since the
 // traversal actions are essentially the same.
-auto move2(int row, int col, char move)
+auto move2(int row, int col, Direction dir)
 {
     if (grid[row][col] == '.')
         return true;
@@ -134,34 +99,25 @@ auto move2(int row, int col, char move)
     if (grid[row][col] == ']')
         col--;
 
-    auto d_row = 0, d_col = 0;
+    auto next_row = row + dir.row, next_col = col + dir.col;
+    if (grid[row][col] == '[' && dir.col == 1) next_col++;
 
-    if (move == '^') d_row = -1;
-    else if (move == 'v') d_row = 1;
-    else if (move == '<') d_col = -1;
-    else if (move == '>') d_col = 1;
-
-    auto next_row = row + d_row, next_col = col + d_col;
-    if (grid[row][col] == '[' && move == '>') next_col++;
-
-    if (move2(next_row, next_col, move) &&
-        (grid[row][col] != '[' || (move != '^' && move != 'v') || move2(next_row, next_col + 1, move)))
+    if (move2(next_row, next_col, dir) &&
+        (grid[row][col] != '[' || dir.row == 0 || move2(next_row, next_col + 1, dir)))
     {
-        if (grid[row][col] == '[' && move == '>') --next_col;
+        if (grid[row][col] == '[' && dir.col == 1) --next_col;
 
         // Special case for double-wide moves
         if (grid[row][col] == '[')
         {
-            if (d_row == 0) // horizontal moves
+            if (dir.row == 0) // horizontal moves
             {
                 grid[next_row][next_col] = '[';
                 grid[next_row][next_col + 1] = ']';
-                if (move == '>')
-                {
+                if (dir.col == 1)
                     grid[row][col] = '.';
-                }
                 else
-                    grid[row - d_row][col - d_col] = '.';
+                    grid[row - dir.row][col - dir.col] = '.';
             }
             else // vertical moves
             {
@@ -173,14 +129,36 @@ auto move2(int row, int col, char move)
         }
         else // single-wide moves
         {
-            if (grid[row][col] == '@') robot_row = next_row, robot_col = next_col;
+            if (grid[row][col] == '@') robot.row = next_row, robot.col = next_col;
             grid[next_row][next_col] = grid[row][col];
             grid[row][col] = '.';
-
         }
     }
 
     return true;
+}
+
+auto do_part1(const std::string &filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line) && line.length())
+    {
+        auto at_pos = line.find('@');
+        if (at_pos != std::string::npos)
+            robot.row = int(grid.size()), robot.col = int(at_pos);
+        grid.push_back(line);
+    }
+
+    auto move = ' ';
+    while (file >> move)
+    {
+        Direction dir(move);
+        do_move(dir);
+    }
+
+    return get_total_for_config();
 }
 
 auto do_part2(const std::string &filename)
@@ -203,20 +181,16 @@ auto do_part2(const std::string &filename)
 
         auto at_pos = line2.find('@');
         if (at_pos != std::string::npos)
-            robot_row = int(grid.size()), robot_col = int(at_pos);
+            robot.row = int(grid.size()), robot.col = int(at_pos);
         grid.push_back(line2);
     }
-
-    std::cout << "Initial State:" << std::endl;
-    dump();
 
     auto move = ' ';
     while (file >> move)
     {
-        //std::cout << "Move " << move << ":" << std::endl;
-        if (can_move2(robot_row, robot_col, move))
-            move2(robot_row, robot_col, move);
-        //dump();
+        Direction dir(move);
+        if (can_move2(robot.row, robot.col, dir))
+            move2(robot.row, robot.col, dir);
     }
 
     return get_total_for_config();
