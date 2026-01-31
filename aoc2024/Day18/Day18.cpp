174bc5984f38a999c3e53bcca4a468e3a4a9777c
Stephen Houchen <shouchen@outlook.com>
2026-01-30 22:57:52 -0800

    Done with Day 18 (both parts). Clean as a whistle!

--- aoc2024/Day18/Day18.cpp
+++ aoc2024/Day18/Day18.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <queue>
 #include <string>
 #include <cassert>
 
-// Real input will be 71x71, test is 7x7   (so SIZE 7 or 71)
-#define SIZE 71
+std::vector<std::string> grid(71, std::string(71, '.'));
 
-std::vector<std::string> grid(SIZE, std::string(SIZE, '.'));
-
-void dump()
+auto get_shortest_distance()
 {
-    for (auto &row : grid)
-    {
-        for (auto c : row)
-            std::cout << (c ? c : '.');
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-auto get_dist()
-{
-    std::vector<std::vector<bool>> visited(SIZE, std::vector<bool>(SIZE, false));
+    std::vector<std::vector<bool>> visited(71, std::vector<bool>(71, false));
 
     struct State { int x, y, cost; };
     std::queue<State> q;
@@ -33,7 +20,7 @@ auto get_dist()
     {
         auto state = q.front(); q.pop();
 
-        if (state.x == SIZE - 1 && state.y == SIZE - 1)
+        if (state.x == 70 && state.y == 70)
             return state.cost;
 
         if (visited[state.y][state.x])
@@ -42,68 +29,55 @@ auto get_dist()
 
         if (state.x > 0 && grid[state.y][state.x - 1] != '#')
             q.push({ state.x - 1, state.y, state.cost + 1 });
-        if (state.x < SIZE - 1 && grid[state.y][state.x + 1] != '#')
+        if (state.x < 70 && grid[state.y][state.x + 1] != '#')
             q.push({ state.x + 1, state.y, state.cost + 1 });
 
         if (state.y > 0 && grid[state.y - 1][state.x] != '#')
             q.push({ state.x, state.y - 1, state.cost + 1 });
-        if (state.y < SIZE - 1 && grid[state.y + 1][state.x] != '#')
+        if (state.y < 70 && grid[state.y + 1][state.x] != '#')
             q.push({ state.x, state.y + 1, state.cost + 1 });
     }
 
-    return -1;
+    return INT_MAX;
 }
 
-auto do_part1(const std::string &filename, int bytes)
+auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
-    auto x = 0, y = 0;
+    auto x = 0, y = 0, num_bytes = 0;
     auto comma = ',';
-
-    auto debug = 0;
-    while (file >> x >> comma >> y)
-    {
-        grid[y][x] = '#';
-        if (++debug == bytes) break;
-    }
-
-    //dump();
-
-    return get_dist();
-}
-
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
-    auto x = 0, y = 0;
-    auto comma = ',';
+    std::pair<int, std::string> retval;
 
     while (file >> x >> comma >> y)
     {
         grid[y][x] = '#';
-        if (get_dist() == -1)
+        if (++num_bytes < 1024)
+            continue;
+
+        auto shortest = get_shortest_distance();
+
+        if (num_bytes == 1024)
+            retval.first = shortest;
+        else if (shortest == INT_MAX)
         {
-            std::cout << x << "," << y << std::endl;
+            std::ostringstream oss;
+            oss << x << "," << y;
+            retval.second = oss.str();
             break;
         }
     }
 
-    //dump();
-
-    return -1;
+    return retval;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt", 1024);
-    std::cout << "Part One: " << part1 << std::endl;
-    assert(part1 == 506);
+    auto answer = solve("input.txt");
+    std::cout << "Part One: " << answer.first << std::endl;
+    std::cout << "Part Two: " << answer.second << std::endl;
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == "62,6");
-
+    assert(answer.first == 506);
+    assert(answer.second == "62,6");
     return 0;
 }
