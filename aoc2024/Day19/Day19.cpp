660062253264c9ca22e432761fb22390db5d5ff4
Stephen Houchen <shouchen@outlook.com>
2026-01-30 23:19:46 -0800

    Day 19 Part 1

--- aoc2024/Day19/Day19.cpp
+++ aoc2024/Day19/Day19.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include <cassert>
+
+std::vector<std::string> patterns;
+
+auto is_possible(const std::string &design)
+{
+    if (design.empty())
+        return true;
+
+    for (auto &p : patterns)
+        if (design.length() >= p.length() && std::equal(p.begin(), p.end(), design.begin()))
+            if (is_possible(design.substr(p.length())))
+                return true;
+
+    return false;
+}
 
 auto do_part1(const std::string &filename)
 {
     std::ifstream file(filename);
-    std::string line;
+    std::string line, pattern, design;
+    auto comma = ',';
 
-    while (file >> line)
-        ;
+    std::getline(file, line);
 
-    return -1;
-}
+    std::istringstream iss(line);
+    while (iss >> pattern)
+    {
+        if (pattern.back() == ',') pattern.pop_back();
+        patterns.push_back(pattern);
+    }
 
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
+    auto retval = 0;
+    while (file >> design)
+    {
+        if (is_possible(design))
+            retval++;
+    }
 
-    while (file >> line)
-        ;
-
-    return -1;
+    return retval;
 }
 
 int main()
 {
     auto part1 = do_part1("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == );
+    assert(part1 == 213);
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == );
+    //auto part2 = do_part2("input.txt");
+    //std::cout << "Part Two: " << part2 << std::endl;
+    ////assert(part2 == );
 
     return 0;
 }
