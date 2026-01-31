b8dc24a69bdb9f6614bb75a2dd64e5cecfc94bf2
Stephen Houchen <shouchen@outlook.com>
2026-01-30 17:21:56 -0800

    Day 18, initial solution (both parts)

--- aoc2024/Day18/Day18.cpp
+++ aoc2024/Day18/Day18.cpp
@@ -1,38 +1,109 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <queue>
+#include <string>
 #include <cassert>
 
-auto do_part1(const std::string &filename)
+// Real input will be 71x71, test is 7x7   (so SIZE 7 or 71)
+#define SIZE 71
+
+std::vector<std::string> grid(SIZE, std::string(SIZE, '.'));
+
+void dump()
+{
+    for (auto &row : grid)
+    {
+        for (auto c : row)
+            std::cout << (c ? c : '.');
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+auto get_dist()
+{
+    std::vector<std::vector<bool>> visited(SIZE, std::vector<bool>(SIZE, false));
+
+    struct State { int x, y, cost; };
+    std::queue<State> q;
+    q.push({ 0, 0, 0 });
+
+    while (!q.empty())
+    {
+        auto state = q.front(); q.pop();
+
+        if (state.x == SIZE - 1 && state.y == SIZE - 1)
+            return state.cost;
+
+        if (visited[state.y][state.x])
+            continue;
+        visited[state.y][state.x] = true;
+
+        if (state.x > 0 && grid[state.y][state.x - 1] != '#')
+            q.push({ state.x - 1, state.y, state.cost + 1 });
+        if (state.x < SIZE - 1 && grid[state.y][state.x + 1] != '#')
+            q.push({ state.x + 1, state.y, state.cost + 1 });
+
+        if (state.y > 0 && grid[state.y - 1][state.x] != '#')
+            q.push({ state.x, state.y - 1, state.cost + 1 });
+        if (state.y < SIZE - 1 && grid[state.y + 1][state.x] != '#')
+            q.push({ state.x, state.y + 1, state.cost + 1 });
+    }
+
+    return -1;
+}
+
+auto do_part1(const std::string &filename, int bytes)
 {
     std::ifstream file(filename);
     std::string line;
+    auto x = 0, y = 0;
+    auto comma = ',';
 
-    while (file >> line)
-        ;
+    auto debug = 0;
+    while (file >> x >> comma >> y)
+    {
+        grid[y][x] = '#';
+        if (++debug == bytes) break;
+    }
 
-    return -1;
+    //dump();
+
+    return get_dist();
 }
 
 auto do_part2(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
+    auto x = 0, y = 0;
+    auto comma = ',';
 
-    while (file >> line)
-        ;
+    while (file >> x >> comma >> y)
+    {
+        grid[y][x] = '#';
+        if (get_dist() == -1)
+        {
+            std::cout << x << "," << y << std::endl;
+            break;
+        }
+    }
+
+    //dump();
 
     return -1;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
+    auto part1 = do_part1("input.txt", 1024);
     std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == );
+    assert(part1 == 506);
 
     auto part2 = do_part2("input.txt");
     std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == );
+    //assert(part2 == "62,6");
 
     return 0;
 }
