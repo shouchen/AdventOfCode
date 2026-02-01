5d6470510a8f71b3ee4526b3c8986b461ef99319
Stephen Houchen <shouchen@outlook.com>
2026-02-01 08:38:22 -0800

    Made shortest path more of a canonical approach

--- aoc2024/Day20/Day20.cpp
+++ aoc2024/Day20/Day20.cpp
@@ -9,56 +9,55 @@ struct Location { int row = 0, col = 0; };
 struct Direction { int row = 0, col = 0; };
 
 std::vector<std::vector<int>> min_cost;
-std::vector<std::vector<bool>> contains_path;
 
 auto get_shortest_path(std::vector<std::string> &grid, Location start, Location end)
 {
     static const Direction dirs[] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
+    auto contains_path = std::vector<std::vector<bool>>(grid.size(), std::vector<bool>(grid[0].size(), false));
     min_cost = std::vector<std::vector<int>>(grid.size(), std::vector<int>(grid[0].size(), INT_MAX));
-    contains_path = std::vector<std::vector<bool>>(grid.size(), std::vector<bool>(grid[0].size(), false));
 
-    struct State
-    {
-        Location loc;
-        int cost;
-    };
-
-    std::queue<State> q;
-    q.push(State{ start, 0 });
+    std::queue<Location> q;
+    q.push(start);
+    min_cost[start.row][start.col] = 0;
 
     while (!q.empty())
     {
-        State state = q.front(); q.pop();
-        auto new_state = state;
-
-        if (min_cost[state.loc.row][state.loc.col] < state.cost)
-            continue;
-        min_cost[state.loc.row][state.loc.col] = state.cost;
+        Location curr = q.front(); q.pop();
+        auto cost = min_cost[curr.row][curr.col];
 
         for (auto &d : dirs)
         {
-            Location new_loc{ state.loc.row + d.row, state.loc.col + d.col };
-            if (grid[new_loc.row][new_loc.col] != '#')
-                q.push({ new_loc, state.cost + 1 });
+            auto next_row = curr.row + d.row, next_col = curr.col + d.col;
+            if (grid[next_row][next_col] != '#' && min_cost[next_row][next_col] == INT_MAX)
+            {
+                min_cost[next_row][next_col] = cost + 1;
+                q.push({ next_row, next_col });
+            }
         }
     }
 
     std::vector<Location> path;
     auto curr = end;
+
     path.push_back(curr);
     contains_path[curr.row][curr.col] = true;
 
     while (curr.row != start.row || curr.col != start.col)
         for (auto &d : dirs)
+        {
+            auto next_row = curr.row + d.row, next_col = curr.col + d.col;
+
             if (min_cost[curr.row + d.row][curr.col + d.col] == min_cost[curr.row][curr.col] - 1)
             {
-                curr = { curr.row + d.row, curr.col + d.col };
+                curr = { next_row, next_col };
                 path.push_back(curr);
-                contains_path[curr.row][curr.col] = true;
+                contains_path[next_row][next_col] = true;
                 break;
             }
+        }
 
+    std::reverse(path.begin(), path.end());
     return path;
 }
 
@@ -78,18 +77,17 @@ auto solve(const std::string &filename)
         grid.push_back(line);
     }
 
-    // Note that this goes from end to start (backwards)
     auto path = get_shortest_path(grid, start, end);
     auto retval = std::make_pair(0, 0);
 
     for (auto p1 = path.cbegin(); p1 != path.cend(); p1++)
         for (auto p2 = p1 + 1; p2 != path.cend(); p2++)
         {
-            auto manhattan_dist = std::abs(p1->row - p2->row) + std::abs(p1->col - p2->col);
+            auto manhattan_dist = std::abs(p2->row - p1->row) + std::abs(p2->col - p1->col);
             if (manhattan_dist > 20)
                 continue;
 
-            auto path_dist = min_cost[p1->row][p1->col] - min_cost[p2->row][p2->col];
+            auto path_dist = min_cost[p2->row][p2->col] - min_cost[p1->row][p1->col];
             if (path_dist - manhattan_dist >= 100)
             {
                 if (manhattan_dist <= 2)
