8303c0a5119a0441c3a4e4f5453488bf33997000
Stephen Houchen <shouchen@outlook.com>
2026-02-04 14:34:42 -0800

    Ugly Day 25

--- aoc2024/Day25/Day25.cpp
+++ aoc2024/Day25/Day25.cpp
@@ -1,34 +1,74 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 #include <cassert>
+
+using Schematic = std::vector<int>;
+
+std::vector<Schematic> keys, locks;
+
+auto process_entity(std::vector<std::string> entity)
+{
+    Schematic heights{ -1, -1, -1, -1, -1 };
+
+    for (auto &r : entity)
+        for (auto i = 0; i < 5; i++)
+            if (r[i] == '#')
+                heights[i]++;
+
+    auto is_key = entity[0][0] == '.';
+    std::vector<Schematic> &schematic_to_update = is_key ? keys : locks;
+    schematic_to_update.push_back(heights);
+}
 
 auto do_part1(const std::string &filename)
 {
     std::ifstream file(filename);
     std::string line;
+    std::vector<std::string> entity;
 
-    while (file >> line)
-        ;
+    while (std::getline(file, line))
+    {
+        if (line.length() == 0)
+        {
+            process_entity(entity);
 
-    return -1;
-}
+            if (!std::getline(file, line))
+                break;
 
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
+            entity.clear();
+        }
 
-    while (file >> line)
-        ;
+        entity.push_back(line);
+    }
 
-    return -1;
+    process_entity(entity);
+
+    auto retval = 0;
+    for (auto &k : keys)
+        for (auto &l : locks)
+        {
+            bool fit = true;
+            for (int i = 0; i < 5; i++)
+                if (k[i] + l[i] > 5)
+                {
+                    fit = false;
+                    break;
+                }
+
+            if (fit)
+                retval++;
+        }
+
+    return retval;
 }
 
 int main()
 {
     auto part1 = do_part1("input.txt");
     std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == );
+    assert(part1 == 3395 );
 
     std::cout << "Part Two: N/A" << std::endl;
     return 0;
