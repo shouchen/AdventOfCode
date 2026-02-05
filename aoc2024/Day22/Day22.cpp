0badbee7aca26e3a2dbcb1dcc86949e265945b2a
Stephen Houchen <shouchen@outlook.com>
2026-02-05 08:32:24 -0800

    Final perf improvements to Day 22 - much better!

--- aoc2024/Day22/Day22.cpp
+++ aoc2024/Day22/Day22.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
+#include <algorithm>
 #include <cassert>
 
-using SequenceToPrice = std::unordered_map<int, int>;
+using SequenceToPrice = std::vector<int>;
 using Buyers = std::vector<SequenceToPrice>;
 
 auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
     Buyers buyers;
-    std::unordered_set<int> all_keys;
-    uint32_t secret = 0;
+    std::vector<int> all_keys;
+    int secret = 0;
     auto retval = std::make_pair(0ULL, 0);
-
-    auto debug_key = ((-2 + 10) << 24) | ((1 + 10) << 16) | ((-1 + 10) << 8) | (3 + 10);
 
     while (file >> secret)
     {
-        buyers.push_back({});
+        buyers.push_back(std::vector<int>(19 * 19 * 19 * 19, -1));
 
         auto prev_price = int(secret % 10), key = 0;
 
@@ -33,12 +30,12 @@ auto solve(const std::string &filename)
             auto price = int(secret % 10);
             auto change = price - prev_price;
 
-            key = (key >> 8) | (change + 10) << 24;
+            key = (key % (19 * 19 * 19)) * 19 + (change + 9);
 
-            if (i > 2 && buyers.back().find(key) == buyers.back().end())
+            if (i > 2 && buyers.back()[key] == -1)
             {
-                buyers.back()[key] += price;
-                all_keys.insert(key);
+                buyers.back()[key] = price;
+                all_keys.push_back(key);
             }
 
             prev_price = price;
@@ -47,11 +44,14 @@ auto solve(const std::string &filename)
         retval.first += secret;
     }
 
+    std::sort(all_keys.begin(), all_keys.end());
+    all_keys.erase(std::unique(all_keys.begin(), all_keys.end()), all_keys.end());
+
     for (auto &i : all_keys)
     {
         auto bananas = 0;
         for (auto &b : buyers)
-            if (b.find(i) != b.end())
+            if (b[i] != -1)
                 bananas += b[i];
 
         retval.second = std::max(retval.second, bananas);
@@ -66,7 +66,7 @@ int main()
     std::cout << "Part One: " << answer.first << std::endl;
     std::cout << "Part Two: " << answer.second << std::endl;
 
-    assert(part1 == 13584398738);
-    assert(part2 == 1612);
+    assert(answer.first == 13584398738);
+    assert(answer.second == 1612);
     return 0;
 }
