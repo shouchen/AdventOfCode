14da6bbbff7638bfb3689a7d1911e5b88df84054
Stephen Houchen <shouchen@outlook.com>
2026-02-06 11:53:52 -0800

    Day 23 solutions (both parts), with clean-up

--- aoc2024/Day23/Day23.cpp
+++ aoc2024/Day23/Day23.cpp
@@ -1,130 +1,105 @@
 #include <iostream>
 #include <fstream>
-#include <map>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 #include <set>
-#include <vector>
 #include <string>
 #include <algorithm>
 #include <cassert>
 
-std::vector<std::vector<bool>> adjacency(26*26, std::vector<bool>(26*26, false));
-std::vector<std::string> computers;
+using ComputerSet = std::set<std::string>;
+using ComputerUSet = std::unordered_set<std::string>;
+using AdjacencyMap = std::unordered_map<std::string, ComputerUSet>;
 
-inline int make_index(const std::string &name)
+inline auto includes_computer_starting_with_t(const ComputerSet &set)
 {
-    return (name[0] - 'a') * 26 + (name[1] - 'a');
+    auto retval = false;
+    for (auto &computer : set)
+        if (computer.front() == 't')
+            return true;
+
+    return false;
 }
 
-auto mark_adjacent(const std::string &c1, const std::string &c2)
+inline auto contains_all(const ComputerSet &s1, const ComputerUSet &s2)
 {
-    auto ndx1 = make_index(c1), ndx2 = make_index(c2);
-    adjacency[ndx1][ndx2] = adjacency[ndx2][ndx1] = true;
-}
-
-auto are_adjacent(const std::string &c1, const std::string &c2)
-{
-    auto ndx1 = make_index(c1), ndx2 = make_index(c2);
-    return adjacency[ndx1][ndx2];
-}
-
-auto do_part1(const std::string &filename)
-{
-    auto num_computers = 0;
-
-    std::ifstream file(filename);
-    std::string line;
-
-    while (file >> line)
-    {
-        auto c1 = line.substr(0, 2), c2 = line.substr(3);
-        computers.push_back(c1);
-        computers.push_back(c2);
-        mark_adjacent(c1, c2);
-    }
-
-    // De-dupe computer names
-    std::sort(computers.begin(), computers.end());
-    computers.erase(std::unique(computers.begin(), computers.end()), computers.end());
-
-    // Find 3-loops
-    auto retval = 0;
-
-    for (auto i = 0; i < computers.size(); i++)
-    {
-        auto c1 = computers[i];
-        auto ndx1 = make_index(c1);
-
-        for (auto j = i + 1; j < computers.size(); j++)
-        {
-            auto c2 = computers[j];
-            auto ndx2 = make_index(c2);
-
-            if (!adjacency[ndx1][ndx2])
-                continue;
-
-            for (auto k = j + 1; k < computers.size(); k++)
-            {
-                auto c3 = computers[k];
-                auto ndx3 = make_index(c3);
-
-                if (!adjacency[ndx1][ndx3] || !adjacency[ndx2][ndx3])
-                    continue;
-
-                //std::cout << c1 << '-' << c2 << '-' << c3 << std::endl;
-                if (c1[0] == 't' || c2[0] == 't' || c3 [0] == 't')
-                    retval++;
-            }
-        }
-    }
-
-    return retval;
-}
-
-void dump_path(const std::vector<int> &path)
-{
-    for (auto i = 0; i < path.size(); i++)
-        std::cout << (i ? "," : "") << computers[i];
-    std::cout << std::endl;
-}
-
-auto prune_not_fully_connected(const std::vector<int> &group)
-{
-    for (auto i = 0; i < group.size(); i++)
-        for (auto j = i + 1; j < group.size(); j++)
-            if (!are_adjacent(computers[i], computers[j]))
-                return false;
+    for (const auto &elem : s1)
+        if (!s2.contains(elem))
+            return false;
 
     return true;
 }
 
-auto do_part2(const std::string &filename)
+auto solve(const std::string &filename)
 {
-    auto retval = 0;
+    std::ifstream file(filename);
+    std::string line;
+    AdjacencyMap map;
+    std::pair<int, std::string> retval;
 
-    for (auto i = 0; i < computers.size(); i++)
+    // read the graph
+    while (std::getline(file, line))
     {
-        std::vector<int> group;
-
-        for (auto j = 0; j < computers.size(); j++)
-            if (are_adjacent(computers[i], computers[j]))
-                group.push_back(j);
-
-        prune_not_fully_connected(group);
-        retval = std::max(retval, int(group.size() + 1)); // +1 is for computer[i]
+        auto c1 = line.substr(0, 2), c2 = line.substr(3);
+        map[c1].insert(c2); map[c2].insert(c1);
     }
 
+    // build sets of size 3 ("triangles") - needed for both parts
+    std::set<ComputerSet> triangles;
+    for (const auto &[c1, c2s] : map)
+        for (const auto &c2 : c2s)
+            for (const auto c3 : map[c2])
+                if (c1 != c3 && map[c3].contains(c1))
+                    triangles.insert({ c1, c2, c3 });
+
+    // compute part 1 result
+    for (auto &triangle : triangles)
+        if (includes_computer_starting_with_t(triangle))
+            retval.first++;
+
+    // create supersets by growing the triangles
+    std::set<ComputerSet> current = triangles;
+
+    for (;;)
+    {
+        std::set<ComputerSet> next;
+
+        for (const auto &s : current)
+            for (const auto &[comp, connections] : map)
+                if (connections.size() >= s.size() && contains_all(s, connections))
+                {
+                    auto superset = s;
+                    superset.insert(comp);
+                    next.insert(superset);
+                }
+
+        if (next.empty()) break;
+        current = next;
+    }
+
+    // format the part 2 result
+    auto lan_party = *current.begin();
+    std::ostringstream oss;
+    std::string separator;
+
+    for (auto &elem : lan_party)
+    {
+        oss << separator << elem;
+        separator = ",";
+    }
+
+    retval.second = oss.str();
     return retval;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    auto part1 = do_part1("input.txt");
-    std::cout << "Part One: " << part1 << std::endl;
-    //assert(part1 == 1064);
+    auto answer = solve("input.txt");
+    std::cout << "Part One: " << answer.first << std::endl;
+    std::cout << "Part Two: " << answer.second << std::endl;
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-    //assert(part2 == "aq,cc,ea,gc,jo,od,pa,rg,rv,ub,ul,vr,yy");
-
+    assert(answer.first == 1064);
+    assert(answer.second == "aq,cc,ea,gc,jo,od,pa,rg,rv,ub,ul,vr,yy");
     return 0;
 }
