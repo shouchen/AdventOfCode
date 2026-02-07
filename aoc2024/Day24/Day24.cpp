45f28eeffbf13c6180df3f127573d916139eb3b5
Stephen Houchen <shouchen@outlook.com>
2026-02-07 07:30:57 -0800

    Combining Day 24 parts 1 & 2

--- aoc2024/Day24/Day24.cpp
+++ aoc2024/Day24/Day24.cpp
@@ -10,78 +10,59 @@
 
 constexpr auto TBD = -1;
 
-struct GateInputs
+struct GateInputs // Part 1
 {
     std::string op1, op2, operation;
     int value = TBD;
 };
 
-std::map<std::string, GateInputs> output;
-
-int get_value(const std::string &label)
+struct Operation // Part 2
 {
-    auto &curr = output[label];
+    std::string w1, w2, out, op;
+};
+
+int get_value(const std::string &label, std::map<std::string, GateInputs> &map) // Part 1
+{
+    auto &curr = map[label];
     if (curr.value == TBD)
     {
-        if      (curr.operation == "AND") curr.value =  get_value(curr.op1) & get_value(curr.op2);
-        else if (curr.operation == "OR")  curr.value =  get_value(curr.op1) | get_value(curr.op2);
-        else if (curr.operation == "XOR") curr.value = (get_value(curr.op1) ^ get_value(curr.op2)) & 0x1;
+        if      (curr.operation == "AND") curr.value =  get_value(curr.op1, map) & get_value(curr.op2, map);
+        else if (curr.operation == "OR")  curr.value =  get_value(curr.op1, map) | get_value(curr.op2, map);
+        else if (curr.operation == "XOR") curr.value = (get_value(curr.op1, map) ^ get_value(curr.op2, map)) & 0x1;
     }
 
     return curr.value;
 }
 
-auto do_part1(const std::string &filename)
+inline auto is_input_wire(const std::string &w) { return w.front() == 'x' || w.front() == 'y'; }
+inline auto is_output_wire(const std::string &w) { return w.front() == 'z'; }
+
+auto solve(const std::string &filename)
 {
     std::ifstream file(filename);
-    std::string line, label1, label2, label3, operation, arrow;
-    auto colon = ':';
-    auto n = 0;
-    auto retval = 0LL;
+    std::string line, label1, label2, label3, op, arrow;
+    std::vector<Operation> operations;
+    std::map<std::string, GateInputs> output_to_op;
+    std::unordered_map<std::string, std::vector<Operation>> inputs_to_op;
+    auto n = 0, max_z = 0;
+    std::pair<long long, std::string> retval;
 
     while (std::getline(file, line) && line.length())
     {
         std::istringstream iss(line);
         iss >> label1 >> n; label1.pop_back(); // colon
-        output[label1] = { "", "", "", n };
+        output_to_op[label1] = { "", "", "", n };
     }
-
-    while (file >> label1 >> operation >> label2 >> arrow >> label3)
-        output[label3] = GateInputs{ label1, label2, operation, TBD };
-
-    for (auto it = output.rbegin(); it->first.front() == 'z'; it++)
-        retval = (retval << 1) | get_value(it->first);
-
-    return retval;
-}
-
-struct Operation
-{
-    std::string w1, w2, out, op;
-};
-
-inline auto is_input_wire(const std::string &w) { return w.front() == 'x' || w.front() == 'y'; }
-inline auto is_output_wire(const std::string &w) { return w.front() == 'z'; }
-
-auto do_part2(const std::string &filename)
-{
-    std::ifstream file(filename);
-    std::string line;
-
-    while (std::getline(file, line) && line.length()); // ignore top section
-
-    std::vector<Operation> operations;
-    std::unordered_map<std::string, std::vector<Operation>> map;
-    std::string label1, label2, label3, op, arrow;
-    auto max_z = 0;
 
     while (file >> label1 >> op >> label2 >> arrow >> label3)
     {
+        output_to_op[label3] = GateInputs{ label1, label2, op, TBD }; // part1
+
         auto operation = Operation{ label1, label2, label3, op };
         operations.push_back(operation);
 
-        map[label1].push_back(operation);
-        map[label2].push_back(operation);
+        inputs_to_op[label1].push_back(operation);
+        inputs_to_op[label2].push_back(operation);
 
         if (operation.out.front() == 'z')
         {
@@ -90,7 +71,10 @@ auto do_part2(const std::string &filename)
         }
     }
 
-    for (auto &[wire, ops] : map)
+    for (auto it = output_to_op.rbegin(); it->first.front() == 'z'; it++)
+        retval.first = (retval.first << 1) | get_value(it->first, output_to_op);
+
+    for (auto &[wire, ops] : inputs_to_op)
     {
         std::sort(std::begin(ops), std::end(ops), [](const auto &op1, const auto &op2) {
             return op1.op < op2.op;
@@ -117,13 +101,13 @@ auto do_part2(const std::string &filename)
             if (is_input1 && is_output)
                 wrong_outputs.insert(out);
 
-            if (is_input1 && (map[out][0].op != "AND" || map[out][1].op != "XOR"))
+            if (is_input1 && (inputs_to_op[out][0].op != "AND" || inputs_to_op[out][1].op != "XOR"))
                 wrong_outputs.insert(out);
 
             if (!is_input1 && !is_output)
                 wrong_outputs.insert(out);
         }
-        else if (op == "AND" && (map[out].size() < 1 || map[out][0].op != "OR"))
+        else if (op == "AND" && (inputs_to_op[out].size() < 1 || inputs_to_op[out][0].op != "OR"))
         {
             wrong_outputs.insert(out);
         }
@@ -132,7 +116,7 @@ auto do_part2(const std::string &filename)
             if (is_input1 || is_input2)
                 wrong_outputs.insert(out);
 
-            if (!map.contains(out) || (map[out].size() != 2 || map[out][0].op != "AND" || map[out][1].op != "XOR"))
+            if (!inputs_to_op.contains(out) || (inputs_to_op[out].size() != 2 || inputs_to_op[out][0].op != "AND" || inputs_to_op[out][1].op != "XOR"))
                 wrong_outputs.insert(out);
         }
     }
@@ -150,18 +134,17 @@ auto do_part2(const std::string &filename)
         separator = ",";
     }
 
-    return oss.str();
+    retval.second = oss.str();
+    return retval;
 }
 
 int main()
 {
-    auto part1 = do_part1("input.txt");
-    std::cout << "Part One: " << part1 << std::endl;
-    assert(part1 == 42410633905894);
+    auto answer = solve("input.txt");
+    std::cout << "Part One: " << answer.first << std::endl;
+    std::cout << "Part Two: " << answer.second << std::endl;
 
-    auto part2 = do_part2("input.txt");
-    std::cout << "Part Two: " << part2 << std::endl;
-    assert(part2 == "cqm,mps,vcv,vjv,vwp,z13,z19,z25");
-
+    assert(answer.first == 42410633905894);
+    assert(answer.second == "cqm,mps,vcv,vjv,vwp,z13,z19,z25");
     return 0;
 }
