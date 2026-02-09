668b058f5f2b6073b00e0774c66513b219c11580
Stephen Houchen <shouchen@outlook.com>
2026-02-09 14:08:27 -0800

    Final clean-up for Day 12

--- aoc2024/Day12/Day12.cpp
+++ aoc2024/Day12/Day12.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include <array>
 #include <vector>
 #include <queue>
-#include <set>
 #include <string>
 #include <cassert>
 
 using Grid = std::vector<std::string>;
 using Visited = std::vector<std::vector<bool>>;
 
-struct Location { int row, col; };
 struct Direction { int dr, dc; } dirs[]{ { 0, -1 }, { -1, 0 }, { 0, 1 }, { 1, 0 } }; // order matters for part2 algorithm!
 
 void recur(const Grid &grid, int row, int col, int &area, int &perimeter, Visited &visited)
@@ -20,8 +17,8 @@ void recur(const Grid &grid, int row, int col, int &area, int &perimeter, Visite
 
     visited[row][col] = true;
 
-    area++;
     auto label = grid[row][col];
+    area++;
 
     for (auto &dir : dirs)
     {
@@ -34,44 +31,41 @@ void recur(const Grid &grid, int row, int col, int &area, int &perimeter, Visite
     }
 }
 
-auto price_region(const Grid &grid, int row, int col, Visited &visited)
+auto price_region_part1(const Grid &grid, int row, int col, Visited &visited)
 {
     auto area = 0, perimeter = 0;
-
     recur(grid, row, col, area, perimeter, visited);
     return area * perimeter;
 }
 
-auto check_corner(const Grid &grid, const Location &loc)
+auto count_corners(const Grid &grid, int row, int col)
 {
-    // Get state of all eight neighbors
-    auto label = grid[loc.row][loc.col];
-    auto above_left = grid[loc.row - 1][loc.col - 1] == label, above = grid[loc.row - 1][loc.col] == label, above_right = grid[loc.row - 1][loc.col + 1] == label;
-    auto left =       grid[loc.row][loc.col - 1] == label,             /* reference point */                 right = grid[loc.row][loc.col + 1] == label;
-    auto below_left = grid[loc.row + 1][loc.col - 1] == label, below = grid[loc.row + 1][loc.col] == label, below_right = grid[loc.row + 1][loc.col + 1] == label;
+    auto label = grid[row][col];
+    auto above_left = grid[row - 1][col - 1] == label, above = grid[row - 1][col] == label, above_right = grid[row - 1][col + 1] == label;
+    auto left =       grid[row][col - 1]     == label,         /* reference point */        right       = grid[row][col + 1]     == label;
+    auto below_left = grid[row + 1][col - 1] == label, below = grid[row + 1][col] == label, below_right = grid[row + 1][col + 1] == label;
 
-    // outside corners
     auto num_corners = 0;
 
-    // check for local upper left corner at p
+    // check for local upper left corner at row, col
     if (!above && !left)                            //  Exterior Corner   Interior Corner
         num_corners++; // exterior corner           //     .   .   .         .   .   .
     else if (above && left && !above_left)          //     .   P  not        .   P   *
         num_corners++; // interior corner           //     .  not  .         .   *  not
 
-    // check for local upper right corner at p
+    // check for local upper right corner at row, col
     if (!above && !right)                           //  Exterior Corner   Interior Corner
         num_corners++; // exterior corner           //     .   .   .         .   .   .
     else if (above && right && !above_right)        //    not  P   .         *   P   . 
         num_corners++; // interior corner           //     .  not  .        not  *   .
 
-    // check for local lower left corner at p
+    // check for local lower left corner at row, col
     if (!below && !left)                            //  Exterior Corner   Interior Corner
         num_corners++; // exterior corner           //     .  not  .         .   *  not
     else if (below && left && !below_left)          //     .   P  not        .   P   *
         num_corners++; // interior corner           //     .   .   .         .   .   .
 
-    // check for local lower right corner at p
+    // check for local lower right corner at row, col
     if (!below && !right)                           //  Exterior Corner   Interior Corner
         num_corners++; // exterior corner           //     .  not   .       not  *   .
     else if (below && right && !below_right)        //    not  P   .         *   P   .
@@ -80,40 +74,39 @@ auto check_corner(const Grid &grid, const Location &loc)
     return num_corners;
 }
 
-auto compute_price(const Grid &grid, int row, int col, Visited &visited)
+auto price_region_part2(const Grid &grid, int row, int col, Visited &visited)
 {
     auto label = grid[row][col];
-    auto start = Location{ row, col };
-    auto cost = 0LL, area = 0LL, corner = 0LL;
+    auto area = 0, num_edges = 0;
 
-    std::queue<Location> q;
-    q.push(start);
+    std::queue<std::pair<int, int>> q;
+    q.push({ row, col });
 
     while (!q.empty())
     {
-        auto loc = q.front(); q.pop();
+        auto row = q.front().first, col = q.front().second; q.pop();
 
-        if (visited[loc.row][loc.col])
+        if (visited[row][col])
             continue;
 
-        visited[loc.row][loc.col] = true;
+        visited[row][col] = true;
 
         area++;
-        corner += check_corner(grid, loc);
+        num_edges += count_corners(grid, row, col);
 
         for (auto &dir : dirs)
         {
-            auto next = Location{ loc.row + dir.dr, loc.col + dir.dc };
+            auto next_row = row + dir.dr, next_col = col + dir.dc;
 
-            if (grid[next.row][next.col] != label)
+            if (grid[next_row][next_col] != label)
                 continue;
 
-            if (!visited[next.row][next.col])
-                q.push(next);
+            if (!visited[next_row][next_col])
+                q.push({ next_row, next_col });
         }
     }
 
-    return area * corner;
+    return area * num_edges;
 }
 
 auto solve(const std::string &filename)
@@ -130,20 +123,19 @@ auto solve(const std::string &filename)
     grid[0] = std::string(grid[1].size(), '#');
     grid.emplace_back(grid[0]);
 
-    // do both parts together
     Visited visited1(grid.size(), std::vector<bool>(grid[0].size(), false));
     Visited visited2(grid.size(), std::vector<bool>(grid[0].size(), false));
-    std::pair<int, long long> retval;
+    std::pair<int, int> retval;
 
     for (auto row = 0; row < grid.size(); row++)
         for (auto col = 0; col < grid[row].size(); col++)
             if (grid[row][col] != '#')
             {
                 if (!visited1[row][col])
-                    retval.first += price_region(grid, row, col, visited1);
+                    retval.first += price_region_part1(grid, row, col, visited1);
 
                 if (!visited2[row][col])
-                    retval.second += compute_price(grid, row, col, visited2);
+                    retval.second += price_region_part2(grid, row, col, visited2);
             }
 
     return retval;
