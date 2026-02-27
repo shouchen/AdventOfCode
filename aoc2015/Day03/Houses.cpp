a0dc2b966fdce2576114b078c7101f3c4dfeca33
shouchen <shouchen@outlook.com>
2026-02-27 12:50:11 -0800

    Moving all 2015 projects under a single solution for consistency

---
+++ aoc2015/Day03/Houses.cpp
@@ -0,0 +1,74 @@
+// Houses.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <cassert>
+
+struct Address
+{
+    int x = 0, y = 0;
+};
+
+struct LessAddress
+{
+    bool operator()(const Address &lhs, const Address &rhs) const
+    {
+        if (lhs.x < rhs.x) return true;
+        if (lhs.x > rhs.x) return false;
+        return lhs.y < rhs.y;
+    }
+};
+
+void ProcessMove(char move, Address &santa)
+{
+    switch (move)
+    {
+        case '^': --santa.y; break;
+        case 'v': santa.y++; break;
+        case '<': --santa.x; break;
+        case '>': santa.x++; break;
+    }
+}
+
+unsigned ProcessFile(const std::string &filename, bool doubleSanta)
+{
+    std::set<Address, LessAddress> visited;
+
+    Address santa1, santa2;
+    visited.insert(santa1);
+
+    std::ifstream f;
+    f.open("Input.txt");
+
+    while (!f.eof())
+    {
+        char move;
+        if (f >> move)
+        {
+            ProcessMove(move, santa1);
+            visited.insert(santa1);
+
+            if (doubleSanta && f >> move)
+            {
+                ProcessMove(move, santa2);
+                visited.insert(santa2);
+            }
+        }
+    }
+
+    return visited.size();
+}
+
+void _tmain(int argc, _TCHAR* argv[])
+{
+    auto part1 = ProcessFile("Input.txt", false);
+    std::cout << "part one: " << part1 << std::endl;
+    assert(part1 == 2565);
+    
+    auto part2 = ProcessFile("Input.txt", true);
+    std::cout << "part two: " << part2 << std::endl;
+    assert(part2 == 2639);
+}
